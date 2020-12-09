eb7a7011019344e117e77c0833343deecc22a9fe
MacRae Linton <macrael@truss.works>
2020-12-08 22:03:32 -0800

    Use the right amendments uploads bucket.

--- src/dev.ts
+++ src/dev.ts
@@ -22,12 +22,16 @@ async function run_api_locally(runner: LabeledProcessRunner) {
 	
 }
 
+// run_s3_locally runs s3 locally
+async function run_s3_locally(runner: LabeledProcessRunner) {
+
+	await runner.run_command_and_output('s3 yarn', ['yarn', 'install'], 'services/uploads')
+	runner.run_command_and_output('s3', ['serverless', '--stage', 'local', 's3', 'start'], 'services/uploads')
+
+}
+
 // run_fe_locally runs the frontend and its dependencies locally
 async function run_fe_locally(runner: LabeledProcessRunner) {
-
-	// S3
-	await runner.run_command_and_output('s3 yarn', ['yarn', 'install'], 'services/ui')
-	runner.run_command_and_output('s3', ['serverless', '--stage', 'local', 's3', 'start'], 'services/ui')
 
 	await runner.run_command_and_output('ui deps', ['yarn', 'install'], 'services/ui-src')
 	await runner.run_command_and_output('ui conf', ['./env.sh', 'local'], 'services/ui-src')
@@ -41,6 +45,7 @@ async function run_all_locally() {
 	const runner = new LabeledProcessRunner()
 
 	run_db_locally(runner)
+	run_s3_locally(runner)
 	run_api_locally(runner)
 	run_fe_locally(runner)
 }
