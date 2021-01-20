6e577851420d312b81b273eb4623bc66b5531768
Hana Worku <hana@truss.works>
2021-01-20 12:29:55 -0600

    Add prettier with basic config; format web code

--- services/app-web/src/App.tsx
+++ services/app-web/src/App.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { CMSHeader } from "./components/Header/Header";
-import "./App.css";
+import React from 'react'
+import { CMSHeader } from './components/Header/Header'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <CMSHeader />
-      <main>Main Content</main>
-    </div>
-  );
+    return (
+        <div className="App">
+            <CMSHeader />
+            <main>Main Content</main>
+        </div>
+    )
 }
 
-export default App;
+export default App
