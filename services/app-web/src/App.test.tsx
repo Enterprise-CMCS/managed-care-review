a5668ca3adfb539068bf4e1f8f27584101ff1645
Hana Worku <hana@truss.works>
2021-01-20 10:53:04 -0600

    Add run tests script

--- services/app-web/src/App.test.tsx
+++ services/app-web/src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByRole("banner");
+  const mainElement = screen.getByRole("main");
+  expect(headerElement).toBeInTheDocument();
+  expect(mainElement).toBeInTheDocument();
 });
