6d374c1526748077c2f2cb0868c5ab31b41efe32
MacRae Linton <macrael@truss.works>
2021-01-21 17:07:32 -0800

    Get rid of the serverless-typescript plugin in favor of serverless-bundle

--- services/app-api/handlers/log_event.ts
+++ services/app-api/handlers/log_event.ts
@@ -1,16 +1,16 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
-export const main: APIGatewayProxyHandler = async (event, context) => {
+export const main: APIGatewayProxyHandler = async (event) => {
 
-  console.log(event.body)
+    console.log(event.body) // eslint-disable-line no-console
 
-  return {
-    statusCode: 200,
-    body: '',
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-    },
-  };
+    return {
+        statusCode: 200,
+        body: '',
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        },
+    };
 
 }
