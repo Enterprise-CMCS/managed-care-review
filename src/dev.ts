19c9cb6969df80723215b0e8d06a0628b7c6a8db
MacRae Linton <macrael@truss.works>
2021-01-26 23:50:41 -0800

    Get all tests into ./dev test

--- src/dev.ts
+++ src/dev.ts
@@ -1,6 +1,7 @@
 import yargs from 'yargs'
 import * as dotenv from 'dotenv'
 import LabeledProcessRunner from './runner.js'
+import * as http from 'http'
 
 // load .env
 dotenv.config()
@@ -49,10 +50,41 @@ async function run_all_locally() {
 	run_web_locally(runner)
 }
 
-async function run_all_tests() {
+async function run_all_tests(run_unit: boolean, run_online: boolean) {
 	const runner = new LabeledProcessRunner()
-	await runner.run_command_and_output('web - unit', ['yarn', 'test:unit'], 'services/app-web')
-	await runner.run_command_and_output('api - unit', ['yarn', 'test'], 'services/app-api')
+	if (run_unit) {
+		run_unit_tests(runner)
+	}
+
+	if (run_online) {
+		run_online_tests(runner)
+	}
+
+}
+
+async function run_unit_tests(runner: LabeledProcessRunner) {
+	runner.run_command_and_output('web - unit', ['yarn', 'test:unit'], 'services/app-web')
+	runner.run_command_and_output('api - unit', ['yarn', 'test'], 'services/app-api')
+}
+
+async function run_online_tests(runner: LabeledProcessRunner) {
+	const base_url = process.env.APPLICATION_ENDPOINT
+
+	if (base_url == undefined) {
+		console.log('You must set APPLICATION_ENDPOINT to run online tests.')
+		return
+	}
+
+	// if the URL doesn't resolve, print an error message
+	http.get(base_url, () => {
+		runner.run_command_and_output('web - a11y', ['yarn', 'test:a11y'], 'services/app-web')
+		runner.run_command_and_output('nightwatch', ['./test.sh'], 'tests')
+	}).on('error', (err) => {
+		console.log(`The URL ${base_url} does not resolve. Make sure the service is running before running online tests.\n`)
+		console.log(err)
+		return
+	})
+
 }
 
 // The command definitions in yargs
@@ -61,8 +93,27 @@ yargs(process.argv.slice(2))
 	.command('local', 'run system locally', {}, () => {
 		run_all_locally()
 	})
-	.command('test', 'run all tests', () => {}, () => {
-		run_all_tests()
+	.command('test', 'run tests. If no flags are passed, runs both --unit and --online', (yargs) => {
+		return yargs.boolean('unit')
+							.boolean('online')
+	}, (args) => {
+		let run_unit = false
+		let run_online = false
+
+		// If no test flags are passed, default to running everything.
+		if (args.unit == null && args.online == null) {
+			run_unit = true
+			run_online = true
+		} else {
+			if (args.unit) {
+				run_unit = true
+			}
+			if (args.online) {
+				run_online = true
+			}
+		}
+
+		run_all_tests(run_unit, run_online)
 	})
 	.demandCommand(1, '') // this prints out the help if you don't call a subcommand
 	.argv
