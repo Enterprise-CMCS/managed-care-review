aa27ca3a5f8cbc65cf26ef28dda476df873c70e5
MacRae Linton <macrael@truss.works>
2021-01-26 21:09:34 -0800

    Get rid of env.sh and use the CRA REACT_APP_* env var system

--- services/app-web/src/App.tsx
+++ services/app-web/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Header } from './components/Header/Header'
 import './App.css'
-// import { logEvent } from './log_event'
+import { logEvent } from './log_event'
 
 function App(): React.ReactElement {
-    // TODO: Commenting out because right now this makes yarn test fail because of how config variables are being used.
-    // Unit tests should be able to pass when app isn't running
-    // logEvent('on_load', { success: true })
+  
+    logEvent('on_load', { success: true })
 
     return (
         <div className="App">
