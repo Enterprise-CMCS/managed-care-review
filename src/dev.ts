ca3670f3e5d432f21b0d993bb929d94f7a883374
MacRae Linton <55759+macrael@users.noreply.github.com>
2021-01-28 11:14:37 -0800

    Update ./dev test (#18)
    * Get rid of env.sh and use the CRA REACT_APP_* env var system
    * Add test for BE
    * Upgrade chrome driver, this now works in CI. Not sure how often we will have to do this.
    
    * Run all tests with ./dev test

--- src/dev.ts
+++ src/dev.ts
@@ -1,5 +1,6 @@
 import yargs from 'yargs'
 import * as dotenv from 'dotenv'
+import request from 'request'
 import LabeledProcessRunner from './runner.js'
 
 // load .env
@@ -34,7 +35,6 @@ async function run_s3_locally(runner: LabeledProcessRunner) {
 async function run_web_locally(runner: LabeledProcessRunner) {
 
 	await runner.run_command_and_output('web deps', ['yarn', 'install'], 'services/app-web')
-	await runner.run_command_and_output('web conf', ['./env.sh', 'local'], 'services/app-web')
 
 	runner.run_command_and_output('web', ['yarn', 'start'], 'services/app-web')
 	
@@ -50,9 +50,71 @@ async function run_all_locally() {
 	run_web_locally(runner)
 }
 
-async function run_all_tests() {
+function check_url_is_up(url: string): Promise<boolean> {
+	return new Promise<boolean>( (resolve) =>  {
+		request(url, {}, (err) => {
+			if (err) { 
+				resolve(false)
+			}
+			resolve(true)
+		})
+	})
+}
+
+async function run_all_tests(run_unit: boolean, run_online: boolean) {
 	const runner = new LabeledProcessRunner()
-	runner.run_command_and_output(' web - unit', ['yarn', 'test:unit'], 'services/app-web')
+
+	try {
+		if (run_unit) {
+			await run_unit_tests(runner)
+		}
+
+		if (run_online) {
+			await run_online_tests(runner)
+		}
+	} catch (e) {
+		console.log("Testing Error", e)
+		process.exit(1)
+	}
+
+}
+
+async function run_unit_tests(runner: LabeledProcessRunner) {
+	const webCode = await runner.run_command_and_output('web - unit', ['yarn', 'test:unit'], 'services/app-web')
+	if (webCode != 0) {
+		throw new Error('web - unit FAILED')
+	}
+
+	const apiCode = await runner.run_command_and_output('api - unit', ['yarn', 'test'], 'services/app-api')
+	if (apiCode != 0) {
+		throw new Error('api - unit failed')
+	}
+}
+
+async function run_online_tests(runner: LabeledProcessRunner) {
+	const base_url = process.env.APPLICATION_ENDPOINT
+
+	if (base_url == undefined) {
+		console.log('You must set APPLICATION_ENDPOINT to run online tests.')
+		return
+	}
+
+	const isUp = await check_url_is_up(base_url)
+	if (!isUp) {
+		throw new Error(`the URL ${base_url} does not resolve, make sure the system is running before runnin online tests`)
+	}
+
+	// TODO: Sort out how to use pa11y in CI. 
+	// const webCode = await runner.run_command_and_output('web - a11y', ['yarn', 'test:a11y'], 'services/app-web')
+	// if (webCode != 0) {
+	// 	throw new Error('web - a11y tests FAILED')
+	// }
+
+	const nightCode = await runner.run_command_and_output('nightwatch', ['./test.sh'], 'tests')
+	if (nightCode != 0) {
+		throw new Error('nightwatch tests FAILED')
+	}
+
 }
 
 // The command definitions in yargs
@@ -61,8 +123,27 @@ yargs(process.argv.slice(2))
 	.command('local', 'run system locally', {}, () => {
 		run_all_locally()
 	})
-	.command('test', 'run all tests', () => {}, () => {
-		run_all_tests()
+	.command('test', 'run tests. If no flags are passed, runs both --unit and --online', (yargs) => {
+		return yargs.boolean('unit')
+							.boolean('online')
+	}, (args) => {
+		let run_unit = false
+		let run_online = false
+
+		// If no test flags are passed, default to running everything.
+		if (args.unit == null && args.online == null) {
+			run_unit = true
+			run_online = true
+		} else {
+			if (args.unit) {
+				run_unit = true
+			}
+			if (args.online) {
+				run_online = true
+			}
+		}
+
+		run_all_tests(run_unit, run_online)
 	})
 	.demandCommand(1, '') // this prints out the help if you don't call a subcommand
 	.argv
