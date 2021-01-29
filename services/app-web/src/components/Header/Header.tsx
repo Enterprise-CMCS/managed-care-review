2cc405785188a544ef4aa9602073d7e8b3fa0cc6
haworku <haworku@users.noreply.github.com>
2021-01-29 10:42:16 -0600

    Add storybook, sass, and uswds (#20)
    * Add storybook with npx sb init and add Header story
    * Cleanup and add Header story with props and text description
    * Add react-uswds storybook
    * Add ts configuration storybook main.js
    * Fix typscript-eslint errors, also fix parserOptions error
    * Add sass and uswds (#21)
    * Add SASS_PATH

--- services/app-web/src/components/Header/Header.tsx
+++ services/app-web/src/components/Header/Header.tsx
@@ -1,13 +1,40 @@
-import { Header as ReactUSWDSHeader, Title } from '@trussworks/react-uswds'
+import { Header as USWDSHeader, Title } from '@trussworks/react-uswds'
 
-export const Header = (): React.ReactElement => {
+type HeaderProps = {
+    stateCode?: string
+}
+
+/**
+ * CMS Header for a logged in state user.
+ */
+export const Header = ({
+    stateCode = 'MI',
+}: HeaderProps): React.ReactElement => {
+    // TODO: Lookup from state.json or wherever we will store this
+    const getStateInfo = (
+        postalCode: string
+    ): { name: string; icon: string } => {
+        return postalCode === 'TN'
+            ? { name: 'Tennessee', icon: '#' }
+            : { name: 'DEFAULT STATE', icon: '#' }
+    }
+
+    const { name } = getStateInfo(stateCode)
+
     return (
-        <ReactUSWDSHeader basic>
+        <USWDSHeader basic>
             <div className="usa-nav-container">
+                <img
+                    src="https://www.medicaid.gov/themes/custom/medicaid/images/headerlogo-medicaid.png"
+                    alt="Medicaid.gov-Keeping America Healthy"
+                />
                 <div className="usa-navbar">
-                    <Title>State Submission Project</Title>
+                    <Title>
+                        <span>{name}</span>
+                        &nbsp;State Submission Project &nbsp;
+                    </Title>
                 </div>
             </div>
-        </ReactUSWDSHeader>
+        </USWDSHeader>
     )
 }
---
+++ services/app-web/src/pages/App/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Header } from '../../components/Header/Header'
+import './App.scss'
+import { logEvent } from '../../log_event'
+
+function App(): React.ReactElement {
+    logEvent('on_load', { success: true })
+
+    return (
+        <div className="App">
+            <Header stateCode="TN" />
+            <main>Main Content</main>
+        </div>
+    )
+}
+
+export default App
