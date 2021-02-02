a7d7993296979f5a8904bf99a84efe76075f39b3
Hana Worku <hana@truss.works>
2021-02-02 13:28:21 -0600

    Tests passing

--- services/app-web/src/components/Header/Header.test.tsx
+++ services/app-web/src/components/Header/Header.test.tsx
@@ -1,11 +1,47 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, getByRole } from '@testing-library/react'
 
 import { Header } from './Header'
 
 describe('Header', () => {
     it('renders without errors', async () => {
-        const { container } = render(<Header />)
-        expect(container).toHaveTextContent('About')
+        const { getByRole } = render(
+            <Header
+                loggedIn
+                stateCode={'MN'}
+                user={{
+                    name: 'Bob test user',
+                    email: 'bob@dmas.mn.gov',
+                }}
+            />
+        )
+        expect(getByRole('banner')).toBeInTheDocument()
+        expect(getByRole('heading')).toBeInTheDocument()
+    })
+    it('renders without errors', async () => {
+        const { container } = render(
+            <Header
+                loggedIn
+                stateCode={'MN'}
+                user={{
+                    name: 'Bob test user',
+                    email: 'bob@dmas.mn.gov',
+                }}
+            />
+        )
+        expect(container).toHaveTextContent('Sign out')
+    })
+    it('renders without errors', async () => {
+        const { container } = render(
+            <Header
+                loggedIn={false}
+                stateCode={'MN'}
+                user={{
+                    name: 'Bob test user',
+                    email: 'bob@dmas.mn.gov',
+                }}
+            />
+        )
+        expect(container).toHaveTextContent('Sign In')
     })
 })
