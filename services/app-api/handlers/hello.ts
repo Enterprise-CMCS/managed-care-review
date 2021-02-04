e4ec49d21208e14f468b6b3405076ee44ef9ea14
MacRae Linton <macrael@truss.works>
2021-02-04 15:12:16 -0800

    Log auth info to see what's up.

--- services/app-api/handlers/hello.ts
+++ services/app-api/handlers/hello.ts
@@ -1,14 +1,14 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
 
 // This endpoint exists to confirm that authentication is working
-export const main: APIGatewayProxyHandler = async () => {
+export const main: APIGatewayProxyHandler = async (event) => {
 
     // says hi
     const hello = {
         'hello': 'there',
     }
 
-    console.log({"name": "hello"}) // eslint-disable-line no-console
+    console.log({"name": "hello", "user": event.requestContext.identity.cognitoIdentityId, "idenityProvider": event.requestContext.identity.cognitoAuthenticationProvider}) // eslint-disable-line no-console
 
     return {
         statusCode: 200,
