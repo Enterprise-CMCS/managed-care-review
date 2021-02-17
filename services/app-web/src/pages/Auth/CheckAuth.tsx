7a1dec1a29c3527065064304c96fa23989b6528f
MacRae Linton <macrael@truss.works>
2021-02-16 19:16:03 -0800

    revert hello

--- services/app-web/src/pages/Auth/CheckAuth.tsx
+++ services/app-web/src/pages/Auth/CheckAuth.tsx
@@ -1,48 +1,38 @@
 import React, { useState } from 'react'
-import { Button, GridContainer } from '@trussworks/react-uswds'
-import { useAuth } from '../App/AuthContext'
+import { Button } from '@trussworks/react-uswds'
+import { isAuthenticated } from './isAuthenticated'
 
 type AuthStatus = 'Unknown' | 'Authenticated' | 'Unauthenticated'
 
 // COMPONENTS
 export function CheckAuth(): React.ReactElement {
-    const { loggedInUser, isLoading, checkAuth } = useAuth()
+    const [isLoading, setIsLoading] = useState(false)
 
     const [authStatus, setAuthStatus] = useState<AuthStatus>('Unknown')
-
-    console.log('Renderinmain', loggedInUser, isLoading, checkAuth, authStatus)
-
-    if (isLoading) {
-        if (authStatus != 'Unknown') {
-            setAuthStatus('Unknown')
-        }
-    } else {
-        if (loggedInUser) {
-            if (authStatus != 'Authenticated') {
-                setAuthStatus('Authenticated')
-            }
-        } else {
-            if (authStatus != 'Unauthenticated') {
-                setAuthStatus('Unauthenticated')
-            }
-        }
-    }
 
     async function handleSubmit(event: React.FormEvent) {
         console.log('checking auth')
         event.preventDefault()
 
-        checkAuth()
+        setIsLoading(true)
+
+        const isAuthed = await isAuthenticated()
+
+        setIsLoading(false)
+
+        if (isAuthed) {
+            setAuthStatus('Authenticated')
+        } else {
+            setAuthStatus('Unauthenticated')
+        }
     }
 
     return (
-        <GridContainer>
-            <form onSubmit={handleSubmit}>
-                <p>Current Auth Status: {authStatus}</p>
-                <Button type="submit" disabled={isLoading}>
-                    Check Auth
-                </Button>
-            </form>
-        </GridContainer>
+        <form onSubmit={handleSubmit}>
+            <p>Current Auth Status: {authStatus}</p>
+            <Button type="submit" disabled={isLoading}>
+                Check Auth
+            </Button>
+        </form>
     )
 }
