7a12f522ea513eed2335f4c2023f1d7d66ee657d
Hana Worku <hana@truss.works>
2021-02-22 10:13:54 -0600

    Mock out tests to be written

--- services/app-web/src/components/Header/Header.test.tsx
+++ services/app-web/src/components/Header/Header.test.tsx
@@ -19,22 +19,8 @@ describe('Header', () => {
         )
         expect(getByRole('heading')).toBeInTheDocument()
     })
-    it('has signout button', async () => {
-        const { getByTestId } = render(
-            <BrowserRouter>
-                <Header
-                    loggedIn
-                    stateCode={'MN'}
-                    user={{
-                        name: 'Bob test user',
-                        email: 'bob@dmas.mn.gov',
-                    }}
-                />
-            </BrowserRouter>
-        )
-        expect(getByTestId('button')).toHaveTextContent('Sign out')
-    })
-    it('has logo link', async () => {
+
+    it('has logo image link that goes to /dashboard', async () => {
         render(
             <BrowserRouter>
                 <Header loggedIn={false} />
@@ -49,13 +35,43 @@ describe('Header', () => {
             '/dashboard'
         )
     })
+    describe('when logged out', () => {
+        it.todo('has Welcome to MCRRS heading')
+        it('displays signin link when logged out', async () => {
+            render(
+                <BrowserRouter>
+                    <Header loggedIn={false} />
+                </BrowserRouter>
+            )
+            expect(screen.getAllByRole('link')[1]).toHaveTextContent('Sign In')
+        })
+        it.todo('signin link goes to /auth')
+    })
+    describe('when logged in', () => {
+        it.todo('has heading with users state')
+        it.todo('has heading with the current program')
+        it('displays sign out button', async () => {
+            const { getByTestId } = render(
+                <BrowserRouter>
+                    <Header
+                        loggedIn
+                        stateCode={'MN'}
+                        user={{
+                            name: 'Bob test user',
+                            email: 'bob@dmas.mn.gov',
+                        }}
+                    />
+                </BrowserRouter>
+            )
+            expect(getByTestId('button')).toHaveTextContent('Sign out')
+        })
 
-    it('has signin link', async () => {
-        render(
-            <BrowserRouter>
-                <Header loggedIn={false} />
-            </BrowserRouter>
+        it.todo('calls logout when signout button is clicked')
+        it.todo(
+            'redirects to auth page when signout button is clicked and logout is successful'
         )
-        expect(screen.getAllByRole('link')[1]).toHaveTextContent('Sign In')
+        it.todo(
+            'displays error when signout button is clicked and logout is unsuccessful'
+        )
     })
 })
