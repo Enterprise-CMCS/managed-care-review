fb0e817df3aa9e381230b289ac69cd8e4c1725d1
Hana Worku <hana@truss.works>
2021-02-26 10:48:56 -0600

    Use generic error

--- services/app-web/src/pages/Errors/GenericError.tsx
+++ services/app-web/src/pages/Errors/GenericError.tsx
@@ -6,14 +6,17 @@ import { GridContainer } from '@trussworks/react-uswds'
 
 export const GenericError = (): React.ReactElement => {
     return (
-  		<section className={styles.errorsContainer}>
-	        <GridContainer>
-	        	<h1>Something went wrong...</h1>
-	        	<p>Please try to refresh the page. </p>
-	        	<p><span>If the issue continues reach out to </span> 
-	        		<Link href="mailto:mcrrs-team@truss.works">mcrrs-team@truss.works</Link>
-	      		</p>
-	        </GridContainer>
-  		</section>
+        <section className={styles.errorsContainer}>
+            <GridContainer>
+                <h1>Something went wrong...</h1>
+                <p>Please try to refresh the page. </p>
+                <p>
+                    <span>If the issue continues reach out to </span>
+                    <Link href="mailto:mcrrs-team@truss.works">
+                        mcrrs-team@truss.works
+                    </Link>
+                </p>
+            </GridContainer>
+        </section>
     )
 }
