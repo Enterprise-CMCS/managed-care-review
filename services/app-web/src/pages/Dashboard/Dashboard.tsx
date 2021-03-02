d95979eb2633713fc9351a222feaf1a7fcdc13f7
Hana Worku <hana@truss.works>
2021-03-02 09:53:54 -0600

    Encapsulate panel display, add flex

--- services/app-web/src/pages/Dashboard/Dashboard.tsx
+++ services/app-web/src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { GridContainer } from '@trussworks/react-uswds'
+import { Button, GridContainer, Grid } from '@trussworks/react-uswds'
 
 import styles from './Dashboard.module.scss'
 import { Tabs } from '../../components/Tabs/Tabs'
@@ -23,6 +23,30 @@ export const Dashboard = (): React.ReactElement => {
         return <div>Loading User Info</div>
     }
 
+    const handleNewSubmissionClick = () => {
+        console.log('Implement redirect')
+    }
+
+    const ProgramContent = ({
+        program,
+    }: {
+        program: UserProgram
+    }): React.ReactElement => {
+        return (
+            <section key={program.name} className={styles.panel}>
+                <div className={styles.panelHeading}>
+                    <h2>Submissions</h2>
+                    <Button type="button" onClick={handleNewSubmissionClick}>
+                        Start new submission
+                    </Button>
+                </div>
+                <div>
+                    <h3>You have no submissions yet.</h3>
+                </div>
+            </section>
+        )
+    }
+
     return (
         <GridContainer className={styles.container} data-testid="dashboardPage">
             <Tabs className={styles.tabs}>
@@ -32,10 +56,7 @@ export const Dashboard = (): React.ReactElement => {
                         id={program.name}
                         tabName={program.name}
                     >
-                        <section key={program.name} className={styles.panel}>
-                            <h2>Submissions</h2>
-                            <p>You have no submissions yet.</p>
-                        </section>
+                        <ProgramContent key={program.name} program={program} />
                     </TabPanel>
                 ))}
             </Tabs>
