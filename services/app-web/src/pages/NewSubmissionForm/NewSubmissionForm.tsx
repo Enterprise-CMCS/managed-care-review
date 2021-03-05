5a40b07cdf8338e9daf4ff58bc8a070ef86fb9d2
Josh Franklin <josh@truss.works>
2021-03-05 13:09:24 -0500

    Fixing form containers

--- services/app-web/src/pages/NewSubmissionForm/NewSubmissionForm.tsx
+++ services/app-web/src/pages/NewSubmissionForm/NewSubmissionForm.tsx
@@ -8,18 +8,20 @@ export const NewSubmissionForm = (): React.ReactElement => {
 	return (
 		<GridContainer>
 			<h2 className={styles.formHeader}>Submission type</h2>
-			<div className={styles.formContainer}>
-				<span>All fields are required</span>
-{/*				<Form>*/}
-					<SubmissionType/>
-				{/*</Form>*/}
+			<div className={styles.formWrapper}>
+				<div className={styles.formContainer}>
+					<span>All fields are required</span>
+	{/*				<Form>*/}
+						<SubmissionType/>
+					{/*</Form>*/}
+				</div>
+				<ButtonGroup type="default">
+					<Link href="#" className="usa-button usa-button--outline">
+					Cancel
+					</Link>
+					<Button type="button">Continue</Button>
+				</ButtonGroup>
 			</div>
-			<ButtonGroup type="default">
-		    <Link href="#" className="usa-button usa-button--outline">
-		      Back
-		    </Link>
-		    <Button type="button">Continue</Button>
-		  </ButtonGroup>
 		</GridContainer>
 	)
 }
