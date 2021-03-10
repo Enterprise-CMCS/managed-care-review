14cedac6533b3fea904a40dba28e7f79e71afaf8
MacRae Linton <macrael@truss.works>
2021-03-10 09:21:40 -0800

    Add auth_mode env var to configure the UI

--- .github/build_vars.sh
+++ .github/build_vars.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 var_list=(
+  'REACT_APP_AUTH_MODE'
   'AWS_ACCESS_KEY_ID'
   'AWS_SECRET_ACCESS_KEY'
   'AWS_DEFAULT_REGION'
@@ -19,30 +20,28 @@ var_list=(
 
 set_value() {
   varname=${1}
-  if [ ! -z "${!varname}" ]; then
+  if [ -n "${!varname}" ]; then
     echo "Setting $varname"
-    echo "${varname}=${!varname}" >> $GITHUB_ENV
+    echo "${varname}=${!varname}" >> "$GITHUB_ENV"
   fi
 }
 
 set_name() {
   varname=${1}
-  echo "BRANCH_SPECIFIC_VARNAME_$varname=${branch_name//-/_}_$varname" >> $GITHUB_ENV
+  echo "BRANCH_SPECIFIC_VARNAME_$varname=${branch_name//-/_}_$varname" >> "$GITHUB_ENV"
 }
-
-action=${1}
 
 case "$1" in
 set_names)
   for i in "${var_list[@]}"
   do
-    set_name $i
+    set_name "$i"
   done
   ;;
 set_values)
   for i in "${var_list[@]}"
   do
-  	set_value $i
+  	set_value "$i"
   done
   ;;
 esac
