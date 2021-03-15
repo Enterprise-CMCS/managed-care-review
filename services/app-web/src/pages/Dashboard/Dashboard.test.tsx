c10d9c9fc82a82d25bddccec12d0711a248cd3ec
haworku <haworku@users.noreply.github.com>
2021-03-15 12:26:03 -0500

    Add Submission Type tests (#64)
    * AddDashboard tests 
    *  Add StateSubmissionForm and SubmissionType 
    * Fix dev script
    * POC - getCurrentUserMock as a function util

---
+++ services/app-web/src/pages/Dashboard/Dashboard.test.tsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import userEvent from '@testing-library/user-event'
+import { createMemoryHistory } from 'history'
+import { screen, waitFor } from '@testing-library/react'
+
+import { Dashboard } from './Dashboard'
+import { getCurrentUserMock } from '../../utils/apolloUtils'
+import { renderWithProviders } from '../../utils/jestUtils'
+
+describe('Dashboard', () => {
+    it('display submission heading', async () => {
+        renderWithProviders(<Dashboard />, {
+            apolloProvider: {
+                mocks: [getCurrentUserMock({ statusCode: 200 })],
+            },
+        })
+
+        await waitFor(() =>
+            expect(
+                screen.getAllByRole('heading', {
+                    level: 2,
+                    name: 'Submissions',
+                })
+            ).toBeDefined()
+        )
+    })
+
+    it('displays new submission link', async () => {
+        renderWithProviders(<Dashboard />, {
+            apolloProvider: {
+                mocks: [getCurrentUserMock({ statusCode: 200 })],
+            },
+        })
+
+        await waitFor(() => {
+            const links = screen.getAllByRole('link', {
+                name: 'Start new submission',
+            })
+            expect(links).toBeDefined()
+            expect(links[0]).toHaveAttribute('href', '/new')
+        })
+    })
+
+    it('displays tabs for available programs based on loggedInUser state', async () => {
+        const mockUser = {
+            role: 'State User',
+            name: 'Bob in Virginia',
+            email: 'bob@dmas.va.gov',
+            state: {
+                name: 'Virginia',
+                code: 'VA',
+                programs: [
+                    { name: 'Program 1' },
+                    { name: 'Program 2' },
+                    { name: 'Program 3' },
+                ],
+            },
+        }
+
+        renderWithProviders(<Dashboard />, {
+            apolloProvider: {
+                mocks: [
+                    getCurrentUserMock({ statusCode: 200, user: mockUser }),
+                ],
+            },
+        })
+
+        await waitFor(() => {
+            const tabs = screen.getAllByRole('tab')
+            expect(tabs.length).toBe(3)
+            expect(tabs[0].textContent).toBe('Program 1')
+            expect(tabs[1].textContent).toBe('Program 2')
+        })
+    })
+
+    it('loads first tab active', async () => {
+        renderWithProviders(<Dashboard />, {
+            apolloProvider: {
+                mocks: [getCurrentUserMock({ statusCode: 200 })],
+            },
+        })
+
+        await waitFor(() => {
+            const tabs = screen.getAllByRole('tab')
+            expect(tabs.length).toBe(3)
+            expect(tabs[0]).toHaveClass('easi-tabs__tab--selected')
+            expect(tabs[1]).not.toHaveClass('easi-tabs__tab--selected')
+            expect(tabs[2]).not.toHaveClass('easi-tabs__tab--selected')
+        })
+    })
+
+    it('on new submission click, redirect to /new', async () => {
+        const history = createMemoryHistory()
+
+        renderWithProviders(<Dashboard />, {
+            apolloProvider: {
+                mocks: [getCurrentUserMock({ statusCode: 200 })],
+            },
+            routerProvider: {
+                routerProps: { history: history },
+            },
+        })
+
+        await waitFor(() => {
+            const links = screen.getAllByRole('link', {
+                name: 'Start new submission',
+            })
+            expect(links).toBeDefined()
+            userEvent.click(links[0])
+        })
+
+        await waitFor(() => {
+            expect(history.location.pathname).toBe('/new')
+        })
+    })
+})
--- services/app-web/src/utils/apolloUtils.tsx
+++ services/app-web/src/utils/apolloUtils.tsx
@@ -1,6 +1,63 @@
-import { GetCurrentUserDocument } from '../gen/gqlClient'
+import { GetCurrentUserDocument, User as UserType } from '../gen/gqlClient'
+import { MockedResponse } from '@apollo/client/testing'
+import { GraphQLError } from 'graphql'
 
-/* Apollo MockedProvider Mocks */
+/* For use with Apollo MockedProvider in jest tests */
+const mockValidUser: UserType = {
+    state: {
+        name: 'Minnesota',
+        code: 'MN',
+        programs: [{ name: 'MSHO' }, { name: 'PMAP' }, { name: 'SNBC' }],
+    },
+    role: 'State User',
+    name: 'Bob it user',
+    email: 'bob@dmas.mn.gov',
+}
+
+type getCurrentUserMockProps = {
+    user?: UserType | Partial<UserType>
+    statusCode: 200 | 403 | 500
+}
+const getCurrentUserMock = ({
+    user = mockValidUser,
+    statusCode,
+}: // eslint-disable-next-line @typescript-eslint/no-explicit-any
+getCurrentUserMockProps): MockedResponse<Record<string, any>> => {
+    switch (statusCode) {
+        case 200:
+            return {
+                request: { query: GetCurrentUserDocument },
+                result: {
+                    data: {
+                        getCurrentUser: user,
+                    },
+                },
+            }
+        case 403:
+            return {
+                request: { query: GetCurrentUserDocument },
+                result: {
+                    // Need to double check types for both cognito and local login
+                    // also double check that local login mocks 403 correctly
+                    // https://www.apollographql.com/docs/react/development-testing/testing/#defining-mocked-responses
+                    // ok: false,
+                    // status: 403,
+                    // statusText: 'Unauthenticated',
+                    // data: {
+                    //     error: 'you are not logged in',
+                    // },
+                    errors: [new GraphQLError('You are not logged in')],
+                },
+            }
+        default:
+            return {
+                request: { query: GetCurrentUserDocument },
+                error: new Error('A network error occurred'),
+            }
+    }
+}
+
+// TO BE DELETED
 const mockGetCurrentUser200 = {
     request: { query: GetCurrentUserDocument },
     result: {
@@ -36,4 +93,4 @@ const mockGetCurrentUser403 = {
     },
 }
 
-export { mockGetCurrentUser200, mockGetCurrentUser403 }
+export { getCurrentUserMock, mockGetCurrentUser200, mockGetCurrentUser403 }
