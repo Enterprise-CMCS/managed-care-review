ab706ffc827e01cbc01b6eee9f57a88f2cef8cd6
Josh Franklin <josh@truss.works>
2021-03-19 12:13:58 -0400

    Create submission list component in storybook (#65)
    * Added a temporary date util as part of this work.
    
    Co-authored-by: Hana Worku <hana@truss.works>

---
+++ services/app-web/src/components/SubmissionCard/SubmissionCard.stories.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Story } from '@storybook/react'
+import styles from './SubmissionCard.module.scss'
+
+import {
+    SubmissionCard,
+    SubmissionCardProps,
+    SubmissionType,
+    SubmissionStatus,
+} from './SubmissionCard'
+
+export default {
+    title: 'Components/SubmissionCard',
+    component: SubmissionCard,
+    argTypes: {
+        date: {
+            control: {
+                type: 'date',
+            },
+        },
+    },
+}
+
+const Template: Story<SubmissionCardProps> = (args) => (
+    <ul className={styles.submissionList}>
+        <SubmissionCard {...args} />
+    </ul>
+)
+
+export const Draft = Template.bind({})
+
+Draft.args = {
+    name: 'VA-CCCPlus-0001',
+    description:
+        'Rates are being adjusted to reflect revised capitation rates based on more recent data as well as benefit changes approved by the General Assembly.',
+    submissionType: SubmissionType.ContractOnly,
+    status: SubmissionStatus.draft,
+}
+
+export const Submitted = Template.bind({})
+
+Submitted.args = {
+    name: 'VA-CCCPlus-0001',
+    description:
+        'Rates are being adjusted to reflect revised capitation rates based on more recent data as well as benefit changes approved by the General Assembly.',
+    submissionType: SubmissionType.ContractAndRates,
+    status: SubmissionStatus.submitted,
+    date: new Date().getTime(),
+}
