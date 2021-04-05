9574e540dbf9ab6b796b9e299e1879d6340ac5ec
Hana Worku <hana@truss.works>
2021-04-05 14:06:45 -0500

    wip

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,19 @@ services=(
   'storybook'
   'app-web'
 )
+# These test users are only available in DEV environments.
+TEST_USERS=(
+  'aang.hhs.local'
+  'toph@cms.hhs.local'
+)
+
+TEST_USER_PASSWORD="Passw0rd!"
+
+# What stages shall NOT have the test users.
+test_users_exclude_stages=(
+  'master'
+  'production'
+)
 
 install_deps() {
   if [ "$CI" == "true" ]; then # If we're in a CI system
@@ -43,6 +56,34 @@ do
 done
 
 pushd services
+
+# Add test users as necessary
+if [ $ALLOW_DEV_LOGIN == true ]
+then
+  # Lets first get the emails of developers that have committed to the repo, so we can add the emails as test users.
+  # This will also result in adding any new developers in the branch (cumulative).
+  dev_emails=`git log --pretty=format:'%ae' | grep -v github.com | sort -u`
+  TEST_USERS+=("${dev_emails[@]}")
+  echo "INFO: Creating the following test users as needed..."
+  echo "${TEST_USERS[@]}"
+  cognito_user_pool_id=`./services/output.sh services/ui-auth UserPoolId $stage`
+  if [ ! -z "$cognito_user_pool_id" ]
+  then
+      for user in "${TEST_USERS[@]}"
+      do
+          # We ignore all the errors if the user exists.
+          set +e
+          aws cognito-idp admin-create-user --user-pool-id $cognito_user_pool_id --message-action SUPPRESS --username $user \
+          --user-attributes Name=given_name,Value=TestFirstName Name=family_name,Value=TestLastName,Name=email,Value=mc-review-test@cms.hhs.local,Name=custom:state_code,Value=MN
+          aws cognito-idp admin-set-user-password --user-pool-id $cognito_user_pool_id --username $user --password $TEST_USER_PASSWORD --permanent
+          set -e
+      done
+  else
+      echo "ERROR: There was an error obtaining AWS resource information to create users."
+      exit 1
+  fi
+fi
+
 echo """
 ------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------
