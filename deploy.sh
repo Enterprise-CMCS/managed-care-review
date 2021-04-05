5326ac8dadf49c92654be992b88d4d9d4fcf5ec7
Hana Worku <hana@truss.works>
2021-04-05 17:01:21 -0500

    with deploy commented out for testing

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,17 @@ set -e
 
 stage=${1:-dev}
 
-services=(
-  'database'
-  'uploads'
-  # 'uploads-scan'
-  'app-api'
-  'stream-functions'
-  'ui'
-  'ui-auth'
-  'storybook'
-  'app-web'
-)
+# services=(
+#   'database'
+#   'uploads'
+#   # 'uploads-scan'
+#   'app-api'
+#   'stream-functions'
+#   'ui'
+#   'ui-auth'
+#   'storybook'
+#   'app-web'
+# )
 # These test users are only available in DEV environments.
 TEST_USERS=(
   'aang.hhs.local'
@@ -29,33 +29,45 @@ test_users_exclude_stages=(
   'production'
 )
 
-install_deps() {
-  if [ "$CI" == "true" ]; then # If we're in a CI system
-    if [ ! -d "node_modules" ]; then # If we don't have any node_modules (CircleCI cache miss scenario), run yarn install --frozen-lockfile.  Otherwise, we're all set, do nothing.
-      yarn install --frozen-lockfile
-    fi
-  else # We're not in a CI system, let's yarn install
-    yarn install
-  fi
-}
+# install_deps() {
+#   if [ "$CI" == "true" ]; then # If we're in a CI system
+#     if [ ! -d "node_modules" ]; then # If we don't have any node_modules (CircleCI cache miss scenario), run yarn install --frozen-lockfile.  Otherwise, we're all set, do nothing.
+#       yarn install --frozen-lockfile
+#     fi
+#   else # We're not in a CI system, let's yarn install
+#     yarn install
+#   fi
+# }
 
-deploy() {
-  service=$1
-  pushd services/$service
-  install_deps
-  serverless deploy  --stage $stage
-  popd
-}
+# deploy() {
+#   service=$1
+#   pushd services/$service
+#   install_deps
+#   serverless deploy  --stage $stage
+#   popd
+# }
 
-install_deps
-export PATH=$(pwd)/node_modules/.bin/:$PATH
+# install_deps
+# export PATH=$(pwd)/node_modules/.bin/:$PATH
 
-for i in "${services[@]}"
+# Identify if we need the test users.
+# Note that we use ALLOW_DEV_LOGIN in ui-src as well to show Dev only login buttons.
+export ALLOW_DEV_LOGIN=true
+for excluded_stage in "${test_users_exclude_stages[@]}"
 do
-	deploy $i
+    if [ $stage == $excluded_stage ]
+    then
+       export ALLOW_DEV_LOGIN=false
+       echo "INFO: Will not set test users in this branch."
+       break
+    fi
 done
+# for i in "${services[@]}"
+# do
+# 	deploy $i
+# done
 
-pushd services
+# pushd services
 
 # Add test users as necessary
 if [ $ALLOW_DEV_LOGIN == true ]
@@ -94,4 +106,4 @@ SSO URL: `./output.sh ui-auth UserPoolSingleSignOnURL $stage`
 AudienceRestriction: `./output.sh ui-auth AudienceRestrictionURI $stage`
 ------------------------------------------------------------------------------------------------
 """
-popd
+# popd
