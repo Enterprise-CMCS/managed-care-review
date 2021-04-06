65e4cad237b94ffccc19c542c898fe5f1162a372
Josh Franklin <josh@truss.works>
2021-04-06 15:12:14 -0400

    Get story controls working

--- services/app-web/src/components/Form/FieldTextarea.stories.tsx
+++ services/app-web/src/components/Form/FieldTextarea.stories.tsx
@@ -1,60 +1,56 @@
 import React from 'react'
+import { Story } from '@storybook/react'
 import { Formik } from 'formik'
-import { FormTextarea } from './FieldTextarea'
+import { FormTextarea, TextAreaProps } from './FieldTextarea'
 import { Link } from '@trussworks/react-uswds'
 
 export default {
     title: 'Components/Forms/FieldTextarea',
-    component: FormTextarea,
+    component: FormTextarea
 }
 
-export const Default = (): React.ReactElement => 
-<Formik
-    initialValues={{input1: ''}}
-    onSubmit={e => console.log('submitted')}
->
-    <FormTextarea 
-        label="Submission description"
-        id="submission-description"
-        showError={false}
-        name="submission-description"
-    />
-</Formik>
+const Template: Story<TextAreaProps> = (args) => (
+    <Formik
+        initialValues={{input1: ''}}
+        onSubmit={e => console.log('submitted')}
+    >
+        <FormTextarea {...args}/>
+    </Formik>
+)
 
+export const Draft = Template.bind({})
 
-export const WithHint = (): React.ReactElement => 
-<Formik
-    initialValues={{input1: ''}}
-    onSubmit={e => console.log('submitted')}
->
-    <FormTextarea 
-        label="Submission description"
-        id="submission-description"
-        showError={false}
-        name="submission-description"
-        hint={
-            <>
-                <Link
-                    variant="nav"
-                    href="https://www.medicaid.gov/medicaid/managed-care/managed-care-authorities/index.html"
-                    target="_blank"
-                >Managed Care entity definitions</Link>
-                <span>Provide a description of any major changes or updates</span>
-            </>
-        }
-    />
-</Formik>
+Draft.args = {
+    label: "Submission description",
+    id: "submission-description",
+    showError: false,
+    name: "submission-description"
+}
 
-export const WithError = (): React.ReactElement => 
-<Formik
-    initialValues={{input1: ''}}
-    onSubmit={e => console.log('submitted')}
->
-    <FormTextarea 
-        label="Submission description"
-        id="submission-description"
-        showError={true}
-        name="submission-description"
-        error="You must provide a description of any major changes or updates"
-    />
-</Formik>
+export const Hint = Template.bind({})
+
+Hint.args = {
+    label: "Submission description",
+    id: "submission-description",
+    showError: false,
+    name: "submission-description",
+    hint:
+        <>
+            <Link
+                variant="nav"
+                href="https://www.medicaid.gov/medicaid/managed-care/managed-care-authorities/index.html"
+                target="_blank"
+            >Managed Care entity definitions</Link>
+            <span>Provide a description of any major changes or updates</span>
+        </>
+}
+
+export const Error = Template.bind({})
+
+Error.args = {
+    label: "Submission description",
+    id: "submission-description",
+    showError: true,
+    name: "submission-description",
+    error: "You must provide a description of any major changes or updates"
+}
