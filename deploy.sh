316d794f75456b2bb2334000a22697fcae0cdfc0
Hana Worku <hana@truss.works>
2021-04-06 14:53:21 -0500

    Still erroring trying to create users

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,17 @@ set -e
 
 stage=${1:-dev}
 
-services=(
-  'database'
-  'uploads'
-  # 'uploads-scan'
-  'app-api'
-  'stream-functions'
-  'ui'
-  'ui-auth'
-  'storybook'
-  'app-web'
-)
+# services=(
+#   'database'
+#   'uploads'
+#   # 'uploads-scan'
+#   'app-api'
+#   'stream-functions'
+#   'ui'
+#   'ui-auth'
+#   'storybook'
+#   'app-web'
+# )
 # These test users are only available in DEV environments.
 TEST_USERS=(
   'aang@dhs.state.mn.us'
@@ -29,26 +29,26 @@ test_users_exclude_stages=(
   'production'
 )
 
-install_deps() {
-  if [ "$CI" == "true" ]; then # If we're in a CI system
-    if [ ! -d "node_modules" ]; then # If we don't have any node_modules (CircleCI cache miss scenario), run yarn install --frozen-lockfile.  Otherwise, we're all set, do nothing.
-      yarn install --frozen-lockfile
-    fi
-  else # We're not in a CI system, let's yarn install
-    yarn install
-  fi
-}
+# install_deps() {
+#   if [ "$CI" == "true" ]; then # If we're in a CI system
+#     if [ ! -d "node_modules" ]; then # If we don't have any node_modules (CircleCI cache miss scenario), run yarn install --frozen-lockfile.  Otherwise, we're all set, do nothing.
+#       yarn install --frozen-lockfile
+#     fi
+#   else # We're not in a CI system, let's yarn install
+#     yarn install
+#   fi
+# }
 
-deploy() {
-  service=$1
-  pushd services/$service
-  install_deps
-  serverless deploy  --stage $stage
-  popd
-}
+# deploy() {
+#   service=$1
+#   pushd services/$service
+#   install_deps
+#   serverless deploy  --stage $stage
+#   popd
+# }
 
-install_deps
-export PATH=$(pwd)/node_modules/.bin/:$PATH
+# install_deps
+# export PATH=$(pwd)/node_modules/.bin/:$PATH
 
 # Identify if we need the test users.
 # Note that we use ALLOW_DEV_LOGIN in ui-src as well to show Dev only login buttons.
@@ -62,12 +62,12 @@ do
        break
     fi
 done
-for i in "${services[@]}"
-do
-	deploy $i
-done
+# for i in "${services[@]}"
+# do
+# 	deploy $i
+# done
 
-pushd services
+# pushd services
 
 # Add test users as necessary
 if [ $ALLOW_DEV_LOGIN == true ]
@@ -78,7 +78,7 @@ then
   TEST_USERS+=("${dev_emails[@]}")
   echo "INFO: Creating the following test users as needed..."
   echo "${TEST_USERS[@]}"
-  cognito_user_pool_id=`./output.sh ui-auth UserPoolId $stage`
+  cognito_user_pool_id=`services/output.sh services/ui-auth UserPoolId $stage`
   if [ ! -z "$cognito_user_pool_id" ]
   then
       for user in "${TEST_USERS[@]}"
