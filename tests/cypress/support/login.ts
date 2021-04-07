70eb060a1ead5448edc12c6b064607cec6435e41
Hana Worku <hana@truss.works>
2021-04-07 10:37:44 -0500

    Use TEST_USERS_PASS to store password

--- tests/cypress/support/login.ts
+++ tests/cypress/support/login.ts
@@ -5,9 +5,12 @@ Cypress.Commands.add('login', () => {
     if (authMode === 'LOCAL') {
         cy.findByTestId('AangButton').click()
     } else if (authMode === 'AWS_COGNITO') {
+        const testUsersPassword = Cypress.env('TEST_USERS_PASS')
+        if (!testUsersPassword) throw Error('Cannot login test user without a password')
+        console.log(testUsersPassword)
         cy.findByText('Show Login Form').click()
         cy.findByTestId('loginEmail').type('aang@dhs.state.mn.us')
-        cy.findByTestId('loginPassword').type('Passw0rd!')
+        cy.findByTestId('loginPassword').type(testUsersPassword )
         cy.findByRole('button', {name: 'Login'}).click()
     } else {
         console.log('Auth mode is not defined or is IDM')
