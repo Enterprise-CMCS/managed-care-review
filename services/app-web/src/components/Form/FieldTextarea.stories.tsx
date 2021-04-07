5608ee99fc10067379ccae68db16ed8494618c1f
Josh Franklin <josh@truss.works>
2021-04-07 11:56:07 -0400

    Refactoring textarea component (#89)
    * First pass at refactoring textarea component
    
    * Get story controls working
    
    * Fix storybook title
    
    * Add Formik mocks to tests

---
+++ services/app-web/src/components/Form/FieldTextarea.stories.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Story } from '@storybook/react'
+import { Formik } from 'formik'
+import { FormTextarea, TextAreaProps } from './FieldTextarea'
+import { Link } from '@trussworks/react-uswds'
+
+export default {
+    title: 'Components/Forms/FieldTextarea',
+    component: FormTextarea
+}
+
+const Template: Story<TextAreaProps> = (args) => (
+    <Formik
+        initialValues={{input1: ''}}
+        onSubmit={e => console.log('submitted')}
+    >
+        <FormTextarea {...args}/>
+    </Formik>
+)
+
+export const Default = Template.bind({})
+
+Default.args = {
+    label: "Submission description",
+    id: "submission-description",
+    showError: false,
+    name: "submission-description"
+}
+
+export const Hint = Template.bind({})
+
+Hint.args = {
+    label: "Submission description",
+    id: "submission-description",
+    showError: false,
+    name: "submission-description",
+    hint:
+        <>
+            <Link
+                variant="nav"
+                href="https://www.medicaid.gov/medicaid/managed-care/managed-care-authorities/index.html"
+                target="_blank"
+            >Managed Care entity definitions</Link>
+            <span>Provide a description of any major changes or updates</span>
+        </>
+}
+
+export const Error = Template.bind({})
+
+Error.args = {
+    label: "Submission description",
+    id: "submission-description",
+    showError: true,
+    name: "submission-description",
+    error: "You must provide a description of any major changes or updates"
+}
--- services/app-web/src/hooks/useTitle.ts
+++ services/app-web/src/hooks/useTitle.ts
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 
 // Useful for dynamically updates to application <title> based on page
 export const useTitle = (title?: string): void => {
-  useEffect(() => { 
-    title && (document.title = title);
-  }, [title]);
-};
+    useEffect(() => {
+        title && (document.title = title)
+    }, [title])
+}
