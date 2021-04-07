2c77e79274110f5eedf53de95e4f763c57c7bf10
MacRae Linton <macrael@truss.works>
2021-04-07 16:45:59 -0700

    Get all the config into cypress. shellcheck add_cypress_test_users.sh

--- scripts/add_cypress_test_users.sh
+++ scripts/add_cypress_test_users.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
+# This script attempts to create two users in the given stage with a password set in $TEST_USERS_PASS
+# These users are created directly in cognito, useful for AWS_COGNITO style login. 
+# It should _NEVER_ be run in production where users are managed by IDM.
+
 set -e
 
 stage=${1:-dev}
 
 # Test users are only available in DEV environments.
-TEST_USER_PASSWORD="Passw0rd!"
+test_user_password=$TEST_USERS_PASS
 
 test_users_exclude_stages=(
   'main'
@@ -12,44 +17,39 @@ test_users_exclude_stages=(
   'prod'
 )
 
-export ALLOW_DEV_LOGIN=true
 for excluded_stage in "${test_users_exclude_stages[@]}"
 do
-    if [ $stage == $excluded_stage ]
+    if [ "$stage" == "$excluded_stage" ]
     then
-       export ALLOW_DEV_LOGIN=false
-       echo "INFO: Will not set test users in this branch."
-       break
+       echo "INFO: Will not set test cognito users in this branch."
+       exit 1
     fi
 done
 
 # Add test users as necessary
-if [ $ALLOW_DEV_LOGIN == true ]
+# Lets first get the emails of developers that have committed to the repo, so we can add the emails as test users.
+# This will also result in adding any new developers in the branch (cumulative).
+echo "INFO: Creating test users .."
+cognito_user_pool_id=$(services/output.sh services/ui-auth UserPoolId "$stage")
+
+if [ -n "$cognito_user_pool_id" ]
 then
-  # Lets first get the emails of developers that have committed to the repo, so we can add the emails as test users.
-  # This will also result in adding any new developers in the branch (cumulative).
-  echo "INFO: Creating test users .."
-  cognito_user_pool_id=`services/output.sh services/ui-auth UserPoolId $stage`
+    
+        # We ignore all the errors if the user exists.
+        echo "add user aang"
+        set +e
+        aws cognito-idp admin-create-user --user-pool-id "$cognito_user_pool_id" --message-action SUPPRESS --username aang@dhs.state.mn.us \
+        --user-attributes Name=given_name,Value=TestFirstName Name=family_name,Value=TestLastName Name=email,Value=aang@dhs.state.mn.us Name=custom:state_code,Value=MN
+        set -e
+        aws cognito-idp admin-set-user-password --user-pool-id "$cognito_user_pool_id" --username aang@dhs.state.mn.us --password "$test_user_password" --permanent
 
-  if [ ! -z "$cognito_user_pool_id" ]
-  then
-      
-          # We ignore all the errors if the user exists.
-          echo "add user aang"
-          set +e
-          aws cognito-idp admin-create-user --user-pool-id $cognito_user_pool_id --message-action SUPPRESS --username aang@dhs.state.mn.us \
-          --user-attributes Name=given_name,Value=TestFirstName Name=family_name,Value=TestLastName Name=email,Value=aang@dhs.state.mn.us Name=custom:state_code,Value=MN
-          aws cognito-idp admin-set-user-password --user-pool-id $cognito_user_pool_id --username aang@dhs.state.mn.us --password $TEST_USER_PASSWORD --permanent
-          set -e
-
-          echo "add user toph"
-          set +e
-          aws cognito-idp admin-create-user --user-pool-id $cognito_user_pool_id --message-action SUPPRESS --username toph@dmas.virginia.gov \
-          --user-attributes Name=given_name,Value=TestFirstName Name=family_name,Value=TestLastName Name=email,Value=toph@dmas.virginia.gov Name=custom:state_code,Value=VA
-          aws cognito-idp admin-set-user-password --user-pool-id $cognito_user_pool_id --username toph@dmas.virginia.gov --password $TEST_USER_PASSWORD --permanent
-          set -e
-  else
-      echo "ERROR: There was an error obtaining AWS resource information to create users or the command was called with the incorrect stage."
-      exit 1
-  fi
+        echo "add user toph"
+        set +e
+        aws cognito-idp admin-create-user --user-pool-id "$cognito_user_pool_id" --message-action SUPPRESS --username toph@dmas.virginia.gov \
+        --user-attributes Name=given_name,Value=TestFirstName Name=family_name,Value=TestLastName Name=email,Value=toph@dmas.virginia.gov Name=custom:state_code,Value=VA
+        set -e
+        aws cognito-idp admin-set-user-password --user-pool-id "$cognito_user_pool_id" --username toph@dmas.virginia.gov --password "$test_user_password" --permanent
+else
+    echo "ERROR: There was an error obtaining AWS resource information to create users or the command was called with the incorrect stage."
+    exit 1
 fi
--- tests/cypress/plugins/index.ts
+++ tests/cypress/plugins/index.ts
@@ -15,7 +15,7 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
     const newConfig = config
-    newConfig.env.AUTH_MODE = process.env.AUTH_MODE
+    newConfig.env.AUTH_MODE = process.env.REACT_APP_AUTH_MODE
     newConfig.env.TEST_USERS_PASS = process.env.TEST_USERS_PASS
 
     return config
