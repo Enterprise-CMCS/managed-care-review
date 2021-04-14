8f28ca9c627efe6839a785be178e4c1f9527cd9a
Josh Franklin <josh@truss.works>
2021-04-14 14:29:19 -0400

    Starting changes to homepage

--- services/app-web/src/pages/Landing/Landing.tsx
+++ services/app-web/src/pages/Landing/Landing.tsx
@@ -1,63 +1,54 @@
 import React from 'react'
-import { GridContainer } from '@trussworks/react-uswds'
+import { Alert, GridContainer, Grid } from '@trussworks/react-uswds'
 import styles from './Landing.module.scss'
 
 export const Landing = (): React.ReactElement => {
     return (
         <>
-            <section className={styles.outlineSection}>
-                <GridContainer>
-                    <h2>How it works</h2>
-                    <ol className={styles.cardList}>
-                        <li>
-                            <span>
-                                We will describe the login process here, once we
-                                know how auth will work
-                            </span>
-                        </li>
-                        <li>
-                            <span>
-                                Fill out the forms and attach all relevant
-                                documents for your submission.
-                            </span>
-                        </li>
-                        <li>
-                            <span>
-                                After you submit, CMS will confirm receipt and
-                                start their review process.
-                            </span>
-                        </li>
-                    </ol>
-                </GridContainer>
-            </section>
             <section className={styles.detailsSection}>
                 <GridContainer className={styles.detailsSectionContent}>
-                    <h2>
-                        In this system, pilot state users can submit Managed
-                        Care contract and rate packages that meet the following
-                        criteria:
-                    </h2>
-                    <ul>
-                        <li>
-                            Contract and rate certifications are fully executed
-                        </li>
-                        <li>
-                            All documents are sent at the time of submission, in
-                            a complete package
-                        </li>
-                        <li>
-                            Populations covered are Medicaid and CHIP or
-                            Medicaid only
-                        </li>
-                    </ul>
-                    <h3>
-                        This system does not currently support the following
-                        submission types:
-                    </h3>
-                    <ul>
-                        <li>State-directed payments</li>
-                        <li>Enrollment broker contracts</li>
-                    </ul>
+                    <Alert type="info" heading="Test submissions only">
+                        This system is still in development and is only accepting test submissions from a small number of pre-selected states. 
+                        Test submissions must be complete at the time of submission and fully executed by all parties.
+                    </Alert>
+                    <Grid row gap>
+                        <Grid col={6}>
+                            <div className={styles.detailsSteps}>
+                                <h2>How it works</h2>
+                                <ul>
+                                    <li>
+                                        <span>Sign in with IDM</span>
+                                        <span>Sign in using your IDM credentials</span>
+                                    </li>
+                                    <li>
+                                        <span>Fill out form and upload documents</span>
+                                        <span>Fill out the submission form and attach all relevant documentation.</span>
+                                    </li>
+                                    <li>
+                                        <span>Recieve an email confirmation</span>
+                                        <span>After you submit, CMS will confirm receipt and start their review process.</span>
+                                    </li>
+                                </ul>
+                            </div>    
+                        </Grid>
+                        <Grid col={6}>
+                            <h2>
+                                You can submit capitation rates and contracts with managed care plans, including:
+                            </h2>
+                            <ul className={styles.detailsList}>
+                                <li>
+                                    Base contracts
+                                </li>
+                                <li>
+                                    Contract amendments
+                                </li>
+                                <li>
+                                    Rate certifications
+                                </li>
+                                <li>Amendments to rate certifications</li>
+                            </ul>
+                        </Grid>
+                    </Grid>
                 </GridContainer>
             </section>
         </>
