dcf64a239e219af46d59ee7612b23d3339347761
Hana Worku <hana@truss.works>
2021-04-21 14:21:13 -0500

    wip

--- services/app-web/src/api/index.ts
+++ services/app-web/src/api/index.ts
@@ -1,2 +1,4 @@
 export { localGQLFetch } from './localGQLFetch'
 export { fakeAmplifyFetch } from './fakeAmplifyFetch'
+// export { s3AmplifyUpload, s3AmplifyGetURL } from './s3Amplify'
+export { s3LocalUploader} from './s3Local'
--- services/app-web/src/pages/Dashboard/Dashboard.tsx
+++ services/app-web/src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { GridContainer, Link } from '@trussworks/react-uswds'
+import { GridContainer, Link, FileInput } from '@trussworks/react-uswds'
 import { NavLink } from 'react-router-dom'
 
 import styles from './Dashboard.module.scss'
@@ -8,6 +8,22 @@ import { TabPanel } from '../../components/Tabs/TabPanel'
 import { useAuth } from '../../contexts/AuthContext'
 import { Program } from '../../gen/gqlClient'
 
+import AWS from 'aws-sdk'
+import { s3LocalUploader } from '../../api/s3Local'
+
+// Local s3
+const localEndpoint = 'http://localhost:4569'
+const s3Client = new AWS.S3({
+    s3ForcePathStyle: true,
+    apiVersion: '2006-03-01',
+    accessKeyId: 'S3RVER', // This specific key is required when working offline
+    secretAccessKey: 'S3RVER',
+    params: { Bucket: 'local-uploads' },
+    endpoint: new AWS.Endpoint(localEndpoint),
+})
+const s3Upload = s3LocalUploader(s3Client)
+// const s3URLResolver = s3LocalGetURL(s3Client)
+
 export const Dashboard = (): React.ReactElement => {
     const { loginStatus, loggedInUser } = useAuth()
     let programs: Program[] = []
@@ -16,6 +32,16 @@ export const Dashboard = (): React.ReactElement => {
         return <div>Loading User Info</div>
     } else {
         programs = loggedInUser.state.programs
+    }
+
+    const handleOnChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e)
+        if (e.currentTarget.files === null) return
+        try {
+            await s3Upload(e.currentTarget.files[0])
+        } catch (error) {
+            console.log('S3 error', error)
+        }
     }
 
     const ProgramContent = ({
@@ -59,6 +85,12 @@ export const Dashboard = (): React.ReactElement => {
                                 <ProgramContent
                                     key={program.name}
                                     program={program}
+                                />
+
+                                <FileInput
+                                    id="test"
+                                    name="test"
+                                    onChange={handleOnChange}
                                 />
                             </GridContainer>
                         </TabPanel>
