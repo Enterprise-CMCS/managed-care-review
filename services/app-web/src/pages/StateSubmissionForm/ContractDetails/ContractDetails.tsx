56080c0daca81e8039a1be39f94e5ab642433bda
Josh Franklin <josh@truss.works>
2021-05-17 18:32:52 -0400

    Starting contract details page

--- services/app-web/src/pages/StateSubmissionForm/ContractDetails/ContractDetails.tsx
+++ services/app-web/src/pages/StateSubmissionForm/ContractDetails/ContractDetails.tsx
@@ -1,7 +1,11 @@
-import { GridContainer, Button, Link } from '@trussworks/react-uswds'
+import { Form as UswdsForm, FormGroup, Fieldset, Button, Link, DateRangePicker, Label } from '@trussworks/react-uswds'
 import { NavLink, useHistory } from 'react-router-dom'
 import PageHeading from '../../../components/PageHeading'
+import { FieldRadio } from '../../../components/Form/FieldRadio/FieldRadio'
+import { FieldCheckbox } from '../../../components/Form/FieldCheckbox/FieldCheckbox'
 import { DraftSubmission } from '../../../gen/gqlClient'
+import { Formik } from 'formik'
+import styles from '../StateSubmissionForm.module.scss'
 
 export const ContractDetails = ({
     draftSubmission,
@@ -14,8 +18,109 @@ export const ContractDetails = ({
         history.push(`/submissions/${draftSubmission.id}/documents`)
     }
     return (
-        <GridContainer>
-            <PageHeading headingLevel="h2"> Contract details </PageHeading>
+        <>  
+            <PageHeading className={styles.formHeader} headingLevel="h2"> Contract details </PageHeading>    
+            <Formik
+                initialValues={{contractType: ""}}
+                onSubmit={handleFormSubmit}
+            >
+                <UswdsForm
+                    className={styles.formContainer}
+                    id="ContractDetailsForm"
+                    aria-label="Contract Details Form"
+                    onSubmit={handleFormSubmit}
+                >
+                    <fieldset className="usa-fieldset">
+                        <legend className="srOnly">
+                            Contract Details
+                        </legend>
+                        <span>All fields are required</span>
+                        <FormGroup>
+                            <Fieldset 
+                                className={styles.radioGroup}
+                                legend="Contract action type"
+                            >
+                                <FieldRadio
+                                    id="baseContract"
+                                    name="contractType"
+                                    label="Base contract"
+                                    value="baseContract"
+                                />
+                                <FieldRadio
+                                    id="amendmentContract"
+                                    name="contractType"
+                                    label="Amendment to base contract"
+                                    value="baseContract"
+                                />
+                            </Fieldset>
+                        </FormGroup>
+                        <FormGroup>
+                            <Fieldset
+                                legend="Amendment effective dates"
+                            >
+                                <DateRangePicker
+                                    className={styles.dateRangePicker}
+                                    endDateHint="mm/dd/yyyy"
+                                    endDateLabel="Start date"
+                                    endDatePickerProps={{
+                                    disabled: false,
+                                    id: 'contract-date-start',
+                                    name: 'contract-date-start'
+                                    }}
+                                    startDateHint="mm/dd/yyyy"
+                                    startDateLabel="End date"
+                                    startDatePickerProps={{
+                                    disabled: false,
+                                    id: 'contract-date-end',
+                                    name: 'contract-date-end'
+                                    }}
+                                />
+                            </Fieldset>
+                        </FormGroup>
+                        <FormGroup>
+                            <Fieldset
+                                legend="Managed Care entities"
+                            >
+                                <Link
+                                    variant="external"
+                                    href={
+                                        ''
+                                    }
+                                    target="_blank"
+                                >
+                                    Items being amended definitions
+                                </Link>
+                                <div className="usa-hint margin-top-1">Check all that apply</div>
+                                <FieldCheckbox
+                                    id="managedCareOrganization"
+                                    name="managedCareEntity"
+                                    label="Managed Care Organization (MCO)"
+                                    value="mco"
+                                />
+                                <FieldCheckbox
+                                    id="prepaidInpatientHealthPlan"
+                                    name="managedCareEntity"
+                                    label="Prepaid Inpatient Health Plan (PIHP)"
+                                    value="pihp"
+                                />
+                                <FieldCheckbox
+                                    id="prepaidAmbulatoryHealthPlans"
+                                    name="managedCareEntity"
+                                    label="Prepaid Ambulatory Health Plans (PAHP)"
+                                    value="pahp"
+                                />
+                                <FieldCheckbox
+                                    id="primaryCareCaseManagementEntity"
+                                    name="managedCareEntity"
+                                    label="Primary Care Case Management Entity (PCCM Entity)"
+                                    value="pccm"
+                                />
+                            </Fieldset>
+                        </FormGroup>
+                    </fieldset>
+                </UswdsForm>
+                
+            </Formik>
             <Link
                 asCustom={NavLink}
                 className="usa-button usa-button--outline"
@@ -27,6 +132,6 @@ export const ContractDetails = ({
             <Button type="button" onClick={handleFormSubmit}>
                 Continue
             </Button>
-        </GridContainer>
+        </>
     )
 }
