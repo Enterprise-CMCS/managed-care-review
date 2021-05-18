c9759b61f3e402ae7693aef20a61dfb8f7cf0a2e
Hana Worku <hana@truss.works>
2021-05-18 15:26:31 -0500

    Add DataDetail and DoubleColumnRow to storybook

---
+++ services/app-web/src/components/DataDetail/DataDetail.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { DataDetail } from './DataDetail'
+
+export default {
+    title: 'Components/Logo',
+    component: DataDetail,
+}
+
+export const WithString = (): React.ReactElement => (
+    <DataDetail
+        id="rainfall"
+        label="Average rainfall in May"
+        data="31.58 inches"
+    />
+)
+export const WithAddress = (): React.ReactElement => (
+    <DataDetail
+        id="disney"
+        label="Disney World Contact Info"
+        data={
+            <address>
+                Mickey Mouse
+                <a href="mailto:mickey@disney.com">mickey@disney.com</a>
+                <a href="tel:555-555-5555">555-555-5555</a>
+            </address>
+        }
+    />
+)
