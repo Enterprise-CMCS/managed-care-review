53a1149a4d4b9e89e90f1921cd6da1153bfb09d3
Josh Franklin <josh@truss.works>
2021-06-02 11:49:35 -0400

    Skeleton of rate details page (#146)
    * Skeleton of all fields on the rate details page
    
    * Fix start and end data labels on daterange components on contract details and rate details pages
    
    * make file upload fill width of form container
    
    * Fix yarn lint errors
    
    * Only run cypress uploads when needed to make the reason for CI failures more clear
    
    * Testing - echo steps.outcome
    
    * Try writing login button click differntly - may address flake tests
    
    * Being back failure() and always()
    
    * Cypress passing for rate details, also add check for loading indicator to login helper
    
    * Remove helper
    
    Co-authored-by: Hana Worku <hana@truss.works>

---
+++ services/app-web/src/pages/StateSubmissionForm/RateDetails/RateDetails.tsx
@@ -0,0 +1,191 @@
+import {
+    Form as UswdsForm,
+    FormGroup,
+    Fieldset,
+    Button,
+    Link,
+    DateRangePicker,
+    DatePicker,
+    ButtonGroup,
+    Label,
+} from '@trussworks/react-uswds'
+import { NavLink, useHistory } from 'react-router-dom'
+import PageHeading from '../../../components/PageHeading'
+import { FieldRadio } from '../../../components/Form/FieldRadio/FieldRadio'
+import { FieldCheckbox } from '../../../components/Form/FieldCheckbox/FieldCheckbox'
+import { DraftSubmission } from '../../../gen/gqlClient'
+import { Formik } from 'formik'
+import styles from '../StateSubmissionForm.module.scss'
+
+export const RateDetails = ({
+    draftSubmission,
+}: {
+    draftSubmission: DraftSubmission
+}): React.ReactElement => {
+    const history = useHistory()
+
+    const handleFormSubmit = () => {
+        history.push(`/submissions/${draftSubmission.id}/documents`)
+    }
+    return (
+        <>
+            <PageHeading className={styles.formHeader} headingLevel="h2">
+                Rate details
+            </PageHeading>
+            <Formik
+                initialValues={{ contractType: '' }}
+                onSubmit={handleFormSubmit}
+            >
+                <UswdsForm
+                    className={styles.formContainer}
+                    id="ContractDetailsForm"
+                    aria-label="Contract Details Form"
+                    onSubmit={handleFormSubmit}
+                >
+                    <fieldset className="usa-fieldset">
+                        <legend className="srOnly">Contract Details</legend>
+                        <span>All fields are required</span>
+                        <FormGroup>
+                            <Fieldset
+                                className={styles.radioGroup}
+                                legend="Rate certification type"
+                            >
+                                <FieldRadio
+                                    id="newRate"
+                                    name="rateType"
+                                    label="New rate certification"
+                                    value="new-rate"
+                                />
+                                <FieldRadio
+                                    id="amendmentRate"
+                                    name="rateType"
+                                    label="Amendment to prior rate certification"
+                                    value="amendment-rate"
+                                />
+                            </Fieldset>
+                        </FormGroup>
+                        <FormGroup>
+                            <Fieldset legend="Rating period">
+                                <DateRangePicker
+                                    className={styles.dateRangePicker}
+                                    endDateHint="mm/dd/yyyy"
+                                    endDateLabel="End date"
+                                    endDatePickerProps={{
+                                        disabled: false,
+                                        id: 'rateDateEnd',
+                                        name: 'rate-date-end',
+                                    }}
+                                    startDateHint="mm/dd/yyyy"
+                                    startDateLabel="Start date"
+                                    startDatePickerProps={{
+                                        disabled: false,
+                                        id: 'rateDateStart',
+                                        name: 'rate-date-start',
+                                    }}
+                                />
+                            </Fieldset>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label
+                                htmlFor="certification-date"
+                                id="certificationDateLabel"
+                            >
+                                Date certified
+                            </Label>
+                            <div
+                                className="usa-hint"
+                                id="certificationDateHint"
+                            >
+                                mm/dd/yyyy
+                            </div>
+                            <DatePicker
+                                aria-describedby="certificationDateLabel certificationDateHint"
+                                id="certificationDate"
+                                name="certification-date"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Fieldset legend="Rating period of original rate certification">
+                                <DateRangePicker
+                                    className={styles.dateRangePicker}
+                                    endDateHint="mm/dd/yyyy"
+                                    endDateLabel="End date"
+                                    endDatePickerProps={{
+                                        disabled: false,
+                                        id: 'originalRateDateEnd',
+                                        name: 'original-rate-date-end',
+                                    }}
+                                    startDateHint="mm/dd/yyyy"
+                                    startDateLabel="Start date"
+                                    startDatePickerProps={{
+                                        disabled: false,
+                                        id: 'originalRateDateStart',
+                                        name: 'original-rate-date-start',
+                                    }}
+                                />
+                            </Fieldset>
+                        </FormGroup>
+                        <FormGroup>
+                            <Fieldset legend="Effective dates of rate amendment">
+                                <FieldCheckbox
+                                    name="same-as-original-rate"
+                                    label="Use same dates as original rating period"
+                                    id="SameAsOriginalRatePeriod"
+                                />
+                                <DateRangePicker
+                                    className={styles.dateRangePicker}
+                                    endDateHint="mm/dd/yyyy"
+                                    endDateLabel="End date"
+                                    endDatePickerProps={{
+                                        disabled: false,
+                                        id: 'amendmentRateDateEnd',
+                                        name: 'amendment-rate-date-end',
+                                    }}
+                                    startDateHint="mm/dd/yyyy"
+                                    startDateLabel="Start date"
+                                    startDatePickerProps={{
+                                        disabled: false,
+                                        id: 'amendmentRateDateStart',
+                                        name: 'amendment-rate-date-start',
+                                    }}
+                                />
+                            </Fieldset>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label
+                                htmlFor="amendment-certification-date"
+                                id="amendmentCertificationDateLabel"
+                            >
+                                Date certified for rate amendment
+                            </Label>
+                            <div
+                                className="usa-hint"
+                                id="amendmentCertificationDateHint"
+                            >
+                                mm/dd/yyyy
+                            </div>
+                            <DatePicker
+                                aria-describedby="amendmentCertificationDateLabel amendmentCertificationDateHint"
+                                id="amendmentCertificationDate"
+                                name="amendment-certification-date"
+                            />
+                        </FormGroup>
+                    </fieldset>
+                    <ButtonGroup className={styles.buttonGroup}>
+                        <Link
+                            asCustom={NavLink}
+                            className={`${styles.outlineButtonLink} usa-button usa-button--outline`}
+                            variant="unstyled"
+                            to="/dashboard"
+                        >
+                            Cancel
+                        </Link>
+                        <Button type="button" onClick={handleFormSubmit}>
+                            Continue
+                        </Button>
+                    </ButtonGroup>
+                </UswdsForm>
+            </Formik>
+        </>
+    )
+}
