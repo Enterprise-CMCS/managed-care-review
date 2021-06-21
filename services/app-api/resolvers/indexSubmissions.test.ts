1c01d3a09a1d1244799758ede90f3901883614fa
MacRae Linton <macrael@truss.works>
2021-06-21 13:49:42 -0500

    Test that we don't return cross-state submissions

--- services/app-api/resolvers/indexSubmissions.test.ts
+++ services/app-api/resolvers/indexSubmissions.test.ts
@@ -73,4 +73,46 @@ describe('indexDraftSubmission', () => {
         expect(draftResult.__typename).toBe('DraftSubmission')
         expect(stateResult.__typename).toBe('StateSubmission')
     })
+
+    it('returns no submissions for a different user', async () => {
+        const server = constructTestServer()
+
+        const { query, mutate } = createTestClient(server)
+
+        // First, create a new submission
+        const draftSub = await createTestDraftSubmission(mutate)
+        const stateSub = await createTestStateSubmission(mutate)
+
+        const otherUserServer = constructTestServer({
+            context: {
+                user: {
+                    name: 'Aang',
+                    state_code: 'VA',
+                    role: 'STATE_USER',
+                    email: 'aang@mn.gov',
+                },
+            },
+        })
+
+        const { query: otherStateQuery } = createTestClient(otherUserServer)
+
+        // then see if we can get that same submission back from the index
+        const result = await otherStateQuery({
+            query: INDEX_SUBMISSIONS,
+        })
+
+        expect(result.errors).toBeUndefined()
+
+        const submissionsIndex = result.data.indexSubmissions
+
+        // Since we don't wipe the DB between tests, here we filter out all
+        // the extraneous submissions and grab the two we started with.
+
+        const theseSubmissions: Submission[] = submissionsIndex.edges
+            .map((edge: SubmissionEdge) => edge.node)
+            .filter((sub: Submission) =>
+                [draftSub.id, stateSub.id].includes(sub.id)
+            )
+        expect(theseSubmissions.length).toBe(0)
+    })
 })
