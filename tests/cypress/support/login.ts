907c17d8042d4dc1639a438d86b60df64d7a794b
MacRae Linton <macrael@truss.works>
2021-06-26 00:55:41 -0700

    try findbyrole

--- tests/cypress/support/login.ts
+++ tests/cypress/support/login.ts
@@ -1,7 +1,7 @@
 Cypress.Commands.add('login', () => {
     cy.visit('/')
     cy.findByRole('progressbar', { name: 'Loading' }).should('not.exist')
-    cy.contains('Sign In').should('exist').click()
+    cy.findByRole('link', {name: 'Sign In'}).click()
     const authMode = Cypress.env('AUTH_MODE')
     console.log(authMode, 'authmode')
     if (authMode === 'LOCAL') {
