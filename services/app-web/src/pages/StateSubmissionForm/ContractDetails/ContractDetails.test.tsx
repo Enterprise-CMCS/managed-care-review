d890a57cea4ba9d9a690d2331c1c8a07e4bc5663
MacRae Linton <55759+macrael@users.noreply.github.com>
2021-07-06 16:56:57 -0700

    fix copy issue for Other in Items Amended list (#190)

--- services/app-web/src/pages/StateSubmissionForm/ContractDetails/ContractDetails.test.tsx
+++ services/app-web/src/pages/StateSubmissionForm/ContractDetails/ContractDetails.test.tsx
@@ -180,7 +180,7 @@ describe('ContractDetails', () => {
 
         // check error for not selected
         expect(
-            screen.getByText('You must enter the other item')
+            screen.getByText('You must enter a description')
         ).toBeInTheDocument()
 
         // click annual rate
@@ -190,7 +190,7 @@ describe('ContractDetails', () => {
         })
 
         // error should be gone
-        expect(screen.queryByText('You must enter the other item')).toBeNull()
+        expect(screen.queryByText('You must enter a description')).toBeNull()
     })
 
     it('progressively discloses option for covid', async () => {
