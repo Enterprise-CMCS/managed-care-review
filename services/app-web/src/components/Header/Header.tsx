60e2fb861374c26ca65e2448b46758c6616f741e
MacRae Linton <macrael@truss.works>
2021-07-21 17:00:39 -0700

    Extract UserLoginInfo component

--- services/app-web/src/components/Header/Header.tsx
+++ services/app-web/src/components/Header/Header.tsx
@@ -1,88 +1,22 @@
 import React from 'react'
-import {
-    Alert,
-    Button,
-    Link,
-    GridContainer,
-    Grid,
-} from '@trussworks/react-uswds'
-import { NavLink } from 'react-router-dom'
+import { Alert, GridContainer, Grid } from '@trussworks/react-uswds'
+import { NavLink, useHistory } from 'react-router-dom'
 
-import { idmRedirectURL } from '../../pages/Auth/cognitoAuth'
 import onemacLogo from '../../assets/images/onemac-logo.svg'
-import styles from './Header.module.scss'
-
 import { getRouteName } from '../../constants/routes'
-import { useHistory } from 'react-router-dom'
-import { LoginStatusType, useAuth } from '../../contexts/AuthContext'
+import { useAuth } from '../../contexts/AuthContext'
 import { usePage } from '../../contexts/PageContext'
 import { AuthModeType } from '../../common-code/domain-models'
 import { Logo } from '../Logo/Logo'
+
 import { PageHeadingRow } from './PageHeadingRow'
-import { User } from '../../gen/gqlClient'
+import { UserLoginInfo } from './UserLoginInfo'
+
+import styles from './Header.module.scss'
 
 export type HeaderProps = {
     authMode: AuthModeType
     setAlert?: React.Dispatch<React.ReactElement>
-}
-
-type LogoutHandlerT = (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-) => void
-
-const LoggedInUserInfo = (
-    user: User,
-    logout: LogoutHandlerT
-): React.ReactElement => {
-    return (
-        <div className={styles.userInfo}>
-            <span>{user.email}</span>
-            <span className={styles.divider}>|</span>
-
-            <Button type="button" unstyled onClick={logout}>
-                Sign out
-            </Button>
-        </div>
-    )
-}
-
-const LoggedOutUserInfo = (authMode: AuthModeType): React.ReactElement => {
-    return authMode === 'IDM' ? (
-        <Link
-            className="usa-button usa-button--outline usa-button--inverse"
-            variant="unstyled"
-            href={idmRedirectURL()}
-        >
-            Sign In
-        </Link>
-    ) : (
-        <Link
-            asCustom={NavLink}
-            className="usa-button usa-button--outline usa-button--inverse"
-            variant="unstyled"
-            to="/auth"
-        >
-            Sign In
-        </Link>
-    )
-}
-
-const UserInfo = ({
-    user,
-    loginStatus,
-    authMode,
-    logout,
-}: {
-    user: User | undefined
-    loginStatus: LoginStatusType
-    authMode: AuthModeType
-    logout: LogoutHandlerT
-}): React.ReactElement | null => {
-    return user
-        ? LoggedInUserInfo(user, logout)
-        : loginStatus === 'LOADING'
-        ? null
-        : LoggedOutUserInfo(authMode)
 }
 
 /**
@@ -132,7 +66,7 @@ export const Header = ({
                             />
                             <span>Managed Care Review</span>
                         </NavLink>
-                        <UserInfo
+                        <UserLoginInfo
                             user={loggedInUser}
                             loginStatus={loginStatus}
                             authMode={authMode}
