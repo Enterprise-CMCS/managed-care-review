d8442945433ca63920c585df02fdd3429433e62a
Harsha Murthy <harsha@truss.works>
2021-07-26 15:39:53 -0500

    Move DynamicStepIndicator into it's own component

--- services/app-web/src/components/DynamicStepIndicator/DynamicStepIndicator.tsx
+++ services/app-web/src/components/DynamicStepIndicator/DynamicStepIndicator.tsx
@@ -1,15 +1,16 @@
-import {
-  StepIndicator,
-  StepIndicatorStep,
-} from '@trussworks/react-uswds'
+import { StepIndicator, StepIndicatorStep } from '@trussworks/react-uswds'
 
-import {
-  getRouteName,
-  PageTitlesRecord,
-  RouteT,
-} from '../../constants/routes'
+import { getRouteName, PageTitlesRecord, RouteT } from '../../constants/routes'
 
 import { SubmissionType as SubmissionTypeT } from '../../gen/gqlClient'
+
+const FormPages = [
+    'SUBMISSIONS_CONTRACT_DETAILS',
+    'SUBMISSIONS_RATE_DETAILS',
+    'SUBMISSIONS_CONTACTS',
+    'SUBMISSIONS_DOCUMENTS',
+    'SUBMISSIONS_REVIEW_SUBMIT',
+] as RouteT[]
 
 export const DynamicStepIndicator = ({
     submissionType,
@@ -18,48 +19,44 @@ export const DynamicStepIndicator = ({
     submissionType?: SubmissionTypeT
     pathname: string
 }): React.ReactElement | null => {
-    const FormPages = [
-        'SUBMISSIONS_CONTRACT_DETAILS',
-        'SUBMISSIONS_RATE_DETAILS',
-        'SUBMISSIONS_CONTACTS',
-        'SUBMISSIONS_DOCUMENTS',
-        'SUBMISSIONS_REVIEW_SUBMIT',
-    ] as RouteT[]
     const currentFormPage = getRouteName(pathname)
 
     let formStepCompleted = true
     let formStepStatus: 'current' | 'complete' | undefined
 
     const activeFormPages = FormPages.filter((formPage) => {
-      return !(submissionType === 'CONTRACT_ONLY' && formPage === 'SUBMISSIONS_RATE_DETAILS')
+        return !(
+            submissionType === 'CONTRACT_ONLY' &&
+            formPage === 'SUBMISSIONS_RATE_DETAILS'
+        )
     })
 
     if (currentFormPage === 'SUBMISSIONS_TYPE') {
-      return null
-  } else {
-      return (
-          <>
-              <StepIndicator>
-                  {activeFormPages.map((formPageName) => {
-                      if (formPageName === currentFormPage) {
-                          formStepCompleted = false
-                          formStepStatus = 'current'
-                      } else if (formStepCompleted) {
-                          formStepStatus = 'complete'
-                      } else {
-                          formStepStatus = undefined
-                      }
+        return null
+    } else {
+        return (
+            <>
+                <StepIndicator>
+                    {activeFormPages.map((formPageName) => {
+                        if (formPageName === currentFormPage) {
+                            formStepCompleted = false
+                            formStepStatus = 'current'
+                        } else if (formStepCompleted) {
+                            formStepStatus = 'complete'
+                        } else {
+                            formStepStatus = undefined
+                        }
 
-                      return (
-                          <StepIndicatorStep
-                              label={PageTitlesRecord[formPageName]}
-                              status={formStepStatus}
-                              key={PageTitlesRecord[formPageName]}
-                          />
-                      )
-                  })}
-              </StepIndicator>
-          </>
-      )
-  }
+                        return (
+                            <StepIndicatorStep
+                                label={PageTitlesRecord[formPageName]}
+                                status={formStepStatus}
+                                key={PageTitlesRecord[formPageName]}
+                            />
+                        )
+                    })}
+                </StepIndicator>
+            </>
+        )
+    }
 }
