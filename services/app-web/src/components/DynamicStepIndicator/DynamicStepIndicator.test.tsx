c7758ac03ed561c13a4f5fbb140a6adfaf83c2b0
Harsha Murthy <harsha@truss.works>
2021-08-03 12:32:51 -0500

    Add tests for DynamicStepIndicator component

--- services/app-web/src/components/DynamicStepIndicator/DynamicStepIndicator.test.tsx
+++ services/app-web/src/components/DynamicStepIndicator/DynamicStepIndicator.test.tsx
@@ -2,10 +2,65 @@ import React from 'react'
 import { screen, render } from '@testing-library/react'
 import { DynamicStepIndicator } from './DynamicStepIndicator'
 
-describe('DynamicStepIndicator component', () => {
-    it('renders without errors', async () => {
-        render(<DynamicStepIndicator delayMS={0} />)
+import { RouteT } from '../../constants/routes'
 
-        expect(await screen.findByText('Loading')).toBeInTheDocument()
+describe('DynamicStepIndicator', () => {
+    const formPages = [
+        'SUBMISSIONS_TYPE',
+        'SUBMISSIONS_CONTRACT_DETAILS',
+        'SUBMISSIONS_RATE_DETAILS',
+        'SUBMISSIONS_CONTACTS',
+        'SUBMISSIONS_DOCUMENTS',
+        'SUBMISSIONS_REVIEW_SUBMIT',
+    ] as RouteT[]
+
+    it('renders without errors', () => {
+        render(
+            <DynamicStepIndicator
+                formPages={formPages}
+                currentFormPage={'SUBMISSIONS_CONTRACT_DETAILS'}
+            />
+        )
+
+        expect(screen.getAllByRole('listitem').length).toBe(formPages.length)
+    })
+
+    it('displays previous steps with the correct styles', () => {
+        render(
+            <DynamicStepIndicator
+                formPages={formPages}
+                currentFormPage={formPages[4]}
+            />
+        )
+
+        expect(screen.getAllByRole('listitem')[3]).toHaveClass(
+            'usa-step-indicator__segment--complete'
+        )
+    })
+
+    it('displays current step with the correct styles', () => {
+        render(
+            <DynamicStepIndicator
+                formPages={formPages}
+                currentFormPage={formPages[4]}
+            />
+        )
+
+        expect(screen.getAllByRole('listitem')[4]).toHaveClass(
+            'usa-step-indicator__segment--current'
+        )
+    })
+
+    it('displays nothing when the current form page is passed an unknown route', () => {
+        render(
+            <div data-testid="container">
+                <DynamicStepIndicator
+                    formPages={formPages}
+                    currentFormPage={'UNKNOWN_ROUTE'}
+                />
+            </div>
+        )
+
+        expect(screen.getByTestId('container')).toBeEmptyDOMElement()
     })
 })
