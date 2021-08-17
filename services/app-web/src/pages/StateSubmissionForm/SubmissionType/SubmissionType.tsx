6582757745089699d7f3a98a60c3b369b289be0e
Harsha Murthy <harsha@truss.works>
2021-08-17 11:02:56 -0500

    Fix linter warnings

--- services/app-web/src/pages/StateSubmissionForm/SubmissionType/SubmissionType.tsx
+++ services/app-web/src/pages/StateSubmissionForm/SubmissionType/SubmissionType.tsx
@@ -14,7 +14,6 @@ import { Formik, FormikHelpers, FormikErrors } from 'formik'
 import { NavLink, useHistory, Link as ReactRouterLink } from 'react-router-dom'
 
 import { MCRouterState } from '../../../constants/routerState'
-import PageHeading from '../../../components/PageHeading'
 import {
     CreateDraftSubmissionInput,
     DraftSubmission,
