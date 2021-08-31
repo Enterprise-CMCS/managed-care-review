eee70ccf8f347d46438d0ab33d59d15b3b5d33d9
Harsha Murthy <harsha@truss.works>
2021-08-31 11:04:08 -0500

    Add additional test to Smoke Test

--- tests/cypress/integration/smokeTest/smokeTest.spec.ts
+++ tests/cypress/integration/smokeTest/smokeTest.spec.ts
@@ -4,6 +4,26 @@ describe('smoke test', () => {
         cy.url().should('eq', Cypress.config().baseUrl + '/dashboard')
     })
 
+    it('can create and submit a new contract only submission', () => {
+        cy.loginAsStateUser()
+
+        cy.startNewContractOnlySubmission()
+
+        cy.fillOutContractDetails()
+        cy.navigateForm('Continue')
+
+        cy.fillOutStateContact()
+        cy.navigateForm('Continue')
+
+        cy.fillOutDocuments()
+        cy.navigateForm('Continue')
+
+        cy.reviewAndSubmitStateSubmissionForm()
+
+        cy.findByText('Dashboard').should('exist')
+        cy.findByText(/was sent to CMS/).should('exist')
+    })
+
     it('can create and submit a new contract and rates submission', () => {
         cy.loginAsStateUser()
 
