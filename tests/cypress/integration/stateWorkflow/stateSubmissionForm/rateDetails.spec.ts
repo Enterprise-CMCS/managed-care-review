c54760a073742b034266fc6fa99e04d011aaf6e5
Harsha Murthy <harsha@truss.works>
2021-09-15 12:28:27 -0500

    Add can navigate to and from rate details page to rate details test

--- tests/cypress/integration/stateWorkflow/stateSubmissionForm/rateDetails.spec.ts
+++ tests/cypress/integration/stateWorkflow/stateSubmissionForm/rateDetails.spec.ts
@@ -1,4 +1,36 @@
 describe('rate details', () => {
+    it.only('can navigate to and from rate details page', () => {
+        cy.logInAsStateUser()
+        cy.startNewContractAndRatesSubmission()
+
+        // Navigate to rate details page
+        cy.location().then((fullUrl) => {
+            const { pathname } = fullUrl
+            const pathnameArray = pathname.split('/')
+            const draftSubmissionId = pathnameArray[2]
+            cy.visit(`/submissions/${draftSubmissionId}/rate-details`)
+
+            // Navigate to contract details page by clicking back for a contract only submission
+            cy.findByRole('link', { name: /Back/ }).click()
+            cy.findByRole('heading', { level: 2, name: /Contract details/ })
+
+            // Navigate to rate details page
+            cy.visit(`/submissions/${draftSubmissionId}/rate-details`)
+
+            // Navigate to dashboard page by clicking save as draft
+            cy.findByRole('button', { name: /Save as draft/ }).click()
+            cy.findByRole('heading', { level: 1, name: /Dashboard/ })
+
+            // Navigate to rate details page
+            cy.visit(`/submissions/${draftSubmissionId}/rate-details`)
+
+            cy.fillOutNewRateCertification()
+
+            // Navigate to contacts page by clicking continue
+            cy.navigateForm('Continue')
+            cy.findByRole('heading', { level: 2, name: /Contacts/ })
+        })
+    })
     it('user can add a rates amendment', () => {
         cy.logInAsStateUser()
         cy.startNewContractAndRatesSubmission()
