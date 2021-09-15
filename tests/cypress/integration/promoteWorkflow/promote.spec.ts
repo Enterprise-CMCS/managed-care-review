56b77dd45bb4fdc2d2cb269ff2940f064c310ae3
Harsha Murthy <harsha@truss.works>
2021-09-15 14:34:49 -0500

    Condense promote.spec.ts further

--- tests/cypress/integration/promoteWorkflow/promote.spec.ts
+++ tests/cypress/integration/promoteWorkflow/promote.spec.ts
@@ -2,13 +2,11 @@ describe('promote', () => {
     it('can navigate to landing page', () => {
         cy.visit('/')
         cy.get('#App').should('exist')
-        cy.get('header').should('exist')
         cy.get('main').should('exist')
         cy.findByRole('heading', { level: 2, name: /How it works/ })
         cy.findByRole('heading', {
             level: 2,
             name: /You can submit capitation rates and contracts/,
         })
-        cy.get('footer').should('exist')
     })
 })
