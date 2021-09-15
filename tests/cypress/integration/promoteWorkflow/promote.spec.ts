6f78630d1dc960207f5c68eac95ff7f2e7d399ca
Harsha Murthy <harsha@truss.works>
2021-09-15 14:18:52 -0500

    Condense promote.spec.ts

--- tests/cypress/integration/promoteWorkflow/promote.spec.ts
+++ tests/cypress/integration/promoteWorkflow/promote.spec.ts
@@ -1,42 +1,14 @@
 describe('promote', () => {
-    it('can load without errors', () => {
+    it('can navigate to landing page', () => {
         cy.visit('/')
         cy.get('#App').should('exist')
-    })
-
-    it('can display header', () => {
-        cy.visit('/')
         cy.get('header').should('exist')
-    })
-
-    it('can display heading at root path', () => {
-        cy.visit('/')
-        cy.findByRole('heading', {
-            name: /How it works/i,
-            level: 2,
-        })
-    })
-
-    it('can displays main', () => {
-        cy.visit('/')
         cy.get('main').should('exist')
-    })
-
-    it('can display expected headings that describe how Managed Care Review works', () => {
-        cy.visit('/')
-        cy.get('#App').should('exist')
+        cy.findByRole('heading', { level: 2, name: /How it works/ })
         cy.findByRole('heading', {
-            name: /How it works/i,
             level: 2,
+            name: /You can submit capitation rates and contracts/,
         })
-        cy.findByRole('heading', {
-            name: /You can submit capitation rates and contracts/i,
-            level: 2,
-        })
-    })
-
-    it('can display footer', () => {
-        cy.visit('/')
         cy.get('footer').should('exist')
     })
 })
