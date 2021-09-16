40221ef5ca7b545551899e3fec396abfb881fadd
Hana Worku <hana@truss.works>
2021-09-16 15:34:46 -0500

    Use cy.intercept in wait command

--- tests/cypress/integration/smokeTest/smokeTest.spec.ts
+++ tests/cypress/integration/smokeTest/smokeTest.spec.ts
@@ -1,15 +1,13 @@
 describe('smoke test', () => {
     it('can log in as a state user', () => {
         cy.logInAsStateUser()
-        cy.waitForLoadingToComplete()
         cy.location('pathname', { timeout: 10_000 }).should('eq', '/')
         cy.findByRole('heading', { level: 1, name: /Dashboard/ })
     })
 
     it('can log in as a CMS user', () => {
-        cy.logInAsCMSUser({ initialURL: '/' })
-        cy.waitForLoadingToComplete()
-        cy.url({ timeout: 10_000 }).should('contain', '/')
+        cy.logInAsCMSUser({ initialURL: '/submissions/test' })
+        cy.url({ timeout: 10_000 }).should('contain', '/submissions/test')
     })
 
     it('can contact the API and connect to the database', () => {
