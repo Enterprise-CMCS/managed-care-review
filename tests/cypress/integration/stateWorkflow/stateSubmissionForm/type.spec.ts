de04bbfa539f7128deeb7d53e9ed82d11c2f2752
Harsha Murthy <harsha@truss.works>
2021-09-16 09:52:09 -0500

    Rename submissionType.spec.ts to type.spec.ts

--- tests/cypress/integration/stateWorkflow/stateSubmissionForm/type.spec.ts
+++ tests/cypress/integration/stateWorkflow/stateSubmissionForm/type.spec.ts
@@ -1,24 +1,4 @@
-describe('submission type', () => {
-    it('can navigate to and from new page', () => {
-        cy.logInAsStateUser()
-
-        // Navigate to new page
-        cy.visit(`/submissions/new`)
-
-        // Navigate to dashboard page by clicking cancel
-        cy.findByRole('link', { name: /Cancel/ }).click()
-        cy.findByRole('heading', { level: 1, name: /Dashboard/ })
-
-        // Navigate to new page
-        cy.visit(`/submissions/new`)
-
-        cy.fillOutContractActionOnlySubmissionType()
-
-        // Navigate to contract details page by clicking continue for a contract only submission
-        cy.navigateForm('Continue')
-        cy.findByRole('heading', { level: 2, name: /Contract details/ })
-    })
-
+describe('type', () => {
     it.only('can navigate to and from type page', () => {
         cy.logInAsStateUser()
         cy.startNewContractOnlySubmission()
