d648e40e2fe6c02bcc968b0b72c3bbb39657b849
Mojo Talantikite <mojo.talantikite@gmail.com>
2021-09-22 17:10:56 -0400

    Remove query engine

--- services/app-api/webpack.config.js
+++ services/app-api/webpack.config.js
@@ -75,34 +75,28 @@ module.exports = {
                 {
                     from: path.resolve(
                         __dirname,
-                        'node_modules/@prisma/client'
+                        './node_modules/@prisma/client'
                     ),
                 },
                 {
                     from: path.resolve(
                         __dirname,
-                        'node_modules/@prisma/engines/dist'
+                        './node_modules/@prisma/engines/dist'
                     ),
                 },
                 {
                     from: path.resolve(
                         __dirname,
-                        'node_modules/@prisma/engines/package.json'
+                        './node_modules/@prisma/engines/package.json'
                     ),
                 },
                 {
                     from: path.resolve(
                         __dirname,
-                        'node_modules/@prisma/engines/query-engine-rhel-openssl-1.0.x'
+                        './node_modules/@prisma/engines/migration-engine-rhel-openssl-1.0.x'
                     ),
                 },
-                {
-                    from: path.resolve(
-                        __dirname,
-                        'node_modules/@prisma/engines/migration-engine-rhel-openssl-1.0.x'
-                    ),
-                },
-                { from: path.resolve(__dirname, 'node_modules/.prisma') },
+                { from: path.resolve(__dirname, './node_modules/.prisma') },
             ],
         }),
     ],
