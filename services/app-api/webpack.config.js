70b7d5b4a40440e20a1db86408b069751188d351
Mojo Talantikite <mojo.talantikite@gmail.com>
2021-09-24 16:06:26 -0400

    run a prisma generate

--- services/app-api/webpack.config.js
+++ services/app-api/webpack.config.js
@@ -3,6 +3,7 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 const path = require('path');
 const slsw = require('serverless-webpack');
+const webpack = require('webpack');
 const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 
 const isLocal = slsw.lib.webpack.isLocal;
@@ -60,10 +61,16 @@ module.exports = {
         ],
     },
     plugins: [
+        new webpack.EnvironmentPlugin({
+            PRISMA_CLI_BINARY_TARGETS: 'rhel-openssl-1.0.x',
+            PRISMA_CLI_QUERY_ENGINE_TYPE: 'binary',
+            PRISMA_CLIENT_ENGINE_TYPE: 'binary',
+        }),
         new WebpackShellPluginNext({
             onBuildStart: {
                 scripts: [
                     'PRISMA_CLI_BINARY_TARGETS=rhel-openssl-1.0.x PRISMA_CLIENT_ENGINE_TYPE=binary PRISMA_CLI_QUERY_ENGINE_TYPE=binary yarn install',
+                    'npx prisma generate',
                 ],
                 blocking: true,
                 parallel: false,
