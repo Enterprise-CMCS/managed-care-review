af2b31e487d601c2d5adc1d2684f0b6e0247b219
Harsha Murthy <harsha@truss.works>
2021-09-30 10:13:25 -0500

    Add jest tests for RateDetailsSummarySection

---
+++ services/app-web/src/components/SubmissionSummary/RateDetailsSummarySection/RateDetailsSummarySection.test.tsx
@@ -0,0 +1,65 @@
+import { screen } from '@testing-library/react'
+import { renderWithProviders } from '../../../testHelpers/jestHelpers'
+import { RateDetailsSummarySection } from './RateDetailsSummarySection'
+import {
+    mockContractAndRatesDraft,
+    mockStateSubmission,
+} from '../../../testHelpers/apolloHelpers'
+
+describe('RateDetailsSummarySection', () => {
+    const draftSubmission = mockContractAndRatesDraft()
+    const stateSubmission = mockStateSubmission()
+
+    it('can render draft submission without errors', () => {
+        renderWithProviders(
+            <RateDetailsSummarySection
+                submission={draftSubmission}
+                navigateTo="rate-details"
+            />
+        )
+
+        expect(
+            screen.getByRole('heading', {
+                level: 2,
+                name: 'Rate details',
+            })
+        ).toBeInTheDocument()
+        expect(
+            screen.getByRole('link', { name: 'Edit Rate details' })
+        ).toHaveAttribute('href', '/rate-details')
+    })
+
+    it('can render state submission without errors', () => {
+        renderWithProviders(
+            <RateDetailsSummarySection submission={stateSubmission} />
+        )
+
+        expect(
+            screen.getByRole('heading', {
+                level: 2,
+                name: 'Rate details',
+            })
+        ).toBeInTheDocument()
+        // Is this the best way to check that the link is not present?
+        expect(screen.queryByText('Edit')).not.toBeInTheDocument()
+    })
+
+    it('can render all rate details fields', () => {
+        renderWithProviders(
+            <RateDetailsSummarySection
+                submission={draftSubmission}
+                navigateTo="rate-details"
+            />
+        )
+
+        expect(
+            screen.getByRole('definition', { name: 'Rate certification type' })
+        ).toBeInTheDocument()
+        expect(
+            screen.getByRole('definition', { name: 'Rating period' })
+        ).toBeInTheDocument()
+        expect(
+            screen.getByRole('definition', { name: 'Date certified' })
+        ).toBeInTheDocument()
+    })
+})
