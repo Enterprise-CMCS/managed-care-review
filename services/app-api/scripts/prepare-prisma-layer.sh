cf9d37fcf22ddd632e084175384487c0e78944cd
Mojo Talantikite <mojo.talantikite@gmail.com>
2021-09-30 16:32:34 -0400

    Try to use it as a func

--- services/app-api/scripts/prepare-prisma-layer.sh
+++ services/app-api/scripts/prepare-prisma-layer.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
-echo "Cleaning up workspace ..."
-rm -rf lambda-layers-prisma-client
+function preparePrismaLayer() {
+    echo pwd
+    echo "Cleaning up workspace ..."
+    rm -rf lambda-layers-prisma-client
 
-echo "Creating layer ..."
-mkdir -p lambda-layers-prisma-client/nodejs/node_modules/.prisma
-mkdir -p lambda-layers-prisma-client/nodejs/node_modules/@prisma
+    echo "Creating layer ..."
+    mkdir -p lambda-layers-prisma-client/nodejs/node_modules/.prisma
+    mkdir -p lambda-layers-prisma-client/nodejs/node_modules/@prisma
 
-echo "Prepare Prisma Client lambda layer ..."
-cp -r node_modules/.prisma/client lambda-layers-prisma-client/nodejs/node_modules/.prisma
-cp -r node_modules/@prisma lambda-layers-prisma-client/nodejs/node_modules
+    echo "Prepare Prisma Client lambda layer ..."
+    cp -r node_modules/.prisma/client lambda-layers-prisma-client/nodejs/node_modules/.prisma
+    cp -r node_modules/@prisma lambda-layers-prisma-client/nodejs/node_modules
 
-echo "Remove Prisma CLI..."
-rm -rf lambda-layers-prisma-client/nodejs/node_modules/@prisma/cli
+    echo "Remove Prisma CLI..."
+    rm -rf lambda-layers-prisma-client/nodejs/node_modules/@prisma/cli
 
-echo "Compressing ..."
-pushd lambda-layers-prisma-client && tar -zcf /tmp/nodejs.tar.gz . && mv /tmp/nodejs.tar.gz ./nodejs.tar.gz
+    echo "Compressing ..."
+    pushd lambda-layers-prisma-client && tar -zcf /tmp/nodejs.tar.gz . && mv /tmp/nodejs.tar.gz ./nodejs.tar.gz
 
-echo "Remove unzipped files ..."
-rm -rf nodejs
+    echo "Remove unzipped files ..."
+    rm -rf nodejs
 
-echo "Stats:"
-ls -lh nodejs.tar.gz
+    echo "Stats:"
+    ls -lh nodejs.tar.gz
 
-popd || exit
+    popd || exit
+}
+preparePrismaLayer
