434e6cf181a3fea30a8c694a5459f8e0a322978c
Mojo Talantikite <mojo.talantikite@gmail.com>
2021-10-05 17:25:04 -0400

    Add migration handler

---
+++ services/app-api/handlers/postgres_migrate.ts
@@ -0,0 +1,60 @@
+import { APIGatewayProxyHandler } from 'aws-lambda'
+import { GetConnectionURL } from '../lib/prisma'
+import execa from 'execa'
+
+const authMode = process.env.REACT_APP_AUTH_MODE
+
+export const main: APIGatewayProxyHandler = async () => {
+    // if we're local, don't migrate this way
+    if (authMode === 'LOCAL') {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                code: 'NO_MIGRATE_LOCAL',
+                message: 'Local env. Do not migrate from handler.',
+            }),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
+        }
+    }
+
+    const postgresURL = await GetConnectionURL()
+    if (postgresURL.isErr()) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                code: 'NO_CONNECTION_STRING',
+                message:
+                    'Could not get the connection string from secrets manager',
+            }),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
+        }
+    }
+
+    const { stdout } = await execa.node(
+        'npx prisma',
+        ['migrate', 'deploy', 'dev', '--preview-feature'],
+        {
+            env: {
+                DATABASE_URL: postgresURL.value,
+            },
+        }
+    )
+
+    // testing this out right now:
+    console.log(stdout)
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify('successfully migrated'),
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+        },
+    }
+}
