76cab39a629177807392d5327af9e0409128db8d
Mojo Talantikite <mojo.talantikite@gmail.com>
2021-10-06 17:35:16 -0400

    Reduce layer size

--- services/app-api/scripts/prepare-prisma-layer.sh
+++ services/app-api/scripts/prepare-prisma-layer.sh
@@ -14,8 +14,15 @@ function preparePrismaLayer() {
     rsync -av node_modules/@prisma/ lambda-layers-prisma-client/nodejs/node_modules/@prisma
     rsync -av node_modules/prisma/ lambda-layers-prisma-client/nodejs/node_modules/prisma
 
-    echo "Remove Prisma CLI..."
+    echo "Remove Prisma CLI ..."
     rm -rf lambda-layers-prisma-client/nodejs/node_modules/@prisma/cli
+
+    echo "Remove non-RHEL bins to save space ..."
+    rm -rf lambda-layers-prisma-client/nodejs/node_modules/prisma/libquery_engine-debian-openssl-1.1.x.so.node
+    rm -rf lambda-layers-prisma-client/nodejs/node_modules/@prisma/introspection-engine-debian-openssl-1.1.x 
+    rm -rf lambda-layers-prisma-client/nodejs/node_modules/@prisma/libquery_engine-debian-openssl-1.1.x.so.node 
+    rm -rf lambda-layers-prisma-client/nodejs/node_modules/@prisma/migration-engine-debian-openssl-1.1.x
+    rm -rf lambda-layers-prisma-client/nodejs/node_modules/@prisma/prisma-fmt-debian-openssl-1.1.x
 
     echo "Compressing ..."
     pushd lambda-layers-prisma-client && tar -zcf /tmp/nodejs.tar.gz . && mv /tmp/nodejs.tar.gz ./nodejs.tar.gz
