c8c608d355e57d3909f6ee1daa89dc83e9930fda
Mojo Talantikite <mojo.talantikite@gmail.com>
2021-10-08 14:11:40 -0400

    Catch execa errors

--- services/app-api/handlers/postgres_migrate.ts
+++ services/app-api/handlers/postgres_migrate.ts
@@ -36,17 +36,29 @@ export const main: APIGatewayProxyHandler = async () => {
         }
     }
 
-    // Aurora can have long cold starts, so we extend connection timeout on migrates
-    const { stdout, stderr } = await execa.command(
-        'node /opt/nodejs/node_modules/prisma/build/index.js migrate deploy --schema=/opt/nodejs/prisma/schema.prisma',
-        {
-            env: {
-                DATABASE_URL: postgresURL.value + '&connect_timeout=45',
+    try {
+        // Aurora can have long cold starts, so we extend connection timeout on migrates
+        await execa.command(
+            'node /opt/nodejs/node_modules/prisma/build/index.js migrate deploy --schema=/opt/nodejs/prisma/schema.prisma',
+            {
+                env: {
+                    DATABASE_URL: postgresURL.value + '&connect_timeout=45',
+                },
+            }
+        )
+    } catch (err) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                code: 'MIGRATION_FAILED',
+                message: 'Could not migrate the database ' + err,
+            }),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
             },
         }
-    )
-    console.log('stdout', stdout)
-    console.log('stderr', stderr)
+    }
 
     return {
         statusCode: 200,
