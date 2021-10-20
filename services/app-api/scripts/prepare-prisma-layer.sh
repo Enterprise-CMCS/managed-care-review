79ad6254816cb991f42e0708e4a4b0e06b580b14
Mojo Talantikite <mojo.talantikite@gmail.com>
2021-10-20 14:07:27 -0400

    Delete extra query engine

--- services/app-api/scripts/prepare-prisma-layer.sh
+++ services/app-api/scripts/prepare-prisma-layer.sh
@@ -30,6 +30,7 @@ function preparePrismaLayer() {
 
     echo "Remove non-RHEL bins to save space ..."
     rm -rf lambda-layers-prisma-client/nodejs/node_modules/.prisma/client/libquery_engine-debian-openssl-1.1.x.so.node
+    rm -rf lambda-layers-prisma-client/nodejs/node_modules/.prisma/client/libquery_engine-rhel-openssl-1.0.x.so.node 
     rm -rf lambda-layers-prisma-client/nodejs/node_modules/prisma/engines
     rm -rf lambda-layers-prisma-client/nodejs/node_modules/prisma/libquery_engine-debian-openssl-1.1.x.so.node
     rm -rf lambda-layers-prisma-client/nodejs/node_modules/@prisma/introspection-engine-debian-openssl-1.1.x 
