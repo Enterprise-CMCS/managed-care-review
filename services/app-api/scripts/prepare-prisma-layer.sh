91509649e56ec843bcc5f96399bc72a146761f36
Mojo Talantikite <mojo.talantikite@gmail.com>
2021-10-20 14:06:36 -0400

    Remove dangling debian libquery

--- services/app-api/scripts/prepare-prisma-layer.sh
+++ services/app-api/scripts/prepare-prisma-layer.sh
@@ -29,6 +29,7 @@ function preparePrismaLayer() {
     rm -rf lambda-layers-prisma-client/nodejs/node_modules/@prisma/cli
 
     echo "Remove non-RHEL bins to save space ..."
+    rm -rf lambda-layers-prisma-client/nodejs/node_modules/.prisma/client/libquery_engine-debian-openssl-1.1.x.so.node
     rm -rf lambda-layers-prisma-client/nodejs/node_modules/prisma/engines
     rm -rf lambda-layers-prisma-client/nodejs/node_modules/prisma/libquery_engine-debian-openssl-1.1.x.so.node
     rm -rf lambda-layers-prisma-client/nodejs/node_modules/@prisma/introspection-engine-debian-openssl-1.1.x 
