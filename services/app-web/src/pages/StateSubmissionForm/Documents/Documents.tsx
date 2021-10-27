5bdcf1e8e8110059d67b74d91e975841c11c6007
Hana Worku <hana@truss.works>
2021-10-27 10:01:14 -0500

    Make FileUpload handle isLabelVisible prop to toggle srOnly class on labels

--- services/app-web/src/pages/StateSubmissionForm/Documents/Documents.tsx
+++ services/app-web/src/pages/StateSubmissionForm/Documents/Documents.tsx
@@ -263,6 +263,7 @@ export const Documents = ({
                         id="documents"
                         name="documents"
                         label="Upload documents"
+                        isLabelVisible={false}
                         hint={
                             <>
                                 <Link
