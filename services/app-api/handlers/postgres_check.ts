9b8595230e8bc97642cf7056ea278a0f33f94335
MacRae Linton <macrael@truss.works>
2021-11-10 17:57:17 -0800

    Update other postgres handlers

--- services/app-api/handlers/postgres_check.ts
+++ services/app-api/handlers/postgres_check.ts
@@ -1,8 +1,42 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
-import { NewPrismaClient } from '../lib/prisma'
+import { NewPrismaClient } from '../postgres'
+import { FetchSecrets } from '../secrets'
 export const main: APIGatewayProxyHandler = async () => {
-    const prismaResult = await NewPrismaClient()
-    if (prismaResult.isErr()) {
+    const dbURL = process.env.DATABASE_URL
+    const secretsManagerSecret = process.env.SECRETS_MANAGER_SECRET
+
+    if (!dbURL) {
+        throw new Error('Init Error: DATABASE_URL is required to run app-api')
+    }
+
+    let dbConnectionURL = dbURL
+    if (dbURL === 'AWS_SM') {
+        if (!secretsManagerSecret) {
+            console.log(
+                'Init Error: The env var SECRETS_MANAGER_SECRET must be set if DATABASE_URL=AWS_SM'
+            )
+            throw new Error(
+                'Init Error: The env var SECRETS_MANAGER_SECRET must be set if DATABASE_URL=AWS_SM'
+            )
+        }
+
+        // We need to pull the db url out of AWS Secrets Manager
+        // if we put more secrets in here, we'll probably need to instantiate it somewhere else
+        const secretsResult = await FetchSecrets(secretsManagerSecret)
+        if (secretsResult instanceof Error) {
+            console.log(
+                'Init Error: Failed to fetch secrets from Secrets Manager',
+                secretsResult
+            )
+            throw secretsResult
+        }
+
+        dbConnectionURL = secretsResult.pgConnectionURL
+    }
+
+    const prismaResult = await NewPrismaClient(dbConnectionURL)
+
+    if (prismaResult instanceof Error) {
         return {
             statusCode: 400,
             body: JSON.stringify({
@@ -15,7 +49,7 @@ export const main: APIGatewayProxyHandler = async () => {
             },
         }
     }
-    const prisma = prismaResult.value
+    const prisma = prismaResult
 
     // just do a rando query for now to test this connection out
     // since we don't have a store for this yet
--- services/app-api/handlers/postgres_migrate.ts
+++ services/app-api/handlers/postgres_migrate.ts
@@ -1,33 +1,59 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
-import { GetConnectionURL } from '../lib/prisma'
+import { FetchSecrets } from '../secrets'
 import { execSync } from 'child_process'
 
-const authMode = process.env.REACT_APP_AUTH_MODE
-
 export const main: APIGatewayProxyHandler = async () => {
+    const authMode = process.env.REACT_APP_AUTH_MODE
+    const dbURL = process.env.DATABASE_URL
+    const secretsManagerSecret = process.env.SECRETS_MANAGER_SECRET
+
+    if (!dbURL) {
+        throw new Error('Init Error: DATABASE_URL is required to run app-api')
+    }
+
+    let dbConnectionURL = dbURL
+    if (dbURL === 'AWS_SM') {
+        if (!secretsManagerSecret) {
+            console.log(
+                'Init Error: The env var SECRETS_MANAGER_SECRET must be set if DATABASE_URL=AWS_SM'
+            )
+            throw new Error(
+                'Init Error: The env var SECRETS_MANAGER_SECRET must be set if DATABASE_URL=AWS_SM'
+            )
+        }
+
+        // We need to pull the db url out of AWS Secrets Manager
+        // if we put more secrets in here, we'll probably need to instantiate it somewhere else
+        const secretsResult = await FetchSecrets(secretsManagerSecret)
+        if (secretsResult instanceof Error) {
+            console.log(
+                'Init Error: Failed to fetch secrets from Secrets Manager',
+                secretsResult
+            )
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    code: 'NO_CONNECTION_STRING',
+                    message:
+                        'Could not get the connection string from secrets manager',
+                }),
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': true,
+                },
+            }
+        }
+
+        dbConnectionURL = secretsResult.pgConnectionURL
+    }
+
     // if we're local, don't migrate this way
-    if (authMode === 'LOCAL') {
+    if (authMode === 'LOCAL' || dbURL !== 'AWS_SM') {
         return {
             statusCode: 400,
             body: JSON.stringify({
                 code: 'NO_MIGRATE_LOCAL',
                 message: 'Local env. Do not migrate from handler.',
-            }),
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': true,
-            },
-        }
-    }
-
-    const postgresURL = await GetConnectionURL()
-    if (postgresURL.isErr()) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                code: 'NO_CONNECTION_STRING',
-                message:
-                    'Could not get the connection string from secrets manager',
             }),
             headers: {
                 'Access-Control-Allow-Origin': '*',
@@ -42,7 +68,7 @@ export const main: APIGatewayProxyHandler = async () => {
             `${process.execPath} /opt/nodejs/node_modules/prisma/build/index.js migrate deploy --schema=/opt/nodejs/prisma/schema.prisma`,
             {
                 env: {
-                    DATABASE_URL: postgresURL.value + '&connect_timeout=60',
+                    DATABASE_URL: dbConnectionURL + '&connect_timeout=60',
                 },
             }
         )
