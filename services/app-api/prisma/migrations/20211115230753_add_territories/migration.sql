57896536c9658e6bf885a171c5de26b7bb36e3e3
MacRae Linton <macrael@truss.works>
2021-11-16 17:06:39 -0800

    Refix Migration

--- services/app-api/prisma/migrations/20211115230753_add_territories/migration.sql
+++ services/app-api/prisma/migrations/20211115230753_add_territories/migration.sql
@@ -1,7 +1,5 @@
--- Add territories into the db
+-- Add missing territories into the db
 
 INSERT INTO "State" VALUES ('AS', 'American Samoa');
 INSERT INTO "State" VALUES ('GU', 'Guam');
-INSERT INTO "State" VALUES ('PR', 'Puerto Rico');
-INSERT INTO "State" VALUES ('VI', 'U.S. Virgin Islands');
 INSERT INTO "State" VALUES ('MP', 'Northern Mariana Islands');
