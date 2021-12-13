a07be74bd84ea78b7f68e4d709bb61c215f4e55a
MacRae Linton <macrael@truss.works>
2021-12-13 10:18:29 -0800

    don't checkin the compiled add_cypreess_test_users.js script

--- tests/cypress/integration/stateWorkflow/login/login.spec.ts
+++ tests/cypress/integration/stateWorkflow/login/login.spec.ts
@@ -12,7 +12,7 @@ describe('login', () => {
     it('can log in and see personal dashboard for their state', () => {
         cy.logInAsStateUser()
 
-        cy.findByText('aang@dhs.state.mn.us').should('exist')
+        cy.findByText('aang@example').should('exist')
         cy.findByRole('heading', { name: 'Minnesota Dashboard' }).should(
             'exist'
         )
