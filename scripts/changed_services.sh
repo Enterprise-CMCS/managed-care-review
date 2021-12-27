9b1d267ddcce3f084d633d9d597af08d8d7242ca
Mojo Talantikite <mojo.talantikite@gmail.com>
2021-12-27 17:48:45 -0500

    Test syntax

--- scripts/changed_services.sh
+++ scripts/changed_services.sh
@@ -4,6 +4,6 @@ set -e
 triggered_commit="$1"
 
 previous_commit=$(git rev-parse --verify --short "$triggered_commit"^)
-changed_services="$(lerna ls --since "$previous_commit" -all --json | jq -c '[.[] | {name: .name}]')"
+changed_services="$(lerna ls --since "$previous_commit" -all --json | jq -c '[.[] | .name]')"
 
 printf "%s" "$changed_services"
