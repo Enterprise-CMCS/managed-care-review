2852199ba224e23ac78bd2daba6d3f30db688815
Mojo Talantikite <mojo.talantikite@gmail.com>
2021-12-27 16:27:49 -0500

    put it in a script

---
+++ scripts/changed_services.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+triggered_commit="$1"
+previous_commit=$(git rev-parse --verify --short "$triggered_commit"^)
+changed_services="$(lerna ls --since "$previous_commit" -all --json | jq '[.[] | .name]')"
+
+echo "$changed_services"
