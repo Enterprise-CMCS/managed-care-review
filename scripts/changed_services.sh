8faf3497ff6e2f4d42f0e8c21f8bf0ee86af2f1b
Mojo Talantikite <mojo.talantikite@gmail.com>
2021-12-27 16:32:14 -0500

    pass a triggered commit

--- scripts/changed_services.sh
+++ scripts/changed_services.sh
@@ -2,6 +2,9 @@
 set -e
 
 triggered_commit="$1"
+
+printf "GitHub Action triggered by commit: %s" "${triggered_commit}"
+
 previous_commit=$(git rev-parse --verify --short "$triggered_commit"^)
 changed_services="$(lerna ls --since "$previous_commit" -all --json | jq '[.[] | .name]')"
 
