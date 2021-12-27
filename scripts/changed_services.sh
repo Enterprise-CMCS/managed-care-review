fe5106edf5fe04c588a339dd1c1b043f78ce74a7
Mojo Talantikite <mojo.talantikite@gmail.com>
2021-12-27 17:06:00 -0500

    test some changes

--- scripts/changed_services.sh
+++ scripts/changed_services.sh
@@ -6,6 +6,6 @@ triggered_commit="$1"
 printf "GitHub Action triggered by commit: %s\n" "${triggered_commit}"
 
 previous_commit=$(git rev-parse --verify --short "$triggered_commit"^)
-changed_services="$(lerna ls --since "$previous_commit" -all --json | jq '[.[] | .name]')"
+changed_services="$(lerna ls --since "$previous_commit" -all --json | jq '[.[] | {name: .name}]')"
 
 echo "$changed_services"
