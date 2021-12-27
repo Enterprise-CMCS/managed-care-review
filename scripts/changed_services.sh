6773f71c52e7a63730324ff4bebbd648c1ee57b4
Mojo Talantikite <mojo.talantikite@gmail.com>
2021-12-27 17:09:44 -0500

    single line output

--- scripts/changed_services.sh
+++ scripts/changed_services.sh
@@ -4,6 +4,6 @@ set -e
 triggered_commit="$1"
 
 previous_commit=$(git rev-parse --verify --short "$triggered_commit"^)
-changed_services="$(lerna ls --since "$previous_commit" -all --json | jq '[.[] | {name: .name}]')"
+changed_services="$(lerna ls --since "$previous_commit" -all --json | jq -c '[.[] | {name: .name}]')"
 
-echo "$changed_services"
+printf "%s" "$changed_services"
