2a6d06321ff06e6e4ec35f1cd8f109b0aedd272f
Mojo Talantikite <mojo.talantikite@gmail.com>
2021-12-28 16:06:00 -0500

    Add an initial ts file for workflow

---
+++ scripts/get_changed_services.ts
@@ -0,0 +1,15 @@
+import { Octokit } from '@octokit/action'
+import * as core from '@actions/core'
+
+// we pass in branchName and stageName as inputs from the action
+
+const octokit = new Octokit()
+const workflowrun = octokit.actions.listWorkflowRuns({
+    owner: 'CMSgov',
+    repo: 'managed-care-review',
+    workflow_id: 'deploy.yml',
+    status: 'success',
+    branch: core.getInput('branchName', { required: true }),
+})
+
+console.log(workflowrun)
