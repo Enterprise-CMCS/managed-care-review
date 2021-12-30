6294f4837fb608b4ac659ebc0900f7821ac0f25f
MacRae Linton <macrael@truss.works>
2021-12-30 00:29:32 -0800

    clean up the script for general use

--- scripts/collect_ci_runtime_stats.ts
+++ scripts/collect_ci_runtime_stats.ts
@@ -1,21 +1,193 @@
 import { Octokit } from 'octokit'
 import fs from 'fs'
+import path from 'path'
+import os from 'os'
 
 // This script collects stats on the 100 most recent successful github
 // action CI runs and prints out how long individual jobs and steps are
 // taking.
 
 // For development, I ran it with entr in this directory
-//        `find . | entr -cs "yarn tsc && node ./collect_ci_runtime_stats.js"`
+//        `find . | entr -cs "yarn tsc && node ./collect_ci_runtime_stats.js --use-cache"`
+
+// USAGE: node ./collect_ci_runtime_stats.js [--use-cache]
+// the --use-cache flag will write the results of the github queries to the CACHE_FILE
+// and if CACHE_FILE exists will read from that instead of talking to github.
+
+const CACHE_FILE = './recent_actions_cache.json'
+const GH_TOKEN_PATH = path.join(os.homedir(), '.github_actions_token.txt')
 
 type StepTimes = { [names: string]: number }
 type TimedRuns = { [id: string]: StepTimes }
 type StepStats = { [names: string]: { mean: number; stdev: number } }
 
-function readToken(path = './action_token.txt') {
+function readToken(path = GH_TOKEN_PATH) {
     return fs.readFileSync(path).toString().trim()
 }
 
+// this is everything we reference out of the github workflow type
+// with everything we reference from the github jobs type added to it.
+interface WorkflowRun {
+    id: number
+    head_branch: string | null
+    jobs: {
+        completed_at: string | undefined | null
+        started_at: string
+        name: string
+        steps?:
+            | {
+                  completed_at?: string | undefined | null
+                  started_at?: string | undefined | null
+                  name: string
+              }[]
+            | undefined
+        id: number
+    }[]
+}
+
+// grab workflow runs we're interested in, then grab their jobs
+// and return them with their jobs included.
+async function fetchDeployRuns(): Promise<WorkflowRun[]> {
+    const token = readToken()
+    const octokit = new Octokit({ auth: token })
+
+    const {
+        data: { workflows },
+    } = await octokit.rest.actions.listRepoWorkflows({
+        owner: 'CMSgov',
+        repo: 'managed-care-review',
+    })
+
+    console.log('Workflows fetched')
+
+    const deploy = workflows.find((w) => {
+        return w.name === 'Deploy'
+    })
+
+    if (deploy === undefined) throw new Error('better be one')
+
+    const {
+        data: { workflow_runs },
+    } = await octokit.rest.actions.listWorkflowRuns({
+        owner: 'CMSgov',
+        repo: 'managed-care-review',
+        workflow_id: deploy.id,
+        status: 'success',
+        per_page: 100,
+    })
+
+    console.log('Runs fetched')
+
+    const runsWithJobs: WorkflowRun[] = []
+
+    let count = workflow_runs.length
+    for (const run of workflow_runs) {
+        const {
+            data: { jobs },
+        } = await octokit.rest.actions.listJobsForWorkflowRun({
+            owner: 'CMSgov',
+            repo: 'managed-care-review',
+            run_id: run.id,
+        })
+
+        console.log('Jobs Fetched', count)
+        count--
+
+        const runWithJobs = { ...run, jobs }
+        runsWithJobs.push(runWithJobs)
+    }
+
+    return runsWithJobs
+}
+
+function processDeployRuns(runs: WorkflowRun[]): [TimedRuns, TimedRuns] {
+    // separate out initial runs from subsequent runs.
+    const firstRunSet: { [names: string]: WorkflowRun } = {}
+    const laterRuns: WorkflowRun[] = []
+
+    // runs come to us in reverse chronological order
+    // reversing them lets us scan through them pulling out
+    // the first time we see any branch
+    runs.reverse()
+    for (const run of runs) {
+        if (run.head_branch === undefined || run.head_branch === null)
+            throw new Error('we shouldnt have a branchless run')
+
+        if (!(run.head_branch in firstRunSet)) {
+            firstRunSet[run.head_branch] = run
+        } else {
+            laterRuns.push(run)
+        }
+    }
+
+    const timedFirstRuns: TimedRuns = {}
+    for (const run of Object.values(firstRunSet)) {
+        const stepTimes = computeStepTimes(run)
+
+        timedFirstRuns[run.id] = stepTimes
+    }
+
+    const timedLaterRuns: TimedRuns = {}
+    for (const run of laterRuns) {
+        const stepTimes = computeStepTimes(run)
+
+        timedLaterRuns[run.id] = stepTimes
+    }
+
+    return [timedFirstRuns, timedLaterRuns]
+}
+
+// compute all the times we care about for a given run.
+function computeStepTimes(run: WorkflowRun): StepTimes {
+    const times: StepTimes = {}
+
+    for (const job of run.jobs) {
+        if (
+            job.completed_at === undefined ||
+            job.completed_at === null ||
+            job.steps === undefined
+        )
+            throw new Error('all have succeeded')
+
+        const jtime =
+            (Date.parse(job.completed_at) - Date.parse(job.started_at)) / 1000
+
+        // console.log(`job: ${job.name} took: ${jtime}`)
+
+        times[job.name] = jtime
+
+        for (const step of job.steps) {
+            if (
+                step.completed_at === undefined ||
+                step.completed_at === null ||
+                step.started_at === undefined ||
+                step.started_at === null
+            )
+                throw new Error('all have succeeded')
+
+            const stime =
+                (Date.parse(step.completed_at) - Date.parse(step.started_at)) /
+                1000
+            // console.log(`    step: ${step.name} took: ${stime}`)
+
+            const stepName = `${job.name} >> ${step.name}`
+            times[stepName] = stime
+        }
+    }
+
+    return times
+}
+
+// this maps a step's names into a normalized form, allowing us to
+// collapse several tasks together.
+function massageNames(names: string): string {
+    // here we collapse cypres-run (1) for instance
+    const massaged = names.replace(/cypress-run\s\(\d\)/, 'cypress-run')
+
+    return massaged
+}
+
+// calculates statistics for all of the runs we have timed.
 function calculateStats(timedRuns: TimedRuns): StepStats {
     const stepTimes = countSteps(timedRuns)
 
@@ -37,19 +209,11 @@ function calculateStats(timedRuns: TimedRuns): StepStats {
         }
     }
 
-    // console.log('mean', stepAverages)
-
     return stepAverages
 }
 
-function massageNames(names: string): string {
-    // here we collapse cypres-run (1) for instance
-
-    const massaged = names.replace(/cypress-run\s\(\d\)/, 'cypress-run')
-
-    return massaged
-}
-
+// this takes our list of all the runs and turns it into a single dictionary
+// with an array of times for each task.
 function countSteps(timedRuns: TimedRuns): { [names: string]: number[] } {
     const stepTimes: { [names: string]: number[] } = {}
 
@@ -120,164 +284,6 @@ function massageRuns(timedRuns: TimedRuns): TimedRuns {
     return massagedRuns
 }
 
-// fetch the run and compute all the times we care about
-function computeStepTimes(run: WorkflowRun): StepTimes {
-    const times: StepTimes = {}
-
-    //job
-    for (const job of run.jobs) {
-        if (
-            job.completed_at === undefined ||
-            job.completed_at === null ||
-            job.steps === undefined
-        )
-            throw new Error('all have succeeded')
-
-        const jtime =
-            (Date.parse(job.completed_at) - Date.parse(job.started_at)) / 1000
-
-        // console.log(`job: ${job.name} took: ${jtime}`)
-
-        times[job.name] = jtime
-
-        for (const step of job.steps) {
-            if (
-                step.completed_at === undefined ||
-                step.completed_at === null ||
-                step.started_at === undefined ||
-                step.started_at === null
-            )
-                throw new Error('all have succeeded')
-
-            const stime =
-                (Date.parse(step.completed_at) - Date.parse(step.started_at)) /
-                1000
-            // console.log(`    step: ${step.name} took: ${stime}`)
-
-            const stepName = `${job.name} >> ${step.name}`
-            times[stepName] = stime
-        }
-    }
-
-    return times
-}
-
-// this is everything we reference out of the github workflow type
-// with everything we reference from the github jobs type added to it.
-interface WorkflowRun {
-    id: number
-    head_branch: string | null
-    jobs: {
-        completed_at: string | undefined | null
-        started_at: string
-        name: string
-        steps?:
-            | {
-                  completed_at?: string | undefined | null
-                  started_at?: string | undefined | null
-                  name: string
-              }[]
-            | undefined
-        id: number
-    }[]
-}
-
-// grab workflow runs we're interested in, then grab their jobs
-// and return them with their jobs included.
-async function fetchDeployRuns(): Promise<WorkflowRun[]> {
-    const token = readToken()
-    const octokit = new Octokit({ auth: token })
-
-    const {
-        data: { workflows },
-    } = await octokit.rest.actions.listRepoWorkflows({
-        owner: 'CMSgov',
-        repo: 'managed-care-review',
-    })
-
-    console.log('Workflows fetched')
-
-    const deploy = workflows.find((w) => {
-        return w.name === 'Deploy'
-    })
-
-    if (deploy === undefined) throw new Error('better be one')
-
-    const {
-        data: { workflow_runs },
-    } = await octokit.rest.actions.listWorkflowRuns({
-        owner: 'CMSgov',
-        repo: 'managed-care-review',
-        workflow_id: deploy.id,
-        status: 'success',
-        per_page: 100,
-    })
-
-    console.log('Runs fetched')
-
-    // const readers = workflow_runs.filter(
-    //     (r) => r.head_branch === 'wml-screen-reader-dates'
-    // )
-
-    const runsWithJobs: WorkflowRun[] = []
-
-    let count = workflow_runs.length
-    for (const run of workflow_runs) {
-        const {
-            data: { jobs },
-        } = await octokit.rest.actions.listJobsForWorkflowRun({
-            owner: 'CMSgov',
-            repo: 'managed-care-review',
-            run_id: run.id,
-        })
-
-        console.log('Jobs Fetched', count)
-        count--
-
-        const runWithJobs = { ...run, jobs }
-        runsWithJobs.push(runWithJobs)
-    }
-
-    return runsWithJobs
-}
-
-function processDeployRuns(runs: WorkflowRun[]): [TimedRuns, TimedRuns] {
-    // separate out first runs from last runs.
-    const firstRunSet: { [names: string]: WorkflowRun } = {}
-    const laterRuns: WorkflowRun[] = []
-
-    // runs come to us in reverse chronological order
-    // reversing them lets us scan through them pulling out
-    // the first time we see any branch
-    runs.reverse()
-    for (const run of runs) {
-        if (run.head_branch === undefined || run.head_branch === null)
-            throw new Error('we shouldnt have a branchless run')
-
-        if (!(run.head_branch in firstRunSet)) {
-            firstRunSet[run.head_branch] = run
-        } else {
-            laterRuns.push(run)
-        }
-    }
-
-    const timedFirstRuns: TimedRuns = {}
-    for (const run of Object.values(firstRunSet)) {
-        const stepTimes = computeStepTimes(run)
-
-        timedFirstRuns[run.id] = stepTimes
-    }
-
-    const timedLaterRuns: TimedRuns = {}
-    for (const run of laterRuns) {
-        const stepTimes = computeStepTimes(run)
-
-        timedLaterRuns[run.id] = stepTimes
-    }
-
-    return [timedFirstRuns, timedLaterRuns]
-}
-
 function printStats(stats: StepStats) {
     // print them out in order of weight
     const outputs: [string, number, number][] = Object.keys(stats).map((k) => {
@@ -303,22 +309,22 @@ async function main() {
     console.log('starting')
     testStandardDev()
 
-    // // write to the cache
-    // const runs = await fetchDeployRuns()
-    // console.log('runs', runs)
-    // fs.writeFileSync('./all_gh_runs.json', JSON.stringify(runs))
-    // console.log('written')
-
-    // read from the cache.
-    const cachedRuns = fs.readFileSync('./all_gh_runs.json').toString()
-    const runs: WorkflowRun[] = JSON.parse(cachedRuns)
+    let runs: WorkflowRun[] = []
+    if (process.argv.includes('--use-cache') && fs.existsSync(CACHE_FILE)) {
+        // read from the cache
+        const cachedRuns = fs.readFileSync(CACHE_FILE).toString()
+        runs = JSON.parse(cachedRuns)
+    } else {
+        // talk to github
+        runs = await fetchDeployRuns()
+        console.log('runs', runs)
+        if (process.argv.includes('--use-cache')) {
+            fs.writeFileSync(CACHE_FILE, JSON.stringify(runs))
+            console.log('cache written')
+        }
+    }
 
     const [timedFirstRuns, timedLaterRuns] = processDeployRuns(runs)
-
-    // const cache = fs.readFileSync('./all_step_times.json').toString()
-    // const timedRuns: TimedRuns = JSON.parse(cache)
-
-    // console.log('GOT', timedRuns)
 
     const firstStats = calculateStats(massageRuns(timedFirstRuns))
     const laterStats = calculateStats(massageRuns(timedLaterRuns))
