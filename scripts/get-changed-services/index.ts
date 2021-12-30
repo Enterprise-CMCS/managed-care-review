d05602ebf432f1093705c80948c32ba4adbfdd26
Mojo Talantikite <mojo.talantikite@gmail.com>
2021-12-30 16:56:06 -0500

    Run in CI

--- scripts/get-changed-services/index.ts
+++ scripts/get-changed-services/index.ts
@@ -15,7 +15,87 @@ process.env.GITHUB_ACTION = 'true'
 process.env.GITHUB_TOKEN = readToken()
 */
 
-// Use lerna to get a list of all of the packages that are in the current repo
+// for now keep this list hardcoded
+const listOfServiceJobs = [
+    'app-api',
+    'app-web',
+    'postgres',
+    'storybook',
+    'ui-auth',
+    'ui',
+    'uploads',
+    'run-migrations',
+    'build-prisma',
+]
+
+const octokit = new Octokit()
+
+async function main() {
+    // get the workflow runs for this branch
+    // we pass in branchName as input from the action
+    const allWorkflowRuns = await octokit.actions.listWorkflowRuns({
+        owner: 'CMSgov',
+        repo: 'managed-care-review',
+        workflow_id: 'deploy.yml',
+        //status: 'success',
+        //branch: 'mt-skip-sls-deploy',
+        branch: core.getInput('branchName', { required: true }),
+    })
+
+    const deployAllServices = listOfServiceJobs
+    // if we haven't had a run on this branch, we need to deploy everything
+    if (allWorkflowRuns.data.total_count === 0) {
+        core.setOutput('changed-services', deployAllServices)
+    }
+
+    // if a run was cancelled by a user then we need to go back further and
+    // find an attempt that actually ran and had an actionable outcome for us
+    // this is either a run that succeeded or one that failed at some point.
+    // failures can still mean that some services have deployed ok, so
+    // we can consider skipping those.
+    const lastCompletedRun = allWorkflowRuns.data.workflow_runs.find(
+        (run) => run.conclusion === 'success' || run.conclusion === 'failure'
+    )
+
+    // if we don't even have a run that hasn't been user cancelled, run everything
+    if (lastCompletedRun === undefined) {
+        core.setOutput('changed-services', deployAllServices)
+        return
+    }
+
+    // has anything changed since that run? were there services that completed
+    // in that last run that we don't need to redeploy?
+    const jobsFromLastSuccess =
+        await octokit.actions.listJobsForWorkflowRunAttempt({
+            owner: 'CMSgov',
+            repo: 'managed-care-review',
+            run_id: lastCompletedRun.id,
+            attempt_number: lastCompletedRun.run_attempt ?? 1,
+        })
+
+    const successfulJobs = jobsFromLastSuccess.data.jobs
+        .map((job) => {
+            if (job.conclusion === 'success') {
+                return job.name.split(' / ')[1] // spaces are significant here
+            }
+        })
+        .filter((name) => {
+            if (typeof name === 'undefined') {
+                return false
+            }
+            return true
+        })
+
+    const jobsToRun = listOfServiceJobs.filter(
+        (x) => !successfulJobs.includes(x)
+    )
+    console.log('All services: ' + listOfServiceJobs)
+    console.log('Successful jobs: ' + successfulJobs)
+    console.log('Jobs to rerun: ' + jobsToRun)
+
+    core.setOutput('changed-services', jobsToRun)
+}
+
 interface LernaListItem {
     name: string
     version: string
@@ -24,7 +104,7 @@ interface LernaListItem {
 }
 
 // a list of all of our deployable service names from lerna
-async function getAllServices(): Promise<string[]> {
+async function getAllServicesFromLerna(): Promise<string[]> {
     const execPromise = util.promisify(exec)
     const { stdout, stderr } = await execPromise('lerna ls -a --json')
     const lernaList: LernaListItem[] = JSON.parse(stdout)
@@ -35,51 +115,4 @@ async function getAllServices(): Promise<string[]> {
     return lernaList.map((i) => i.name)
 }
 
-// get the workflow runs for this branch
-// we pass in branchName as input from the action
-const octokit = new Octokit()
-const allWorkflowRuns = await octokit.actions.listWorkflowRuns({
-    owner: 'CMSgov',
-    repo: 'managed-care-review',
-    workflow_id: 'deploy.yml',
-    //status: 'success',
-    branch: 'mt-skip-sls-deploy',
-    //branch: core.getInput('branchName', { required: true }),
-})
-
-const services = await getAllServices()
-// if we haven't had a run on this branch, we need to deploy everything
-if (allWorkflowRuns.data.total_count === 0) {
-    core.setOutput('changed-services', services)
-}
-
-const workflow_runs = allWorkflowRuns.data.workflow_runs
-
-const single_run = await octokit.actions.listJobsForWorkflowRunAttempt({
-    owner: 'CMSgov',
-    repo: 'managed-care-review',
-    run_id: workflow_runs[0].id,
-    attempt_number: workflow_runs[0].run_attempt ?? 1,
-})
-
-//console.log(single_run.data.jobs)
-
-const successfulJobs = single_run.data.jobs
-    .map((job) => {
-        if (job.conclusion === 'success') {
-            return job.name.split(' / ')[1] // spaces are significant here
-        }
-    })
-    .filter((name) => {
-        if (typeof name === 'undefined') {
-            return false
-        }
-        return true
-    })
-
-const jobsToRun = services.filter((x) => !successfulJobs.includes(x))
-console.log('All services: ' + services)
-console.log('Successful jobs: ' + successfulJobs)
-console.log('Jobs to rerun: ' + jobsToRun)
-
-core.setOutput('changed-services', jobsToRun)
+main()
