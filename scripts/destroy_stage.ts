ede8cfcdd69a093c0f18479f9d06c107b4aacd56
Mojo Talantikite <mojo.talantikite@gmail.com>
2022-01-14 16:16:25 -0500

    refactor func out

--- scripts/destroy_stage.ts
+++ scripts/destroy_stage.ts
@@ -22,26 +22,35 @@ let cf = new AWS.CloudFormation()
 await Promise.all(
     stackPrefixes.map(async (prefix) => {
         try {
-            const stackParams = {
-                StackName: `${prefix}-${stage}`,
-            }
-            // find the stack and make sure we get one
-            const stack = await cf.describeStacks(stackParams).promise()
-            if (stack.Stacks === undefined) {
-                console.log('could not find stack')
-                return
-            }
-
-            // get the stack ID so we can check it's status
-            let stackId = stack.Stacks[0].StackId
-            await cf.deleteStack(stackParams).promise()
-            await cf
-                .waitFor('stackDeleteComplete', { StackName: stackId })
-                .promise()
-            console.log(stack)
+            const sn = `${prefix}-${stage}`
+            deleteStack(sn)
             return
         } catch (err) {
             return Promise.reject(err)
         }
     })
 )
+
+async function deleteStack(stackName: string) {
+    const stackParams = {
+        StackName: stackName,
+    }
+
+    try {
+        // find the stack and make sure we get one
+        const stack = await cf.describeStacks(stackParams).promise()
+        if (stack.Stacks === undefined) {
+            console.log('could not find stack')
+            return
+        }
+
+        // get the stack ID so we can check it's status
+        let stackId = stack.Stacks[0].StackId
+        await cf.deleteStack(stackParams).promise()
+        await cf
+            .waitFor('stackDeleteComplete', { StackName: stackId })
+            .promise()
+    } catch (err) {
+        console.log(err)
+    }
+}
