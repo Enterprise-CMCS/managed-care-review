74c685149bc505a7adf07c0bb6a4bc76c469d60f
Hana Worku <hana@truss.works>
2022-01-20 09:31:33 -0600

    Display file types list visually in the file upload hint

--- services/app-web/src/pages/StateSubmission/Documents/Documents.tsx
+++ services/app-web/src/pages/StateSubmission/Documents/Documents.tsx
@@ -255,7 +255,7 @@ export const Documents = ({
                                 >
                                     Document definitions and requirements
                                 </Link>
-                                <span className="srOnly">
+                                <span>
                                     This input only accepts PDF, CSV, DOC, DOCX,
                                     XLS, XLSX files.
                                 </span>
