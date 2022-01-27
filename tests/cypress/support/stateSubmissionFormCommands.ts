bcf6de14f2a8bf6a7ec31dcfd375ec0ed9798377
Mazdak Atighi <maz@truss.works>
2022-01-26 18:17:34 -0600

    should exist

--- tests/cypress/support/stateSubmissionFormCommands.ts
+++ tests/cypress/support/stateSubmissionFormCommands.ts
@@ -155,7 +155,7 @@ Cypress.Commands.add('fillOutDocuments', () => {
     )
 
     cy.verifyDocumentsHaveNoErrors()
-    cy.findByTestId('upload-finished-indicator', {timeout: 120000})
+    cy.findByTestId('upload-finished-indicator', {timeout: 120000}).should("exist")
     cy.findAllByTestId('errorMessage').should('have.length', 0)
 })
 
