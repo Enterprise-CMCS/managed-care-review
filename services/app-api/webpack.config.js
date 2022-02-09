508a05c263d1ea66d0bbd6e15d9c5e7003ba41b0
Mojo Talantikite <mojo.talantikite@gmail.com>
2022-02-09 18:28:35 -0500

    Try to use copy webpack directly

--- services/app-api/webpack.config.js
+++ services/app-api/webpack.config.js
@@ -18,92 +18,86 @@ const extensions = [
 ];
 const servicePath = '';
 
-module.exports = (async () => {
-    const nr_license_key = await slsw.lib.serverless.configurationInput.custom
-        .nrLicenseKey;
-    return {
-        entry: slsw.lib.entries,
-        target: 'node',
-        context: __dirname,
-        mode: isLocal ? 'development' : 'production',
-        performance: {
-            hints: false,
-        },
-        externals: [
-            nodeExternals(),
-            nodeExternals({
-                modulesDir: path.resolve(__dirname, '../../node_modules'),
-            }),
-            'aws-sdk',
-        ],
-        devtool: 'source-map',
-        resolve: {
-            symlinks: false,
-            extensions: extensions,
-            modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
-            plugins: [
-                new TsconfigPathsPlugin({
-                    configFile: tsConfigPath,
-                    extensions: extensions,
-                }),
-            ],
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.mjs$/,
-                    include: /node_modules/,
-                    type: 'javascript/auto',
-                },
-                {
-                    test: /\.(ts|tsx)$/,
-                    use: [
-                        {
-                            loader: 'ts-loader',
-                            options: {
-                                projectReferences: true,
-                                configFile: tsConfigPath,
-                                experimentalWatchApi: true,
-                            },
-                        },
-                    ],
-                    exclude: [
-                        path.resolve(servicePath, 'node_modules'),
-                        path.resolve(servicePath, '.serverless'),
-                        path.resolve(servicePath, '.webpack'),
-                    ],
-                },
-                {
-                    test: /\.(graphql|gql)$/,
-                    exclude: /node_modules/,
-                    loader: 'graphql-tag/loader',
-                },
-                {
-                    test: /\.yml$/,
-                    loader: 'string-replace-loader',
-                    options: {
-                        search: 'NRLICENSEKEY',
-                        replace: `${nr_license_key}`,
-                        flags: 'g',
-                        strict: true,
-                    },
-                },
-            ],
-        },
+module.exports = {
+    entry: slsw.lib.entries,
+    target: 'node',
+    context: __dirname,
+    mode: isLocal ? 'development' : 'production',
+    performance: {
+        hints: false,
+    },
+    externals: [
+        nodeExternals(),
+        nodeExternals({
+            modulesDir: path.resolve(__dirname, '../../node_modules'),
+        }),
+        'aws-sdk',
+    ],
+    devtool: 'source-map',
+    resolve: {
+        symlinks: false,
+        extensions: extensions,
+        modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
         plugins: [
-            new CopyWebpackPlugin({
-                patterns: [
+            new TsconfigPathsPlugin({
+                configFile: tsConfigPath,
+                extensions: extensions,
+            }),
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.mjs$/,
+                include: /node_modules/,
+                type: 'javascript/auto',
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                use: [
                     {
-                        from: path.resolve(
-                            __dirname,
-                            '../../node_modules/.prisma/client/schema.prisma'
-                        ),
-                    },
-                    {
-                        from: path.resolve(__dirname, 'collector.yml'),
+                        loader: 'ts-loader',
+                        options: {
+                            projectReferences: true,
+                            configFile: tsConfigPath,
+                            experimentalWatchApi: true,
+                        },
                     },
                 ],
-            }),
+                exclude: [
+                    path.resolve(servicePath, 'node_modules'),
+                    path.resolve(servicePath, '.serverless'),
+                    path.resolve(servicePath, '.webpack'),
+                ],
+            },
+            {
+                test: /\.(graphql|gql)$/,
+                exclude: /node_modules/,
+                loader: 'graphql-tag/loader',
+            },
         ],
-    };
-})();
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(
+                        __dirname,
+                        '../../node_modules/.prisma/client/schema.prisma'
+                    ),
+                },
+                {
+                    from: path.resolve(__dirname, 'collector.yml'),
+                    transform(content) {
+                        return content
+                            .toString()
+                            .replace(
+                                '$NR_LICENSE_KEY',
+                                process.env.NR_LICENSE_KEY
+                            );
+                    },
+                },
+            ],
+        }),
+    ],
+};
