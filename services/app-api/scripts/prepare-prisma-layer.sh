a122ceb39b945f878709b326293ef8f5c25f4647
Mojo Talantikite <mojo.talantikite@gmail.com>
2022-03-01 14:21:40 -0500

    some clean up

--- services/app-api/scripts/prepare-prisma-layer.sh
+++ services/app-api/scripts/prepare-prisma-layer.sh
@@ -24,6 +24,7 @@ function preparePrismaLayer() {
     rsync -av ../../node_modules/@prisma/client/ lambda-layers-prisma-client-query/nodejs/node_modules/@prisma/client
     rsync -av ../../node_modules/prisma/ lambda-layers-prisma-client-query/nodejs/node_modules/prisma
     rsync -av ../../node_modules/.prisma/ lambda-layers-prisma-client-query/nodejs/node_modules/.prisma
+    cp ../../node_modules/@prisma/engines/migration-engine-rhel-openssl-1.0.x lambda-layers-prisma-client-query/nodejs/node_modules/@prisma/engines/
     cp ../../node_modules/@prisma/engines/libquery_engine-rhel-openssl-1.0.x.so.node lambda-layers-prisma-client-query/nodejs/node_modules/@prisma/engines/
     cp ../../node_modules/@prisma/engines/package.json lambda-layers-prisma-client-query/nodejs/node_modules/@prisma/engines/package.json
     rsync -av ../../node_modules/@prisma/engines/dist/ lambda-layers-prisma-client-query/nodejs/node_modules/@prisma/engines/dist
@@ -32,20 +33,22 @@ function preparePrismaLayer() {
     rsync -av ../../node_modules/@prisma/client/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/client
     rsync -av ../../node_modules/prisma/ lambda-layers-prisma-client-migration/nodejs/node_modules/prisma
     rsync -av ../../node_modules/.prisma/ lambda-layers-prisma-client-migration/nodejs/node_modules/.prisma
-    cp ../../node_modules/@prisma/engines/libquery_engine-rhel-openssl-1.0.x.so.node lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/
     cp ../../node_modules/@prisma/engines/migration-engine-rhel-openssl-1.0.x lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/
+    cp ../../node_modules/@prisma/engines/libquery_engine-rhel-openssl-1.0.x.so.node lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/
     cp ../../node_modules/@prisma/engines/package.json lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/package.json
     rsync -av ../../node_modules/@prisma/engines/dist/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/dist
 
     echo "Copy migration files to layer..."
     rsync -av prisma/ lambda-layers-prisma-client-migration/nodejs/prisma
+    rsync -av prisma/ lambda-layers-prisma-client-query/nodejs/prisma
 
     echo "Remove Prisma CLI ..."
-    rm -rf lambda-layers-prisma-client-query/nodejs/node_modules/@prisma/cli
     rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/cli
+    rm -rf lambda-layers-prisma-client-query/nodejs/node_modules/@prisma/cli
 
     echo "Remove non-RHEL bins to save space ..."
     rm -rf lambda-layers-prisma-client-query/nodejs/node_modules/.prisma/client/libquery_engine-debian-openssl-1.1.x.so.node
+    rm -rf lambda-layers-prisma-client-query/nodejs/node_modules/.prisma/client/libquery_engine-rhel-openssl-1.0.x.so.node 
     rm -rf lambda-layers-prisma-client-query/nodejs/node_modules/prisma/engines
     rm -rf lambda-layers-prisma-client-query/nodejs/node_modules/prisma/libquery_engine-debian-openssl-1.1.x.so.node
     rm -rf lambda-layers-prisma-client-query/nodejs/node_modules/@prisma/introspection-engine-debian-openssl-1.1.x 
@@ -53,13 +56,14 @@ function preparePrismaLayer() {
     rm -rf lambda-layers-prisma-client-query/nodejs/node_modules/@prisma/migration-engine-debian-openssl-1.1.x
     rm -rf lambda-layers-prisma-client-query/nodejs/node_modules/@prisma/prisma-fmt-debian-openssl-1.1.x
 
-    rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/.prisma/client/libquery_engine-debian-openssl-1.1.x.so.node
-    rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/prisma/engines
-    rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/prisma/libquery_engine-debian-openssl-1.1.x.so.node
-    rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/introspection-engine-debian-openssl-1.1.x 
-    rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/libquery_engine-debian-openssl-1.1.x.so.node 
-    rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/migration-engine-debian-openssl-1.1.x
-    rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/prisma-fmt-debian-openssl-1.1.x
+    rm -rf lambda-layers-prisma-migration/nodejs/node_modules/.prisma/client/libquery_engine-debian-openssl-1.1.x.so.node
+    rm -rf lambda-layers-prisma-migration/nodejs/node_modules/.prisma/client/libquery_engine-rhel-openssl-1.0.x.so.node 
+    rm -rf lambda-layers-prisma-migration/nodejs/node_modules/prisma/engines
+    rm -rf lambda-layers-prisma-migration/nodejs/node_modules/prisma/libquery_engine-debian-openssl-1.1.x.so.node
+    rm -rf lambda-layers-prisma-migration/nodejs/node_modules/@prisma/introspection-engine-debian-openssl-1.1.x 
+    rm -rf lambda-layers-prisma-migration/nodejs/node_modules/@prisma/libquery_engine-debian-openssl-1.1.x.so.node 
+    rm -rf lambda-layers-prisma-migration/nodejs/node_modules/@prisma/migration-engine-debian-openssl-1.1.x
+    rm -rf lambda-layers-prisma-migration/nodejs/node_modules/@prisma/prisma-fmt-debian-openssl-1.1.x
 
     echo "Compressing and cleaning query engine..."
     pushd lambda-layers-prisma-client-query && tar -zcf /tmp/nodejs.tar.gz . && mv /tmp/nodejs.tar.gz ./nodejs.tar.gz
