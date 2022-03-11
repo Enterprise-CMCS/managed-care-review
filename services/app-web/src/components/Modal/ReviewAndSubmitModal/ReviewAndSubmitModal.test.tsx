7a1fd40cda83e00764d128c84d9dc77da7b6d6bb
Jason Lin <maolin@truss.works>
2022-03-10 23:55:10 -0500

    Add test for ReviewAndSubmitModal component.

---
+++ services/app-web/src/components/Modal/ReviewAndSubmitModal/ReviewAndSubmitModal.test.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { screen, render } from '@testing-library/react'
+import { ReviewAndSubmitModal, ModalProps } from './ReviewAndSubmitModal';
+
+describe('DoubleColumnGrid', () => {
+    it('Renders element with modal hidden', () => {
+        render(
+            <div>
+                <ReviewAndSubmitModal
+                    id="hiddenModal"
+                    modalTitle="Test Modal Title"
+                    showModal={false}
+                >
+                </ReviewAndSubmitModal>
+
+            </div>
+        )
+
+        expect(screen.getByRole('dialog')).toHaveClass('is-hidden')
+    })
+
+    it('Renders element with modal visible with modal title', () => {
+        render(
+            <div>
+                <ReviewAndSubmitModal
+                    id="hiddenModal"
+                    modalTitle="Test Modal Title"
+                    showModal={true}
+                >
+                    <textarea
+                        id="textarea"
+                        data-testid="textarea"
+                    />
+                </ReviewAndSubmitModal>
+            </div>
+        )
+
+        expect(screen.getByRole('dialog')).toHaveClass('is-visible')
+        expect(screen.getByText('Test Modal Title')).toBeInTheDocument()
+        expect(screen.getByTestId('textarea')).toBeInTheDocument()
+    })
+
+    it('Renders element with modal visible without title', async() => {
+        render(
+            <div>
+                <ReviewAndSubmitModal
+                    id="hiddenModal"
+                    showModal={true}
+                >
+                    <textarea
+                        id="textarea"
+                        data-testid="textarea"
+                    />
+                </ReviewAndSubmitModal>
+            </div>
+        )
+
+        expect(screen.getByRole('dialog')).toHaveClass('is-visible')
+        expect(await screen.queryByText('Test Modal Title')).toBeNull()
+        expect(screen.getByTestId('textarea')).toBeInTheDocument()
+    })
+})
