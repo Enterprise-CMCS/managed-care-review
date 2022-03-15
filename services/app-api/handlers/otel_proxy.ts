e4fd8760e39b7ccd6b6134b2edd67dfb1060a454
Mojo Talantikite <mojo.talantikite@gmail.com>
2022-03-15 17:54:54 -0400

    test to see if axios will do it

--- services/app-api/handlers/otel_proxy.ts
+++ services/app-api/handlers/otel_proxy.ts
@@ -1,35 +1,12 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
-import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
-import { Resource, ResourceAttributes } from '@opentelemetry/resources'
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base'
-
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
-import {
-    SimpleSpanProcessor,
-    ConsoleSpanExporter,
-} from '@opentelemetry/sdk-trace-base'
+import axios from 'axios'
 
 export const main: APIGatewayProxyHandler = async (event) => {
-    if (event.body == null) {
-        return {
-            statusCode: 200,
-            body: '{"error": "no span received. continuing."}',
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': true,
-            },
-        }
+    const options = {
+        headers: { 'content-type': 'application/json' },
     }
-    const span: ResourceAttributes = JSON.parse(event.body)
-    const provider = new NodeTracerProvider({
-        resource: new Resource(span),
-    })
 
-    provider.addSpanProcessor(
-        new SimpleSpanProcessor(new ConsoleSpanExporter())
-    )
-    provider.addSpanProcessor(new BatchSpanProcessor(new OTLPTraceExporter()))
-    provider.register()
+    await axios.post('localhost:55681/api/v1/trace', event.body, options)
 
     return {
         statusCode: 200,
