aee4de20ef095506227fbf246ee4c1ffd40cd484
Josh Franklin <josh@truss.works>
2022-03-16 12:26:13 -0400

    Fixing spacing again

--- services/app-api/emailer/templates.ts
+++ services/app-api/emailer/templates.ts
@@ -170,14 +170,10 @@ const unlockPackageCMSEmail = (
     const bodyHTML = `
         ${testEmailAlert}
         <br /><br />
-        Submission ${unlockData.submissionName} was unlocked
-        <br /><br />
-        <b>Unlocked by:</b> ${unlockData.updatedBy}
-        <br /><br />
-        <b>Unlocked on:</b> ${formatCalendarDate(unlockData.updatedAt)}
-        <br /><br />
-        <b>Reason for unlock:</b> ${unlockData.updatedReason}
-        <br /><br />
+        Submission ${unlockData.submissionName} was unlocked<br /><br />
+        <b>Unlocked by:</b> ${unlockData.updatedBy}<br /><br />
+        <b>Unlocked on:</b> ${formatCalendarDate(unlockData.updatedAt)}<br /><br />
+        <b>Reason for unlock:</b> ${unlockData.updatedReason}<br /><br />
         You will receive another notification when the state resubmits.
     `
     return {
@@ -204,14 +200,10 @@ const unlockPackageStateEmail = (
     const bodyHTML = `
         ${testEmailAlert}
         <br /><br />
-        Submission ${unlockData.submissionName} was unlocked by CMS
-        <br /><br />
-        <b>Unlocked by:</b> ${unlockData.updatedBy}
-        <br /><br />
-        <b>Unlocked on:</b> ${formatCalendarDate(unlockData.updatedAt)}
-        <br /><br />
-        <b>Reason for unlock:</b> ${unlockData.updatedReason}
-        <br /><br />
+        Submission ${unlockData.submissionName} was unlocked by CMS<br /><br />
+        <b>Unlocked by:</b> ${unlockData.updatedBy}<br /><br />
+        <b>Unlocked on:</b> ${formatCalendarDate(unlockData.updatedAt)}<br /><br />
+        <b>Reason for unlock:</b> ${unlockData.updatedReason}<br /><br />
         <a href="${submissionURL}">Open the submission in MC-Review to make edits.</a>
     `
     return {
