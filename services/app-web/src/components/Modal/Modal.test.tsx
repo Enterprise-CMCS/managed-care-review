5c89cc621195bd1cd2fc94e15200e192d1577662
Hana Worku <hana@truss.works>
2022-03-17 10:26:11 -0500

    Use react-uswds modal ref functions instead of adding our own open close logic

--- services/app-web/src/components/Modal/Modal.test.tsx
+++ services/app-web/src/components/Modal/Modal.test.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { screen, render } from '@testing-library/react'
+import React , {createRef} from 'react'
+import {ModalRef} from '@trussworks/react-uswds'
+import { screen, render, waitFor } from '@testing-library/react'
 import { Modal } from './Modal';
 
 describe('Modal', () => {
     it('Renders element with modal hidden', () => {
+          const modalRef = createRef<ModalRef>()
         render(
             <div>
                 <Modal
                     id="hiddenModal"
                     modalHeading="Test Modal Title"
-                    showModal={false}
+                    modalRef={modalRef}
                 >
                     <textarea
                         id="textarea"
@@ -23,42 +25,44 @@ describe('Modal', () => {
         expect(screen.getByRole('dialog')).toHaveClass('is-hidden')
     })
 
-    it('Renders element with modal visible with modal title', () => {
+    it('Renders element with modal visible when open is true', async () => {
+        const modalRef = createRef<ModalRef>()
+          const handleOpen = () =>
+            modalRef.current?.toggleModal(undefined, true)
         render(
             <div>
                 <Modal
                     id="hiddenModal"
                     modalHeading="Test Modal Title"
-                    showModal={true}
+                    modalRef={modalRef}
                 >
-                    <textarea
-                        id="textarea"
-                        data-testid="textarea"
-                    />
+                    <textarea id="textarea" data-testid="textarea" />
                 </Modal>
             </div>
         )
+        await waitFor(() => handleOpen())
 
+        expect(modalRef.current?.modalIsOpen).toBe(true)
         expect(screen.getByRole('dialog')).toHaveClass('is-visible')
         expect(screen.getByText('Test Modal Title')).toBeInTheDocument()
         expect(screen.getByTestId('textarea')).toBeInTheDocument()
     })
 
     it('Renders element with modal visible without title', async() => {
+        const modalRef = createRef<ModalRef>()
+        const handleOpen = () =>
+            modalRef.current?.toggleModal(undefined, true)
         render(
             <div>
-                <Modal
-                    id="hiddenModal"
-                    showModal={true}
-                >
-                    <textarea
-                        id="textarea"
-                        data-testid="textarea"
-                    />
+                <Modal id="hiddenModal" modalRef={modalRef}>
+                    <textarea id="textarea" data-testid="textarea" />
                 </Modal>
             </div>
         )
+        
+          await waitFor(() => handleOpen())
 
+          expect(modalRef.current?.modalIsOpen).toBe(true)
         expect(screen.getByRole('dialog')).toHaveClass('is-visible')
         expect(screen.queryByText('Test Modal Title')).toBeNull()
         expect(screen.getByTestId('textarea')).toBeInTheDocument()
--- services/app-web/src/components/Modal/Modal.tsx
+++ services/app-web/src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React from 'react'
 import {
     Button,
     ButtonGroup,
@@ -6,66 +6,62 @@ import {
     ModalFooter,
     ModalHeading,
     ModalRef,
-    ModalProps as UswdsModalProps
+    ModalProps as UswdsModalProps,
+    ModalToggleButton
 } from '@trussworks/react-uswds';
 import styles from './Modal.module.scss'
+import { stringMap } from 'aws-sdk/clients/backup';
 
 interface ModalComponentProps {
+    id: string,
     modalHeading?: string,
-    modalHeadingId?: string,
     onSubmit?: () => void,
     onCancel?: () => void,
-    showModal: boolean,
     className?: string,
+    modalRef: React.RefObject<ModalRef>
 }
 
 export type ModalProps = ModalComponentProps & UswdsModalProps
 
 export const Modal = ({
+    id,
     children,
     modalHeading,
-    modalHeadingId,
     onSubmit,
     onCancel,
-    showModal,
     className,
+    modalRef,
     ...divProps
 }: ModalProps): React.ReactElement  => {
-    const modalRef = useRef<ModalRef>(null)
 
-    useEffect(() => {
-        modalRef.current?.toggleModal(undefined, showModal)
-    },[showModal])
 
     return (
         <UswdsModal
             {...divProps}
+            id={id}
             ref={modalRef}
             className={`${styles.modal} ${className}`}
         >
             {modalHeading && (
-                <ModalHeading id={modalHeadingId}>
-                    {modalHeading}
-                </ModalHeading>
+                <ModalHeading id={`${id}-heading`}>{modalHeading}</ModalHeading>
             )}
             {children}
             <ModalFooter>
                 <ButtonGroup className="float-right">
-                    <Button
-                        type="button"
-                        key="cancelButton"
-                        aria-label="Cancel"
+                    <ModalToggleButton
                         data-testid="modal-cancel"
-                        onClick={onCancel}
+                        modalRef={modalRef}
+                        id={`${id}=closer`}
+                        closer
                         outline
                     >
                         Cancel
-                    </Button>
+                    </ModalToggleButton>
                     <Button
                         type="button"
-                        key="submitButton"
                         aria-label="Submit"
                         data-testid="modal-submit"
+                        id={`${id}=submit`}
                         onClick={onSubmit}
                     >
                         Submit
