a82e001be34b17a2257d9ae37580cd35de05eeb2
Mojo Talantikite <mojo.talantikite@gmail.com>
2022-03-17 20:35:15 -0400

    Switch to basic provider for tracer

--- services/otel/otel_handler.ts
+++ services/otel/otel_handler.ts
@@ -1,24 +1,28 @@
 import opentelemetry from '@opentelemetry/api'
-import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
 import { Resource } from '@opentelemetry/resources'
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 import {
     SimpleSpanProcessor,
     ConsoleSpanExporter,
+    BasicTracerProvider,
 } from '@opentelemetry/sdk-trace-base'
 
-const provider = new NodeTracerProvider({
-    resource: Resource.default().merge(
-        new Resource({
-            'service.name': 'mc-review',
-        })
-    ),
+const serviceName = 'app-api-' + process.env.REACT_APP_STAGE_NAME
+const provider = new BasicTracerProvider({
+    resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+    }),
 })
 
 // log to console and send to New Relic
+const exporter = new OTLPTraceExporter({
+    url: process.env.REACT_APP_OTEL_COLLECTOR_URL,
+    headers: {},
+})
 
 provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()))
-provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter()))
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter))
 
 // Initialize the OpenTelemetry APIs to use the NodeTracerProvider bindings
 provider.register()
