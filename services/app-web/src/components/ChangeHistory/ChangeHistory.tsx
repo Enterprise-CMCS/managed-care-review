fe052c14abedeea1349af6bb5ea5a4442c401824
Mazdak Atighi <maz@truss.works>
2022-03-23 23:04:28 -0500

    use accordion

--- services/app-web/src/components/ChangeHistory/ChangeHistory.tsx
+++ services/app-web/src/components/ChangeHistory/ChangeHistory.tsx
@@ -1,44 +1,80 @@
+import React from 'react'
 import { SectionHeader } from '../SectionHeader'
-import { Table } from '@trussworks/react-uswds'
+import { Accordion } from '@trussworks/react-uswds'
 import { Submission2 } from '../../gen/gqlClient'
-import styles from '../SubmissionSummarySection/SubmissionSummarySection.module.scss'
+import { UpdateInfoType } from '../../common-code/domain-models'
+import styles from './ChangeHistory.module.scss'
 export type ChangeHistoryProps = {
     submission: Submission2
+}
+
+type flatRevisions = UpdateInfoType & {
+    kind: 'submit' | 'unlock'
 }
 
 export const ChangeHistory = ({
     submission,
 }: ChangeHistoryProps): React.ReactElement => {
+    const flattenedRevisions = (): flatRevisions[] => {
+        const result: flatRevisions[] = []
+        submission.revisions.forEach((r) => {
+            if (r.revision.submitInfo) {
+                const newSubmit: flatRevisions = {} as flatRevisions
+                newSubmit.updatedAt = r.revision.submitInfo.updatedAt
+                newSubmit.updatedBy = r.revision.submitInfo.updatedBy
+                newSubmit.updatedReason = r.revision.submitInfo.updatedReason
+                newSubmit.kind = 'submit'
+                result.push(newSubmit)
+            }
+            if (r.revision.unlockInfo) {
+                const newUnlock: flatRevisions = {} as flatRevisions
+                newUnlock.updatedAt = r.revision.unlockInfo.updatedAt
+                newUnlock.updatedBy = r.revision.unlockInfo.updatedBy
+                newUnlock.updatedReason = r.revision.unlockInfo.updatedReason
+                newUnlock.kind = 'unlock'
+                result.push(newUnlock)
+            }
+        })
+        return result
+    }
+    const revisedItems = flattenedRevisions().map((r) => {
+        const isInitialSubmission = r.updatedReason === 'Initial submission'
+        const isSubsequentSubmission = r.kind === 'submit'
+        return {
+            title: (
+                <span>
+                    {r.updatedAt} -{' '}
+                    {isSubsequentSubmission ? 'Submission' : 'Unlock'}
+                </span>
+            ),
+            content: isInitialSubmission ? (
+                <React.Fragment>
+                    <div>Submitted by: {r.updatedBy}</div>
+                </React.Fragment>
+            ) : (
+                <React.Fragment>
+                    <div>
+                        {isSubsequentSubmission
+                            ? 'Submitted by: '
+                            : 'Unlocked by: '}{' '}
+                        {r.updatedBy}
+                    </div>
+                    <div>
+                        {isSubsequentSubmission
+                            ? 'Changes made: '
+                            : 'Reason for unlock: '}
+                        {r.updatedReason}
+                    </div>
+                </React.Fragment>
+            ),
+            expanded: false,
+            id: r.updatedAt.toString(),
+        }
+    })
     return (
         <section id="changeHistory" className={styles.summarySection}>
             <SectionHeader header="Change history" />
-            <Table>
-                <tbody>
-                    <tr>
-                        <td>row to expand</td>
-                        <td>
-                            {submission.revisions.map((r) => {
-                                return (
-                                    <div key={r.revision.id}>
-                                        <div>
-                                            {r.revision.submitInfo?.updatedBy}
-                                        </div>
-                                        <div>
-                                            {r.revision.submitInfo?.updatedAt}
-                                        </div>
-                                        <div>
-                                            {
-                                                r.revision.submitInfo
-                                                    ?.updatedReason
-                                            }
-                                        </div>
-                                    </div>
-                                )
-                            })}
-                        </td>
-                    </tr>
-                </tbody>
-            </Table>
+            <Accordion items={revisedItems} />
         </section>
     )
 }
