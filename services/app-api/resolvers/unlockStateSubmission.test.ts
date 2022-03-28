cbfce4df91c564e0c8d306acc0c6a7a9fa1db12f
MacRae Linton <macrael@truss.works>
2022-03-28 16:57:05 -0700

    Linter fixes

--- services/app-api/resolvers/unlockStateSubmission.test.ts
+++ services/app-api/resolvers/unlockStateSubmission.test.ts
@@ -3,14 +3,15 @@ import UNLOCK_STATE_SUBMISSION from '../../app-graphql/src/mutations/unlockState
 import { Submission2 } from '../gen/gqlServer'
 import { todaysDate } from '../testHelpers/dateHelpers'
 import {
-    constructTestPostgresServer, createAndUpdateTestDraftSubmission,
+    constructTestPostgresServer,
+    createAndUpdateTestDraftSubmission,
     createTestStateSubmission,
     defaultFloridaProgram,
     fetchTestDraftSubmissionById,
     submitTestDraftSubmission,
     unlockTestDraftSubmission,
     updateTestDraftSubmission,
-    resubmitTestDraftSubmission
+    resubmitTestDraftSubmission,
 } from '../testHelpers/gqlHelpers'
 import { mockStoreThatErrors } from '../testHelpers/storeHelpers'
 
@@ -38,7 +39,7 @@ describe('unlockStateSubmission', () => {
             variables: {
                 input: {
                     submissionID: stateSubmission.id,
-                    unlockedReason: 'Super duper good reason.'
+                    unlockedReason: 'Super duper good reason.',
                 },
             },
         })
@@ -51,24 +52,36 @@ describe('unlockStateSubmission', () => {
 
         const unlockedSub: Submission2 =
             unlockResult.data.unlockStateSubmission.submission
-        
-        // After unlock, we should get a draft submission back
-        expect(unlockedSub.status).toEqual('UNLOCKED')
 
-        expect(unlockedSub.revisions.length).toEqual(2)
+        // After unlock, we should get a draft submission back
+        expect(unlockedSub.status).toBe('UNLOCKED')
+
+        expect(unlockedSub.revisions).toHaveLength(2)
 
         expect(unlockedSub.revisions[0].revision.submitInfo).toBeNull()
         expect(unlockedSub.revisions[1].revision.submitInfo).toBeDefined()
-        expect(unlockedSub.revisions[1].revision.submitInfo?.updatedAt.toISOString()).toContain(todaysDate())
+        expect(
+            unlockedSub.revisions[1].revision.submitInfo?.updatedAt.toISOString()
+        ).toContain(todaysDate())
         // check that the date has full ISO time eg. 2022-03-25T03:09:54.864Z
-        expect(unlockedSub.revisions[1].revision.submitInfo?.updatedAt.toISOString()).toContain('Z')
+        expect(
+            unlockedSub.revisions[1].revision.submitInfo?.updatedAt.toISOString()
+        ).toContain('Z')
 
         expect(unlockedSub.revisions[0].revision.unlockInfo).toBeDefined()
-        expect(unlockedSub.revisions[0].revision.unlockInfo?.updatedBy).toEqual('zuko@example.com')
-        expect(unlockedSub.revisions[0].revision.unlockInfo?.updatedReason).toEqual('Super duper good reason.')
-        expect(unlockedSub.revisions[0].revision.unlockInfo?.updatedAt.toISOString()).toContain(todaysDate())
+        expect(unlockedSub.revisions[0].revision.unlockInfo?.updatedBy).toBe(
+            'zuko@example.com'
+        )
+        expect(
+            unlockedSub.revisions[0].revision.unlockInfo?.updatedReason
+        ).toBe('Super duper good reason.')
+        expect(
+            unlockedSub.revisions[0].revision.unlockInfo?.updatedAt.toISOString()
+        ).toContain(todaysDate())
         // check that the date has full ISO time eg. 2022-03-25T03:09:54.864Z
-        expect(unlockedSub.revisions[0].revision.unlockInfo?.updatedAt.toISOString()).toContain('Z')
+        expect(
+            unlockedSub.revisions[0].revision.unlockInfo?.updatedAt.toISOString()
+        ).toContain('Z')
     })
 
     it('returns a DraftSubmission that can be updated without errors', async () => {
@@ -94,23 +107,30 @@ describe('unlockStateSubmission', () => {
             variables: {
                 input: {
                     submissionID: stateSubmission.id,
-                    unlockedReason: 'Super duper good reason.'
+                    unlockedReason: 'Super duper good reason.',
                 },
             },
         })
 
         expect(unlockResult.errors).toBeUndefined()
-        const unlockedSub =
-            unlockResult?.data?.unlockStateSubmission.submission
-        
+        const unlockedSub = unlockResult?.data?.unlockStateSubmission.submission
+
         // After unlock, we should get a draft submission back
-        expect(unlockedSub.status).toEqual('UNLOCKED')
+        expect(unlockedSub.status).toBe('UNLOCKED')
         expect(unlockedSub.revisions[0].revision.unlockInfo).toBeDefined()
-        expect(unlockedSub.revisions[0].revision.unlockInfo?.updatedBy).toEqual('zuko@example.com')
-        expect(unlockedSub.revisions[0].revision.unlockInfo?.updatedReason).toEqual('Super duper good reason.')
-        expect(unlockedSub.revisions[0].revision.unlockInfo?.updatedAt.toISOString()).toContain(todaysDate())
+        expect(unlockedSub.revisions[0].revision.unlockInfo?.updatedBy).toBe(
+            'zuko@example.com'
+        )
+        expect(
+            unlockedSub.revisions[0].revision.unlockInfo?.updatedReason
+        ).toBe('Super duper good reason.')
+        expect(
+            unlockedSub.revisions[0].revision.unlockInfo?.updatedAt.toISOString()
+        ).toContain(todaysDate())
         // check that the date has full ISO time eg. 2022-03-25T03:09:54.864Z
-        expect(unlockedSub.revisions[0].revision.unlockInfo?.updatedAt.toISOString()).toContain('Z')
+        expect(
+            unlockedSub.revisions[0].revision.unlockInfo?.updatedAt.toISOString()
+        ).toContain('Z')
 
         // after unlock we should be able to update that draft submission and get the results
         const updates = {
@@ -127,11 +147,18 @@ describe('unlockStateSubmission', () => {
             actuaryContacts: [],
         }
 
-        await updateTestDraftSubmission(stateServer, stateSubmission.id, updates)
+        await updateTestDraftSubmission(
+            stateServer,
+            stateSubmission.id,
+            updates
+        )
 
-        const refetched = await fetchTestDraftSubmissionById(stateServer, stateSubmission.id)
+        const refetched = await fetchTestDraftSubmissionById(
+            stateServer,
+            stateSubmission.id
+        )
 
-        expect(refetched.submissionDescription).toEqual('UPDATED_AFTER_UNLOCK')
+        expect(refetched.submissionDescription).toBe('UPDATED_AFTER_UNLOCK')
     })
 
     it('can be unlocked repeatedly', async () => {
@@ -150,21 +177,49 @@ describe('unlockStateSubmission', () => {
             },
         })
 
-        await unlockTestDraftSubmission(cmsServer, stateSubmission.id, 'Super duper good reason.')
+        await unlockTestDraftSubmission(
+            cmsServer,
+            stateSubmission.id,
+            'Super duper good reason.'
+        )
 
-        await resubmitTestDraftSubmission(stateServer, stateSubmission.id, 'Test first resubmission reason')
+        await resubmitTestDraftSubmission(
+            stateServer,
+            stateSubmission.id,
+            'Test first resubmission reason'
+        )
 
-        await unlockTestDraftSubmission(cmsServer, stateSubmission.id, 'Super duper duper good reason.')
+        await unlockTestDraftSubmission(
+            cmsServer,
+            stateSubmission.id,
+            'Super duper duper good reason.'
+        )
 
-        await resubmitTestDraftSubmission(stateServer, stateSubmission.id, 'Test second resubmission reason')
+        await resubmitTestDraftSubmission(
+            stateServer,
+            stateSubmission.id,
+            'Test second resubmission reason'
+        )
 
-        const draft = await unlockTestDraftSubmission(cmsServer, stateSubmission.id, 'Very super duper good reason.')
-        expect(draft.status).toEqual('UNLOCKED')
-        expect(draft.revisions[0].revision.unlockInfo?.updatedBy).toEqual('zuko@example.com')
-        expect(draft.revisions[0].revision.unlockInfo?.updatedReason).toEqual('Very super duper good reason.')
-        expect(draft.revisions[0].revision.unlockInfo?.updatedAt.toISOString()).toContain(todaysDate())
+        const draft = await unlockTestDraftSubmission(
+            cmsServer,
+            stateSubmission.id,
+            'Very super duper good reason.'
+        )
+        expect(draft.status).toBe('UNLOCKED')
+        expect(draft.revisions[0].revision.unlockInfo?.updatedBy).toBe(
+            'zuko@example.com'
+        )
+        expect(draft.revisions[0].revision.unlockInfo?.updatedReason).toBe(
+            'Very super duper good reason.'
+        )
+        expect(
+            draft.revisions[0].revision.unlockInfo?.updatedAt.toISOString()
+        ).toContain(todaysDate())
         // check that the date has full ISO time eg. 2022-03-25T03:09:54.864Z
-        expect(draft.revisions[0].revision.unlockInfo?.updatedAt.toISOString()).toContain('Z')
+        expect(
+            draft.revisions[0].revision.unlockInfo?.updatedAt.toISOString()
+        ).toContain('Z')
     })
 
     it('returns errors if a state user tries to unlock', async () => {
@@ -180,7 +235,7 @@ describe('unlockStateSubmission', () => {
             variables: {
                 input: {
                     submissionID: stateSubmission.id,
-                    unlockedReason: 'Super duper good reason.'
+                    unlockedReason: 'Super duper good reason.',
                 },
             },
         })
@@ -188,8 +243,8 @@ describe('unlockStateSubmission', () => {
         expect(unlockResult.errors).toBeDefined()
         const err = (unlockResult.errors as GraphQLError[])[0]
 
-        expect(err.extensions['code']).toEqual('FORBIDDEN')
-        expect(err.message).toEqual('user not authorized to unlock submission')
+        expect(err.extensions['code']).toBe('FORBIDDEN')
+        expect(err.message).toBe('user not authorized to unlock submission')
     })
 
     it('returns errors if unlocked from the wrong state', async () => {
@@ -205,7 +260,9 @@ describe('unlockStateSubmission', () => {
         })
 
         // First, create a new draft submission
-        const stateSubmission = await createAndUpdateTestDraftSubmission(stateServer)
+        const stateSubmission = await createAndUpdateTestDraftSubmission(
+            stateServer
+        )
 
         // Attempt Unlock Draft
         const unlockDraftResult = await cmsServer.executeOperation({
@@ -213,7 +270,7 @@ describe('unlockStateSubmission', () => {
             variables: {
                 input: {
                     submissionID: stateSubmission.id,
-                    unlockedReason: 'Super duper good reason.'
+                    unlockedReason: 'Super duper good reason.',
                 },
             },
         })
@@ -221,13 +278,19 @@ describe('unlockStateSubmission', () => {
         expect(unlockDraftResult.errors).toBeDefined()
         const err = (unlockDraftResult.errors as GraphQLError[])[0]
 
-        expect(err.extensions['code']).toEqual('BAD_USER_INPUT')
-        expect(err.message).toEqual('Attempted to unlock submission with wrong status')
+        expect(err.extensions['code']).toBe('BAD_USER_INPUT')
+        expect(err.message).toBe(
+            'Attempted to unlock submission with wrong status'
+        )
 
         await submitTestDraftSubmission(stateServer, stateSubmission.id)
 
         // Unlock Submission
-        await unlockTestDraftSubmission(cmsServer, stateSubmission.id, 'Super duper good reason.')
+        await unlockTestDraftSubmission(
+            cmsServer,
+            stateSubmission.id,
+            'Super duper good reason.'
+        )
 
         // Attempt Unlock Unlocked
         const unlockUnlockedResult = await cmsServer.executeOperation({
@@ -235,7 +298,7 @@ describe('unlockStateSubmission', () => {
             variables: {
                 input: {
                     submissionID: stateSubmission.id,
-                    unlockedReason: 'Super duper good reason.'
+                    unlockedReason: 'Super duper good reason.',
                 },
             },
         })
@@ -243,8 +306,10 @@ describe('unlockStateSubmission', () => {
         expect(unlockUnlockedResult.errors).toBeDefined()
         const unlockErr = (unlockUnlockedResult.errors as GraphQLError[])[0]
 
-        expect(unlockErr.extensions['code']).toEqual('BAD_USER_INPUT')
-        expect(unlockErr.message).toEqual('Attempted to unlock submission with wrong status')
+        expect(unlockErr.extensions['code']).toBe('BAD_USER_INPUT')
+        expect(unlockErr.message).toBe(
+            'Attempted to unlock submission with wrong status'
+        )
     })
 
     it('returns an error if the submission does not exit', async () => {
@@ -268,7 +333,7 @@ describe('unlockStateSubmission', () => {
             variables: {
                 input: {
                     submissionID: 'foo-bar',
-                    unlockedReason: 'Super duper good reason.'
+                    unlockedReason: 'Super duper good reason.',
                 },
             },
         })
@@ -276,12 +341,13 @@ describe('unlockStateSubmission', () => {
         expect(unlockResult.errors).toBeDefined()
         const err = (unlockResult.errors as GraphQLError[])[0]
 
-        expect(err.extensions['code']).toEqual('BAD_USER_INPUT')
-        expect(err.message).toEqual('A submission must exist to be unlocked: foo-bar')
+        expect(err.extensions['code']).toBe('BAD_USER_INPUT')
+        expect(err.message).toBe(
+            'A submission must exist to be unlocked: foo-bar'
+        )
     })
 
     it('returns an error if the DB errors', async () => {
-
         const errorStore = mockStoreThatErrors()
 
         const cmsServer = await constructTestPostgresServer({
@@ -302,7 +368,7 @@ describe('unlockStateSubmission', () => {
             variables: {
                 input: {
                     submissionID: 'foo-bar',
-                    unlockedReason: 'Super duper good reason.'
+                    unlockedReason: 'Super duper good reason.',
                 },
             },
         })
@@ -310,8 +376,10 @@ describe('unlockStateSubmission', () => {
         expect(unlockResult.errors).toBeDefined()
         const err = (unlockResult.errors as GraphQLError[])[0]
 
-        expect(err.extensions['code']).toEqual('INTERNAL_SERVER_ERROR')
-        expect(err.message).toEqual('Issue finding a state submission of type UNEXPECTED_EXCEPTION. Message: this error came from the generic store with errors mock')
+        expect(err.extensions['code']).toBe('INTERNAL_SERVER_ERROR')
+        expect(err.message).toBe(
+            'Issue finding a state submission of type UNEXPECTED_EXCEPTION. Message: this error came from the generic store with errors mock'
+        )
     })
 
     it('returns errors if unlocked reason is undefined', async () => {
@@ -344,7 +412,9 @@ describe('unlockStateSubmission', () => {
         expect(unlockedResult.errors).toBeDefined()
         const err = (unlockedResult.errors as GraphQLError[])[0]
 
-        expect(err.extensions['code']).toEqual('BAD_USER_INPUT')
-        expect(err.message).toContain('Field "unlockedReason" of required type "String!" was not provided.')
+        expect(err.extensions['code']).toBe('BAD_USER_INPUT')
+        expect(err.message).toContain(
+            'Field "unlockedReason" of required type "String!" was not provided.'
+        )
     })
 })
