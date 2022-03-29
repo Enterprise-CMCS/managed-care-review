f6d130972d5746c8d2d6c2289fc52f2059f3641c
Hana Worku <hana@truss.works>
2022-03-29 13:35:11 -0500

    update script

--- services/app-web/src/pages/App/App.tsx
+++ services/app-web/src/pages/App/App.tsx
@@ -23,6 +23,7 @@ function ErrorFallback({
     error: Error
     resetErrorBoundary?: () => void
 }): React.ReactElement {
+    console.error(error)
     return <GenericErrorPage />
 }
 
@@ -35,15 +36,12 @@ function App({
     apolloClient: ApolloClient<NormalizedCacheObject>
     s3Client: S3ClientT
 }): React.ReactElement {
-    useEffect(() => {
-        logEvent('on_load', { success: true })
-    }, [])
+
     // This is a hacky way to fake feature flags before we have feature flags.
     // please avoid reading env vars outside of index.tsx in general.
     const environmentName = process.env.REACT_APP_STAGE_NAME || ''
     const isProdEnvironment = ['prod', 'val'].includes(environmentName)
-
-    const jiraTicketCollectorHTML = `<script type="text/javascript" src="https://truss-learning.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/-9zew5j/b/7/c95134bc67d3a521bb3f4331beb9b804/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-US&collectorId=1b8f91f1"></script>`
+    const jiraTicketCollectorHTML = `<script type="text/javascript" src="<script type="text/javascript" src="https://meghantest.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/-9zew5j/b/7/c95134bc67d3a521bb3f4331beb9b804/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-US&collectorId=e59b8faf"></script>`
 
     return (
         <ErrorBoundary FallbackComponent={ErrorFallback}>
