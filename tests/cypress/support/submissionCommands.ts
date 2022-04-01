995eb22fd836d4e151f99cb6518b5e730ca2685a
Jason Lin <maolin@truss.works>
2022-04-01 17:01:32 -0400

    Refactor commands into one.

--- tests/cypress/support/submissionCommands.ts
+++ tests/cypress/support/submissionCommands.ts
@@ -1,26 +1,12 @@
 import { aliasQuery } from '../utils/graphql-test-utils'
 
 Cypress.Commands.add(
-    'navigateBackToCurrentSubmission',
-    () => {
+    'navigateToSubmissionByUserInteraction',
+    (testId: string) => {
         cy.intercept('POST', '*/graphql', (req) => {
             aliasQuery(req, 'fetchSubmission2')
         })
-        cy.findByText('View most recent version of this submission').click()
-        cy.wait('@fetchSubmission2Query', { timeout: 20000 })
-    }
-)
-
-Cypress.Commands.add(
-    'navigateToSubmissionRevision',
-    (linkTestId: string) => {
-        cy.intercept('POST', '*/graphql', (req) => {
-            aliasQuery(req, 'fetchSubmission2')
-        })
-
-        cy.findByTestId(linkTestId)
-            .should('exist')
-            .click()
+        cy.findByTestId(testId).should('exist').click()
         cy.wait('@fetchSubmission2Query', { timeout: 20000 })
     }
 )
