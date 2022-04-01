41aca2e93fad8d16bf5111d5c56a1d1dfd77bda5
haworku <haworku@users.noreply.github.com>
2022-04-01 09:59:25 -0500

    OY2-16868: Add Jira Issue Collector Feedback Tab to Dev Environment (#891)

---
+++ services/app-web/src/hooks/useScript.ts
@@ -0,0 +1,22 @@
+import { useEffect } from 'react'
+
+// Add script to bottom of page, with handling for some generic boolean value to flag on or off
+const useScript = (url: string, featureFlag: boolean): void => {
+    useEffect(() => {
+        if (featureFlag) {
+            const script = document.createElement('script')
+
+            script.src = url
+            script.async = true
+            script.text = 'text/javascript'
+
+            document.body.appendChild(script)
+
+            return () => {
+                document.body.removeChild(script)
+            }
+        }
+    }, [url, featureFlag])
+}
+
+export { useScript }
