056ca7db021eed62f9ba8d434fc5fb2c19177e91
Mojo Talantikite <mojo.talantikite@gmail.com>
2022-04-06 18:01:39 -0400

    Change health endpoint code with ld

--- services/app-api/handlers/health_check.ts
+++ services/app-api/handlers/health_check.ts
@@ -1,13 +1,30 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
+import LaunchDarkly from 'launchdarkly-node-server-sdk'
+
+const ldClientKey = process.env.LD_SDK_KEY ?? ''
+if (ldClientKey === '') {
+    throw new Error('LD_SDK_KEY environment variable is not set')
+}
+const ldClient = LaunchDarkly.init(ldClientKey)
 
 export const main: APIGatewayProxyHandler = async () => {
     // returns stage and version
     const health = {
         stage: process.env.stage,
         version: process.env.appVersion,
+        ld: '',
     }
 
     console.log({ name: 'healthcheck' }) // eslint-disable-line no-console
+
+    await ldClient.waitForInitialization()
+    const changeHealthResponse = await ldClient.variation(
+        'enable-health-endpoint',
+        { key: 'mojo@truss.works' },
+        false
+    )
+    health.ld =
+        changeHealthResponse.variation === 'true' ? 'healthy' : 'unhealthy'
 
     return {
         statusCode: 200,
