28cafe83833aae9fd7eb318a73a1e66ae8f46463
Jason Lin <maolin@truss.works>
2022-04-07 11:29:08 -0400

    Fix rate name html.

--- services/app-web/src/components/SubmissionSummarySection/RateDetailsSummarySection/RateDetailsSummarySection.tsx
+++ services/app-web/src/components/SubmissionSummarySection/RateDetailsSummarySection/RateDetailsSummarySection.tsx
@@ -109,11 +109,7 @@ export const RateDetailsSummarySection = ({
                     )}
                 </SectionHeader>
 
-                <div aria-label="Rate ID" id="rateID" role="group">
-                    <h4 role="definition" aria-labelledby="rateID">
-                        {rateName}
-                    </h4>
-                </div>
+                <h3 aria-label={`Rate ID: ${rateName}`}>{rateName}</h3>
 
                 <DoubleColumnGrid>
                     <DataDetail
