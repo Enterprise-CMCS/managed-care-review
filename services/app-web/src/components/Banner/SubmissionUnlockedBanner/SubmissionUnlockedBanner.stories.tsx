9f63684d94eac4d255097d891070009518629e3b
Jason Lin <maolin@truss.works>
2022-04-15 05:13:49 -0400

    Make reason text longer.

--- services/app-web/src/components/Banner/SubmissionUnlockedBanner/SubmissionUnlockedBanner.stories.tsx
+++ services/app-web/src/components/Banner/SubmissionUnlockedBanner/SubmissionUnlockedBanner.stories.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { Story } from '@storybook/react'
-import { SubmissionUnlockedBanner, UnlockedProps } from './SubmissionUnlockedBanner';
+import {
+    SubmissionUnlockedBanner,
+    UnlockedProps,
+} from './SubmissionUnlockedBanner'
 
 export default {
     title: 'Components/Banner/SubmissionUnlockedBanner',
     component: SubmissionUnlockedBanner,
 }
 
-const Template: Story<UnlockedProps> = (args) => <SubmissionUnlockedBanner {...args} />
+const Template: Story<UnlockedProps> = (args) => (
+    <SubmissionUnlockedBanner {...args} />
+)
 
 export const SubmissionUnlockedBannerCMSUser = Template.bind({})
 SubmissionUnlockedBannerCMSUser.args = {
     userType: 'CMS_USER',
     unlockedBy: 'Loremipsum@email.com',
     unlockedOn: new Date(),
-    reason: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut justo non nisl congue efficitur. Praesent porta condimentum imperdiet. Curabitur.',
+    reason: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut justo non nisl congue efficitur. Praesent porta condimentum imperdiet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut justo non nisl congue efficitur. Praesent porta condimentum imperdiet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut justo non nisl congue efficitur. Praesent porta condimentum imperdiet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut justo non nisl congue efficitur. Praesent porta condimentum imperdiet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut justo non nisl congue efficitur. Praesent porta condimentum imperdiet.',
 }
 
 export const SubmissionUnlockedBannerStateUser = Template.bind({})
