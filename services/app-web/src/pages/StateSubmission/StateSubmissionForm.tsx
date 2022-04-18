99c3d1497cdea257317c64974e6e10d95c81f36d
MacRae Linton <macrael@truss.works>
2022-04-18 12:20:33 -0700

    keep the revision up to date on update

--- services/app-web/src/pages/StateSubmission/StateSubmissionForm.tsx
+++ services/app-web/src/pages/StateSubmission/StateSubmissionForm.tsx
@@ -160,6 +160,33 @@ export const StateSubmissionForm = (): React.ReactElement => {
                 setShowPageErrorMessage(true)
                 return new Error('Failed to update form data')
             }
+            // Apollo is letting us load new pages without having finished re-fetching the new data
+            // and isn't updating the cache the way I expected here, so for now, we set our current
+            // revision manually when update returns.
+            const protoResult =
+                getCurrentRevisionFromHealthPlanPackage(updatedSubmission)
+            if (protoResult instanceof Error) {
+                console.log(
+                    'Proto returned by update mutation is invalid',
+                    protoResult
+                )
+                setFormDataError('MALFORMATTED_DATA')
+                return protoResult
+            }
+            const planFormData = protoResult[1]
+
+            if (planFormData.status !== 'DRAFT') {
+                console.log(
+                    'Proto returned by update mutation is wrong state',
+                    protoResult
+                )
+                setFormDataError('WRONG_SUBMISSION_STATUS')
+                return new Error(
+                    'Proto returned by update mutation is wrong state'
+                )
+            }
+
+            setFormDataFromLatestRevision(planFormData)
 
             return updatedSubmission
         } catch (serverError) {
