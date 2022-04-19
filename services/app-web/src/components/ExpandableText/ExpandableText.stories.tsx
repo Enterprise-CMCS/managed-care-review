fb87450472adeb21913eae597303bbc85706c9a7
Jason Lin <98117700+JasonLin0991@users.noreply.github.com>
2022-04-19 11:32:48 -0400

    MR-2081: State and CMS have no character limit on their unlock or resubmission reason (#918)
    * Remove character limit for unlock and resubmission reason.
    
    * ExpandableTest component
    
    * SubmissionUpdatedBanner storybook
    
    * Export ExpandableText component.
    
    * ExpandableText component tests.
    
    * ExpandableText component tests.
    
    * Add ExpandableText export.
    
    * Remove test for character limit.
    
    * Make reason text longer.
    
    * Update Unlock and Updated banner to use ExpandableText component.
    
    * Remove console logs
    
    * Rename variables
    
    * Use inline span HTML instead of block element divs. Wrap entire component in a p tag
    
    * Fix button outline covering text.
    
    * Remove unncessary duplicate useLayoutEffect call
    
    * Fix storybook only bug in firefox - don't let line-height mess with offsetHeight calculation
    
    * Don't let actions tab flood storybook; only opt into actions for specific events on a story by story basis
    
    * Other misc cleanup
    
    * Remove prop spread.
    
    * Remove `clamp` prop, pass children instead and update Tests.
    
    Co-authored-by: Hana Worku <hana@truss.works>

---
+++ services/app-web/src/components/ExpandableText/ExpandableText.stories.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Story } from '@storybook/react'
+import { ExpandableText, ExpandableTextProps } from './ExpandableText'
+import { GridContainer } from '@trussworks/react-uswds'
+
+export default {
+    title: 'Components/ExpandableText',
+    component: ExpandableText,
+}
+
+const longText =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut justo non nisl congue efficitur. Praesent porta condimentum imperdiet.' +
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut justo non nisl congue efficitur. Praesent porta condimentum imperdiet.' +
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut justo non nisl congue efficitur. Praesent porta condimentum imperdiet.' +
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut justo non nisl congue efficitur. Praesent porta condimentum imperdiet.' +
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut justo non nisl congue efficitur. Praesent porta condimentum imperdiet.' +
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut justo non nisl congue efficitur. Praesent porta condimentum imperdiet.' +
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut justo non nisl congue efficitur. Praesent porta condimentum.'
+
+const shortText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+
+const Template: Story<
+    ExpandableTextProps & React.HTMLAttributes<HTMLParagraphElement>
+> = (args) => {
+    const { children, clampedLines } = args
+    return (
+        <GridContainer className="app-container-horizontal-pad">
+            <ExpandableText clampedLines={clampedLines}>
+                {children}
+            </ExpandableText>
+        </GridContainer>
+    )
+}
+
+export const ExpandableTextClampedToTwoLines = Template.bind({})
+ExpandableTextClampedToTwoLines.args = {
+    children: <>{longText}</>,
+}
+
+export const ExpandableTextClampedToFourLines = Template.bind({})
+ExpandableTextClampedToFourLines.args = {
+    clampedLines: 4,
+    children: <>{longText}</>,
+}
+
+export const ExpandableTextNoClamp = Template.bind({})
+ExpandableTextNoClamp.args = {
+    children: <>{shortText}</>,
+}
+
+export const ExpandableTextWithPrefix = Template.bind({})
+ExpandableTextWithPrefix.args = {
+    children: (
+        <>
+            <span className="text-bold">Some bold header:&nbsp; </span>
+            {longText}
+        </>
+    ),
+}
