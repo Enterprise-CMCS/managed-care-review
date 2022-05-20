62214b296b2542614dde4acffd95fdedb2e4f293
Mazdak Atighi <maz@truss.works>
2022-05-20 09:02:13 -0500

    yay, mocking useauth

--- services/app-web/src/pages/Wrapper/AuthenticatedRouteWrapper.test.tsx
+++ services/app-web/src/pages/Wrapper/AuthenticatedRouteWrapper.test.tsx
@@ -1,6 +1,7 @@
 import { screen, waitFor } from '@testing-library/react'
 import { renderWithProviders } from '../../testHelpers/jestHelpers'
 import { AuthenticatedRouteWrapper } from './AuthenticatedRouteWrapper'
+import * as AuthContext from '../../contexts/AuthContext'
 
 describe('AuthenticatedRouteWrapper', () => {
     it('renders without errors', async () => {
@@ -23,5 +24,39 @@ describe('AuthenticatedRouteWrapper', () => {
         )
         const dialog = screen.getByRole('dialog')
         await waitFor(() => expect(dialog).toHaveClass('is-hidden'))
+    })
+
+    it('shows the modal when isSessionExpiring is true', async () => {
+        jest.spyOn(AuthContext, 'useAuth').mockReturnValue({
+            loggedInUser: {
+                __typename: 'StateUser' as const,
+                state: {
+                    name: 'Minnesota',
+                    code: 'MN',
+                    programs: [
+                        { id: 'msho', name: 'MSHO' },
+                        { id: 'pmap', name: 'PMAP' },
+                        { id: 'snbc', name: 'SNBC' },
+                    ],
+                },
+                role: 'State User',
+                name: 'Bob it user',
+                email: 'bob@dmas.mn.gov',
+            },
+            loginStatus: 'LOGGED_IN',
+            checkAuth: () => Promise.reject(Error('Auth context error')),
+            logout: () => Promise.resolve(),
+            isSessionExpiring: true,
+            updateSessionExpiry: () => void 0,
+            timeUntilLogout: 120,
+        })
+        renderWithProviders(
+            <AuthenticatedRouteWrapper
+                authMode="LOCAL"
+                children={<div>children go here</div>}
+            />
+        )
+        const dialog = screen.getByRole('dialog')
+        await waitFor(() => expect(dialog).toHaveClass('is-visible'))
     })
 })
