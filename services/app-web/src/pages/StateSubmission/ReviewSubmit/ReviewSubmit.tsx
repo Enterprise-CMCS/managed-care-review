ff91cd0c28bcb31dfc6c1f5cfbb0e57489591e57
Jason Lin <maolin@truss.works>
2022-05-25 14:51:03 -0400

    Pull review submit modal into its own component.

--- services/app-web/src/pages/StateSubmission/ReviewSubmit/ReviewSubmit.tsx
+++ services/app-web/src/pages/StateSubmission/ReviewSubmit/ReviewSubmit.tsx
@@ -1,12 +1,10 @@
 import {
     Alert,
-    FormGroup,
     GridContainer,
     ModalRef,
     ModalToggleButton,
-    Textarea,
 } from '@trussworks/react-uswds'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import {
     ContactsSummarySection,
@@ -16,16 +14,12 @@ import {
     SupportingDocumentsSummarySection,
 } from '../../../components/SubmissionSummarySection'
 import { useAuth } from '../../../contexts/AuthContext'
-import { useSubmitHealthPlanPackageMutation } from '../../../gen/gqlClient'
 import { PageActionsContainer } from '../PageActions'
-import { Modal } from '../../../components/Modal'
 import styles from './ReviewSubmit.module.scss'
-import { PoliteErrorMessage } from '../../../components'
-import { useFormik } from 'formik'
-import * as Yup from 'yup'
 import { UnlockedHealthPlanFormDataType } from '../../../common-code/healthPlanFormDataType'
 import { ActionButton } from '../../../components/ActionButton'
 import { DocumentDateLookupTable } from '../../SubmissionSummary/SubmissionSummary'
+import { ReviewSubmitModal } from './ReviewSubmitModal'
 
 export const ReviewSubmit = ({
     draftSubmission,
@@ -41,10 +35,10 @@ export const ReviewSubmit = ({
     const [userVisibleError, setUserVisibleError] = useState<
         string | undefined
     >(undefined)
-    const [focusErrorsInModal, setFocusErrorsInModal] = useState(true)
     const history = useHistory()
     const modalRef = useRef<ModalRef>(null)
     const { loggedInUser } = useAuth()
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
 
     // pull the programs off the user
     const statePrograms =
@@ -53,96 +47,12 @@ export const ReviewSubmit = ({
             loggedInUser.state.programs) ||
         []
 
-    const [submitDraftSubmission, { loading: submitMutationLoading }] =
-        useSubmitHealthPlanPackageMutation()
-
     const showError = (error: string) => {
         setUserVisibleError(error)
     }
 
-    const modalFormInitialValues = {
-        submittedReason: '',
-    }
-
-    const formik = useFormik({
-        initialValues: modalFormInitialValues,
-        validationSchema: Yup.object().shape({
-            submittedReason: Yup.string().defined(
-                'You must provide a summary of changes'
-            ),
-        }),
-        onSubmit: (values) => onModalSubmit(values),
-    })
-
-    const submitHandler = async () => {
-        setFocusErrorsInModal(true)
-        if (unlocked) {
-            formik.handleSubmit()
-        } else {
-            await onSubmit(undefined)
-        }
-    }
-
-    const onModalSubmit = async (values: typeof modalFormInitialValues) => {
-        const { submittedReason } = values
-        await onSubmit(submittedReason)
-    }
-
-    const onSubmit = async (
-        submittedReason: string | undefined
-    ): Promise<void> => {
-        const input = { pkgID: draftSubmission.id }
-
-        if (unlocked) {
-            Object.assign(input, {
-                submittedReason: submittedReason,
-            })
-        }
-
-        try {
-            const data = await submitDraftSubmission({
-                variables: {
-                    input: input,
-                },
-            })
-
-            if (data.errors) {
-                showError('Error attempting to submit. Please try again.')
-                modalRef.current?.toggleModal(undefined, false)
-            }
-
-            if (data.data?.submitHealthPlanPackage) {
-                modalRef.current?.toggleModal(undefined, false)
-                history.push(`/dashboard?justSubmitted=${submissionName}`)
-            } else {
-                console.error('Got nothing back from submit')
-                showError('Error attempting to submit. Please try again.')
-                modalRef.current?.toggleModal(undefined, false)
-            }
-        } catch (error) {
-            showError('Error attempting to submit. Please try again.')
-            modalRef.current?.toggleModal(undefined, false)
-        }
-    }
-
     const isContractActionAndRateCertification =
         draftSubmission.submissionType === 'CONTRACT_AND_RATES'
-
-    // Focus submittedReason field in submission modal on Resubmit click when errors exist
-    useEffect(() => {
-        if (focusErrorsInModal && formik.errors.submittedReason) {
-            const fieldElement: HTMLElement | null = document.querySelector(
-                `[name="submittedReason"]`
-            )
-
-            if (fieldElement) {
-                fieldElement.focus()
-                setFocusErrorsInModal(false)
-            } else {
-                console.log('Attempting to focus element that does not exist')
-            }
-        }
-    }, [focusErrorsInModal, formik.errors])
 
     return (
         <GridContainer className={styles.reviewSectionWrapper}>
@@ -191,7 +101,7 @@ export const ReviewSubmit = ({
                         type="button"
                         variant="linkStyle"
                         onClick={() => history.push('/dashboard')}
-                        disabled={formik.isSubmitting}
+                        disabled={isSubmitting}
                     >
                         Save as draft
                     </ActionButton>
@@ -201,7 +111,7 @@ export const ReviewSubmit = ({
                     type="button"
                     variant="outline"
                     onClick={() => history.push('documents')}
-                    disabled={formik.isSubmitting}
+                    disabled={isSubmitting}
                 >
                     Back
                 </ActionButton>
@@ -215,56 +125,14 @@ export const ReviewSubmit = ({
                 </ModalToggleButton>
             </PageActionsContainer>
 
-            <Modal
+            <ReviewSubmitModal
+                draftSubmission={draftSubmission}
+                submissionName={submissionName}
+                unlocked={unlocked}
                 modalRef={modalRef}
-                id="review-and-submit"
-                modalHeading={
-                    unlocked ? 'Summarize changes' : 'Ready to submit?'
-                }
-                submitButtonProps={{ className: styles.submitButton }}
-                onSubmitText={unlocked ? 'Resubmit' : undefined}
-                onSubmit={submitHandler}
-                isSubmitting={formik.isSubmitting || submitMutationLoading}
-            >
-                {unlocked ? (
-                    <form>
-                        <p>
-                            Once you submit, this package will be sent to CMS
-                            for review and you will no longer be able to make
-                            changes.
-                        </p>
-                        <FormGroup
-                            error={Boolean(formik.errors.submittedReason)}
-                        >
-                            {formik.errors.submittedReason && (
-                                <PoliteErrorMessage role="alert">
-                                    {formik.errors.submittedReason}
-                                </PoliteErrorMessage>
-                            )}
-                            <span id="submittedReason-hint" role="note">
-                                Provide summary of all changes made to this
-                                submission
-                            </span>
-                            <Textarea
-                                id="submittedReasonCharacterCount"
-                                name="submittedReason"
-                                data-testid="submittedReason"
-                                aria-labelledby="submittedReason-hint"
-                                className={styles.submittedReasonTextarea}
-                                aria-required
-                                error={!!formik.errors.submittedReason}
-                                onChange={formik.handleChange}
-                                defaultValue={formik.values.submittedReason}
-                            />
-                        </FormGroup>
-                    </form>
-                ) : (
-                    <p>
-                        Submitting this package will send it to CMS to begin
-                        their review.
-                    </p>
-                )}
-            </Modal>
+                showError={showError}
+                isSubmitting={setIsSubmitting}
+            />
         </GridContainer>
     )
 }
---
+++ services/app-web/src/pages/StateSubmission/ReviewSubmit/ReviewSubmitModal.tsx
@@ -0,0 +1,165 @@
+import React, { useEffect, useState } from 'react'
+import styles from './ReviewSubmit.module.scss'
+import { FormGroup, ModalRef, Textarea } from '@trussworks/react-uswds'
+import { Modal, PoliteErrorMessage } from '../../../components'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import { UnlockedHealthPlanFormDataType } from '../../../common-code/healthPlanFormDataType'
+import { useHistory } from 'react-router-dom'
+import { useSubmitHealthPlanPackageMutation } from '../../../gen/gqlClient'
+
+export const ReviewSubmitModal = ({
+    draftSubmission,
+    submissionName,
+    unlocked,
+    modalRef,
+    showError,
+    isSubmitting,
+}: {
+    draftSubmission: UnlockedHealthPlanFormDataType
+    submissionName: string
+    unlocked: boolean
+    modalRef: React.RefObject<ModalRef>
+    showError: (error: string) => void
+    isSubmitting: React.Dispatch<React.SetStateAction<boolean>>
+}): React.ReactElement => {
+    const [focusErrorsInModal, setFocusErrorsInModal] = useState(true)
+    const history = useHistory()
+
+    const [submitDraftSubmission, { loading: submitMutationLoading }] =
+        useSubmitHealthPlanPackageMutation()
+
+    const modalFormInitialValues = {
+        submittedReason: '',
+    }
+
+    const formik = useFormik({
+        initialValues: modalFormInitialValues,
+        validationSchema: Yup.object().shape({
+            submittedReason: Yup.string().defined(
+                'You must provide a summary of changes'
+            ),
+        }),
+        onSubmit: (values) => onModalSubmit(values),
+    })
+
+    const submitHandler = async () => {
+        setFocusErrorsInModal(true)
+        if (unlocked) {
+            isSubmitting(formik.isSubmitting)
+            formik.handleSubmit()
+        } else {
+            await onSubmit(undefined)
+        }
+    }
+
+    const onModalSubmit = async (values: typeof modalFormInitialValues) => {
+        const { submittedReason } = values
+        await onSubmit(submittedReason)
+    }
+
+    const onSubmit = async (
+        submittedReason: string | undefined
+    ): Promise<void> => {
+        const input = { pkgID: draftSubmission.id }
+
+        if (unlocked) {
+            Object.assign(input, {
+                submittedReason: submittedReason,
+            })
+        }
+
+        try {
+            isSubmitting(formik.isSubmitting)
+            const data = await submitDraftSubmission({
+                variables: {
+                    input: input,
+                },
+            })
+
+            if (data.errors) {
+                isSubmitting(formik.isSubmitting)
+                showError('Error attempting to submit. Please try again.')
+                modalRef.current?.toggleModal(undefined, false)
+            }
+
+            if (data.data?.submitHealthPlanPackage) {
+                modalRef.current?.toggleModal(undefined, false)
+                history.push(`/dashboard?justSubmitted=${submissionName}`)
+            } else {
+                isSubmitting(formik.isSubmitting)
+                console.error('Got nothing back from submit')
+                showError('Error attempting to submit. Please try again.')
+                modalRef.current?.toggleModal(undefined, false)
+            }
+        } catch (error) {
+            isSubmitting(formik.isSubmitting)
+            showError('Error attempting to submit. Please try again.')
+            modalRef.current?.toggleModal(undefined, false)
+        }
+    }
+
+    // Focus submittedReason field in submission modal on Resubmit click when errors exist
+    useEffect(() => {
+        if (focusErrorsInModal && formik.errors.submittedReason) {
+            const fieldElement: HTMLElement | null = document.querySelector(
+                `[name="submittedReason"]`
+            )
+
+            if (fieldElement) {
+                fieldElement.focus()
+                setFocusErrorsInModal(false)
+            } else {
+                console.log('Attempting to focus element that does not exist')
+            }
+        }
+    }, [focusErrorsInModal, formik.errors])
+
+    return (
+        <Modal
+            modalRef={modalRef}
+            id="review-and-submit"
+            modalHeading={unlocked ? 'Summarize changes' : 'Ready to submit?'}
+            submitButtonProps={{ className: styles.submitButton }}
+            onSubmitText={unlocked ? 'Resubmit' : undefined}
+            onSubmit={submitHandler}
+            isSubmitting={formik.isSubmitting || submitMutationLoading}
+        >
+            {unlocked ? (
+                <form>
+                    <p>
+                        Once you submit, this package will be sent to CMS for
+                        review and you will no longer be able to make changes.
+                    </p>
+                    <FormGroup error={Boolean(formik.errors.submittedReason)}>
+                        {formik.errors.submittedReason && (
+                            <PoliteErrorMessage role="alert">
+                                {formik.errors.submittedReason}
+                            </PoliteErrorMessage>
+                        )}
+                        <span id="submittedReason-hint" role="note">
+                            Provide summary of all changes made to this
+                            submission
+                        </span>
+                        <Textarea
+                            id="submittedReasonCharacterCount"
+                            name="submittedReason"
+                            data-testid="submittedReason"
+                            aria-labelledby="submittedReason-hint"
+                            className={styles.submittedReasonTextarea}
+                            aria-required
+                            error={!!formik.errors.submittedReason}
+                            onChange={formik.handleChange}
+                            defaultValue={formik.values.submittedReason}
+                        />
+                    </FormGroup>
+                </form>
+            ) : (
+                <p>
+                    Submitting this package will send it to CMS to begin their
+                    review.
+                </p>
+            )}
+        </Modal>
+    )
+}
