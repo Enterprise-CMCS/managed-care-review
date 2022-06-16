b28277b0d79fab4d201102c519d0398d5900c0a3
Hana Worku <hana@truss.works>
2022-06-16 13:22:22 -0500

    Rename from route change to page view, might be more accurate

--- services/app-web/src/hooks/useOTEL.ts
+++ services/app-web/src/hooks/useOTEL.ts
@@ -5,7 +5,7 @@ import { useAuth } from '../contexts/AuthContext'
 import { useCurrentRoute } from './useCurrentRoute'
 import { getTracer } from '../otelHelpers'
 
-// Adds a new OTEL span for each route change with some custom attributes
+// Adds a new OTEL span for each route change with some custom attributes. Proxy to users time on a given page.
 export const useOTEL = (title?: string): void => {
     const { currentRoute } = useCurrentRoute()
     const { loggedInUser } = useAuth()
@@ -13,9 +13,9 @@ export const useOTEL = (title?: string): void => {
     useEffect(() => {
         const tracer = getTracer()
 
-        const span: Span = tracer.startSpan('React: Route Change')
+        const span: Span = tracer.startSpan('React: Page View')
         span.setAttribute('current_route', currentRoute.toString())
-        console.log('in main effect', currentRoute)
+
         if (loggedInUser) {
             span.setAttribute('user_id', loggedInUser.email)
             span.setAttribute('user_role', loggedInUser.role)
