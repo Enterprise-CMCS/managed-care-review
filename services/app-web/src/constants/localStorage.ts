bb4c38eb93caa871bf116047574efe7106b6fdba
Hana Worku <hana@truss.works>
2022-06-17 14:39:06 -0500

    Only add keys in use currently

--- services/app-web/src/constants/localStorage.ts
+++ services/app-web/src/constants/localStorage.ts
@@ -3,7 +3,7 @@
  * around local storage keys
  */
 
-const LOCAL_STORAGE_KEYS = ['LOGIN_REDIRECT', 'LOGIN_ATTEMPTED'] as const
+const LOCAL_STORAGE_KEYS = ['LOGIN_REDIRECT'] as const
 
 export type LocalStorageKeyType = typeof LOCAL_STORAGE_KEYS[number]
 
@@ -12,8 +12,4 @@ export const LocalStorageKeys: Record<LocalStorageKeyType, string> = {
      * Stores application URL path that requires authentication. Used when a logged out user tries to visit an authenticated path. We will redirect them after a successful login in these cases.
      */
     LOGIN_REDIRECT: 'LOGIN_REDIRECT',
-    /**
-     * Stores stringified boolean to determine if login has already been attempted. Used to display errors to logged out users when login as failed.
-     */
-    LOGIN_ATTEMPTED: 'LOGIN_ATTEMPTED',
 }
