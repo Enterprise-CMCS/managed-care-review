9e071536d13bbeb9d19b82490a9d1167d2f3c4e0
Jason Lin <maolin@truss.works>
2022-07-28 23:48:38 -0400

    Console error for CMS users.

--- services/app-web/src/hooks/useStatePrograms.ts
+++ services/app-web/src/hooks/useStatePrograms.ts
@@ -1,13 +1,17 @@
 import { useAuth } from '../contexts/AuthContext'
 import { Program } from '../gen/gqlClient'
 
-// Get state programs from logged in state user data
+// Get state programs from logged in state users data
 const useStatePrograms = (): Program[] | [] => {
     const { loggedInUser } = useAuth()
     let statePrograms: Program[] = []
 
     if (loggedInUser && loggedInUser.__typename === 'StateUser') {
         statePrograms = loggedInUser.state.programs
+    } else {
+        console.error(
+            `CODING ERROR: useStatePrograms does not currently support CMSUser and will return [].`
+        )
     }
     return statePrograms
 }
