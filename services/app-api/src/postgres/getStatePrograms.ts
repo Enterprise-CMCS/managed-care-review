b326cc41f1f79af60aed80353a7015b4c372d118
Jason Lin <maolin@truss.works>
2022-08-08 18:37:45 -0400

    new store function.

---
+++ services/app-api/src/postgres/getStatePrograms.ts
@@ -0,0 +1,17 @@
+import statePrograms from '../data/statePrograms.json'
+import { ProgramType } from '../domain-models'
+
+function getStatePrograms(stateCode: string): ProgramType[] | Error {
+    const programs = statePrograms.states.find(
+        (state) => state.code === stateCode
+    )?.programs
+
+    if (!programs) {
+        const errMessage = `Can't find programs for state ${stateCode}`
+        return new Error(errMessage)
+    }
+
+    return programs
+}
+
+export { getStatePrograms }
