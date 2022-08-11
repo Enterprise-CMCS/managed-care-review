284091c88ef061f69fc8eaa6dc1b8274cf47bb97
Mazdak Atighi <maz@truss.works>
2022-08-11 16:01:27 -0500

    remove brackets

--- run_build.sh
+++ run_build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo stage name
 echo $REACT_APP_STAGE_NAME
-if [[ $REACT_APP_STAGE_NAME == "prod" || $REACT_APP_STAGE_NAME == "val" ]]; 
+if $REACT_APP_STAGE_NAME == "prod" || $REACT_APP_STAGE_NAME == "val"
     then
         react-scripts build
     else
