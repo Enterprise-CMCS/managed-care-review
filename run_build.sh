181e77f17d23b4ce9e43f05fce9fc5afc923eef5
Mazdak Atighi <maz@truss.works>
2022-08-11 15:43:01 -0500

    dollar sign

--- run_build.sh
+++ run_build.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
-$1 = REACT_APP_STAGE_NAME
 echo stage name
-echo $1
-
-if [[ $1 == "prod" || $1 == "val" ]]; 
+echo $REACT_APP_STAGE_NAME
+if [[ $REACT_APP_STAGE_NAME == "prod" || $REACT_APP_STAGE_NAME == "val" ]]; 
     then
         react-scripts build
     else
         CYPRESS_INSTRUMENT_PRODUCTION=true react-scripts -r @cypress/instrument-cra build
 fi
+
