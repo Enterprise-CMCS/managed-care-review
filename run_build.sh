791e300a7854491bb10547d609dd107040c10f49
Mazdak Atighi <maz@truss.works>
2022-08-11 16:43:07 -0500

    think I got the syntax

--- run_build.sh
+++ run_build.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 echo stage name
 echo $REACT_APP_STAGE_NAME
-if $REACT_APP_STAGE_NAME == "prod" || $REACT_APP_STAGE_NAME == "val"
+if [ "$REACT_APP_STAGE_NAME" == "prod" ] || [ "$REACT_APP_STAGE_NAME" == "val" ];
     then
+        echo "prod or val"
         react-scripts build
     else
+        echo "instrumenting"
         CYPRESS_INSTRUMENT_PRODUCTION=true react-scripts -r @cypress/instrument-cra build
 fi
 
