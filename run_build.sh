dedf05d9c80e83fffc70743b60fc7f4d54304c72
Mazdak Atighi <maz@truss.works>
2022-08-11 18:11:19 -0500

    try running from package.json

--- run_build.sh
+++ run_build.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 echo stage name
+cd services/app-web
+echo $(pwd)
 echo $REACT_APP_STAGE_NAME
 if [ "$REACT_APP_STAGE_NAME" == "prod" ] || [ "$REACT_APP_STAGE_NAME" == "val" ];
     then
         echo "prod or val"
-        react-scripts build
+        yarn run build_prod
     else
         echo "instrumenting"
-        CYPRESS_INSTRUMENT_PRODUCTION=true react-scripts -r @cypress/instrument-cra build
+        yarn run build_instrumented
 fi
 
