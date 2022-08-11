c09d9937017cc6fde1d35aa92611f8a4ecdd7702
Mazdak Atighi <maz@truss.works>
2022-08-11 15:14:38 -0500

    try variable

--- run_build.sh
+++ run_build.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
+$1 = REACT_APP_STAGE_NAME
 echo stage name
-echo $(REACT_APP_STAGE_NAME)
-if [[ $(REACT_APP_STAGE_NAME) == "prod" || $(REACT_APP_STAGE_NAME) == "val" ]]; 
+echo $1
+
+if [[ $1 == "prod" || $1 == "val" ]]; 
     then
         react-scripts build
     else
         CYPRESS_INSTRUMENT_PRODUCTION=true react-scripts -r @cypress/instrument-cra build
 fi
-
