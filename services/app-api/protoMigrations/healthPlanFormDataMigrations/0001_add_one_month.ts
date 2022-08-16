e8a2714d11aa80c70399fab6bb316ac7bee42f21
MacRae Linton <macrael@truss.works>
2022-08-16 02:39:12 -0700

    copy fix

--- services/app-api/protoMigrations/healthPlanFormDataMigrations/0001_add_one_month.ts
+++ services/app-api/protoMigrations/healthPlanFormDataMigrations/0001_add_one_month.ts
@@ -4,7 +4,7 @@ import { mcreviewproto } from '../gen/healthPlanFormDataProto'
  * When we were encoding CalendarDates as js Date objects, we were writing those
  * dates into and out of proto without accounting for the fact that js Date's months
  * are zero indexed. So we were storing months in protos as zero indexed. This migration
- * adds one to the month in every Date in the proto, so we can have 1 indexed months.
+ * adds one to the month in every CalendarDate in the proto, so we can have 1 indexed months.
  */
 
 export function migrateProto(
