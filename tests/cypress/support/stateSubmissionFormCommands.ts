a42b5e4a93923e0c3b6052c3347d8035d05b1974
Jason Lin <maolin@truss.works>
2022-08-18 17:03:43 -0400

    Enable rate program selection commands.

--- tests/cypress/support/stateSubmissionFormCommands.ts
+++ tests/cypress/support/stateSubmissionFormCommands.ts
@@ -198,11 +198,10 @@ Cypress.Commands.add('fillOutNewRateCertification', () => {
     cy.findByLabelText('End date').type('02/28/2025')
     cy.findByLabelText('Date certified').type('03/01/2024')
 
-    // TODO: Disabled until we figure out how we want to implement feature flags for cypress testing
-    // cy.findByRole('combobox', { name: 'programs (required)' }).click({
-    //     force: true,
-    // })
-    // cy.findByText('PMAP').click()
+    cy.findByRole('combobox', { name: 'programs (required)' }).click({
+        force: true,
+    })
+    cy.findByText('PMAP').click()
 
     cy.findByTestId('file-input-input').attachFile(
         'documents/trussel-guide.pdf'
@@ -226,11 +225,10 @@ Cypress.Commands.add('fillOutAmendmentToPriorRateCertification', () => {
     cy.findAllByLabelText('Start date').eq(1).type('03/01/2024')
     cy.findAllByLabelText('End date').eq(1).type('03/01/2025')
 
-    // TODO: Disabled until we figure out how we want to implement feature flags for cypress testing
-    // cy.findByRole('combobox', { name: 'programs (required)' }).click({
-    //     force: true,
-    // })
-    // cy.findByText('PMAP').click()
+    cy.findByRole('combobox', { name: 'programs (required)' }).click({
+        force: true,
+    })
+    cy.findByText('PMAP').click()
 
     cy.findByLabelText('Date certified for rate amendment').type('03/01/2024')
     cy.findByTestId('file-input-input').attachFile(
