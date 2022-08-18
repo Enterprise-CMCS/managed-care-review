91e33722c6f9ece15a1ba5106cbfb1839b44f781
MacRae Linton <macrael@truss.works>
2022-08-18 12:29:45 -0700

    first pass at running migrations in prod

--- services/app-api/src/handlers/postgres_migrate.ts
+++ services/app-api/src/handlers/postgres_migrate.ts
@@ -18,6 +18,7 @@ export const main: APIGatewayProxyHandler = async () => {
 
     const dbConnectionURL: string = dbConnResult
 
+    // first, run prisma db migrations
     try {
         // Aurora can have long cold starts, so we extend connection timeout on migrates
         execSync(
@@ -34,6 +35,33 @@ export const main: APIGatewayProxyHandler = async () => {
             body: JSON.stringify({
                 code: 'MIGRATION_FAILED',
                 message: 'Could not migrate the database ' + err,
+            }),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
+        }
+    }
+
+    // and now, build and run our proto migrations
+    try {
+        console.log('DEBUG WD', process.cwd)
+        // Build the script
+        process.chdir('services/app-api/protoMigrations')
+        execSync(`../node_modules/.bin/tsc`)
+
+        // Run the script
+        execSync(`${process.execPath} build/migrate_protos.js db`, {
+            env: {
+                DATABASE_URL: dbConnectionURL,
+            },
+        })
+    } catch (err) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                code: 'MIGRATION_FAILED',
+                message: 'Could not migrate the protos ' + err,
             }),
             headers: {
                 'Access-Control-Allow-Origin': '*',
