e7abcc65007eb9095f3883575ea3bc39107fb3ef
Jason Lin <98117700+JasonLin0991@users.noreply.github.com>
2022-08-23 11:30:43 -0400

    MR-2532: State user cannot submit unlocked contract only package with rate data (#1134)
    * Wait for fetchHealthPlanPackageQuery.
    
    * Add SES_REVIEW_HELP_EMAIL_ADDRESS offline mode back in.
    
    * Clear rate fields on CONTRACT_ONLY submission
    
    * Remove rate documents only.
    
    * Remove code that clears rate data.
    
    * Add functions to delete all rate data and add tests.
    
    * Delete rate data on CONTRACT_ONLY submissions containing rate data.
    
    * Enable rate program selection commands.
    
    * Fix `removeRateRelatedDocuments()` and update tests.
    
    * Refactor setting rate indexes to undefined when index is -1.
    
    * Disable rate program selection.
    
    * Test for no loss of rate data on CONTRACT_AND_RATES type submission.
    
    * Add cy.wait after cy.visit

--- tests/cypress/integration/stateWorkflow/stateSubmissionForm/contacts.spec.ts
+++ tests/cypress/integration/stateWorkflow/stateSubmissionForm/contacts.spec.ts
@@ -8,7 +8,9 @@ describe('contacts', () => {
             const { pathname } = fullUrl
             const pathnameArray = pathname.split('/')
             const draftSubmissionId = pathnameArray[2]
+
             cy.visit(`/submissions/${draftSubmissionId}/edit/contacts`)
+            cy.wait('@fetchHealthPlanPackageQuery')
 
             // On contacts page, navigate BACK
             cy.navigateForm('BACK')
@@ -16,12 +18,16 @@ describe('contacts', () => {
 
             // On contacts page, SAVE_DRAFT
             cy.visit(`/submissions/${draftSubmissionId}/edit/contacts`)
+            cy.wait('@fetchHealthPlanPackageQuery')
+
             cy.findByRole('heading', { level: 2, name: /Contacts/ })
             cy.navigateForm('SAVE_DRAFT')
             cy.findByRole('heading', { level: 1, name: /Dashboard/ })
 
             // On contacts page, fill out information and CONTINUE
             cy.visit(`/submissions/${draftSubmissionId}/edit/contacts`)
+            cy.wait('@fetchHealthPlanPackageQuery')
+
             cy.fillOutStateContact()
             cy.navigateForm('CONTINUE')
             cy.findByRole('heading', { level: 2, name: /Supporting documents/ })
@@ -46,6 +52,7 @@ describe('contacts', () => {
             const pathnameArray = pathname.split('/')
             const draftSubmissionId = pathnameArray[2]
             cy.visit(`/submissions/${draftSubmissionId}/edit/contacts`)
+            cy.wait('@fetchHealthPlanPackageQuery')
 
             // On contacts page, navigate BACK
             cy.navigateForm('BACK')
@@ -53,12 +60,16 @@ describe('contacts', () => {
 
             // On contacts page, SAVE_DRAFT
             cy.visit(`/submissions/${draftSubmissionId}/edit/contacts`)
+            cy.wait('@fetchHealthPlanPackageQuery')
+
             cy.findByRole('heading', { level: 2, name: /Contacts/ })
             cy.navigateForm('SAVE_DRAFT')
             cy.findByRole('heading', { level: 1, name: /Dashboard/ })
 
             // On contacts page, fill out information and CONTINUE
             cy.visit(`/submissions/${draftSubmissionId}/edit/contacts`)
+            cy.wait('@fetchHealthPlanPackageQuery')
+
             cy.fillOutStateContact()
             cy.fillOutActuaryContact()
             cy.navigateForm('CONTINUE')
