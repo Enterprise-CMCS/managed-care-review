a9a5c6bac1f6217dd0f378acff3a95127583cca7
Mojo Talantikite <mojo.talantikite@gmail.com>
2022-09-02 13:28:37 -0400

    try keeping libquery

--- services/app-api/scripts/prepare-prisma-layer.sh
+++ services/app-api/scripts/prepare-prisma-layer.sh
@@ -51,7 +51,6 @@ function preparePrismaLayer() {
 
     echo "Remove non-RHEL bins to save space ..."
     rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/.prisma/client/libquery_engine-debian-openssl-1.1.x.so.node
-    rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/.prisma/client/libquery_engine-rhel-openssl-1.0.x.so.node 
     rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/prisma/engines
     rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/prisma/libquery_engine-debian-openssl-1.1.x.so.node
     rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/introspection-engine-debian-openssl-1.1.x 
