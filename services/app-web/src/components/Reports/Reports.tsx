4967647c412b40e6db0815da9367ea7cc4357898
Mazdak Atighi <maz@truss.works>
2022-09-07 18:44:32 -0500

    catch error on front end

--- services/app-web/src/components/Reports/Reports.tsx
+++ services/app-web/src/components/Reports/Reports.tsx
@@ -9,30 +9,34 @@ export const Reports = (): React.ReactElement => {
         await API.get('api', '/reports', {
             responseType: 'blob',
             response: true,
-        }).then((response) => {
-            const blob = new Blob([response.data], {
-                type: 'application/octet-stream',
-            })
-            const filename = 'report.csv'
-            const blobURL = window.URL.createObjectURL(blob)
-            const tempLink = document.createElement('a')
-            tempLink.style.display = 'none'
-            tempLink.href = blobURL
-            tempLink.setAttribute('download', filename)
-
-            // Safari thinks _blank anchor are pop ups. We only want to set _blank
-            // target if the browser does not support the HTML5 download attribute.
-            // This allows you to download files in desktop safari if pop up blocking
-            // is enabled.
-            if (typeof tempLink.download === 'undefined') {
-                tempLink.setAttribute('target', '_blank')
-            }
-
-            document.body.appendChild(tempLink)
-            tempLink.click()
-            document.body.removeChild(tempLink)
-            window.URL.revokeObjectURL(blobURL)
         })
+            .then((response) => {
+                const blob = new Blob([response.data], {
+                    type: 'application/octet-stream',
+                })
+                const filename = 'report.csv'
+                const blobURL = window.URL.createObjectURL(blob)
+                const tempLink = document.createElement('a')
+                tempLink.style.display = 'none'
+                tempLink.href = blobURL
+                tempLink.setAttribute('download', filename)
+
+                // Safari thinks _blank anchor are pop ups. We only want to set _blank
+                // target if the browser does not support the HTML5 download attribute.
+                // This allows you to download files in desktop safari if pop up blocking
+                // is enabled.
+                if (typeof tempLink.download === 'undefined') {
+                    tempLink.setAttribute('target', '_blank')
+                }
+
+                document.body.appendChild(tempLink)
+                tempLink.click()
+                document.body.removeChild(tempLink)
+                window.URL.revokeObjectURL(blobURL)
+            })
+            .catch((e) => {
+                console.log('Error downloading report: ', e)
+            })
     }
     return (
         <>
