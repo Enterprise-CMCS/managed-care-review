60c8787e82cf0066fb1e71ae5e54adb6adb4fb5f
Mojo Talantikite <mojo.talantikite@gmail.com>
2022-10-14 16:48:56 -0400

    issue the delete command on old snapshots

--- services/app-api/src/handlers/cleanup.ts
+++ services/app-api/src/handlers/cleanup.ts
@@ -2,19 +2,21 @@ import {
     RDSClient,
     DescribeDBClusterSnapshotsCommandInput,
     DescribeDBClusterSnapshotsCommand,
+    DeleteDBClusterSnapshotCommandInput,
+    DeleteDBClusterSnapshotCommand,
 } from '@aws-sdk/client-rds'
 
 export const main = async () => {
     const client = new RDSClient({ region: 'us-east-1' })
 
     // find all snapshots in our account
-    const input: DescribeDBClusterSnapshotsCommandInput = {}
-    const command = new DescribeDBClusterSnapshotsCommand(input)
-    const response = await client.send(command)
+    const describeInput: DescribeDBClusterSnapshotsCommandInput = {}
+    const describeCommand = new DescribeDBClusterSnapshotsCommand(describeInput)
+    const snapshots = await client.send(describeCommand)
 
     // find snapshots older than 14 days, filter them out
     const timestamp = new Date().getTime() - 14 * 24 * 60 * 60 * 1000 // d * hr * min * s * ms
-    const oldSnapshots = response.DBClusterSnapshots?.filter((snapshot) => {
+    const oldSnapshots = snapshots.DBClusterSnapshots?.filter((snapshot) => {
         if (
             snapshot.SnapshotCreateTime != undefined &&
             timestamp > snapshot.SnapshotCreateTime.getTime()
@@ -23,5 +25,14 @@ export const main = async () => {
         }
     })
 
-    console.log(oldSnapshots)
+    // delete the older snapshots
+    oldSnapshots?.forEach(async (snapshot) => {
+        const deleteInput: DeleteDBClusterSnapshotCommandInput = {
+            DBClusterSnapshotIdentifier: snapshot.DBClusterSnapshotIdentifier,
+        }
+
+        const deleteCommand = new DeleteDBClusterSnapshotCommand(deleteInput)
+        const response = await client.send(deleteCommand)
+        console.log('Deleted old snapshot: ' + response)
+    })
 }
