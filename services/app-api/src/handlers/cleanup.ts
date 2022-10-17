9ad8e4f7a03b3eb4622cbad5d079c91788d2b4e7
Mojo Talantikite <mojo.talantikite@gmail.com>
2022-10-17 15:40:07 -0400

    fix comment

--- services/app-api/src/handlers/cleanup.ts
+++ services/app-api/src/handlers/cleanup.ts
@@ -14,7 +14,7 @@ export const main = async () => {
     const describeCommand = new DescribeDBClusterSnapshotsCommand(describeInput)
     const snapshots = await client.send(describeCommand)
 
-    // find snapshots older than 14 days, filter them out
+    // find snapshots older than 30 days, filter them out
     const timestamp = new Date().getTime() - 30 * 24 * 60 * 60 * 1000 // d * hr * min * s * ms
     const oldSnapshots = snapshots.DBClusterSnapshots?.filter((snapshot) => {
         if (
