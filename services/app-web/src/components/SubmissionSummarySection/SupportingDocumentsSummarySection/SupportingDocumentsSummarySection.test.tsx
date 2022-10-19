81784c686598d38b4683d7d7f1901b9f0df55234
Jason Lin <maolin@truss.works>
2022-10-19 01:10:37 -0400

    Fix errors in passing tests.

--- services/app-web/src/components/SubmissionSummarySection/SupportingDocumentsSummarySection/SupportingDocumentsSummarySection.test.tsx
+++ services/app-web/src/components/SubmissionSummarySection/SupportingDocumentsSummarySection/SupportingDocumentsSummarySection.test.tsx
@@ -2,6 +2,7 @@ import { screen, waitFor } from '@testing-library/react'
 import { renderWithProviders } from '../../../testHelpers/jestHelpers'
 import { SupportingDocumentsSummarySection } from './SupportingDocumentsSummarySection'
 import {
+    fetchCurrentUserMock,
     mockContractAndRatesDraft,
     mockStateSubmission,
 } from '../../../testHelpers/apolloHelpers'
@@ -10,7 +11,7 @@ describe('SupportingDocumentsSummarySection', () => {
     const draftSubmission = mockContractAndRatesDraft()
     const stateSubmission = mockStateSubmission()
 
-    it('can render uncategorized documents in draft submission without errors', async() => {
+    it('can render uncategorized documents in draft submission without errors', async () => {
         const testSubmission = {
             ...draftSubmission,
             documents: [
@@ -36,58 +37,67 @@ describe('SupportingDocumentsSummarySection', () => {
             <SupportingDocumentsSummarySection
                 submission={testSubmission}
                 navigateTo="documents"
-            />
+            />,
+            {
+                apolloProvider: {
+                    mocks: [fetchCurrentUserMock({ statusCode: 200 })],
+                },
+            }
         )
 
-        await waitFor (() => {
+        await waitFor(() => {
             expect(
-            screen.getByRole('heading', {
-                level: 2,
-                name: 'Supporting documents',
-            })
-        ).toBeInTheDocument()
-        expect(
-            screen.getByRole('link', { name: 'Edit Supporting documents' })
-        ).toHaveAttribute('href', '/documents')
-          expect(screen.queryByText('supporting docs test 2')).toBeNull()
-        expect(screen.getByText('supporting docs test 3')).toBeInTheDocument()
+                screen.getByRole('heading', {
+                    level: 2,
+                    name: 'Supporting documents',
+                })
+            ).toBeInTheDocument()
+            expect(
+                screen.getByRole('link', { name: 'Edit Supporting documents' })
+            ).toHaveAttribute('href', '/documents')
+            expect(screen.queryByText('supporting docs test 2')).toBeNull()
+            expect(
+                screen.getByText('supporting docs test 3')
+            ).toBeInTheDocument()
         })
     })
 
     it('can render uncategorized documents state submission without errors', async () => {
-          const testSubmission = {
-              ...stateSubmission,
-              documents: [
-                  {
-                      s3URL: 's3://foo/bar/test-1',
-                      name: 'supporting docs test 1',
-                      documentCategories: [],
-                  },
-                  {
-                      s3URL: 's3://foo/bar/test-2',
-                      name: 'supporting docs test 2',
-                      documentCategories: [],
-                  },
-              ],
-          }
-
+        const testSubmission = {
+            ...stateSubmission,
+            documents: [
+                {
+                    s3URL: 's3://foo/bar/test-1',
+                    name: 'supporting docs test 1',
+                    documentCategories: [],
+                },
+                {
+                    s3URL: 's3://foo/bar/test-2',
+                    name: 'supporting docs test 2',
+                    documentCategories: [],
+                },
+            ],
+        }
 
         renderWithProviders(
             <SupportingDocumentsSummarySection submission={testSubmission} />
         )
-        
-        await waitFor( () => {
- expect(
-     screen.getByRole('heading', {
-         level: 2,
-         name: 'Supporting documents',
-     })
- ).toBeInTheDocument()
 
- expect(screen.queryByText('Edit')).not.toBeInTheDocument()
- expect(screen.getByText('supporting docs test 1')).toBeInTheDocument()
- expect(screen.getByText('supporting docs test 2')).toBeInTheDocument()
+        await waitFor(() => {
+            expect(
+                screen.getByRole('heading', {
+                    level: 2,
+                    name: 'Supporting documents',
+                })
+            ).toBeInTheDocument()
+
+            expect(screen.queryByText('Edit')).not.toBeInTheDocument()
+            expect(
+                screen.getByText('supporting docs test 1')
+            ).toBeInTheDocument()
+            expect(
+                screen.getByText('supporting docs test 2')
+            ).toBeInTheDocument()
         })
-       
     })
 })
