59b11231127ce007bbb98e197b0ede264d14d5ac
Jason Lin <maolin@truss.works>
2022-10-25 17:46:53 -0400

    Pull second actuary and beyond into addtlActuaryContacts, update tests.

--- services/app-web/src/common-code/proto/healthPlanFormDataProto/__tests__/toLatestProtoVersion.test.ts
+++ services/app-web/src/common-code/proto/healthPlanFormDataProto/__tests__/toLatestProtoVersion.test.ts
@@ -3,11 +3,12 @@ import { toLatestProtoVersion } from '../toLatestVersion'
 import { decodeOrError } from '../toDomain'
 
 describe('v2 to v3', () => {
-    it('should convert a single actuary contact and communication preference as expected', () => {
+    it('should convert actuary contacts after the first to addtlActuaryContacts and communication preference as expected', () => {
         const protoBytes = fs.readFileSync(
             'src/common-code/proto/healthPlanFormDataProto/testData/unlockedWithALittleBitOfEverything-2022-08-19.proto'
         )
         const oldProto = decodeOrError(protoBytes)
+
         if (oldProto instanceof Error) {
             throw oldProto
         }
@@ -20,12 +21,96 @@ describe('v2 to v3', () => {
             )
         }
 
+        console.log(oldProto.rateInfos[0].actuaryContacts)
+
         const latestProto = toLatestProtoVersion(oldProto)
         expect(oldProto.rateInfos[0]).toBeDefined()
         expect(latestProto.protoVersion).toBe(3)
         expect(latestProto.addtlActuaryCommunicationPreference).toBe(1)
+        expect(latestProto.rateInfos[0].actuaryContacts).toEqual(
+            expect.arrayContaining([
+                {
+                    contact: {
+                        name: 'foo bar',
+                        titleRole: 'manager',
+                        email: 'soandso@example.com',
+                    },
+                    actuarialFirmType: 7,
+                    actuarialFirmOther: 'ACME',
+                },
+                {
+                    contact: {
+                        name: 'Fine Bab',
+                        titleRole: 'supervisor',
+                        email: 'lodar@example.com',
+                    },
+                    actuarialFirmType: 1,
+                },
+            ])
+        )
         expect(latestProto.addtlActuaryContacts).toEqual(
-            expect.arrayContaining(oldProto.rateInfos[0].actuaryContacts)
+            expect.arrayContaining([
+                expect.objectContaining({
+                    contact: {
+                        name: 'Fine Bab',
+                        titleRole: 'supervisor',
+                        email: 'lodar@example.com',
+                    },
+                    actuarialFirmType: 1,
+                }),
+            ])
+        )
+    })
+
+    it('should return empty array for addtlActuaryContacts when only a single actuary contact exists', () => {
+        const protoBytes = fs.readFileSync(
+            'src/common-code/proto/healthPlanFormDataProto/testData/unlockedWithALittleBitOfEverything-2022-08-19.proto'
+        )
+        const oldProto = decodeOrError(protoBytes)
+
+        if (oldProto instanceof Error) {
+            throw oldProto
+        }
+        if (
+            !oldProto.rateInfos[0].actuaryContacts ||
+            !oldProto.rateInfos[0].actuaryCommunicationPreference
+        ) {
+            throw new Error(
+                'Test invalid. Expecting an old proto with a rate cert actuary information present.'
+            )
+        }
+
+        oldProto.rateInfos[0].actuaryContacts = [
+            {
+                contact: {
+                    name: 'foo bar',
+                    titleRole: 'manager',
+                    email: 'soandso@example.com',
+                },
+                actuarialFirmType: 7,
+                actuarialFirmOther: 'ACME',
+            },
+        ]
+
+        const latestProto = toLatestProtoVersion(oldProto)
+        expect(oldProto.rateInfos[0]).toBeDefined()
+        expect(latestProto.protoVersion).toBe(3)
+        expect(latestProto.addtlActuaryCommunicationPreference).toBe(1)
+        expect(latestProto.rateInfos[0].actuaryContacts).toEqual(
+            expect.arrayContaining([
+                {
+                    contact: {
+                        name: 'foo bar',
+                        titleRole: 'manager',
+                        email: 'soandso@example.com',
+                    },
+                    actuarialFirmType: 7,
+                    actuarialFirmOther: 'ACME',
+                },
+            ])
+        )
+        expect(latestProto.addtlActuaryContacts).toEqual(
+            expect.arrayContaining([])
         )
     })
 })
