d6b5ac9915ed23be15638b8b19fceddf340886bc
Hana Worku <hana@truss.works>
2022-11-01 13:30:31 -0500

    Add a debugger statement too, why not

--- tests/cypress/support/e2e.ts
+++ tests/cypress/support/e2e.ts
@@ -2,5 +2,6 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
     console.error(err)
+     debugger
     return false
 })
