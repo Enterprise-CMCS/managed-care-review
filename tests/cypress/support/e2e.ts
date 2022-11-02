14c90840abbea343974109973ed9b6ba5dc0be24
Hana Worku <hana@truss.works>
2022-11-02 10:51:23 -0500

    replace cypress debugger with console.trace since this is about JS errors not Cypress anyway

--- tests/cypress/support/e2e.ts
+++ tests/cypress/support/e2e.ts
@@ -2,6 +2,6 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
     console.error(err)
-     debugger
+    console.trace()
     return false
 })
