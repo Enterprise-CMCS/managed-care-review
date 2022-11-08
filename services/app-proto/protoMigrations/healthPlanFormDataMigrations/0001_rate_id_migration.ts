85ef037c57716d9332d80fc06b02c2b9aac6075c
Jason Lin <maolin@truss.works>
2022-11-08 10:56:54 -0500

    import uuid a different way

--- services/app-proto/protoMigrations/healthPlanFormDataMigrations/0001_rate_id_migration.ts
+++ services/app-proto/protoMigrations/healthPlanFormDataMigrations/0001_rate_id_migration.ts
@@ -1,5 +1,4 @@
 import { mcreviewproto } from '../../gen/healthPlanFormDataProto'
-import { v4 as uuidv4 } from 'uuid'
 
 /**
  * In preparation for rates across submissions. This migration is to generate an uuid for on existing submissions
@@ -10,13 +9,14 @@ import { v4 as uuidv4 } from 'uuid'
 export function migrateProto(
     oldProto: mcreviewproto.IHealthPlanFormData
 ): mcreviewproto.IHealthPlanFormData {
+    const { v4: uuidv4 } = require('uuid');
     //Only generate rate certification id for submissions with at least 1 rate certification and protoVersion v3 and
     // earlier. Protobuf version 3 is when uuids are being generated for rate certification ids.
-    if (oldProto.rateInfos && oldProto.rateInfos?.length > 0 && oldProto.protoVersion && oldProto.protoVersion <= 3) {
-        oldProto.rateInfos = oldProto.rateInfos.map(rateInfo => rateInfo.id ? rateInfo : (
+    if (oldProto.rateInfos && oldProto.rateInfos?.length) {
+        oldProto.rateInfos = oldProto.rateInfos.map(rateInfo => (
             {
                 ...rateInfo,
-                id: uuidv4()
+                id: rateInfo.id ?? uuidv4()
             }
         ))
     }
