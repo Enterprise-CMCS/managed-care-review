99a2fa47486e1a81f85950b5e7740abf2d4e9f0e
Jason Lin <maolin@truss.works>
2022-11-10 15:42:55 -0500

    Rate programs migration tests.

--- services/app-web/src/common-code/proto/healthPlanFormDataProto/unlockedWithALittleBitOfEverything.test.ts
+++ services/app-web/src/common-code/proto/healthPlanFormDataProto/unlockedWithALittleBitOfEverything.test.ts
@@ -3,14 +3,15 @@ import {
     toDomain,
     toProtoBuffer,
 } from 'app-web/src/common-code/proto/healthPlanFormDataProto'
-import { migrateProto as rateIDMigration } from '../../../../../app-proto/protoMigrations/healthPlanFormDataMigrations/0001_rate_id_migration'
 import { migrateProto as initialMigration } from '../../../../../app-proto/protoMigrations/healthPlanFormDataMigrations/0000_initial_migration'
+import { migrateProto as rateIDMigration } from '../../../../../app-proto/protoMigrations/healthPlanFormDataMigrations/0001_rate_id_migration'
+import { migrateProto as rateProgramsMigration } from '../../../../../app-proto/protoMigrations/healthPlanFormDataMigrations/0002_rate_programs_migration'
 import { mcreviewproto } from '../../../gen/healthPlanFormDataProto'
 import * as genproto from '../../../gen/healthPlanFormDataProto'
 
-function decodeOrError(
+const decodeOrError = (
     buff: Uint8Array
-): mcreviewproto.HealthPlanFormData | Error {
+): mcreviewproto.HealthPlanFormData | Error => {
     try {
         const message = mcreviewproto.HealthPlanFormData.decode(buff)
         return message
@@ -171,5 +172,134 @@ describe('0001_rate_id_migration', () => {
 
         //Rate id should not have changed.
         expect(migratedFormData.rateInfos[0].id).toEqual(rateID)
+    })
+})
+
+describe('0002_rate_programs_migration', () => {
+    it('correctly copies package programs to rate programs for old contract and rates submissions', () => {
+        //Get old proto
+        const oldProtoBytes = fs.readFileSync(
+            'src/common-code/proto/healthPlanFormDataProto/testData/unlockedWithALittleBitOfEverything-2022-08-19.proto'
+        )
+        const oldProto = decodeOrError(oldProtoBytes)
+
+        if (oldProto instanceof Error) {
+            throw oldProto
+        }
+
+        // Turn draft package to submitted package, since we only have unlocked protos to work with.
+        oldProto.status = 'SUBMITTED'
+        // This proto version was also missing contractDocuments, required for a SUBMITTED package.
+        oldProto.contractInfo = {
+            ...oldProto.contractInfo,
+            contractDocuments: [
+                {
+                    s3Url: 's3://bucketname/key/foo.png',
+                    name: 'contract doc',
+                    documentCategories: [1],
+                },
+            ],
+        }
+
+        //We do not have ay old protos from before rate programs was introduced. So we have to manually modify the data
+        // by removing rate programs from rateInfos.
+        oldProto.rateInfos = oldProto.rateInfos.map((rateInfo) => ({
+            ...rateInfo,
+            rateProgramIds: undefined,
+        }))
+
+        // encode modified proto into proto bytes
+        const modifiedOldProtoBytes =
+            genproto.mcreviewproto.HealthPlanFormData.encode(oldProto).finish()
+
+        // Decode modified proto
+        const modifiedOldProto = decodeOrError(modifiedOldProtoBytes)
+
+        if (modifiedOldProto instanceof Error) {
+            throw modifiedOldProto
+        }
+
+        //Run previous migrations then current migration on modified proto
+        const migratedProto = rateProgramsMigration(
+            rateIDMigration(initialMigration(modifiedOldProto))
+        )
+        const migratedProtoBytes =
+            genproto.mcreviewproto.HealthPlanFormData.encode(
+                migratedProto
+            ).finish()
+
+        console.log(decodeOrError(migratedProtoBytes))
+
+        const migratedFormData = toDomain(migratedProtoBytes)
+
+        if (migratedFormData instanceof Error) {
+            throw migratedFormData
+        }
+
+        //Encoding to proto and back to domain should still be symmetric
+        expect(toDomain(toProtoBuffer(migratedFormData))).toEqual(
+            migratedFormData
+        )
+
+        //Rate id assertions
+        expect(migratedFormData.rateInfos[0].rateProgramIDs).toHaveLength(3)
+    })
+
+    it('does not override existing rate ids', () => {
+        //Get proto with rate ids
+        const oldProtoBytes = fs.readFileSync(
+            'src/common-code/proto/healthPlanFormDataProto/testData/unlockedWithALittleBitOfEverything-2022-11-07.proto'
+        )
+
+        const oldProto = decodeOrError(oldProtoBytes)
+
+        if (oldProto instanceof Error) {
+            throw oldProto
+        }
+
+        // Turn draft package to submitted package, since we only have unlocked protos to work with.
+        oldProto.status = 'SUBMITTED'
+
+        // encode modified proto into proto bytes
+        const modifiedOldProtoBytes =
+            genproto.mcreviewproto.HealthPlanFormData.encode(oldProto).finish()
+
+        // Decode modified proto
+        const modifiedOldProto = decodeOrError(modifiedOldProtoBytes)
+
+        if (modifiedOldProto instanceof Error) {
+            throw modifiedOldProto
+        }
+
+        //Run Migration
+        const migratedProto = rateProgramsMigration(
+            rateIDMigration(initialMigration(modifiedOldProto))
+        )
+        const migratedProtoBytes =
+            genproto.mcreviewproto.HealthPlanFormData.encode(
+                migratedProto
+            ).finish()
+
+        const migratedFormData = toDomain(migratedProtoBytes)
+
+        if (migratedFormData instanceof Error) {
+            throw migratedFormData
+        }
+
+        //Get old package data to compare against migrated.
+        const modifiedOldFormData = toDomain(modifiedOldProtoBytes)
+        if (modifiedOldFormData instanceof Error) {
+            throw modifiedOldFormData
+        }
+
+        //Encoding to proto and back to domain should still be symmetric
+        expect(toDomain(toProtoBuffer(migratedFormData))).toEqual(
+            modifiedOldFormData
+        )
+
+        //Rate programs should not have changed
+        expect(migratedFormData.rateInfos[0].rateProgramIDs).toEqual(
+            modifiedOldFormData.rateInfos[0].rateProgramIDs
+        )
     })
 })
