d1eeeefd72948ec9a55c9b2a767064b232748873
Mojo Talantikite <mojo.talantikite@gmail.com>
2022-11-21 17:06:37 -0500

    first pass updateUserAssignedState

---
+++ services/app-api/src/postgres/updateUserAssignedState.ts
@@ -0,0 +1,38 @@
+import {
+    convertPrismaErrorToStoreError,
+    isStoreError,
+    StoreError,
+} from './storeError'
+import { PrismaClient, User, State } from '@prisma/client'
+
+export async function updateUserAssignedState(
+    client: PrismaClient,
+    userID: string,
+    state: State
+): Promise<User | StoreError> {
+    try {
+        const updateResult = client.user.update({
+            where: {
+                id: userID,
+            },
+            data: {
+                states: {
+                    update: {
+                        where: {
+                            stateCode: state.stateCode,
+                        },
+                        data: state,
+                    },
+                },
+            },
+        })
+
+        if (isStoreError(updateResult)) {
+            return updateResult
+        }
+
+        return updateResult
+    } catch (err) {
+        return convertPrismaErrorToStoreError(err)
+    }
+}
