492971cfd20f47e2d0aa91b540efb55077d6c333
Mojo Talantikite <mojo.talantikite@gmail.com>
2022-11-28 16:37:41 -0500

    Save more space if possible

--- services/app-api/scripts/prepare-prisma-layer.sh
+++ services/app-api/scripts/prepare-prisma-layer.sh
@@ -53,6 +53,7 @@ function preparePrismaLayer() {
     echo "Remove non-RHEL bins to save space ..."
     rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/.prisma/client/libquery_engine-debian-openssl-1.1.x.so.node
     rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/prisma/engines
+    rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines
     rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/prisma/libquery_engine-debian-openssl-1.1.x.so.node
     rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/introspection-engine-debian-openssl-1.1.x 
     rm -rf lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/libquery_engine-debian-openssl-1.1.x.so.node 
