5732c09fc7a97a1822c7aab58d215e9dc81f9302
Mazdak Atighi <maz@truss.works>
2022-12-15 21:22:55 -0600

    log docs in ci

--- services/app-web/src/components/SubmissionSummarySection/UploadedDocumentsTable/UploadedDocumentsTable.tsx
+++ services/app-web/src/components/SubmissionSummarySection/UploadedDocumentsTable/UploadedDocumentsTable.tsx
@@ -141,7 +141,8 @@ export const UploadedDocumentsTable = ({
 
         void refreshDocuments()
     }, [documents, getKey, getURL])
-
+    console.log('jj documents', documents)
+    console.log('jj refreshedDocs', refreshedDocs)
     // Empty State
     if (refreshedDocs.length === 0) {
         return (
