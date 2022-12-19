eb125fd43a8046399f778993fec6514da57b6b01
Hana Worku <hana@truss.works>
2022-12-18 20:15:06 -0600

    Clean up error handling in unlock and submit. Add errors constant. Ensure invalid user input errors on submit/resubmit have specific message

--- services/app-web/src/constants/errors.ts
+++ services/app-web/src/constants/errors.ts
@@ -1,4 +1,18 @@
+/*
+    This file contains lang constants related to user facing error messages across the application. 
+    - Use caution editing this file, you could be changing content used across multiple pages
+*/
+const ERROR_MESSAGES = {
+    generic_error: "We're having trouble loading this page.",
+    submit_error_generic: 'Error attempting to submit.',
+    submit_missing_field:
+        'Error attempting to submit. Please fill out required fields, including any new fields.',
+    unlock_error_generic: 'Error attempting to unlock.',
+    unlock_invalid_package_status:
+        'Error attempting to unlock. Submission may be already unlocked.',
+}
+
 const MAIL_TO_SUPPORT =
     'mailto: mc-review@cms.hhs.gov, mc-review-team@truss.works'
 
-export { MAIL_TO_SUPPORT }
+export { MAIL_TO_SUPPORT, ERROR_MESSAGES }
