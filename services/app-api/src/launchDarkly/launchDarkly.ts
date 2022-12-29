271c96e99b49d93ce41a949a5c45735990c49efe
Jason Lin <maolin@truss.works>
2022-12-29 12:11:44 -0500

    LaunchDarkly dependency and test dependency

---
+++ services/app-api/src/launchDarkly/launchDarkly.ts
@@ -0,0 +1,28 @@
+import { UserType } from '../domain-models'
+import {
+    FeatureFlagTypes,
+    FlagValueTypes,
+} from '../../../app-web/src/common-code/featureFlags'
+import { LDClient } from 'launchdarkly-node-server-sdk'
+
+type LDService = {
+    getFeatureFlag: (
+        user: UserType,
+        flag: FeatureFlagTypes
+    ) => Promise<FlagValueTypes>
+}
+
+async function ldService(ldClient: LDClient): Promise<LDService> {
+    return {
+        getFeatureFlag: async (user, flag) => {
+            const context = {
+                kind: 'user',
+                key: user.email,
+            }
+            return await ldClient.variation(flag, context, false)
+        },
+    }
+}
+
+export { ldService }
+export type { LDService }
---
+++ services/app-api/src/testHelpers/launchDarklyHelpers.ts
@@ -0,0 +1,43 @@
+import { LDService } from '../launchDarkly/launchDarkly'
+import {
+    featureFlags,
+    featureFlagEnums,
+    FeatureFlagTypes,
+    FlagValueTypes,
+} from 'app-web/src/common-code/featureFlags'
+
+type FeatureFlagObject = Record<FeatureFlagTypes, FlagValueTypes>
+
+//Set up default feature flag values used to returned data
+const defaultFeatureFlags: FeatureFlagObject = featureFlagEnums.reduce(
+    (a, c) => {
+        const flag = featureFlags[c].flag
+        const defaultValue = featureFlags[c].defaultValue
+        return Object.assign(a, { [flag]: defaultValue })
+    },
+    {} as FeatureFlagObject
+)
+
+function testLDService(
+    mockFeatureFlags?: Partial<FeatureFlagObject>
+): LDService {
+    const featureFlags = defaultFeatureFlags
+
+    //Update featureFlags with mock flag values.
+    if (mockFeatureFlags) {
+        for (const flag in mockFeatureFlags) {
+            const featureFlag = flag as FeatureFlagTypes
+            featureFlags[featureFlag] = mockFeatureFlags[
+                featureFlag
+            ] as FlagValueTypes
+        }
+    }
+
+    return {
+        getFeatureFlag: async (user, flag) => {
+            return featureFlags[flag]
+        },
+    }
+}
+
+export { testLDService }
