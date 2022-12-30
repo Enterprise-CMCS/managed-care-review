54b72b333968c8b48235e76845d9b6aa22f4c717
Jason Lin <maolin@truss.works>
2022-12-30 12:46:49 -0500

    Turn rate-cert-assurance flag on in tests that submit.

--- tests/cypress/integration/stateWorkflow/stateSubmissionForm/reviewAndSubmit.spec.ts
+++ tests/cypress/integration/stateWorkflow/stateSubmissionForm/reviewAndSubmit.spec.ts
@@ -30,6 +30,7 @@ describe('review and submit', () => {
     })
 
     it('can not submit an incomplete submission', () => {
+        cy.interceptFeatureFlags({'rate-cert-assurance': true})
         cy.logInAsStateUser()
         cy.startNewContractAndRatesSubmission()
 
