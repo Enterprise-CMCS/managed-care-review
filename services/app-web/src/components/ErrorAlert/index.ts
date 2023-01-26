5cbdc80b41e04d7f4c2169a6c356dd7aa7589753
MacRae Linton <55759+macrael@users.noreply.github.com>
2023-01-26 11:38:51 -0800

    update maintenance banner (#1467)
    * add scheduled maintenace banner, display landing page when active

--- services/app-web/src/components/ErrorAlert/index.ts
+++ services/app-web/src/components/ErrorAlert/index.ts
@@ -3,5 +3,6 @@ export { ErrorAlertSignIn } from './ErrorAlertSignIn'
 export { ErrorAlertSessionExpired } from './ErrorAlertSessionExpired'
 export { ErrorAlertSiteUnavailable } from './ErrorAlertSiteUnavailable'
 export { ErrorAlertFailedRequest } from './ErrorAlertFailedRequest'
+export { ErrorAlertScheduledMaintenance } from './ErrorAlertScheduledMaintenance'
 
 export type { ErrorAlertProps } from './ErrorAlert'
