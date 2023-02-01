3b424feb698ce0b5966a1a2eb2ece840726dda81
Jason Lin <maolin@truss.works>
2023-02-01 11:24:00 -0500

    UploadQuestions division parameter test.

---
+++ services/app-web/src/pages/QuestionsAnswers/UploadQuestions/UploadQuestions.test.tsx
@@ -0,0 +1,38 @@
+import { screen, waitFor } from '@testing-library/react'
+import { Route, Routes } from 'react-router-dom'
+import { UploadQuestions } from '../../QuestionsAnswers'
+import { renderWithProviders } from '../../../testHelpers'
+import { RoutesRecord } from '../../../constants/routes'
+import React from 'react'
+
+describe('UploadQuestions', () => {
+    it('displays correct cms division', async () => {
+        const division = 'testDivision'
+        renderWithProviders(
+            <Routes>
+                <Route
+                    path={RoutesRecord.SUBMISSIONS_UPLOAD_QUESTION}
+                    element={<UploadQuestions />}
+                />
+            </Routes>,
+            {
+                routerProvider: {
+                    route: `/submissions/15/q&a/${division}/upload-questions`,
+                },
+            }
+        )
+
+        // Expect text to display correct division from url parameters.
+        await waitFor(() => {
+            expect(
+                screen.queryByRole('heading', {
+                    name: /Add questions/,
+                    level: 2,
+                })
+            ).toBeInTheDocument()
+            expect(
+                screen.queryByText(`Questions from ${division.toUpperCase()}`)
+            ).toBeInTheDocument()
+        })
+    })
+})
