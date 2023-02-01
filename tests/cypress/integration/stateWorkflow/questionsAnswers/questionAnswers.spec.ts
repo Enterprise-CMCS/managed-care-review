d1f7d854a712c51e2f5b7c10c692a274cfeb18d1
Jason Lin <maolin@truss.works>
2023-02-01 16:56:47 -0500

    Contract only submission for tests and test for cms-questions flag off.

--- tests/cypress/integration/stateWorkflow/questionsAnswers/questionAnswers.spec.ts
+++ tests/cypress/integration/stateWorkflow/questionsAnswers/questionAnswers.spec.ts
@@ -3,28 +3,17 @@ describe('Q&A', () => {
         cy.stubFeatureFlags()
     })
 
-    it('can navigate to and from Q&A page', () => {
+    it('cannot navigate to and from Q&A page with cms-questions flag off ', () => {
         cy.interceptFeatureFlags({
             'rate-cert-assurance': true,
-            'cms-questions': true
+            'cms-questions': false
         })
         cy.logInAsStateUser()
 
-        // add a draft submission
-        cy.startNewContractAndRatesSubmission()
-        cy.navigateFormByButtonClick('SAVE_DRAFT')
-
         // a submitted submission
-        cy.startNewContractAndRatesSubmission()
+        cy.startNewContractOnlySubmissionWithBaseContract()
 
         cy.fillOutBaseContractDetails()
-        cy.navigateFormByButtonClick('CONTINUE')
-
-        cy.findByRole('heading', {
-            level: 2,
-            name: /Rate details/
-        }).should('exist')
-        cy.fillOutNewRateCertification()
         cy.navigateFormByButtonClick('CONTINUE')
 
         cy.findByRole('heading', {
@@ -32,11 +21,74 @@ describe('Q&A', () => {
             name: /Contacts/,
         }).should('exist')
         cy.fillOutStateContact()
-        cy.fillOutAdditionalActuaryContact()
         cy.navigateFormByButtonClick('CONTINUE')
 
         cy.findByRole('heading', { level: 2, name: /Supporting documents/ })
-        cy.fillOutSupportingDocuments()
+        cy.navigateFormByButtonClick('CONTINUE')
+
+        // Store submission name and url for reference later
+        let submissionId = ''
+        let submissionUrl = ''
+        cy.location().then((fullUrl) => {
+            const { pathname } = fullUrl
+            const pathnameArray = pathname.split('/')
+            submissionId = pathnameArray[2]
+            submissionUrl = fullUrl.toString()
+        })
+        cy.findByRole('heading', { level: 2, name: /Review and submit/ })
+
+        // Submit, sent to dashboard
+        cy.submitStateSubmissionForm()
+        cy.findByText('Dashboard').should('exist')
+        cy.findByText('Programs').should('exist')
+
+        // View submission summary
+        cy.location().then((loc) => {
+            expect(loc.search).to.match(/.*justSubmitted=*/)
+            const submissionName = loc.search.split('=').pop()
+            if (submissionName === undefined) {
+                throw new Error('No submission name found' + loc.search)
+            }
+            cy.findByText(`${submissionName} was sent to CMS`).should('exist')
+            cy.get('table')
+                .findByRole('link', { name: submissionName })
+                .should('exist')
+            cy.findByRole('link', { name: submissionName }).click()
+            cy.url({ timeout: 10_000 }).should('contain', submissionId)
+            cy.findByTestId('submission-summary').should('exist')
+            cy.findByRole('heading', {
+                name: `Minnesota ${submissionName}`,
+            }).should('exist')
+
+            //Try to visit Q&A page
+            cy.visit(`/summary/${submissionId}/q&a`)
+
+            // Look for 404 text on page
+            cy.findByRole('heading', { name: '404 / Page not found', level: 1}).should('exist')
+        })
+    })
+
+    it('can navigate to and from Q&A page', () => {
+        cy.interceptFeatureFlags({
+            'rate-cert-assurance': true,
+            'cms-questions': true
+        })
+        cy.logInAsStateUser()
+
+        // a submitted submission
+        cy.startNewContractOnlySubmissionWithBaseContract()
+
+        cy.fillOutBaseContractDetails()
+        cy.navigateFormByButtonClick('CONTINUE')
+
+        cy.findByRole('heading', {
+            level: 2,
+            name: /Contacts/,
+        }).should('exist')
+        cy.fillOutStateContact()
+        cy.navigateFormByButtonClick('CONTINUE')
+
+        cy.findByRole('heading', { level: 2, name: /Supporting documents/ })
         cy.navigateFormByButtonClick('CONTINUE')
 
         // Store submission name for reference later
