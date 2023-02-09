7a724d86c956643143ad0503c9670551b494100b
MacRae Linton <macrael@truss.works>
2023-02-09 12:15:25 -0800

    TMPCMT

---
+++ services/uploads/src/auditUploads/auditUploads.test.ts
@@ -0,0 +1,13 @@
+describe('auditUploads', () => {
+    it('will audit a bucket', () => {
+
+        // empty the bucket
+
+        // upload a mess of files -- with tags.
+
+        // audit
+
+        throw new Error('NO THANKS')
+
+    })
+})
