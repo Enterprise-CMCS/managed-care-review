0644c9d06d1fb9d56403639de5d691d375793c0b
Mazdak Atighi <maz@truss.works>
2023-02-17 15:45:45 -0600

    add eicar virus and cypress test

---
+++ tests/cypress/integration/stateWorkflow/virusScan/virusScan.spec.ts
@@ -0,0 +1,30 @@
+describe('documents', () => {
+    beforeEach(() => {
+        cy.stubFeatureFlags()
+    })
+    it('will error in CI when a virus is uploaded', () => {
+        cy.logInAsStateUser()
+        cy.startNewContractAndRatesSubmission()
+
+        // Navigate to documents page
+        cy.location().then((fullUrl) => {
+            const { pathname } = fullUrl
+            const draftSubmissionID = pathname.split('/')[2]
+            cy.navigateFormByDirectLink(
+                `/submissions/${draftSubmissionID}/edit/documents`
+            )
+            cy.findByTestId('file-input-input').attachFile(
+                'documents/eicar-adobe-acrobat-attachment.pdf'
+            )
+            if (Cypress.env('AUTH_MODE') === 'LOCAL') {
+                cy.findByText('Failed security scan, please remove').should(
+                    'not.exist'
+                )
+            } else {
+                cy.findByText('Failed security scan, please remove').should(
+                    'exist'
+                )
+            }
+        })
+    })
+})
