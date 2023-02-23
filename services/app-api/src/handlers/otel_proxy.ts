7d7e3d65d01f9d2cbde3833290c1651141f31e23
Mojo Talantikite <mojo.talantikite@gmail.com>
2023-02-23 14:07:03 -0500

    debug a little

--- services/app-api/src/handlers/otel_proxy.ts
+++ services/app-api/src/handlers/otel_proxy.ts
@@ -5,6 +5,7 @@ export const main: APIGatewayProxyHandler = async (event) => {
     const options = {
         headers: { 'content-type': 'application/json' },
     }
+    console.info(`Received payload from app-web: ${JSON.stringify(event.body)}`)
 
     try {
         await axios.post('http://localhost:4318/v1/traces', event.body, options)
