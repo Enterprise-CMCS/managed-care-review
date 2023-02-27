fcb33005bb649f273b70a8d902c0db6bb9176b5c
Jason Lin <maolin@truss.works>
2023-02-27 17:55:15 -0500

    Rename QuestionAnswers to QuestionsResponses

---
+++ services/app-web/src/pages/QuestionsResponses/QATable/QATable.test.tsx
@@ -0,0 +1,142 @@
+import { renderWithProviders } from '../../../testHelpers'
+import { Route, Routes } from 'react-router-dom'
+import { mockValidCMSUser } from '../../../testHelpers/apolloHelpers'
+import { screen, waitFor, within } from '@testing-library/react'
+import React from 'react'
+import { QATable } from './QATable'
+import { mockUser } from 'app-api/src/testHelpers/emailerHelpers'
+import { CmsUser } from '../../../gen/gqlClient'
+
+const stateUser = mockUser()
+const cmsUser = mockValidCMSUser() as CmsUser
+
+const testQuestionData = {
+    id: 'question-1-id',
+    pkgID: '15',
+    createdAt: new Date('2022-12-23T00:00:00.000Z'),
+    addedBy: cmsUser,
+    documents: [
+        {
+            s3URL: 's3://bucketname/key/question-1-document-1',
+            name: 'question-1-document-1',
+            url: 'https://fakes3.com/key?sekret=deadbeef',
+        },
+    ],
+}
+
+it('renders question correctly as a CMS user that created the question', async () => {
+    renderWithProviders(
+        <Routes>
+            <Route
+                path={'/'}
+                element={<QATable question={testQuestionData} user={cmsUser} />}
+            />
+        </Routes>
+    )
+
+    await waitFor(() => {
+        const rows = screen.getAllByRole('row')
+        expect(rows).toHaveLength(2)
+        expect(rows[1]).toHaveTextContent('question-1-document-1')
+        expect(rows[1]).toHaveTextContent('12/23/22')
+        expect(rows[1]).toHaveTextContent('You')
+    })
+})
+it('renders question correctly as a CMS user that did not create the question', async () => {
+    const otherStateUser = {
+        __typename: 'CMSUser' as const,
+        id: 'bar2-id',
+        role: 'CMS_USER',
+        givenName: 'mike',
+        familyName: 'ddmas2',
+        email: 'mike@dmas.mn.gov',
+        stateAssignments: [],
+    }
+    renderWithProviders(
+        <Routes>
+            <Route
+                path={'/'}
+                element={
+                    <QATable
+                        question={testQuestionData}
+                        user={otherStateUser}
+                    />
+                }
+            />
+        </Routes>
+    )
+
+    await waitFor(() => {
+        const rows = screen.getAllByRole('row')
+        expect(rows).toHaveLength(2)
+        expect(rows[1]).toHaveTextContent('question-1-document-1')
+        expect(rows[1]).toHaveTextContent('12/23/22')
+        expect(rows[1]).toHaveTextContent(`${cmsUser.givenName} (CMS)`)
+    })
+})
+it('renders question correctly as a state user', async () => {
+    renderWithProviders(
+        <Routes>
+            <Route
+                path={'/'}
+                element={
+                    <QATable question={testQuestionData} user={stateUser} />
+                }
+            />
+        </Routes>
+    )
+
+    await waitFor(() => {
+        const rows = screen.getAllByRole('row')
+        expect(rows).toHaveLength(2)
+        expect(rows[1]).toHaveTextContent('question-1-document-1')
+        expect(rows[1]).toHaveTextContent('12/23/22')
+        expect(rows[1]).toHaveTextContent(`${cmsUser.givenName} (CMS)`)
+    })
+})
+it('renders multiple documents and links correctly', async () => {
+    const testQuestionDocLinks = {
+        id: 'question-1-id',
+        pkgID: '15',
+        createdAt: new Date('2022-12-23T00:00:00.000Z'),
+        addedBy: cmsUser,
+        documents: [
+            {
+                s3URL: 's3://bucketname/key/question-1-document-1',
+                name: 'question-1-document-1',
+                url: 'https://fakes3.com/key?sekret=deadbeef',
+            },
+            {
+                s3URL: '',
+                name: 'question-1-document-2',
+                url: undefined,
+            },
+        ],
+    }
+    renderWithProviders(
+        <Routes>
+            <Route
+                path={'/'}
+                element={
+                    <QATable question={testQuestionDocLinks} user={cmsUser} />
+                }
+            />
+        </Routes>
+    )
+
+    await waitFor(() => {
+        const rows = screen.getAllByRole('row')
+        expect(rows).toHaveLength(3)
+
+        // expect first document to have a link
+        expect(rows[1]).toHaveTextContent('question-1-document-1')
+        expect(within(rows[1]).getByRole('link')).toHaveAttribute(
+            'href',
+            'https://fakes3.com/key?sekret=deadbeef'
+        )
+
+        // expect second document to not have a link
+        expect(rows[2]).toHaveTextContent('question-1-document-2')
+        expect(within(rows[2]).queryByRole('link')).toBeNull()
+    })
+})
---
+++ services/app-web/src/pages/QuestionsResponses/QATable/QATable.tsx
@@ -0,0 +1,76 @@
+import styles from './QATable.module.scss'
+import React from 'react'
+import { CmsUser, User } from '../../../gen/gqlClient'
+import { Link } from '@trussworks/react-uswds'
+import dayjs from 'dayjs'
+
+export type QuestionDocument = {
+    name: string
+    url?: string
+}
+
+export type QuestionData = {
+    id: string
+    pkgID: string
+    createdAt: Date
+    addedBy: CmsUser
+    documents: QuestionDocument[]
+}
+
+export const QATable = ({
+    question,
+    user,
+}: {
+    question: QuestionData
+    user: User
+}) => {
+    const getAddedByName = (addedBy: User) => {
+        if (user.id === addedBy.id) {
+            return 'You'
+        }
+        if (addedBy.__typename === 'CMSUser') {
+            return `${addedBy.givenName} (CMS)`
+        }
+        if (addedBy.__typename === 'StateUser') {
+            return `${addedBy.givenName} (${addedBy.state})`
+        }
+        return `${addedBy.givenName}`
+    }
+
+    return (
+        <table
+            className={`borderTopLinearGradient ${styles.qaDocumentTable}`}
+            data-testid={`${question.id}-table`}
+        >
+            <thead>
+                <tr>
+                    <th scope="col">Document name</th>
+                    <th scope="col">Date added</th>
+                    <th scope="col">Added by</th>
+                </tr>
+            </thead>
+            <tbody>
+                {question.documents.map((doc, index) => (
+                    <tr key={doc.name + index}>
+                        <td>
+                            {doc.url ? (
+                                <Link
+                                    className={styles.inlineLink}
+                                    aria-label={`${doc.name} (opens in new window)`}
+                                    href={doc.url}
+                                    target="_blank"
+                                >
+                                    {doc.name}
+                                </Link>
+                            ) : (
+                                doc.name
+                            )}
+                        </td>
+                        <td>{dayjs(question.createdAt).format('M/D/YY')}</td>
+                        <td>{getAddedByName(question.addedBy)}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
