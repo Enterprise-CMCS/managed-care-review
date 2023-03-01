6532993c08f246814b8b058fdb8951bbf6b47885
Hana Worku <hana@truss.works>
2023-03-01 10:13:47 -0600

    backend work for createQuestionResponse WIP

---
+++ services/app-api/src/resolvers/questionResponse/createQuestionResponse.ts
@@ -0,0 +1,47 @@
+import { MutationResolvers } from '../../gen/gqlServer'
+import { isStateUser } from '../../domain-models'
+import { logError, logSuccess } from '../../logger'
+import {
+    setErrorAttributesOnActiveSpan,
+    setSuccessAttributesOnActiveSpan,
+} from '../attributeHelper'
+import { ForbiddenError, UserInputError } from 'apollo-server-lambda'
+import { isStoreError, Store } from '../../postgres'
+
+export function createQuestionResponseResolver(
+    store: Store
+): MutationResolvers['createQuestionResponse'] {
+    return async (_parent, { input }, context) => {
+        const { user, span } = context
+
+        if (!isStateUser(user)) {
+            const msg = 'user not authorized to create a question'
+            logError('createQuestionResponse', msg)
+            setErrorAttributesOnActiveSpan(msg, span)
+            throw new ForbiddenError(msg)
+        }
+
+        if (input.documents.length === 0) {
+            const msg = 'question response documents are required'
+            logError('createQuestionResponse', msg)
+            setErrorAttributesOnActiveSpan(msg, span)
+            throw new UserInputError(msg)
+        }
+
+        const responseResult = await store.insertQuestionResponse(input, user)
+
+        if (isStoreError(responseResult)) {
+            const errMessage = `Issue creating question response for question ${input.questionID} of type ${responseResult.code}. Message: ${responseResult.message}`
+            logError('createQuestionResponse', errMessage)
+            setErrorAttributesOnActiveSpan(errMessage, span)
+            throw new Error(errMessage)
+        }
+
+        logSuccess('createQuestionResponse')
+        setSuccessAttributesOnActiveSpan(span)
+
+        return {
+            response: responseResult,
+        }
+    }
+}
---
+++ services/app-api/src/resolvers/questionResponse/index.ts
@@ -0,0 +1,3 @@
+export { indexQuestionsResolver } from './indexQuestions'
+export { createQuestionResolver } from './createQuestion'
+export { createQuestionResponseResolver } from './createQuestionResponse'
