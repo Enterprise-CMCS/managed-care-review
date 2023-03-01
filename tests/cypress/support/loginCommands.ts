38a183d2f929ed281f248256de4a7701d1a4a16b
Jason Lin <maolin@truss.works>
2023-03-01 10:12:38 -0500

    Remove pulling out domain. Wait only for fetch package with questions query

--- tests/cypress/support/loginCommands.ts
+++ tests/cypress/support/loginCommands.ts
@@ -43,9 +43,7 @@ Cypress.Commands.add(
             aliasQuery(req, 'indexHealthPlanPackages')
         })
 
-        const initialURLPath = initialURL?.replace(/^(?:\S+\.\S+?\/|\/)/, '/') || '/'
-
-        cy.visit(initialURLPath)
+        cy.visit(initialURL)
         //Add assertion looking for test on the page before findByRole
         cy.findByText(
             'Medicaid and CHIP Managed Care Reporting and Review System'
@@ -69,24 +67,8 @@ Cypress.Commands.add(
             throw new Error(`Auth mode is not defined or is IDM: ${authMode}`)
         }
         cy.wait('@fetchCurrentUserQuery', { timeout: 20000 })
-        if (initialURLPath === '/') {
-            cy.wait('@indexHealthPlanPackagesQuery', { timeout: 80000 })
-        } else if (initialURLPath) {
-            const pathnameArray = initialURLPath.split('/')
-
-            // Check for submission summary page. Url should split into array of 3 strings. Second should be
-            // 'submissions` and third should be anything expect 'new'.
-            const isSubmissionSummaryPage = pathnameArray.length === 3 && pathnameArray[1] === 'submissions' && pathnameArray[2] !== 'new'
-
-            // Check for QA pages. Url should split into array of at least 4 strings where the 4th string is 'question-and-answers'
-            const isQAPages = pathnameArray.length >= 4 && pathnameArray[3] === 'question-and-answers'
-
-            // QA and Submission Summary page uses a different graphql query
-            if (isSubmissionSummaryPage || isQAPages) {
-                cy.wait('@fetchHealthPlanPackageWithQuestionsQuery', { timeout: 20000 })
-            } else {
-                cy.wait('@fetchHealthPlanPackageQuery', { timeout: 20000 }) // for cases where CMs user goes to specific submission on login
-            }
+        if (initialURL !== '/') {
+            cy.wait('@fetchHealthPlanPackageWithQuestionsQuery', { timeout: 20000 }) // for cases where CMs user goes to specific submission on login
         } else {
             cy.wait('@fetchHealthPlanPackageQuery', { timeout: 20000 })
         }
