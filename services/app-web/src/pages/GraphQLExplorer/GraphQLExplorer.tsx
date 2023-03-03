c6a85bde1ed53f67dbad81423a1a8711d8c52cdf
Jason Lin <maolin@truss.works>
2023-03-02 20:45:33 -0500

    Load schema from gen folder.

--- services/app-web/src/pages/GraphQLExplorer/GraphQLExplorer.tsx
+++ services/app-web/src/pages/GraphQLExplorer/GraphQLExplorer.tsx
@@ -1,125 +1,18 @@
 import { ApolloExplorer } from '@apollo/explorer/react'
 import styles from './GraphQLExplorer.module.scss'
-import { useQuery, gql } from '@apollo/client'
-import { Loading } from '../../components'
 import { useAuth } from '../../contexts/AuthContext'
 import { GenericErrorPage } from '../Errors/GenericErrorPage'
-
-const INTROSPECTION_QUERY = gql`
-    query IntrospectionQuery {
-        __schema {
-            queryType {
-                name
-            }
-            mutationType {
-                name
-            }
-            subscriptionType {
-                name
-            }
-            types {
-                ...FullType
-            }
-            directives {
-                name
-                description
-
-                locations
-                args {
-                    ...InputValue
-                }
-            }
-        }
-    }
-
-    fragment FullType on __Type {
-        kind
-        name
-        description
-
-        fields(includeDeprecated: true) {
-            name
-            description
-            args {
-                ...InputValue
-            }
-            type {
-                ...TypeRef
-            }
-            isDeprecated
-            deprecationReason
-        }
-        inputFields {
-            ...InputValue
-        }
-        interfaces {
-            ...TypeRef
-        }
-        enumValues(includeDeprecated: true) {
-            name
-            description
-            isDeprecated
-            deprecationReason
-        }
-        possibleTypes {
-            ...TypeRef
-        }
-    }
-
-    fragment InputValue on __InputValue {
-        name
-        description
-        type {
-            ...TypeRef
-        }
-        defaultValue
-    }
-
-    fragment TypeRef on __Type {
-        kind
-        name
-        ofType {
-            kind
-            name
-            ofType {
-                kind
-                name
-                ofType {
-                    kind
-                    name
-                    ofType {
-                        kind
-                        name
-                        ofType {
-                            kind
-                            name
-                            ofType {
-                                kind
-                                name
-                                ofType {
-                                    kind
-                                    name
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-`
+import { loader } from 'graphql.macro'
 
 export const GraphQLExplorer = () => {
     const endpointUrl = process.env.REACT_APP_API_URL
     const stageName = process.env.REACT_APP_STAGE_NAME
     const { loggedInUser } = useAuth()
-    const { loading, error, data } = useQuery(INTROSPECTION_QUERY)
+    const gqlSchema = loader('../../gen/schema.graphql')
 
-    if (loading || !data) {
-        return <Loading />
-    }
+    const source = gqlSchema.loc?.source.body
 
-    if (error || !loggedInUser || !endpointUrl) {
+    if (!loggedInUser || !endpointUrl || !source) {
         return <GenericErrorPage />
     }
 
@@ -132,7 +25,7 @@ export const GraphQLExplorer = () => {
             <ApolloExplorer
                 className={styles.explorer}
                 endpointUrl={`${endpointUrl}/graphql`}
-                schema={data}
+                schema={source}
                 initialState={{
                     displayOptions: {
                         docsPanelState: 'open',
