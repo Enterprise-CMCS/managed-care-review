423c5650fac1dbb158c2a6b4f4c94af8f304ccbd
Mazdak Atighi <maz@truss.works>
2023-03-03 14:44:54 -0600

    better test

--- services/app-web/src/pages/Settings/Settings.test.tsx
+++ services/app-web/src/pages/Settings/Settings.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { screen } from '@testing-library/react'
+import { screen, within } from '@testing-library/react'
 import { Settings } from './Settings'
 import { indexUsersQueryMock } from '../../testHelpers/apolloMocks/indexUserQueryMock'
 import { fetchCurrentUserMock } from '../../testHelpers/apolloMocks'
@@ -17,11 +17,28 @@ describe('Settings', () => {
                 ],
             },
         })
-        const familyNameCell = await screen.findByText('Hotman')
-        const givenNameCell = await screen.findByText('Zuko')
-        const emailCell = await screen.findByText('zuko@example.com')
-        expect(familyNameCell).toBeInTheDocument()
-        expect(givenNameCell).toBeInTheDocument()
-        expect(emailCell).toBeInTheDocument()
+        // Find the table by its caption
+        const table = await screen.findByRole('table', { name: 'CMS Users' })
+        expect(table).toBeInTheDocument()
+
+        // Count the table rows
+        const tableRows = await screen.findAllByRole('row')
+        expect(tableRows).toHaveLength(2)
+
+        // Check the table headers
+        expect(
+            screen.getByRole('columnheader', { name: 'Family Name' })
+        ).toBeInTheDocument()
+        expect(
+            screen.getByRole('columnheader', { name: 'Given Name' })
+        ).toBeInTheDocument()
+        expect(
+            screen.getByRole('columnheader', { name: 'Email' })
+        ).toBeInTheDocument()
+
+        // Check the table cells
+        expect(within(table).getByText('Hotman')).toBeInTheDocument()
+        expect(within(table).getByText('Zuko')).toBeInTheDocument()
+        expect(within(table).getByText('zuko@example.com')).toBeInTheDocument()
     })
 })
