5e2e4d02a2dd5d18a1e78c36b642d1f12e323374
Mazdak Atighi <maz@truss.works>
2023-03-06 13:11:17 -0600

    skip test locally

--- tests/cypress/integration/stateWorkflow/virusScan/virusScan.spec.ts
+++ tests/cypress/integration/stateWorkflow/virusScan/virusScan.spec.ts
@@ -2,33 +2,29 @@ describe.only('documents', () => {
     beforeEach(() => {
         cy.stubFeatureFlags()
     })
-    it('will error in CI when a virus is uploaded', () => {
-        cy.logInAsStateUser()
-        cy.startNewContractAndRatesSubmission()
+    if (Cypress.env('AUTH_MODE') !== 'LOCAL') {
+        it('will error in CI when a virus is uploaded', () => {
+            cy.logInAsStateUser()
+            cy.startNewContractAndRatesSubmission()
 
-        // Navigate to documents page
-        cy.location().then((fullUrl) => {
-            const { pathname } = fullUrl
-            const draftSubmissionID = pathname.split('/')[2]
-            cy.navigateFormByDirectLink(
-                `/submissions/${draftSubmissionID}/edit/documents`
-            )
-            cy.findByTestId('file-input-input').attachFile({
-                filePath: 'documents/eicar_com.pdf',
-                encoding: 'binary',
-            })
-            cy.findAllByTestId('upload-finished-indicator', {
-                timeout: 150000,
-            })
-            if (Cypress.env('AUTH_MODE') === 'LOCAL') {
-                cy.findByText('Failed security scan, please remove').should(
-                    'not.exist'
+            // Navigate to documents page
+            cy.location().then((fullUrl) => {
+                const { pathname } = fullUrl
+                const draftSubmissionID = pathname.split('/')[2]
+                cy.navigateFormByDirectLink(
+                    `/submissions/${draftSubmissionID}/edit/documents`
                 )
-            } else {
+                cy.findByTestId('file-input-input').attachFile({
+                    filePath: 'documents/eicar_com.pdf',
+                    encoding: 'binary',
+                })
+                cy.findAllByTestId('upload-finished-indicator', {
+                    timeout: 150000,
+                })
                 cy.findByText('Failed security scan, please remove').should(
                     'exist'
                 )
-            }
+            })
         })
-    })
+    }
 })
