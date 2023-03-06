9ebdd38d34aa8dc42f8a0c7a7146ce987ffa0c0d
MacRae Linton <55759+macrael@users.noreply.github.com>
2023-03-06 10:51:07 -0800

    Revert "Productionize Audit Bucket Function (#1495)" (#1526)
    This reverts commit b44eec0058ca06bd967ac0a59bd6bd0909c13c6f.

---
+++ services/uploads/src/avScan.test.ts
@@ -0,0 +1,17 @@
+describe('avScan', () => {
+    it('can be called', async () => {
+
+        // upload a file to s3
+
+
+        // invoke.
+
+        // check tags
+
+        throw new Error('NOPE')
+    })
+
+    it.todo('marks skipped for too big a file (config a smaller max size)')
+    it.todo('marks infected for an infected file')
+    it.todo('marks error if ClamAV errors')
+})
