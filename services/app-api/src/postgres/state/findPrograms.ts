78b198b446cd0d8d1e89a75ba65be191895f00c0
Hana Worku <hana@truss.works>
2023-03-24 13:24:55 -0500

    Use relative imports in postgres functions to get to common code

--- services/app-api/src/postgres/state/findPrograms.ts
+++ services/app-api/src/postgres/state/findPrograms.ts
@@ -1,5 +1,5 @@
 import { ProgramType } from '../../domain-models'
-import statePrograms from 'app-web/src/common-code/data/statePrograms.json'
+import statePrograms from '../../../../app-web/src/common-code/data/statePrograms.json'
 
 // Currently relies on state programs json; does not query postgres
 function findPrograms(
