8303d3db1f014763cfb453b48ee5bc38dfba5eeb
Jason Lin <maolin@truss.works>
2023-03-27 18:07:34 -0400

    return ses.send.

--- services/app-api/src/emailer/awsSES.ts
+++ services/app-api/src/emailer/awsSES.ts
@@ -68,14 +68,10 @@ class AWSResponseError extends Error {
 async function sendSESEmail(
     params: SendEmailRequest
 ): Promise<SendEmailResponse | SESServiceException> {
-    try {
-        console.info('SendEmailCommand params: ', params)
-        const command = new SendEmailCommand(params)
-        console.info('SendEmailCommand: ', command)
-        return await ses.send(command)
-    } catch (err) {
-        return err
-    }
+    console.info('SendEmailCommand params: ', params)
+    const command = new SendEmailCommand(params)
+    console.info('SendEmailCommand: ', command)
+    return await ses.send(command)
 }
 
 export { getSESEmailParams, sendSESEmail, AWSResponseError }
