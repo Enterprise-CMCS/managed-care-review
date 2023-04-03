0af4f21721e7ef75dbce1772ed420c80b56eda61
Mazdak Atighi <maz@truss.works>
2023-04-03 17:09:11 -0500

    generate migration file

---
+++ services/app-api/prisma/migrations/20230403220812_add_user_audit_table/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "AuditAction" AS ENUM ('CHANGED_STATE_ASSIGNMENT', 'CHANGED_DIVISION_ASSIGNMENT');
+
+-- CreateTable
+CREATE TABLE "UserAudit" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    "updatedByUserId" TEXT NOT NULL,
+    "action" "AuditAction" NOT NULL,
+    "description" TEXT NOT NULL,
+    "priorValue" JSONB,
+
+    CONSTRAINT "UserAudit_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "UserAudit" ADD CONSTRAINT "UserAudit_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserAudit" ADD CONSTRAINT "UserAudit_updatedByUserId_fkey" FOREIGN KEY ("updatedByUserId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
