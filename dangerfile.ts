f8cbe64bff0246a425f6d1d8687cd0793f206f55
Mojo Talantikite <mojo.talantikite@gmail.com>
2023-04-12 18:03:19 -0400

    check first and last line for begin/commit

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,6 @@
 import * as fs from 'fs'
+import * as readline from 'readline'
+import * as stream from 'stream'
 import { danger } from 'danger'
 
 // verifyMigrationTransactions() checks that all migrations are wrapped in a transaction
@@ -12,9 +14,52 @@ const verifyMigrationTransactions = async () => {
     )
 
     for (const file of migrationFiles) {
-        const content = fs.readFileSync(file).toString()
-        console.info(content)
+        const firstLine = await readFirstLine(file)
+        const lastLine = await readLastLine(file)
+
+        if (firstLine !== 'BEGIN;' || lastLine !== 'COMMIT;') {
+            console.error(`Migration ${file} is not wrapped in a transaction`)
+        }
     }
+}
+
+async function readFirstLine(file: string) {
+    const fileStream = fs.createReadStream(file, { encoding: 'utf8' })
+    const reader = readline.createInterface({ input: fileStream })
+
+    const line = await new Promise((resolve) => {
+        reader.on('line', (line) => {
+            reader.close()
+            resolve(line)
+        })
+    })
+    fileStream.close()
+    return line
+}
+
+async function readLastLine(file: string, minLength: number = 1) {
+    let fileStream = fs.createReadStream(file, { encoding: 'utf8' })
+    let outStream = new stream.Writable()
+
+    let rl = readline.createInterface(fileStream, outStream)
+    let lastLine = ''
+
+    const line = await new Promise((resolve) => {
+        rl.on('line', (line) => {
+            if (line.length >= minLength) {
+                lastLine = line
+            }
+        })
+        rl.on('error', function (err) {
+            console.error(err)
+        })
+
+        rl.on('close', function () {
+            resolve(lastLine)
+        })
+    })
+    fileStream.close()
+    return line
 }
 
 ;(async function () {
