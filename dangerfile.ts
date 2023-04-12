2a54e8cb35a4141f0d5ca5fded21f6f969ae61e2
Mojo Talantikite <mojo.talantikite@gmail.com>
2023-04-12 14:11:01 -0400

    don't join if it doesn't exist

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,7 @@
 import { message, danger } from 'danger'
 
-const modifiedMD = danger.git.modified_files.join('- ')
+const modifiedMD = danger.git.modified_files
+if (modifiedMD.length === 0) {
+    message('No files changed in this PR')
+}
 message('Changed Files in this PR: \n - ' + modifiedMD)
