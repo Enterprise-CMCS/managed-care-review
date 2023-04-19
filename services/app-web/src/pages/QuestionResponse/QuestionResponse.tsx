7efe757ab0bb7e95573418bf119a3ce0dd179de5
Jason Lin <maolin@truss.works>
2023-04-19 00:31:02 -0400

    Refactoring and cleanup.

--- services/app-web/src/pages/QuestionResponse/QuestionResponse.tsx
+++ services/app-web/src/pages/QuestionResponse/QuestionResponse.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { GridContainer, Link } from '@trussworks/react-uswds'
 import styles from './QuestionResponse.module.scss'
 
-import { Loading, SectionHeader } from '../../components'
+import { SectionHeader } from '../../components'
 import { NavLink, useLocation, useOutletContext } from 'react-router-dom'
 import { usePage } from '../../contexts/PageContext'
 import { SideNavOutletContextType } from '../SubmissionSideNav/SubmissionSideNav'
@@ -14,19 +14,9 @@ import { QATable, QuestionData, Division } from './QATable/QATable'
 import { CmsUser, QuestionEdge, StateUser } from '../../gen/gqlClient'
 import { CMSUserType } from 'app-api/src/domain-models'
 
-type QADivisionQuestions = {
-    dmco: {
-        totalCount: number
-        questions: QuestionData[]
-    }
-    dmcp: {
-        totalCount: number
-        questions: QuestionData[]
-    }
-    oact: {
-        totalCount: number
-        questions: QuestionData[]
-    }
+type divisionQuestionDataType = {
+    division: Division
+    questions: QuestionData[]
 }
 
 const extractQuestions = (edges?: QuestionEdge[]): QuestionData[] => {
@@ -43,14 +33,23 @@ const extractQuestions = (edges?: QuestionEdge[]): QuestionData[] => {
     }))
 }
 
-const getDivision = (user: CMSUserType): Division | undefined =>
+const getUserDivision = (user: CMSUserType): Division | undefined =>
     user.divisionAssignment
+
+const getDivisionOrder = (division?: Division): Division[] => {
+    const order = ['DMCO', 'DMCP', 'OACT'] as Division[]
+    if (division) {
+        order.splice(order.indexOf(division), 1)
+        order.unshift(division)
+    }
+    return order
+}
 
 export const QuestionResponse = () => {
     // router context
     const location = useLocation()
     const submitType = new URLSearchParams(location.search).get('submit')
-    const { user, packageData, packageName, pkg } =
+    const { user, packageName, pkg } =
         useOutletContext<SideNavOutletContextType>()
     let division: Division | undefined = undefined
 
@@ -62,70 +61,58 @@ export const QuestionResponse = () => {
         updateHeading({ customHeading: packageName })
     }, [packageName, updateHeading])
 
-    if (!packageData || !user) {
-        return (
-            <GridContainer>
-                <Loading />
-            </GridContainer>
-        )
-    }
-
     if (isCMSUser) {
-        division = getDivision(user as CMSUserType)
+        division = getUserDivision(user as CMSUserType)
     }
 
-    const questions: QADivisionQuestions = {
-        dmco: {
-            totalCount: pkg.questions?.DMCOQuestions.totalCount ?? 0,
-            questions: extractQuestions(pkg.questions?.DMCOQuestions.edges),
-        },
-        dmcp: {
-            totalCount: pkg.questions?.DMCPQuestions.totalCount ?? 0,
-            questions: extractQuestions(pkg.questions?.DMCPQuestions.edges),
-        },
-        oact: {
-            totalCount: pkg.questions?.OACTQuestions.totalCount ?? 0,
-            questions: extractQuestions(pkg.questions?.OACTQuestions.edges),
-        },
-    }
+    const divisionOrder = getDivisionOrder(division)
+    const questions: divisionQuestionDataType[] = []
 
-    const mapQuestionTable = (
-        divisionQuestions: QuestionData[],
-        division: Division
-    ) => {
-        return divisionQuestions.length ? (
-            divisionQuestions.map((question, index) => (
-                <QATable
-                    key={question.id}
-                    question={question}
-                    division={division}
-                    round={divisionQuestions.length - index}
-                    user={user}
-                />
-            ))
-        ) : (
-            <div>
-                <p>This division has not submitted questions yet.</p>
-            </div>
-        )
-    }
+    divisionOrder.forEach(
+        (division) =>
+            pkg.questions?.[`${division}Questions`].totalCount &&
+            questions.push({
+                division: division,
+                questions: extractQuestions(
+                    pkg.questions?.[`${division}Questions`].edges
+                ),
+            })
+    )
+
+    const mapQASections = () =>
+        questions.map((divisionQuestions) => (
+            <section
+                key={divisionQuestions.division}
+                className={styles.questionSection}
+                data-testid={`${divisionQuestions.division.toLowerCase()}-qa-section`}
+            >
+                <h4>{`Asked by ${divisionQuestions.division}`}</h4>
+                {divisionQuestions.questions.map((question, index) => (
+                    <QATable
+                        key={question.id}
+                        question={question}
+                        division={divisionQuestions.division}
+                        round={divisionQuestions.questions.length - index}
+                        user={user}
+                    />
+                ))}
+            </section>
+        ))
 
     return (
         <div className={styles.background}>
             <GridContainer className={styles.container}>
-                {!division && (
+                {isCMSUser && !division && (
                     <UserAccountWarningBanner
-                        header={'Incomplete account setup'}
-                        message={
-                            'Your account is not fulling set up. Please contact your administrator to assign a division to your account.'
-                        }
+                        header={'Missing division'}
+                        message={`You must be assigned to a division in order to ask questions about a submission. Contact mc-review@cms.hhs.gov to add your division.`}
                     />
                 )}
                 {submitType && (
                     <QuestionResponseSubmitBanner submitType={submitType} />
                 )}
                 <section>
-                    <SectionHeader header="Q&A" hideBorder>
+                    <SectionHeader header="Contract questions" hideBorder>
                         {isCMSUser && division && (
                             <Link
                                 asCustom={NavLink}
@@ -138,27 +125,15 @@ export const QuestionResponse = () => {
                         )}
                     </SectionHeader>
                 </section>
-                <section
-                    className={styles.questionSection}
-                    data-testid="dmco-qa-section"
-                >
-                    <h3>Questions from DMCO</h3>
-                    {mapQuestionTable(questions.dmco.questions, 'DMCO')}
-                </section>
-                <section
-                    className={styles.questionSection}
-                    data-testid="dmcp-qa-section"
-                >
-                    <h3>Questions from DMCP</h3>
-                    {mapQuestionTable(questions.dmcp.questions, 'DMCP')}
-                </section>
-                <section
-                    className={styles.questionSection}
-                    data-testid="oact-qa-section"
-                >
-                    <h3>Questions from OACT</h3>
-                    {mapQuestionTable(questions.oact.questions, 'OACT')}
-                </section>
+                {questions.length ? (
+                    mapQASections()
+                ) : (
+                    <section key={division} className={styles.questionSection}>
+                        <div>
+                            <p>No questions have been submitted yet</p>
+                        </div>
+                    </section>
+                )}
             </GridContainer>
         </div>
     )
