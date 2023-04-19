82283ac0929e7586117cd01e94a23c8da8baf9a8
Jason Lin <maolin@truss.works>
2023-04-19 17:26:26 -0400

    Squash Question table migration into one file.

--- services/app-api/prisma/migrations/20230411034930_add_optional_division_to_question/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Question" ADD COLUMN     "division" "Division";
--- services/app-api/prisma/migrations/20230419145411_data_migration_question_division/migration.sql
+++
@@ -1,2 +0,0 @@
--- UpdateTable
-UPDATE "Question" SET "division" = COALESCE("User"."divisionAssignment", 'DMCO') FROM "User" WHERE "Question"."addedByUserID" = "User"."id";
