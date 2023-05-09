68b9fe1ecc8d170cff40f2a0af07526e13238ae8
Mojo Talantikite <mojo.talantikite@gmail.com>
2023-05-09 15:57:59 -0400

    return values changed all of a sudden?

--- services/app-api/scripts/invoke-migrate-lambda.sh
+++ services/app-api/scripts/invoke-migrate-lambda.sh
@@ -7,9 +7,9 @@ lambda_version="${2:-\$LATEST}"
 cli_read_timeout=240
 
 if (set -x ; aws lambda invoke --qualifier "$lambda_version" --cli-read-timeout "$cli_read_timeout" --function "$function_name" lambda_response.json) ; then
-  exitCode="$(jq '.statusCode' < lambda_response.json)"
+  exitCode="$(jq '.StatusCode' < lambda_response.json)"
   if [[ "$exitCode" != 200 ]] ; then
-    jq '.errorMessage' < lambda_response.json
+    jq '.ErrorMessage' < lambda_response.json
     echo "Migration of the database failed." 1>&2
     exit 1
   fi
