84e738556fae132dcc8f6b612850382983b9a8cf
Mojo Talantikite <mojo.talantikite@gmail.com>
2023-05-22 14:01:43 -0400

    add docs

--- scripts/get-changed-services/index.ts
+++ scripts/get-changed-services/index.ts
@@ -28,7 +28,6 @@ async function main() {
     // get the latest commit in the branch to see if we are forcing a run
     const latestCommit = await getLatestCommitSHA()
     const commitMessage = await getLatestCommitMessage(latestCommit)
-    console.info(commitMessage)
     if (commitMessage.includes('ci-force-run')) {
         core.setOutput('changed-services', deployAllServices)
         return
