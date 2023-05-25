f3118b7852dd24966530db56096a5baa2bd7c07e
Mojo Talantikite <mojo.talantikite@gmail.com>
2023-05-25 16:39:45 -0400

    fix run script for name

--- run_build.sh
+++ run_build.sh
@@ -4,9 +4,9 @@ echo "$REACT_APP_STAGE_NAME"
 if [ "$REACT_APP_STAGE_NAME" == "prod" ] || [ "$REACT_APP_STAGE_NAME" == "val" ];
     then
         echo "building for prod or val without instrumentation"
-        lerna run build:prod --scope=app-web
+        lerna run build:prod --scope=@managed-care-review/app-web
     else
         echo "instrumenting for cypress"
-        lerna run build:instrumented --scope=app-web
+        lerna run build:instrumented --scope=@managed-care-review/app-web
 fi
 
