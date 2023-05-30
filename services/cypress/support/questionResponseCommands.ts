fac8b7a9ce24d269853c31e89e9e25305801d9ba
Jason Lin <maolin@truss.works>
2023-05-30 11:18:28 -0400

    Add cypress apiCommands and amplify fetch helpers.

--- services/cypress/support/questionResponseCommands.ts
+++ services/cypress/support/questionResponseCommands.ts
@@ -1,48 +1,61 @@
-Cypress.Commands.add('addQuestion', ({
-    documentPath
-}: {
-    documentPath: string
-}) => {
-    // Find Add questions button and click
-    cy.findByRole('link', { name: /Add questions/}).should('exist').click()
-    // Check we are on the Add questions page
-    cy.findByRole('heading', { level: 2, name: /Add questions/}).should('exist')
+import {aliasMutation, aliasQuery} from '../utils/graphql-test-utils';
 
-    // Add document to question form
-    cy.findByTestId('file-input-input').attachFile(
-        documentPath
-    )
-    cy.waitForDocumentsToLoad()
-
-    // Submit question
-    cy.findByRole('button', { name: 'Add questions'}).should('exist').click()
-
-    // Wait for re-fetching of health plan package.
-    cy.wait('@fetchHealthPlanPackageWithQuestionsQuery', { timeout: 20000})
-})
-
-Cypress.Commands.add('addResponse', ({
-    documentPath
-}: {
-    documentPath: string
-}) => {
-    // Find Upload response button in DMCO section and click
-    cy.findByTestId('dmco-qa-section').within(() => {
-        cy.findByRole('link', {name: /Upload response/}).should('exist').click()
+Cypress.Commands.add(
+    'addQuestion',
+    ({ documentPath }: { documentPath: string }) => {
+    cy.intercept('POST', '*/graphql', (req) => {
+            aliasQuery(req, 'fetchHealthPlanPackageWithQuestions')
+            aliasMutation(req, 'createQuestion')
     })
+        // Find Add questions button and click
+        cy.findByRole('link', { name: /Add questions/ })
+            .should('exist')
+            .click()
+        // Check we are on the Add questions page
+        cy.findByRole('heading', { level: 2, name: /Add questions/ }).should(
+            'exist'
+        )
 
-    // Check we are on the Add questions page
-    cy.findByRole('heading', { level: 2, name: /New response/}).should('exist')
+        // Add document to question form
+        cy.findByTestId('file-input-input').attachFile(documentPath)
+        cy.waitForDocumentsToLoad()
 
-    // Add document to question form
-    cy.findByTestId('file-input-input').attachFile(
-        documentPath
-    )
-    cy.waitForDocumentsToLoad()
+        // Submit question
+        cy.findByRole('button', { name: 'Add questions' })
+            .should('exist')
+            .click()
 
-    // Submit question
-    cy.findByRole('button', { name: 'Send response'}).should('exist').click()
+        // Wait for re-fetching of health plan package.
+        cy.wait('@createQuestionMutation', { timeout: 20000 })
+        cy.wait('@fetchHealthPlanPackageWithQuestionsQuery', { timeout: 20000 })
+    }
+)
 
-    // Wait for re-fetching of health plan package.
-    cy.wait('@fetchHealthPlanPackageWithQuestionsQuery', { timeout: 20000})
-})
+Cypress.Commands.add(
+    'addResponse',
+    ({ documentPath }: { documentPath: string }) => {
+        // Find Upload response button in DMCO section and click
+        cy.findByTestId('dmco-qa-section').within(() => {
+            cy.findByRole('link', { name: /Upload response/ })
+                .should('exist')
+                .click()
+        })
+
+        // Check we are on the Add questions page
+        cy.findByRole('heading', { level: 2, name: /New response/ }).should(
+            'exist'
+        )
+
+        // Add document to question form
+        cy.findByTestId('file-input-input').attachFile(documentPath)
+        cy.waitForDocumentsToLoad()
+
+        // Submit question
+        cy.findByRole('button', { name: 'Send response' })
+            .should('exist')
+            .click()
+
+        // Wait for re-fetching of health plan package.
+        cy.wait('@fetchHealthPlanPackageWithQuestionsQuery', { timeout: 20000 })
+    }
+)
