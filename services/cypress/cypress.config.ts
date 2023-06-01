8b34c2ed9ed297b18e768a976e7da0e7ec6b5831
Jason Lin <maolin@truss.works>
2023-05-31 21:41:25 -0400

    Tell github to mask

--- services/cypress/cypress.config.ts
+++ services/cypress/cypress.config.ts
@@ -27,14 +27,13 @@ const defineConfig = {
             console.log(process.env.REACT_APP_ENDPOINT)
             console.log('REACT_APP_API_URL')
             console.log(process.env.REACT_API_URL)
-            console.log('CYPRESS_USER_POOL_ID')
-            console.log(process.env.CYPRESS_USER_POOL_ID)
             console.log('USER_POOL_WEB_CLIENT_ID')
             console.log(process.env.USER_POOL_WEB_CLIENT_ID)
             console.log('-----------')
 
             // Configure env for Amplify authorization
             newConfig.env.API_URL = process.env.REACT_API_URL
+            newConfig.env.USER_POOL_ID = process.env.USER_POOL_ID
             newConfig.env.COGNITO_REGION = process.env.COGNITO_REGION
             newConfig.env.IDENTITY_POOL_ID = process.env.IDENTITY_POOL_ID
             newConfig.env.USER_POOL_WEB_CLIENT_ID = process.env.USER_POOL_WEB_CLIENT_ID
