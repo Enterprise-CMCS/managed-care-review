29e19599d93e7fbf88fd0b04d70766c8811d9ef0
Mazdak Atighi <maz@truss.works>
2023-06-01 10:45:29 -0500

    test invalidation

--- services/app-api/src/postgres/contractAndRates/submitContract.test.ts
+++ services/app-api/src/postgres/contractAndRates/submitContract.test.ts
@@ -2,10 +2,7 @@ import { sharedTestPrismaClient } from '../../testHelpers/storeHelpers'
 import { v4 as uuidv4 } from 'uuid'
 import { submitContract } from './submitContract'
 import { insertDraftContract } from './insertContract'
-
-// async function delay(ms: number) {
-//     return new Promise((resolve) => setTimeout(resolve, ms))
-// }
+import { insertDraftRate } from './insertRate'
 
 // For use in TESTS only. Throws a returned error
 function must<T>(maybeErr: T | Error): T {
@@ -29,16 +26,6 @@ describe('submitContract', () => {
                 stateCode: 'NM',
             },
         })
-
-        // const cmsUser = await client.user.create({
-        //     data: {
-        //         id: uuidv4(),
-        //         givenName: 'Zuko',
-        //         familyName: 'Hotman',
-        //         email: 'zuko@example.com',
-        //         role: 'CMS_USER',
-        //     },
-        // })
 
         // submitting before there's a draft should be an error
         expect(
@@ -70,5 +57,91 @@ describe('submitContract', () => {
                 'initial submit'
             )
         ).toBeInstanceOf(Error)
+    })
+
+    it('invalidates old revisions when new revisions are submitted', async () => {
+        const client = await sharedTestPrismaClient()
+
+        const stateUser = must(
+            await client.user.create({
+                data: {
+                    id: uuidv4(),
+                    givenName: 'Aang',
+                    familyName: 'Avatar',
+                    email: 'aang@example.com',
+                    role: 'STATE_USER',
+                    stateCode: 'NM',
+                },
+            })
+        )
+
+        // create a draft contract
+        const contractA = must(
+            await insertDraftContract(client, 'first contract')
+        )
+
+        // create a draft rate
+        const rateA = must(await insertDraftRate(client, 'first rate'))
+
+        // submit the first draft contract
+        const submittedContractA = must(
+            await submitContract(
+                client,
+                contractA.id,
+                stateUser.id,
+                'initial submit'
+            )
+        )
+        // set up the relation between the submitted contract and the rate
+        await client.rateRevisionsOnContractRevisionsTable.create({
+            data: {
+                contractRevisionID: submittedContractA.revisions[0].id,
+                rateRevisionID: rateA.revisions[0].id,
+                validAfter: new Date(),
+            },
+        })
+
+        // create a second draft contract
+        const contractASecondRevision = must(
+            await client.contractTable.update({
+                where: {
+                    id: contractA.id,
+                },
+                data: {
+                    revisions: {
+                        create: {
+                            id: uuidv4(),
+                            name: 'second contract revision',
+                        },
+                    },
+                },
+                include: {
+                    revisions: true,
+                },
+            })
+        )
+
+        // submit the second draft contract
+        must(
+            await submitContract(
+                client,
+                contractASecondRevision.id,
+                stateUser.id,
+                'second submit'
+            )
+        )
+
+        /* now that the second contract revision has been submitted, the first contract revision should be invalidated.
+        Something is invalidated when it gets a validUntil value, which marks the time it stopped being valid */
+        const invalidatedRevision = must(
+            await client.rateRevisionsOnContractRevisionsTable.findFirst({
+                where: {
+                    contractRevisionID: submittedContractA.revisions[0].id,
+                    validUntil: { not: null },
+                },
+            })
+        )
+
+        expect(invalidatedRevision).not.toBeNull()
     })
 })
