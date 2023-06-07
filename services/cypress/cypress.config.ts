76221a4669bd8518a1a9cbbefd260cb9e2d38f68
Jason Lin <maolin@truss.works>
2023-06-07 14:48:03 -0400

    Try using esbuild for cypress.

--- services/cypress/cypress.config.ts
+++ services/cypress/cypress.config.ts
@@ -3,6 +3,7 @@ const { gql } = require('@apollo/client')
 const { pa11y, prepareAudit } = require('@cypress-audit/pa11y')
 const fs = require('fs')
 const path = require('path')
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
 
 module.exports = defineConfig({
     e2e: {
@@ -15,18 +16,25 @@ module.exports = defineConfig({
         viewportHeight: 1080,
         viewportWidth: 1440,
         setupNodeEvents(on, config) {
+            on('file:preprocessor', createBundler({
+                define: {
+                    global: 'window'
+                }
+            }))
             require('@cypress/code-coverage/task')(on, config)
 
+            const gqlSchema = fs.readFileSync(path.resolve(__dirname, './gen/schema.graphql'), 'utf-8')
             const newConfig = config
+
             newConfig.env.AUTH_MODE = process.env.REACT_APP_AUTH_MODE
             newConfig.env.TEST_USERS_PASS = process.env.TEST_USERS_PASS
 
             // Configure env for Amplify authorization
             newConfig.env.API_URL = process.env.REACT_APP_API_URL
-            newConfig.env.COGNITO_USER_POOL_ID = process.env.COGNITO_USER_POOL_ID
-            newConfig.env.COGNITO_REGION = process.env.COGNITO_REGION
-            newConfig.env.COGNITO_IDENTITY_POOL_ID = process.env.COGNITO_IDENTITY_POOL_ID
-            newConfig.env.COGNITO_USER_POOL_WEB_CLIENT_ID = process.env.COGNITO_USER_POOL_WEB_CLIENT_ID
+            newConfig.env.COGNITO_USER_POOL_ID = process.env.REACT_COGNITO_USER_POOL_ID
+            newConfig.env.COGNITO_REGION = process.env.REACT_COGNITO_REGION
+            newConfig.env.COGNITO_IDENTITY_POOL_ID = process.env.REACT_COGNITO_IDENTITY_POOL_ID
+            newConfig.env.COGNITO_USER_POOL_WEB_CLIENT_ID = process.env.REACT_COGNITO_USER_POOL_WEB_CLIENT_ID
 
             on('before:browser:launch', (browser, launchOptions) => {
                 prepareAudit(launchOptions)
@@ -36,7 +44,6 @@ module.exports = defineConfig({
             on('task', {
                 pa11y: pa11y(),
                 readGraphQLSchema() {
-                    const gqlSchema = fs.readFileSync(path.resolve(__dirname, './gen/schema.graphql'), 'utf-8')
                     return gql(`${gqlSchema}`)
                 }
             })
