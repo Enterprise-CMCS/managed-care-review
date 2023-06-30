c7e55d4f08c376f3d58e603095d233bc1fb99941
Mojo Talantikite <mojo.talantikite@gmail.com>
2023-06-30 11:14:31 -0400

    use esbuild. webpack is totally broken

---
+++ services/app-api/esbuild.config.js
@@ -0,0 +1,33 @@
+const {
+    generateGraphQLString,
+    generateContentsFromGraphqlString,
+} = require('@luckycatfactory/esbuild-graphql-loader');
+
+module.exports = () => {
+    return {
+        packager: 'npm',
+        packagePath: './package.json',
+        bundle: true,
+        minify: true,
+        sourcemap: false,
+        keepNames: true,
+        external: ['aws-sdk', 'prisma', '@prisma/client'],
+        plugins: [
+            {
+                name: 'graphql-loader',
+                setup(build) {
+                    build.onLoad({ filter: /\.graphql$|\.gql$/ }, (args) =>
+                        generateGraphQLString(args.path).then(
+                            (graphqlString) => ({
+                                contents:
+                                    generateContentsFromGraphqlString(
+                                        graphqlString
+                                    ),
+                            })
+                        )
+                    );
+                },
+            },
+        ],
+    };
+};
