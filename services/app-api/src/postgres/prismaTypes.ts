b6ff06db705d09e2ce749fa617f354a9452d1126
Jason Lin <maolin@truss.works>
2023-07-10 12:35:13 -0400

    Add full contract revision type and draft contract prisma query.

--- services/app-api/src/postgres/prismaTypes.ts
+++ services/app-api/src/postgres/prismaTypes.ts
@@ -1,4 +1,17 @@
-import { PrismaClient, UpdateInfoTable, User } from '@prisma/client'
+import {
+    ActuaryContact,
+    ContractDocument,
+    ContractRevisionTable,
+    ContractSupportingDocument,
+    FederalAuthority,
+    ManagedCareEntity,
+    PrismaClient,
+    RateRevisionsOnContractRevisionsTable,
+    RateRevisionTable,
+    StateContact,
+    UpdateInfoTable,
+    User,
+} from '@prisma/client'
 
 // This is the type returned by client.$transaction
 type PrismaTransactionType = Omit<
@@ -13,8 +26,50 @@ const updateInfoIncludeUpdater = {
     },
 }
 
+const draftContractRevisionsWithDraftRates = {
+    stateContacts: true,
+    addtlActuaryContacts: true,
+    contractDocuments: true,
+    supportingDocuments: true,
+    draftRates: {
+        include: {
+            revisions: {
+                include: {
+                    submitInfo: updateInfoIncludeUpdater,
+                    unlockInfo: updateInfoIncludeUpdater,
+                },
+                where: {
+                    submitInfoID: { not: null },
+                },
+                take: 1,
+                orderBy: {
+                    createdAt: 'desc',
+                },
+            },
+        },
+    },
+} as const
+
+type ContractRevisionTableWithRelations = ContractRevisionTable & {
+    submitInfo?: UpdateInfoTableWithUpdater | null
+    unlockInfo?: UpdateInfoTableWithUpdater | null
+    stateContacts: StateContact[]
+    addtlActuaryContacts: ActuaryContact[]
+    contractDocuments: ContractDocument[]
+    supportingDocuments: ContractSupportingDocument[]
+    managedCareEntities: ManagedCareEntity[]
+    federalAuthorities: FederalAuthority[]
+    rateRevisions: (RateRevisionsOnContractRevisionsTable & {
+        rateRevision: RateRevisionTable
+    })[]
+}
+
 type UpdateInfoTableWithUpdater = UpdateInfoTable & { updatedBy: User }
 
-export type { PrismaTransactionType, UpdateInfoTableWithUpdater }
+export type {
+    PrismaTransactionType,
+    UpdateInfoTableWithUpdater,
+    ContractRevisionTableWithRelations,
+}
 
-export { updateInfoIncludeUpdater }
+export { updateInfoIncludeUpdater, draftContractRevisionsWithDraftRates }
