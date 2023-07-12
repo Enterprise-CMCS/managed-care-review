fc514899dc529211513f91f5c94eb2e36b464b7a
Mazdak Atighi <maz@truss.works>
2023-07-12 10:09:25 -0500

    fix key name

--- services/app-api/scripts/invoke-proto_to_db.sh
+++ services/app-api/scripts/invoke-proto_to_db.sh
@@ -7,7 +7,7 @@ lambda_version="${2:-\$LATEST}"
 cli_read_timeout=240
 
 if (set -x ; aws lambda invoke --qualifier "$lambda_version" --cli-read-timeout "$cli_read_timeout" --function "$function_name" lambda_response.json) ; then
-  packageIDExists="$(jq '.pkgID' < lambda_response.json)"
+  packageIDExists="$(jq '.packageId' < lambda_response.json)"
   if [[ "$packageIDExists" == null ]] ; then
     cat lambda_response.json
     echo "Lambda function did not complete successfully." 1>&2
