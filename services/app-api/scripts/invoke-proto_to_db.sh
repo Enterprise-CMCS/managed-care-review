a7f80d7e6738d2b9cad275c86a9e76522c8d09e7
Mazdak Atighi <maz@truss.works>
2023-07-12 11:01:48 -0500

    regular status

--- services/app-api/scripts/invoke-proto_to_db.sh
+++ services/app-api/scripts/invoke-proto_to_db.sh
@@ -7,13 +7,11 @@ lambda_version="${2:-\$LATEST}"
 cli_read_timeout=240
 
 if (set -x ; aws lambda invoke --qualifier "$lambda_version" --cli-read-timeout "$cli_read_timeout" --function "$function_name" lambda_response.json) ; then
-  packageIDExists="$(jq '.packageId' < lambda_response.json)"
-  if [[ "$packageIDExists" == null ]] ; then
+  exitCode="$(jq '.statusCode' < lambda_response.json)"
+  if [[ "$exitCode" != 200 ]] ; then
     cat lambda_response.json
-    echo "Lambda function did not complete successfully." 1>&2
+    echo "Migration of the protos to the database failed." 1>&2
     exit 1
-  else
-    echo "Lambda function executed successfully, package ID: $packageIDExists"
   fi
 else
   exit
