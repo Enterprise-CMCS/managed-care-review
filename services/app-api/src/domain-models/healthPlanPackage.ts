b6e67aa181553a5a59105c54781560ab3f2d933a
Jason Lin <maolin@truss.works>
2023-07-14 13:41:31 -0400

    Create function to convert Contract type to HealthPlanPackageType.

--- services/app-api/src/domain-models/healthPlanPackage.ts
+++ services/app-api/src/domain-models/healthPlanPackage.ts
@@ -4,6 +4,15 @@ import {
     HealthPlanPackageType,
 } from './HealthPlanPackageType'
 import { pruneDuplicateEmails } from '../emailer/formatters'
+import { Contract } from './contractAndRates/contractAndRatesZodSchema'
+import {
+    SubmissionDocument,
+    UnlockedHealthPlanFormDataType,
+} from 'app-web/src/common-code/healthPlanFormDataType'
+import {
+    toProtoBuffer,
+    toDomain,
+} from 'app-web/src/common-code/proto/healthPlanFormDataProto'
 
 // submissionStatus computes the current status of the submission based on
 // the submit/unlock info on its revisions.
@@ -60,9 +69,142 @@ function packageSubmitters(pkg: HealthPlanPackageType): string[] {
     return pruneDuplicateEmails(submitters)
 }
 
+function convertContractToUnlockedHealthPlanPackage(
+    contract: Contract
+): HealthPlanPackageType | Error {
+    console.info('Attempting to convert contract to health plan package')
+
+    try {
+        const healthPlanRevisions =
+            convertContractRevisionToHealthPlanRevision(contract)
+        return {
+            id: contract.id,
+            stateCode: contract.stateCode,
+            revisions: healthPlanRevisions,
+        }
+    } catch (err) {
+        console.warn(
+            `Error: convertContractToUnlockedHealthPlanPackage encountered an error: ${err.message}`
+        )
+        return err
+    }
+}
+
+function convertContractRevisionToHealthPlanRevision(
+    contract: Contract
+): HealthPlanRevisionType[] {
+    if (contract.status !== 'DRAFT') {
+        throw new Error(
+            'Contract status is not "DRAFT". Cannot convert to unlocked health plan package'
+        )
+    }
+
+    const healthPlanRevisions: HealthPlanRevisionType[] = []
+    for (const contractRev of contract.revisions) {
+        const unlockedHealthPlanFormData: UnlockedHealthPlanFormDataType = {
+            id: contractRev.id,
+            createdAt: contractRev.createdAt,
+            updatedAt: contractRev.updatedAt,
+            status: contract.status,
+            stateCode: contract.stateCode,
+            stateNumber: contract.stateNumber,
+            programIDs: contractRev.formData.programIDs,
+            populationCovered: contractRev.formData.populationCovered,
+            submissionType: contractRev.formData.submissionType,
+            riskBasedContract: contractRev.formData.riskBasedContract,
+            submissionDescription: contractRev.formData.submissionDescription,
+            stateContacts: contractRev.formData.stateContacts,
+            addtlActuaryCommunicationPreference: undefined,
+            addtlActuaryContacts: [],
+            documents: contractRev.formData.supportingDocuments.map((doc) => ({
+                ...doc,
+                documentCategories: doc.documentCategories.filter(
+                    (category) => category !== undefined
+                ),
+            })) as SubmissionDocument[],
+            contractType: contractRev.formData.contractType,
+            contractExecutionStatus:
+                contractRev.formData.contractExecutionStatus,
+            contractDocuments: contractRev.formData.contractDocuments.map(
+                (doc) => ({
+                    ...doc,
+                    documentCategories: doc.documentCategories.filter(
+                        (category) => category !== undefined
+                    ),
+                })
+            ) as SubmissionDocument[],
+            contractDateStart: contractRev.formData.contractDateStart,
+            contractDateEnd: contractRev.formData.contractDateEnd,
+            managedCareEntities: contractRev.formData.managedCareEntities,
+            federalAuthorities: contractRev.formData.federalAuthorities,
+            contractAmendmentInfo: {
+                modifiedProvisions: {
+                    inLieuServicesAndSettings:
+                        contractRev.formData.inLieuServicesAndSettings,
+                    modifiedBenefitsProvided:
+                        contractRev.formData.modifiedBenefitsProvided,
+                    modifiedGeoAreaServed:
+                        contractRev.formData.modifiedGeoAreaServed,
+                    modifiedMedicaidBeneficiaries:
+                        contractRev.formData.modifiedMedicaidBeneficiaries,
+                    modifiedRiskSharingStrategy:
+                        contractRev.formData.modifiedRiskSharingStrategy,
+                    modifiedIncentiveArrangements:
+                        contractRev.formData.modifiedIncentiveArrangements,
+                    modifiedWitholdAgreements:
+                        contractRev.formData.modifiedWitholdAgreements,
+                    modifiedStateDirectedPayments:
+                        contractRev.formData.modifiedStateDirectedPayments,
+                    modifiedPassThroughPayments:
+                        contractRev.formData.modifiedPassThroughPayments,
+                    modifiedPaymentsForMentalDiseaseInstitutions:
+                        contractRev.formData
+                            .modifiedPaymentsForMentalDiseaseInstitutions,
+                    modifiedMedicalLossRatioStandards:
+                        contractRev.formData.modifiedMedicalLossRatioStandards,
+                    modifiedOtherFinancialPaymentIncentive:
+                        contractRev.formData
+                            .modifiedOtherFinancialPaymentIncentive,
+                    modifiedEnrollmentProcess:
+                        contractRev.formData.modifiedEnrollmentProcess,
+                    modifiedGrevienceAndAppeal:
+                        contractRev.formData.modifiedGrevienceAndAppeal,
+                    modifiedNetworkAdequacyStandards:
+                        contractRev.formData.modifiedNetworkAdequacyStandards,
+                    modifiedLengthOfContract:
+                        contractRev.formData.modifiedLengthOfContract,
+                    modifiedNonRiskPaymentArrangements:
+                        contractRev.formData.modifiedNonRiskPaymentArrangements,
+                },
+            },
+            rateInfos: [],
+        }
+
+        // check that we can encode then decode with no issues
+        const domainData = toDomain(toProtoBuffer(unlockedHealthPlanFormData))
+
+        if (domainData instanceof Error) {
+            throw domainData
+        }
+
+        const healthPlanRevision: HealthPlanRevisionType = {
+            id: contractRev.id,
+            unlockInfo: contractRev.unlockInfo,
+            submitInfo: contractRev.submitInfo,
+            createdAt: contractRev.createdAt,
+            formDataProto: toProtoBuffer(unlockedHealthPlanFormData),
+        }
+
+        healthPlanRevisions.push(healthPlanRevision)
+    }
+
+    return healthPlanRevisions
+}
+
 export {
     packageCurrentRevision,
     packageStatus,
     packageSubmittedAt,
     packageSubmitters,
+    convertContractToUnlockedHealthPlanPackage,
 }
