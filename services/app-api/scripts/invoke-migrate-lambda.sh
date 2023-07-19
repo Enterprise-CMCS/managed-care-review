24324b0c8ac768712650f596b6ea6c07a72e122c
Mojo Talantikite <mojo.talantikite@gmail.com>
2023-07-19 15:10:01 -0400

    statuscode jq change

--- services/app-api/scripts/invoke-migrate-lambda.sh
+++ services/app-api/scripts/invoke-migrate-lambda.sh
@@ -8,7 +8,7 @@ error_message="${3:-}"
 cli_read_timeout=240
 
 if (set -x ; aws lambda invoke --qualifier "$lambda_version" --cli-read-timeout "$cli_read_timeout" --function "$function_name" lambda_response.json) ; then
-  exitCode="$(jq '.statusCode' < lambda_response.json)"
+  exitCode="$(jq '.StatusCode' < lambda_response.json)"
   if [[ "$exitCode" != 200 ]] ; then
     cat lambda_response.json
     echo "$error_message" 1>&2
