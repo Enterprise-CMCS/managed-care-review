a6bf5f9a7e86adee7afdb8170f29548464f1bf3b
Mojo Talantikite <mojo.talantikite@gmail.com>
2023-07-21 16:48:24 -0400

    init scripts tied into CF

---
+++ services/postgres/scripts/vm-shutdown.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+webhook_url=$SLACK_WEBHOOK
+msg_content="The ec2 instance to access postgres is being shut down in ${stage}."
+curl -X POST -H 'Content-type: application/json' --data "{'text':'${msg_content}'}" "${webhook_url}"
