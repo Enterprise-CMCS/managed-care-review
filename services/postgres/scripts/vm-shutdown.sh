71b7e6f862dcfae8b11f5db0814e061296c9055a
Mojo Talantikite <mojo.talantikite@gmail.com>
2023-08-01 16:06:20 -0400

    modify the scripts

--- services/postgres/scripts/vm-shutdown.sh
+++ services/postgres/scripts/vm-shutdown.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-webhook_url=$SLACK_WEBHOOK
-msg_content="The ec2 instance to access postgres is being shut down in ${stage}."
+webhook_url="SLACK_WEBHOOK"
+msg_content="The ec2 instance to access postgres is being shut down in STAGE."
 curl -X POST -H 'Content-type: application/json' --data "{'text':'${msg_content}'}" "${webhook_url}"
