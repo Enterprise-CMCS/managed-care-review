e1bb21844f4a930ca70543b884d2660e0827dd4a
Hana Worku <hana@truss.works>
2023-08-23 16:29:11 -0500

    updateDraftContract handler is passing tests

--- services/app-api/src/postgres/contractAndRates/updateDraftContract.ts
+++ services/app-api/src/postgres/contractAndRates/updateDraftContract.ts
@@ -4,9 +4,11 @@ import type { ContractType } from '../../domain-models/contractAndRates'
 import type { PrismaClient } from '@prisma/client'
 import type { ContractFormDataType } from '../../domain-models/contractAndRates'
 
+type ContractFormEditable = Partial<ContractFormDataType>
+
 type UpdateContractArgsType = {
     contractID: string
-    formData: Partial<ContractFormDataType>
+    formData: ContractFormEditable
     rateIDs: string[]
 }
 // Update the given draft
@@ -52,7 +54,7 @@ async function updateDraftContract(
     } = formData
 
     try {
-        // Given all the Rates associated with this draft, find the most recent submitted
+        // Given all the Contracts associated with this draft, find the most recent submitted
         // rateRevision to update.
         const currentRev = await client.contractRevisionTable.findFirst({
             where: {
@@ -66,62 +68,82 @@ async function updateDraftContract(
             return new NotFoundError(err)
         }
 
-        await client.contractRevisionTable.update({
-            where: {
-                id: currentRev.id,
-            },
-            data: {
-                populationCovered: populationCovered,
-                programIDs: programIDs,
-                riskBasedContract: riskBasedContract,
-                submissionType: submissionType,
-                submissionDescription: submissionDescription,
-                contractType: contractType,
-                contractExecutionStatus,
-                contractDocuments: {
-                    create: contractDocuments,
+        await client.$transaction([
+            //  Clear all related resources on the revision
+            client.contractRevisionTable.update({
+                where: {
+                    id: currentRev.id,
                 },
-                supportingDocuments: {
-                    create: supportingDocuments,
+                data: {
+                    contractDocuments: {
+                        deleteMany: {},
+                    },
+                    supportingDocuments: {
+                        deleteMany: {},
+                    },
+                    stateContacts: {
+                        deleteMany: {},
+                    },
                 },
-                stateContacts: {
-                    create: stateContacts,
+            }),
+            // Then update resource, adjusting all simple fields and creating new linked resources for fields holding relationships to other day,
+            client.contractRevisionTable.update({
+                where: {
+                    id: currentRev.id,
                 },
-                contractDateStart,
-                contractDateEnd,
-                managedCareEntities,
-                federalAuthorities,
-                modifiedBenefitsProvided,
-                modifiedGeoAreaServed,
-                modifiedMedicaidBeneficiaries,
-                modifiedRiskSharingStrategy,
-                modifiedIncentiveArrangements,
-                modifiedWitholdAgreements,
-                modifiedStateDirectedPayments,
-                modifiedPassThroughPayments,
-                modifiedPaymentsForMentalDiseaseInstitutions,
-                modifiedMedicalLossRatioStandards,
-                modifiedOtherFinancialPaymentIncentive,
-                modifiedEnrollmentProcess,
-                modifiedGrevienceAndAppeal,
-                modifiedNetworkAdequacyStandards,
-                modifiedLengthOfContract,
-                modifiedNonRiskPaymentArrangements,
-                inLieuServicesAndSettings,
-                draftRates: {
-                    set: rateIDs.map((rID) => ({
-                        id: rID,
-                    })),
+                data: {
+                    populationCovered: populationCovered,
+                    programIDs: programIDs,
+                    riskBasedContract: riskBasedContract,
+                    submissionType: submissionType,
+                    submissionDescription: submissionDescription,
+                    contractType: contractType,
+                    contractExecutionStatus,
+                    contractDocuments: {
+                        create: contractDocuments,
+                    },
+                    supportingDocuments: {
+                        create: supportingDocuments,
+                    },
+                    stateContacts: {
+                        create: stateContacts,
+                    },
+                    contractDateStart,
+                    contractDateEnd,
+                    managedCareEntities,
+                    federalAuthorities,
+                    modifiedBenefitsProvided,
+                    modifiedGeoAreaServed,
+                    modifiedMedicaidBeneficiaries,
+                    modifiedRiskSharingStrategy,
+                    modifiedIncentiveArrangements,
+                    modifiedWitholdAgreements,
+                    modifiedStateDirectedPayments,
+                    modifiedPassThroughPayments,
+                    modifiedPaymentsForMentalDiseaseInstitutions,
+                    modifiedMedicalLossRatioStandards,
+                    modifiedOtherFinancialPaymentIncentive,
+                    modifiedEnrollmentProcess,
+                    modifiedGrevienceAndAppeal,
+                    modifiedNetworkAdequacyStandards,
+                    modifiedLengthOfContract,
+                    modifiedNonRiskPaymentArrangements,
+                    inLieuServicesAndSettings,
+                    draftRates: {
+                        set: rateIDs.map((rID) => ({
+                            id: rID,
+                        })),
+                    },
                 },
-            },
-        })
+            }),
+        ])
 
         return findContractWithHistory(client, contractID)
     } catch (err) {
-        console.error('UPDATE PRISMA CONTRACT ERR', err)
+        console.error('Prisma error updating contract', err)
         return err
     }
 }
 
 export { updateDraftContract }
-export type { UpdateContractArgsType }
+export type { UpdateContractArgsType, ContractFormEditable }
