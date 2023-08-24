d7c3b36e9bdfdc1caf5bc97e7026c029cc6581bb
Jason Lin <maolin@truss.works>
2023-08-24 15:55:19 -0400

    Add find contracts functions to index resolver.

--- services/app-api/src/resolvers/healthPlanPackage/indexHealthPlanPackages.ts
+++ services/app-api/src/resolvers/healthPlanPackage/indexHealthPlanPackages.ts
@@ -1,18 +1,26 @@
 import type { Span } from '@opentelemetry/api'
 import { ForbiddenError } from 'apollo-server-lambda'
 import type { HealthPlanPackageType } from '../../domain-models'
-import { isStateUser, isCMSUser, isAdminUser } from '../../domain-models'
+import {
+    isStateUser,
+    isCMSUser,
+    isAdminUser,
+    convertContractToUnlockedHealthPlanPackage,
+} from '../../domain-models'
 import { isHelpdeskUser } from '../../domain-models/user'
 import type { QueryResolvers } from '../../gen/gqlServer'
 import { logError, logSuccess } from '../../logger'
 import type { Store, StoreError } from '../../postgres'
-import { isStoreError } from '../../postgres'
+import { isStoreError, NotFoundError } from '../../postgres'
 import {
     setErrorAttributesOnActiveSpan,
     setResolverDetailsOnActiveSpan,
     setSuccessAttributesOnActiveSpan,
 } from '../attributeHelper'
 import type { LDService } from '../../launchDarkly/launchDarkly'
+import type { ContractOrErrorArrayType } from '../../postgres/contractAndRates'
+import type { ContractType } from '../../domain-models/contractAndRates'
+import { GraphQLError } from 'graphql/index'
 
 const validateAndReturnHealthPlanPackages = (
     results: HealthPlanPackageType[] | StoreError,
@@ -40,6 +48,56 @@ const validateAndReturnHealthPlanPackages = (
     return { totalCount: edges.length, edges }
 }
 
+const validateContractsAndConvert = (
+    parsedContractsOrErrors: ContractOrErrorArrayType,
+    span?: Span
+): HealthPlanPackageType[] => {
+    // separate valid contracts and errors
+    const parsedContracts: ContractType[] = []
+    const errorParseContracts: string[] = []
+    parsedContractsOrErrors.forEach((parsed) => {
+        if (parsed.contract instanceof Error) {
+            errorParseContracts.push(parsed.contractID)
+        } else {
+            parsedContracts.push(parsed.contract)
+        }
+    })
+
+    // log all contracts that failed parsing to otel.
+    if (errorParseContracts.length > 0) {
+        const errMessage = `Failed to parse the following contracts:\n${errorParseContracts.join(
+            '\n'
+        )}`
+        logError('indexHealthPlanPackagesResolver', errMessage)
+        setErrorAttributesOnActiveSpan(errMessage, span)
+    }
+
+    // convert contract type to health plan package type
+    const convertedContracts: HealthPlanPackageType[] = []
+    const errorConvertContracts: string[] = []
+    // convert all valid contracts to HPP type
+    parsedContracts.forEach((contract) => {
+        const parsedContract =
+            convertContractToUnlockedHealthPlanPackage(contract)
+        if (parsedContract instanceof Error) {
+            errorConvertContracts.push(contract.id)
+        } else {
+            convertedContracts.push(parsedContract)
+        }
+    })
+
+    // log all contracts that failed converting
+    if (errorConvertContracts.length > 0) {
+        const errMessage = `Failed to covert the following contracts to health plan packages:\n${errorConvertContracts.join(
+            '\n'
+        )}`
+        logError('indexHealthPlanPackagesResolver', errMessage)
+        setErrorAttributesOnActiveSpan(errMessage, span)
+    }
+
+    return convertedContracts
+}
+
 export function indexHealthPlanPackagesResolver(
     store: Store,
     launchDarkly: LDService
@@ -48,10 +106,48 @@ export function indexHealthPlanPackagesResolver(
         const { user, span } = context
         setResolverDetailsOnActiveSpan('fetchHealthPlanPackage', user, span)
 
+        const ratesDatabaseRefactor = await launchDarkly.getFeatureFlag(
+            context,
+            'rates-db-refactor'
+        )
+
         if (isStateUser(user)) {
-            const results = await store.findAllHealthPlanPackagesByState(
-                user.stateCode
-            )
+            let results: StoreError | HealthPlanPackageType[] = []
+            if (ratesDatabaseRefactor) {
+                const contractsWithHistory =
+                    await store.findAllContractsWithHistoryByState(
+                        user.stateCode
+                    )
+
+                if (contractsWithHistory instanceof Error) {
+                    const errMessage = `Issue finding a contract with history by stateCode: ${user.stateCode}. Message: ${contractsWithHistory.message}`
+                    logError('fetchHealthPlanPackage', errMessage)
+                    setErrorAttributesOnActiveSpan(errMessage, span)
+
+                    if (contractsWithHistory instanceof NotFoundError) {
+                        throw new GraphQLError(errMessage, {
+                            extensions: {
+                                code: 'NOT_FOUND',
+                                cause: 'DB_ERROR',
+                            },
+                        })
+                    }
+
+                    throw new GraphQLError(errMessage, {
+                        extensions: {
+                            code: 'INTERNAL_SERVER_ERROR',
+                            cause: 'DB_ERROR',
+                        },
+                    })
+                }
+
+                results = validateContractsAndConvert(contractsWithHistory)
+            } else {
+                results = await store.findAllHealthPlanPackagesByState(
+                    user.stateCode
+                )
+            }
+
             return validateAndReturnHealthPlanPackages(results, span)
         } else if (
             isCMSUser(user) ||
