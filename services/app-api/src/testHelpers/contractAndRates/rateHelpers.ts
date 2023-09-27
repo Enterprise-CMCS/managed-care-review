abdb8464f045eb5d6f78952119cd57c17a8150de
Jason Lin <maolin@truss.works>
2023-09-27 12:53:50 -0400

    Replace using getProgramsFromState with findStatePrograms and throwing errors in tests if programs are not found.

--- services/app-api/src/testHelpers/contractAndRates/rateHelpers.ts
+++ services/app-api/src/testHelpers/contractAndRates/rateHelpers.ts
@@ -4,8 +4,9 @@ import type {
     RateTableFullPayload,
     RateRevisionTableWithContracts,
 } from '../../postgres/contractAndRates/prismaSubmittedRateHelpers'
-import { getProgramsFromState } from '../stateHelpers'
-import type { StateCodeType } from 'app-web/src/common-code/healthPlanFormDataType'
+import type { StateCodeType } from '../../../../app-web/src/common-code/healthPlanFormDataType'
+import { findStatePrograms } from '../../postgres'
+import { must } from '../errorHelpers'
 
 const createInsertRateData = (
     rateArgs?: Partial<InsertRateArgsType>
@@ -58,79 +59,83 @@ const createRateData = (
 const createRateRevision = (
     revision?: Partial<RateRevisionTableWithContracts>,
     stateCode: StateCodeType = 'MN'
-): RateRevisionTableWithContracts => ({
-    id: uuidv4(),
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    submitInfo: {
+): RateRevisionTableWithContracts => {
+    const statePrograms = must(findStatePrograms(stateCode))
+
+    return {
         id: uuidv4(),
+        createdAt: new Date(),
         updatedAt: new Date(),
-        updatedByID: 'someone',
-        updatedReason: 'submit',
-        updatedBy: {
-            id: 'someone',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            givenName: 'Bob',
-            familyName: 'Law',
-            email: 'boblaw@example.com',
-            role: 'STATE_USER',
-            divisionAssignment: null,
-            stateCode: stateCode,
-        },
-    },
-    unlockInfo: null,
-    submitInfoID: null,
-    unlockInfoID: null,
-    rateType: 'NEW',
-    rateID: 'rateID',
-    rateCertificationName: 'testState-123',
-    rateProgramIDs: [getProgramsFromState(stateCode)[0].id],
-    rateCapitationType: 'RATE_CELL',
-    rateDateStart: new Date(),
-    rateDateEnd: new Date(),
-    rateDateCertified: new Date(),
-    amendmentEffectiveDateEnd: new Date(),
-    amendmentEffectiveDateStart: new Date(),
-    actuaryCommunicationPreference: 'OACT_TO_ACTUARY',
-    certifyingActuaryContacts: [],
-    addtlActuaryContacts: [],
-    supportingDocuments: [
-        {
+        submitInfo: {
             id: uuidv4(),
-            rateRevisionID: 'rateRevisionID',
-            createdAt: new Date(),
             updatedAt: new Date(),
-            name: 'rate supporting doc',
-            s3URL: 'fakeS3URL',
-            sha256: '2342fwlkdmwvw',
+            updatedByID: 'someone',
+            updatedReason: 'submit',
+            updatedBy: {
+                id: 'someone',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                givenName: 'Bob',
+                familyName: 'Law',
+                email: 'boblaw@example.com',
+                role: 'STATE_USER',
+                divisionAssignment: null,
+                stateCode: stateCode,
+            },
         },
-        {
-            id: uuidv4(),
-            rateRevisionID: 'rateRevisionID',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            name: 'rate supporting doc 2',
-            s3URL: 'fakeS3URL',
-            sha256: '45662342fwlkdmwvw',
-        },
-    ],
-    rateDocuments: [
-        {
-            id: uuidv4(),
-            rateRevisionID: 'rateRevisionID',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            name: 'contract doc',
-            s3URL: 'fakeS3URL',
-            sha256: '8984234fwlkdmwvw',
-        },
-    ],
-    contractRevisions: [],
-    draftContracts: [],
-    contractsWithSharedRateRevision: [],
-    ...revision,
-})
+        unlockInfo: null,
+        submitInfoID: null,
+        unlockInfoID: null,
+        rateType: 'NEW',
+        rateID: 'rateID',
+        rateCertificationName: 'testState-123',
+        rateProgramIDs: [statePrograms[0].id],
+        rateCapitationType: 'RATE_CELL',
+        rateDateStart: new Date(),
+        rateDateEnd: new Date(),
+        rateDateCertified: new Date(),
+        amendmentEffectiveDateEnd: new Date(),
+        amendmentEffectiveDateStart: new Date(),
+        actuaryCommunicationPreference: 'OACT_TO_ACTUARY',
+        certifyingActuaryContacts: [],
+        addtlActuaryContacts: [],
+        supportingDocuments: [
+            {
+                id: uuidv4(),
+                rateRevisionID: 'rateRevisionID',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                name: 'rate supporting doc',
+                s3URL: 'fakeS3URL',
+                sha256: '2342fwlkdmwvw',
+            },
+            {
+                id: uuidv4(),
+                rateRevisionID: 'rateRevisionID',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                name: 'rate supporting doc 2',
+                s3URL: 'fakeS3URL',
+                sha256: '45662342fwlkdmwvw',
+            },
+        ],
+        rateDocuments: [
+            {
+                id: uuidv4(),
+                rateRevisionID: 'rateRevisionID',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                name: 'contract doc',
+                s3URL: 'fakeS3URL',
+                sha256: '8984234fwlkdmwvw',
+            },
+        ],
+        contractRevisions: [],
+        draftContracts: [],
+        contractsWithSharedRateRevision: [],
+        ...revision,
+    }
+}
 
 export {
     createInsertRateData,
