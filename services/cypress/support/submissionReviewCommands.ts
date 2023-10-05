0ad0e6c96b09c248df0b355fdd030d29171f0405
Jason Lin <98117700+JasonLin0991@users.noreply.github.com>
2023-10-05 15:27:24 -0400

    MR-2369: turn on rates db refactor flag for cypress (#1957)
    * Enable `rates-db-refactor` and `supporting-docs-by-rate` flag for cypress tests.
    
    * Skip Q&A tests, resolver not modified for refactor yet.
    
    * cypress re-run
    
    * Update virusScan test.

---
+++ services/cypress/support/submissionReviewCommands.ts
@@ -0,0 +1,17 @@
+Cypress.Commands.add('unlockSubmission', (unlockReason) => {
+    // click on the unlock button, type in reason and confirm
+    cy.findByRole('button', { name: 'Unlock submission', timeout: 5_000 }).click()
+    cy.findAllByTestId('modalWindow').eq(1).should('be.visible')
+    cy.get('#unlockSubmitModalInput').type(
+        unlockReason || 'Unlock submission reason.'
+    )
+    cy.findByRole('button', { name: 'Unlock' }).click()
+
+    cy.findByRole('button', { name: 'Unlock submission'}).should(
+        'be.disabled', {timeout: 50_000 }
+    )
+    cy.findAllByTestId('modalWindow').eq(1).should('be.hidden')
+
+    // Unlock banner for CMS user to be present with correct data.
+    cy.findByTestId('unlockedBanner').should('exist')
+})
