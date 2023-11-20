84712967ea07c2182685f90cece828dd920ef248
pearl-truss <pearl@truss.works>
2023-11-20 11:13:06 -0500

    copy tweak for attestation field

--- services/app-web/src/constants/statutoryRegulatoryAttestation.ts
+++ services/app-web/src/constants/statutoryRegulatoryAttestation.ts
@@ -7,7 +7,7 @@ const StatutoryRegulatoryAttestationQuestion =
     'Do you attest that this contract complies with all applicable statutory and regulatory requirements including those contained in Title 42 Part 438 and Part 457 of the Code of Federal Regulations (CFR)?'
 
 const StatutoryRegulatoryAttestationDescription =
-    'Provide a brief description of the contract’s non-compliance (with regulatory citations) and expected timeframe for remediation'
+    'Provide a brief description of any contractual or operational non-compliance, including regulatory citations and expected timeframe for remediation'
 
 export {
     StatutoryRegulatoryAttestation,
