0f2e5161c5a6213a6ebf1492545df775ba2299e9
Mojo Talantikite <mojo.talantikite@gmail.com>
2023-12-18 17:28:31 -0500

    tar for zip

--- services/uploads/src/avLayer/build/build.sh
+++ services/uploads/src/avLayer/build/build.sh
@@ -7,7 +7,7 @@ echo "prepping clamav (${VERSION})"
 
 yum update -y
 amazon-linux-extras install epel -y
-yum install -y cpio yum-utils zip
+yum install -y cpio yum-utils tar
 
 # extract binaries for clamav, json-c, pcre
 mkdir -p /tmp/build
