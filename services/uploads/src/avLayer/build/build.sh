52ac2b53abc4dd3300a2ce7e283054cbcd7f4df8
Mojo Talantikite <mojo.talantikite@gmail.com>
2023-12-18 17:20:58 -0500

    use tar instead

--- services/uploads/src/avLayer/build/build.sh
+++ services/uploads/src/avLayer/build/build.sh
@@ -64,5 +64,4 @@ cp -R /tmp/build/usr/lib64/* lib/.
 cp -R /tmp/build/lib64/* lib/.
 cp freshclam.conf bin/freshclam.conf
 
-zip -r9 /opt/app/lambda_layer.zip bin
-zip -r9 /opt/app/lambda_layer.zip lib
+tar -czvf /opt/app/lambda_layer.tar.gz -C /opt/app bin lib
