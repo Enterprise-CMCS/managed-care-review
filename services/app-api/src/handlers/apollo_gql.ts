668384071046b717bba3c6b3e537df1a5257ee7b
pearl-truss <pearl@truss.works>
2023-12-21 12:41:23 -0500

    initial authorizer

--- services/app-api/src/handlers/apollo_gql.ts
+++ services/app-api/src/handlers/apollo_gql.ts
@@ -4,6 +4,8 @@ import { ApolloServer } from 'apollo-server-lambda'
 import type {
     APIGatewayProxyEvent,
     APIGatewayProxyHandler,
+    APIGatewayAuthorizerResult,
+    PolicyDocument,
     Handler,
 } from 'aws-lambda'
 import typeDefs from '../../../app-graphql/src/schema.graphql'
@@ -101,6 +103,48 @@ function contextForRequestForFetcher(userFetcher: userFromAuthProvider): ({
         }
     }
 }
+
+const thirdPartyAPIAuthorizer = async function (event: any): Promise<APIGatewayAuthorizerResult> {
+    // authentication step by getting and validating JWT token
+    const authToken = event.headers['authorization'] || '';
+    try {
+      // @ts-ignore
+      const decodedJWT = await cognitoJwtVerifier.verify(authToken);
+  
+      // After the token is verified we can do Authorization check here if needed.
+      // If the request doesn't meet authorization conditions then we should return a Deny policy.
+      const policyDocument: PolicyDocument = {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow', // return Deny if you want to reject the request
+            Resource: event['methodArn'],
+          },
+        ],
+      };
+  
+      // This is the place you inject custom data into request context which will be available
+      // inside `event.requestContext.authorizer` in API Lambdas.
+      const context = {
+        'userId': 123,
+        'companyId': 456,
+        'role': 'ADMIN',
+      };
+  
+      const response: APIGatewayAuthorizerResult = {
+        principalId: decodedJWT.sub,
+        policyDocument,
+        context,
+      };
+      console.log(`response => ${JSON.stringify(response)}`);
+  
+      return response;
+    } catch (err) {
+      console.error('Invalid auth token. err => ', err);
+      throw new Error('Unauthorized');
+    }
+  };
 
 // This middleware returns an error if the local request is missing authentication info
 function localAuthMiddleware(wrapped: APIGatewayProxyHandler): Handler {
