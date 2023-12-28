df921d4c373c667fec57fabd84dcd440903be4a0
Mojo Talantikite <mojo.talantikite@gmail.com>
2023-12-28 17:33:04 -0500

    test autoinstrumentation

--- services/app-api/webpack.config.js
+++ services/app-api/webpack.config.js
@@ -43,6 +43,7 @@ module.exports = {
         '@opentelemetry/instrumentation',
         '@opentelemetry/instrumentation-http',
         '@opentelemetry/propagator-aws-xray',
+        '@opentelemetry/auto-instrumentations-node',
         '@opentelemetry/exporter-metrics-otlp-http',
         '@opentelemetry/sdk-metrics',
     ],
--- services/uploads/src/lib/otel.ts
+++ services/uploads/src/lib/otel.ts
@@ -2,10 +2,10 @@ import opentelemetry from '@opentelemetry/api'
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 import { Resource } from '@opentelemetry/resources'
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
+//import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base'
 import { AWSXRayIdGenerator } from '@opentelemetry/id-generator-aws-xray'
-import { registerInstrumentations } from '@opentelemetry/instrumentation'
+//import { registerInstrumentations } from '@opentelemetry/instrumentation'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http'
@@ -17,9 +17,11 @@ import {
 export function initTracer(serviceName: string, otelCollectorURL: string) {
     console.info('-----Setting OTEL instrumentation-----')
 
+    /*
     registerInstrumentations({
         instrumentations: [getNodeAutoInstrumentations()],
     })
+    */
 
     const resource = Resource.default().merge(
         new Resource({
