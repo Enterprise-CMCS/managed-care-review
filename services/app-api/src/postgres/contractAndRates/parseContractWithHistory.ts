9711261a73a188290bb31a0e0393b003716ac6d1
Jason Lin <maolin@truss.works>
2024-01-10 18:59:44 -0500

    Refine matching single rate revision to contract revision for independent rate unlock/resubmit.

--- services/app-api/src/postgres/contractAndRates/parseContractWithHistory.ts
+++ services/app-api/src/postgres/contractAndRates/parseContractWithHistory.ts
@@ -111,7 +111,8 @@ function contractWithHistoryToDomainModel(
     const contractRevisions = contract.revisions
     let draftRevision: ContractRevisionWithRatesType | Error | undefined =
         undefined
-    for (const contractRev of contractRevisions) {
+
+    for (const [contractRevIndex, contractRev] of contractRevisions.entries()) {
         // We set the draft revision aside, all ordered revisions are submitted
         if (!contractRev.submitInfo) {
             if (draftRevision) {
@@ -142,8 +143,6 @@ function contractWithHistoryToDomainModel(
             unlockInfo: contractRev.unlockInfo || undefined,
             rateRevisions: [],
         }
-
-        allRevisionSets.push(initialEntry)
 
         // This code below was used to construct rate change history and add into our contract revision history by pushing
         //  new contract revisions into the array. This however caused issues with the frontend apollo cache because we
@@ -194,33 +193,72 @@ function contractWithHistoryToDomainModel(
         //     }
         // }
 
-        // This loop is finding the rate revision submitted along with this contract revision to preserve the historical
-        // rate data for the submission history.
-        for (const rateRev of contractRev.rateRevisions) {
+        /**
+         * Below a temporary approach to finding the matching rate revision to the contract revision. The correct way
+         * for this is to build the actual contract and rate history. This will be done in the Rate Change History epic
+         * https://qmacbis.atlassian.net/browse/MCR-3607
+         *
+         * The approach to finding the **single** rate revision for the submitted contract revision is to find
+         * the latest rate revision submitted before the next contract revision unlock date. The latest rate revision
+         * and not the one submitted with the contract, because rates and be unlocked and resubmitted independently of
+         * the contract.
+         *
+         * The idea is that once a contract is unlocked again, the new contract revision created is now the "active"
+         * revision with most up-to-date data and previous submitted contract revision is now historical and changes
+         * should not be reflected on it, including rate changes.
+         **/
+
+        // Get next contract revision's unlockInfo.updatedAt. revisions are queried from DB in asc order by createdAt.
+        const nextContractRev: ContractRevisionTableWithFormData | undefined =
+            contractRevisions[contractRevIndex + 1]
+
+        // rateRevisions from DB are in asc order by created at, we need it to be desc order to find the latest rev first.
+        const rateRevisions = contractRev.rateRevisions.reverse()
+
+        for (const rateRev of rateRevisions) {
             if (!rateRev.rateRevision.submitInfo) {
                 return new Error(
                     'Programming Error: a contract is associated with an unsubmitted rate'
                 )
             }
 
-            // Make sure this rate revision was not submitted after this contract revision, and it was not removed.
+            // Check if rate revision submitted date is earlier than the proceeding contract revisions unlocked date.
+            const isRateSubmittedDateValid = (
+                rateSubmittedDate: Date
+            ): boolean => {
+                if (nextContractRev && nextContractRev.unlockInfo) {
+                    // Invalid if the rate revision submit date was after the proceeding contract revisions unlocked date
+                    return (
+                        rateSubmittedDate.getTime() <
+                        nextContractRev.unlockInfo.updatedAt.getTime()
+                    )
+                }
+                // If no proceeding contract, then there is no date constraint
+                return true
+            }
+
+            // Does initial entries rateRevisions already include a revision for this rate
+            const isRateIncluded = !!initialEntry.rateRevisions.find(
+                (rr) => rr.rateID === rateRev.rateRevision.rateID
+            )
+
+            // Rate revision that belong to this contract revision has to be:
+            // - Submitted before the next contract rev unlock date
+            // - Not already in the initial entry. We are looping through this in desc order, so the first rate rev is the latest.
+            // - Not removed from the contract
             if (
-                rateRev.rateRevision.submitInfo.updatedAt <=
-                    contractRev.submitInfo.updatedAt &&
+                isRateSubmittedDateValid(
+                    rateRev.rateRevision.submitInfo.updatedAt
+                ) &&
+                !isRateIncluded &&
                 !rateRev.isRemoval
             ) {
-                // take out the previous rate revision this revision supersedes
-                const filteredRevisions = initialEntry.rateRevisions.filter(
-                    (rr) => rr.rateID !== rateRev.rateRevision.rateID
-                )
-
-                // add latest revision
-                filteredRevisions.push(rateRev.rateRevision)
-
-                // Sort to retain order by rate.createdAt.
-                initialEntry.rateRevisions = filteredRevisions
+                // unshift rate revision into entries to reverse order back to asc
+                initialEntry.rateRevisions.unshift(rateRev.rateRevision)
             }
         }
+
+        allRevisionSets.push(initialEntry)
     }
 
     const revisions = contractSetsToDomainModel(allRevisionSets)
