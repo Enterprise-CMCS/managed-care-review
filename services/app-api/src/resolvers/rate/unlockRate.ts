3d3e82c1d8311bbb9db7fc3976bbc4847dd2b58a
haworku <haworku@users.noreply.github.com>
2024-01-12 14:17:20 -0600

    MCR-3767 CMS has unlock rate button (#2166)
    * Initial backend commit
    
    * initial frontend commit
    
    * Get button working and cleanup unused scss
    
    * Add tests
    
    * and add the feature flag
    
    * Finish app-api tests, add assertionHelpers
    
    * Fix padding by moving dl
    
    * Fix word wrap in rate names with non zero space
    
    * Revert "Fix word wrap in rate names with non zero space"
    
    This reverts commit cf47ef17c6ffcc513d7a7a805bc10c4d103f59b4.
    
    * Cleanup from code review

---
+++ services/app-api/src/resolvers/rate/unlockRate.ts
@@ -0,0 +1,90 @@
+import { ForbiddenError, UserInputError } from 'apollo-server-lambda'
+import type { RateType } from '../../domain-models'
+import { isCMSUser } from '../../domain-models'
+import type { MutationResolvers } from '../../gen/gqlServer'
+import { logError, logSuccess } from '../../logger'
+import { NotFoundError } from '../../postgres'
+import type { Store } from '../../postgres'
+import {
+    setErrorAttributesOnActiveSpan,
+    setResolverDetailsOnActiveSpan,
+    setSuccessAttributesOnActiveSpan,
+} from '../attributeHelper'
+import { GraphQLError } from 'graphql'
+
+export function unlockRate(store: Store): MutationResolvers['unlockRate'] {
+    return async (_parent, { input }, context) => {
+        const { user, span } = context
+        const { unlockedReason, rateID } = input
+        setResolverDetailsOnActiveSpan('unlockRate', user, span)
+        span?.setAttribute('mcreview.rate_id', rateID)
+
+        // This resolver is only callable by CMS users
+        if (!isCMSUser(user)) {
+            logError('unlockRate', 'user not authorized to unlock rate')
+            setErrorAttributesOnActiveSpan(
+                'user not authorized to unlock rate',
+                span
+            )
+            throw new ForbiddenError('user not authorized to unlock rate')
+        }
+
+        const initialRateResult = await store.findRateWithHistory(rateID)
+
+        if (initialRateResult instanceof Error) {
+            if (initialRateResult instanceof NotFoundError) {
+                const errMessage = `A rate must exist to be unlocked: ${rateID}`
+                logError('unlockRate', errMessage)
+                setErrorAttributesOnActiveSpan(errMessage, span)
+                throw new UserInputError(errMessage, {
+                    argumentName: 'rateID',
+                })
+            }
+
+            const errMessage = `Issue finding a rate. Message: ${initialRateResult.message}`
+            logError('unlockRate', errMessage)
+            setErrorAttributesOnActiveSpan(errMessage, span)
+            throw new GraphQLError(errMessage, {
+                extensions: {
+                    code: 'INTERNAL_SERVER_ERROR',
+                    cause: 'DB_ERROR',
+                },
+            })
+        }
+
+        const rate: RateType = initialRateResult
+
+        if (rate.draftRevision) {
+            const errMessage = `Attempted to unlock rate with wrong status`
+            logError('unlockRate', errMessage)
+            setErrorAttributesOnActiveSpan(errMessage, span)
+            throw new UserInputError(errMessage, {
+                argumentName: 'rateID',
+                cause: 'INVALID_PACKAGE_STATUS',
+            })
+        }
+
+        const unlockRateResult = await store.unlockRate({
+            rateID: rate.id,
+            unlockReason: unlockedReason,
+            unlockedByUserID: user.id,
+        })
+
+        if (unlockRateResult instanceof Error) {
+            const errMessage = `Failed to unlock rate revision with ID: ${rate.id}; ${unlockRateResult.message}`
+            logError('unlockRate', errMessage)
+            setErrorAttributesOnActiveSpan(errMessage, span)
+            throw new GraphQLError(errMessage, {
+                extensions: {
+                    code: 'INTERNAL_SERVER_ERROR',
+                    cause: 'DB_ERROR',
+                },
+            })
+        }
+
+        logSuccess('unlockRate')
+        setSuccessAttributesOnActiveSpan(span)
+
+        return { rate: unlockRateResult }
+    }
+}
--- services/app-api/src/testHelpers/errorHelpers.ts
+++
@@ -1,9 +0,0 @@
-// For use in TESTS only. Throws a returned error
-function must<T>(maybeErr: T | Error): T {
-    if (maybeErr instanceof Error) {
-        throw maybeErr
-    }
-    return maybeErr
-}
-
-export { must }
