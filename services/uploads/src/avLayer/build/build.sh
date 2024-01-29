2aabcee5e86bfe5eccc6dfc32278d9bdac2ca36f
Mojo Talantikite <mojo.talantikite@gmail.com>
2024-01-29 15:59:46 -0500

    remove debug and +libcrypt.so.1

--- services/uploads/src/avLayer/build/build.sh
+++ services/uploads/src/avLayer/build/build.sh
@@ -16,6 +16,10 @@ yum install -y cpio yum-utils tar.x86_64 gzip zip
 mkdir -p /tmp/build
 pushd /tmp/build
 
+# Download libcrypt.so.1
+curl -L --output glibc-2.17-317.el7.x86_64.rpm http://mirror.centos.org/centos/7/os/x86_64/Packages/glibc-2.17-317.el7.x86_64.rpm
+rpm2cpio glibc*.rpm | cpio -vimd
+
 # Download other package dependencies
 yumdownloader -x \*i686 --archlist=x86_64 clamav clamav-lib clamav-update clamd json-c pcre2 libtool-ltdl libxml2 bzip2-libs xz-libs libprelude gnutls nettle libcurl libnghttp2 libidn2 libssh2 openldap libffi krb5-libs keyutils-libs libunistring cyrus-sasl-lib nss nspr libselinux openssl-libs libcrypt
 rpm2cpio clamav-0*.rpm | cpio -vimd
@@ -47,9 +51,6 @@ rpm2cpio libselinux*.rpm | cpio -vimd
 rpm2cpio openssl-libs*.rpm | cpio -vimd
 rpm2cpio libcrypt*.rpm | cpio -vimd
 
-# Print out the contents of the /tmp/build directory
-ls -la /tmp/build
-
 # reset the timestamps so that we generate a reproducible zip file where
 # running with the same file contents we get the exact same hash even if we
 # run the same build on different days
@@ -62,10 +63,6 @@ cp /tmp/build/usr/bin/clamscan /tmp/build/usr/bin/freshclam /tmp/build/usr/bin/c
 cp -R /tmp/build/usr/lib64/* lib/.
 cp -R /tmp/build/lib64/* lib/.
 cp freshclam.conf bin/freshclam.conf
-
-# Print out the contents of the bin and lib directories
-ls -la bin
-ls -la lib
 
 zip -r9 lambda_layer.zip bin
 zip -r9 lambda_layer.zip lib
