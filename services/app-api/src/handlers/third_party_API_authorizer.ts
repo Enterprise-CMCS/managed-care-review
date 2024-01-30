153b120c7f47de7d48f782f48bb76adefa5a6979
pearl-truss <pearl@truss.works>
2024-01-30 12:36:40 -0500

    re-implement restriction by ip address in authorizer lambda

--- services/app-api/src/handlers/third_party_API_authorizer.ts
+++ services/app-api/src/handlers/third_party_API_authorizer.ts
@@ -8,6 +8,7 @@ import { newJWTLib } from '../jwt'
 
 const stageName = process.env.stage
 const jwtSecret = process.env.JWT_SECRET
+const allowedIpAddresses = process.env.ALLOWED_IP_ADDRESSES
 
 if (stageName === undefined) {
     throw new Error('Configuration Error: stage is required')
@@ -16,6 +17,12 @@ if (stageName === undefined) {
 if (jwtSecret === undefined || jwtSecret === '') {
     throw new Error(
         'Configuration Error: JWT_SECRET is required to run app-api.'
+    )
+}
+
+if (allowedIpAddresses === undefined || allowedIpAddresses === '') {
+    throw new Error(
+        'Configuration Error: ALLOWD_IP_ADDRESSES is required to run app-api.'
     )
 }
 
@@ -29,33 +36,21 @@ export const main: APIGatewayTokenAuthorizerHandler = async (
     event
 ): Promise<APIGatewayAuthorizerResult> => {
     const authToken = event.authorizationToken.replace('Bearer ', '')
+    const parsedEvent = JSON.parse(JSON.stringify(event))
+    const host = parsedEvent.headers.Host
+    // host is formatted as ipAddress:port
+    // the following will remove the :port to leave just the ip address
+    const ipAddress = host.slice(0, host.indexOf(':'))
+    const ipAddressIsValid = allowedIpAddresses.includes(ipAddress)
+
     try {
         // authentication step for validating JWT token
         const userId = jwtLib.userIDFromToken(authToken)
-        // const parsedEvent = JSON.parse(JSON.stringify(event))
-        // const host = parsedEvent.headers.Host
-        // host is formatted as ipAddress:port
-        // the following will remove the :port to leave just the ip address
-        // const ipAddress = host.slice(0, host.indexOf(':'))
-        // const validIpAddresses = process.env.ALLOWED_IP_ADDRESSES
-        // if validIpAddresses === undefined or length(validIpAddresses) === 0 {
-
-        // }
-        // const validIPAddress =
-        // ipAddress && process.env.ALLOWED_IP_ADDRESSES?.includes(ipAddress)
         if (userId instanceof Error) {
-            const msg = 'Invalid auth token'
-            console.error(msg)
+            console.error('Invalid auth token')
 
-            return generatePolicy(undefined, event)
+            return generatePolicy(undefined, event, ipAddressIsValid)
         }
-
-        // if (ipAddress === undefined || ipAddress === '') {
-        //     const msg = 'Invalid ip address'
-        //     console.error(msg)
-
-        //     return generatePolicy(undefined, event, ipAddress)
-        // }
 
         console.info({
             message: 'third_party_API_authorizer succeeded',
@@ -63,31 +58,30 @@ export const main: APIGatewayTokenAuthorizerHandler = async (
             status: 'SUCCESS',
         })
 
-        return generatePolicy(userId, event)
+        return generatePolicy(userId, event, ipAddressIsValid)
     } catch (err) {
         console.error(
             'unexpected exception attempting to validate authorization',
             err
         )
-        return generatePolicy(undefined, event)
+        return generatePolicy(undefined, event, ipAddressIsValid)
     }
 }
 
 const generatePolicy = function (
     userId: string | undefined,
-    event: APIGatewayTokenAuthorizerEvent
-    // ipAddress: string | undefined
+    event: APIGatewayTokenAuthorizerEvent,
+    ipAddressIsValid: boolean
 ): APIGatewayAuthorizerResult {
     // If the JWT is verified as valid, and the request comes from an allowed IP address
     // send an Allow policy
     // otherwise a Deny policy is returned which restricts access
-    // console.info(ipAddress, '====== ipaddress ======')
     const policyDocument: PolicyDocument = {
         Version: '2012-10-17', // current version of the policy language
         Statement: [
             {
                 Action: 'execute-api:Invoke',
-                Effect: 'Allow',
+                Effect: userId && ipAddressIsValid ? 'Allow' : 'Deny',
                 Resource: event['methodArn'],
             },
         ],
