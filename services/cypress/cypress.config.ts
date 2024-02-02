0aa011a93548a97997ddb005464373405594aa8c
pearl-truss <pearl@truss.works>
2024-02-02 14:02:36 -0500

    update cypress config to read env for allow ip addresses

--- services/cypress/cypress.config.ts
+++ services/cypress/cypress.config.ts
@@ -36,6 +36,8 @@ module.exports = defineConfig({
             newConfig.env.COGNITO_REGION = process.env.COGNITO_REGION
             newConfig.env.COGNITO_IDENTITY_POOL_ID = process.env.COGNITO_IDENTITY_POOL_ID
             newConfig.env.COGNITO_USER_POOL_WEB_CLIENT_ID = process.env.COGNITO_USER_POOL_WEB_CLIENT_ID
+            // configure env for third party API access
+            newConfig.env.ALLOWED_IP_ADDRESSES = process.env.ALLOWED_IP_ADDRESSES
 
             on('before:browser:launch', (browser, launchOptions) => {
                 prepareAudit(launchOptions)
