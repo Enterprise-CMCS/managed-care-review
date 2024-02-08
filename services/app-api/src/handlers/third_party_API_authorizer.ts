920d6f88811d026a74864091d8b0867b4a3ae60e
pearl-truss <pearl@truss.works>
2024-02-08 11:44:19 -0500

    add ip check to authorizer

--- services/app-api/src/handlers/third_party_API_authorizer.ts
+++ services/app-api/src/handlers/third_party_API_authorizer.ts
@@ -31,12 +31,31 @@ export const main: APIGatewayTokenAuthorizerHandler = async (
     const authToken = event.authorizationToken.replace('Bearer ', '')
     try {
         // authentication step for validating JWT token
-        const userId = jwtLib.userIDFromToken(authToken)
-        if (userId instanceof Error) {
-            console.error('Invalid auth token')
+        const userId = await jwtLib.userIDFromToken(authToken)
+        const parsedEvent = JSON.parse(JSON.stringify(event))
+        const host = parsedEvent.headers.Host
+        // host is formatted as ipAddress:port
+        // the following will remove the :port to leave just the ip address
+        const ipAddress = host.slice(0, host.indexOf(':'))
+        // const validIpAddresses = process.env.ALLOWED_IP_ADDRESSES
+        // if validIpAddresses === undefined or length(validIpAddresses) === 0 {
 
-            return generatePolicy(undefined, event)
+        // }
+        // const validIPAddress =
+        // ipAddress && process.env.ALLOWED_IP_ADDRESSES?.includes(ipAddress)
+        if (userId instanceof Error) {
+            const msg = 'Invalid auth token'
+            console.error(msg)
+
+            return generatePolicy(undefined, event, ipAddress)
         }
+
+        // if (ipAddress === undefined || ipAddress === '') {
+        //     const msg = 'Invalid ip address'
+        //     console.error(msg)
+
+        //     return generatePolicy(undefined, event, ipAddress)
+        // }
 
         console.info({
             message: 'third_party_API_authorizer succeeded',
@@ -44,29 +63,33 @@ export const main: APIGatewayTokenAuthorizerHandler = async (
             status: 'SUCCESS',
         })
 
-        return generatePolicy(userId, event)
+        return generatePolicy(userId, event, ipAddress)
     } catch (err) {
         console.error(
             'unexpected exception attempting to validate authorization',
             err
         )
-        return generatePolicy(undefined, event)
+        return generatePolicy(undefined, event, undefined)
     }
 }
 
 const generatePolicy = function (
     userId: string | undefined,
-    event: APIGatewayTokenAuthorizerEvent
+    event: APIGatewayTokenAuthorizerEvent,
+    ipAddress: string | undefined
 ): APIGatewayAuthorizerResult {
-    // If the JWT is verified as valid, send an Allow policy
-    // this will allow the request to go through
+    // If the JWT is verified as valid, and the request comes from an allowed IP address
+    // send an Allow policy
     // otherwise a Deny policy is returned which restricts access
+    console.info(ipAddress, '====== ipaddress ======')
+    const validIPAddress =
+        ipAddress && process.env.ALLOWED_IP_ADDRESSES?.includes(ipAddress)
     const policyDocument: PolicyDocument = {
         Version: '2012-10-17', // current version of the policy language
         Statement: [
             {
                 Action: 'execute-api:Invoke',
-                Effect: userId ? 'Allow' : 'Deny',
+                Effect: userId && validIPAddress ? 'Allow' : 'Deny',
                 Resource: event['methodArn'],
             },
         ],
