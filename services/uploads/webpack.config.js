9f2f2fae1e275f33a7bfddeba1ede7e6c0eddf7a
Mojo Talantikite <mojo.talantikite@gmail.com>
2024-02-16 13:12:18 -0500

    remove my branch

--- services/uploads/webpack.config.js
+++ services/uploads/webpack.config.js
@@ -72,13 +72,9 @@ module.exports = {
                     from: path.resolve(__dirname, 'src/avLayer/clamd.conf'),
                     transform(content) {
                         const stage = slsw.lib.options.stage;
-                        console.log('this is the stage: ' + stage);
-                        const stageHostname = [
-                            'main',
-                            'val',
-                            'prod',
-                            'mtclamavdns',
-                        ].includes(stage)
+                        const stageHostname = ['main', 'val', 'prod'].includes(
+                            stage
+                        )
                             ? `clamav.mc-review-${stage}.local`
                             : 'clamav.mc-review-main.local';
 
