9c46fe9428c81ff4e783029bbed534536308c86b
Hana Worku <hana@truss.works>
2024-02-26 14:11:25 -0600

    MCR-3826 adjust width so StepIndicator fills screen

--- services/app-web/src/pages/StateSubmission/StateSubmissionForm.tsx
+++ services/app-web/src/pages/StateSubmission/StateSubmissionForm.tsx
@@ -240,7 +240,7 @@ export const StateSubmissionForm = (): React.ReactElement => {
         latestRevision.unlockInfo || undefined
 
     return (
-        <div>
+        <div className={styles.fullWidth}>
             <div className={styles.stepIndicator}>
                 <DynamicStepIndicator
                     formPages={activeFormPages(formDataFromLatestRevision)}
