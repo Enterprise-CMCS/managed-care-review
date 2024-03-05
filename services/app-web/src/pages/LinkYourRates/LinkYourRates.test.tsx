d84061ef9476d3f9dc383311fd8a7dbc8abccbd7
ruizajtruss <ruizaj@truss.works>
2024-03-05 17:38:21 +0000

    added testing for radio buttons

--- services/app-web/src/pages/LinkYourRates/LinkYourRates.test.tsx
+++ services/app-web/src/pages/LinkYourRates/LinkYourRates.test.tsx
@@ -2,7 +2,7 @@ import { screen, waitFor } from '@testing-library/react'
 import {
     fetchCurrentUserMock,
     indexRatesMockSuccess,
-    mockValidCMSUser,
+    mockValidStateUser,
 } from '../../testHelpers/apolloMocks'
 import { renderWithProviders } from '../../testHelpers/jestHelpers'
 import { LinkYourRates } from './LinkYourRates'
@@ -14,7 +14,7 @@ describe('LinkYourRates', () => {
                 mocks: [
                     fetchCurrentUserMock({
                         statusCode: 200,
-                        user: mockValidCMSUser(),
+                        user: mockValidStateUser(),
                     }),
                     indexRatesMockSuccess(),
                 ],
@@ -23,6 +23,56 @@ describe('LinkYourRates', () => {
 
         await waitFor(() => {
             expect(screen.queryByTestId('linkYourRates')).toBeInTheDocument()
+        })
+    })
+
+    it('does not display dropdown menu if no is selected', async () => {
+        renderWithProviders(<LinkYourRates />, {
+            apolloProvider: {
+                mocks: [
+                    fetchCurrentUserMock({
+                        statusCode: 200,
+                        user: mockValidStateUser(),
+                    }),
+                    indexRatesMockSuccess(),
+                ],
+            },
+        })
+
+        screen
+            .getByLabelText(
+                'Yes, this rate certification is part of another submission'
+            )
+            .click()
+
+        await waitFor(() => {
+            expect(screen.queryByTestId('linkRateSelect')).toBeInTheDocument()
+        })
+    })
+
+    it('displays dropdown menu if yes is selected', async () => {
+        renderWithProviders(<LinkYourRates />, {
+            apolloProvider: {
+                mocks: [
+                    fetchCurrentUserMock({
+                        statusCode: 200,
+                        user: mockValidStateUser(),
+                    }),
+                    indexRatesMockSuccess(),
+                ],
+            },
+        })
+
+        screen
+            .getByLabelText(
+                'No, this rate certification was not included with any other submissions'
+            )
+            .click()
+
+        await waitFor(() => {
+            expect(
+                screen.queryByTestId('linkRateSelect')
+            ).not.toBeInTheDocument()
         })
     })
 })
