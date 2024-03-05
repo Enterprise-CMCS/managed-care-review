ebc0a143831c34daa6e9727f0c4ab2db70f8752e
ruizajtruss <ruizaj@truss.works>
2024-03-05 18:16:04 +0000

    unit testing

--- services/app-web/src/pages/LinkYourRates/LinkYourRates.test.tsx
+++ services/app-web/src/pages/LinkYourRates/LinkYourRates.test.tsx
@@ -6,62 +6,63 @@ import {
 } from '../../testHelpers/apolloMocks'
 import { renderWithProviders } from '../../testHelpers/jestHelpers'
 import { LinkYourRates } from './LinkYourRates'
+import { Formik } from 'formik'
 
 describe('LinkYourRates', () => {
     it('renders without errors', async () => {
-        renderWithProviders(<LinkYourRates />, {
-            apolloProvider: {
-                mocks: [
-                    fetchCurrentUserMock({
-                        statusCode: 200,
-                        user: mockValidStateUser(),
-                    }),
-                    indexRatesMockSuccess(),
-                ],
-            },
-        })
+        renderWithProviders(
+            <Formik
+                initialValues={{ ratePreviouslySubmitted: '' }}
+                onSubmit={(values) => console.info('submitted', values)}
+            >
+                <form>
+                    <LinkYourRates />
+                </form>
+            </Formik>,
+            {
+                apolloProvider: {
+                    mocks: [
+                        fetchCurrentUserMock({
+                            statusCode: 200,
+                            user: mockValidStateUser(),
+                        }),
+                        indexRatesMockSuccess(),
+                    ],
+                },
+            }
+        )
 
         await waitFor(() => {
-            expect(screen.queryByTestId('linkYourRates')).toBeInTheDocument()
+            expect(
+                screen.queryByText(
+                    'Was this rate certification included with another submission?'
+                )
+            ).toBeInTheDocument()
         })
     })
 
     it('does not display dropdown menu if no is selected', async () => {
-        renderWithProviders(<LinkYourRates />, {
-            apolloProvider: {
-                mocks: [
-                    fetchCurrentUserMock({
-                        statusCode: 200,
-                        user: mockValidStateUser(),
-                    }),
-                    indexRatesMockSuccess(),
-                ],
-            },
-        })
-
-        screen
-            .getByLabelText(
-                'Yes, this rate certification is part of another submission'
-            )
-            .click()
-
-        await waitFor(() => {
-            expect(screen.queryByTestId('linkRateSelect')).toBeInTheDocument()
-        })
-    })
-
-    it('displays dropdown menu if yes is selected', async () => {
-        renderWithProviders(<LinkYourRates />, {
-            apolloProvider: {
-                mocks: [
-                    fetchCurrentUserMock({
-                        statusCode: 200,
-                        user: mockValidStateUser(),
-                    }),
-                    indexRatesMockSuccess(),
-                ],
-            },
-        })
+        renderWithProviders(
+            <Formik
+                initialValues={{ ratePreviouslySubmitted: '' }}
+                onSubmit={(values) => console.info('submitted', values)}
+            >
+                <form>
+                    <LinkYourRates />
+                </form>
+            </Formik>,
+            {
+                apolloProvider: {
+                    mocks: [
+                        fetchCurrentUserMock({
+                            statusCode: 200,
+                            user: mockValidStateUser(),
+                        }),
+                        indexRatesMockSuccess(),
+                    ],
+                },
+            }
+        )
 
         screen
             .getByLabelText(
@@ -71,8 +72,44 @@ describe('LinkYourRates', () => {
 
         await waitFor(() => {
             expect(
-                screen.queryByTestId('linkRateSelect')
+                screen.queryByText('Which rate certification was it?')
             ).not.toBeInTheDocument()
+        })
+    })
+
+    it('displays dropdown menu if yes is selected', async () => {
+        renderWithProviders(
+            <Formik
+                initialValues={{ ratePreviouslySubmitted: '' }}
+                onSubmit={(values) => console.info('submitted', values)}
+            >
+                <form>
+                    <LinkYourRates />
+                </form>
+            </Formik>,
+            {
+                apolloProvider: {
+                    mocks: [
+                        fetchCurrentUserMock({
+                            statusCode: 200,
+                            user: mockValidStateUser(),
+                        }),
+                        indexRatesMockSuccess(),
+                    ],
+                },
+            }
+        )
+
+        screen
+            .getByLabelText(
+                'Yes, this rate certification is part of another submission'
+            )
+            .click()
+
+        await waitFor(() => {
+            expect(
+                screen.queryByText('Which rate certification was it?')
+            ).toBeInTheDocument()
         })
     })
 })
