de259c812af8af5dbe14b7487777a863dd7c35e2
haworku <haworku@users.noreply.github.com>
2024-04-08 12:50:57 -0700

    MCR-3905 CMS links from rate summary to parent submission to unlock a rate (#2351)
    * Initial commit - setup alternative unlock rate flow. Write test
    
    * Clear async error warnings out of this file - need linting to enforce this not getting worse
    
    * Wrap interim button also in linked rates feature flag
    
    * Be more precise in naming of contract id - clarify intent
    
    * Access the first created index -1

--- services/app-web/src/components/SubmissionSummarySection/RateDetailsSummarySection/SingleRateSummarySection.tsx
+++ services/app-web/src/components/SubmissionSummarySection/RateDetailsSummarySection/SingleRateSummarySection.tsx
@@ -24,7 +24,7 @@ import { useS3 } from '../../../contexts/S3Context'
 import useDeepCompareEffect from 'use-deep-compare-effect'
 import { recordJSException } from '../../../otelHelpers'
 import { Link } from '@trussworks/react-uswds'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import { packageName } from '../../../common-code/healthPlanFormDataType'
 import { UploadedDocumentsTableProps } from '../UploadedDocumentsTable/UploadedDocumentsTable'
 import { useAuth } from '../../../contexts/AuthContext'
@@ -129,6 +129,7 @@ export const SingleRateSummarySection = ({
     statePrograms: Program[]
 }): React.ReactElement | null => {
     const { loggedInUser } = useAuth()
+    const navigate = useNavigate()
     const rateRevision = rate.revisions[0]
     const formData: RateFormData = rateRevision?.formData
     const documentDateLookupTable = makeRateDocumentDateTable(rate.revisions)
@@ -143,6 +144,10 @@ export const SingleRateSummarySection = ({
     const showRateUnlock: boolean = ldClient?.variation(
         featureFlags.RATE_EDIT_UNLOCK.flag,
         featureFlags.RATE_EDIT_UNLOCK.defaultValue
+    )
+    const showLinkedRates: boolean = ldClient?.variation(
+        featureFlags.LINK_RATES.flag,
+        featureFlags.LINK_RATES.defaultValue
     )
 
     // TODO BULK DOWNLOAD
@@ -225,7 +230,10 @@ export const SingleRateSummarySection = ({
             )
         }
     }
-
+    const parentContractSubmissionID =
+        rate.revisions[0].contractRevisions[
+            rate.revisions[0].contractRevisions.length - 1
+        ].contract.id
     return (
         <React.Fragment key={rate.id}>
             <SectionCard
@@ -238,14 +246,27 @@ export const SingleRateSummarySection = ({
                         'Unknown rate name'
                     }
                 >
-                    {showRateUnlock && isCMSUser ? (
+                    {isCMSUser && showRateUnlock && (
                         <UnlockRateButton
                             disabled={isUnlocked || unlockLoading}
                             onClick={handleUnlockRate}
                         >
                             Unlock rate
                         </UnlockRateButton>
-                    ) : null}
+                    )}
+                    {/* This second option is an interim state for unlock rate button (when linked rates is turned on but unlock and edit rate is not available yet). Remove when rate unlock is permanently on. */}
+                    {isCMSUser && showLinkedRates && !showRateUnlock && (
+                        <UnlockRateButton
+                            disabled={isUnlocked || unlockLoading}
+                            onClick={() => {
+                                navigate(
+                                    `/submissions/${parentContractSubmissionID}`
+                                )
+                            }}
+                        >
+                            Unlock rate
+                        </UnlockRateButton>
+                    )}
                 </SectionHeader>
                 {documentError && (
                     <DocumentWarningBanner className={styles.banner} />
