4041564ef1c62da47b1d1420cf88c7779d5d953d
pearl-truss <pearl@truss.works>
2024-06-07 13:49:22 -0400

    working locally

--- services/app-api/src/resolvers/shared/genericDocumentResolver.ts
+++ services/app-api/src/resolvers/shared/genericDocumentResolver.ts
@@ -1,7 +1,6 @@
 import type { Resolvers } from '../../gen/gqlServer'	
 import { GetObjectCommand } from '@aws-sdk/client-s3'
 import Url from 'url-parse'
-import { Readable } from 'stream'
 
 // tslint:disable-next-line
 const isValidS3URLFormat = (url: {
@@ -15,7 +14,6 @@ const isValidS3URLFormat = (url: {
         url.pathname.split('/').length === 3
     )
 }
-
 const parseBucketName = (maybeS3URL: string): string | Error => {
     const url = new Url(maybeS3URL)
     if (!isValidS3URLFormat(url)) throw new Error('Not valid S3URL')
@@ -37,49 +35,28 @@ new Promise((resolve, reject) => {
 export function genericDocumentResolver(s3: any): Resolvers['GenericDocument'] {
     return {
         downloadURL: async (parent) => {
-            const s3URL = parent.s3URL ?? ''
-            const key = parseKey(s3URL)
-            const bucket = parseBucketName(s3URL)
-            if (key instanceof Error || bucket instanceof Error) {
-                // todo throw error
-                return 'err'
-            }
-            const getCommand = new GetObjectCommand({
-                Bucket: bucket,
-                Key: key,
-            })
-        
             try {
-                const s3Item = await s3.send(getCommand)
-        
-                return new Promise((resolve, reject) => {
-                    if (!s3Item.Body) {
-                        reject(
-                            new Error(`stream for ${key} returned undefined`)
-                        )
-                        return
-                    }
-        
-                    if (!(s3Item.Body instanceof Readable)) {
-                        console.error('Unexpected S3 Item Body: ', s3Item.Body)
-                        reject(new Error('Unexpected S3 Item Body returned'))
-                        return
-                    }
-        
-                    s3Item.Body.on('end', function () {
-                        console.info(`Finished downloading new object ${key}`)
-                        resolve(s3Item.Body)
+                const s3URL = parent.s3URL ?? ''
+                const key = parseKey(s3URL)
+                const bucket = parseBucketName(s3URL)
+                if (key instanceof Error || bucket instanceof Error) {
+                    // todo throw error
+                    return 'err'
+                }
+                const url = await s3.send(
+                    new GetObjectCommand({
+                        Bucket: bucket,
+                        Key: key,
                     })
-                    .on('error', function (err: Error) {
-                        console.error('Error writing file', err)
-                        reject(err)
-                    })
-                    
-                })
-            } catch (err) {
-                console.error('failed to download the file from s3', err)
-                return err
-            }
+                )
+                .then((data:any) => console.log(data))
+                .catch((err: any) => console.log(err));
+                // const content = await streamToString(stream);
+                return url
+        } catch(e) {
+            return 'test'
+            // throw error
         }
+    }
     }  
 }
