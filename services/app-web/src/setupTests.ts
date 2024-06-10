6f0fabfebaf0a4b2d294c307830ef2369576cb3f
Jason Lin <maolin@truss.works>
2024-06-10 15:08:22 -0400

    Use `peculiar/webcrypt` to fix crypto.subtle.digest undefined issue.

--- services/app-web/src/setupTests.ts
+++ services/app-web/src/setupTests.ts
@@ -1,5 +1,7 @@
-import crypto from 'crypto'
+import crypto from 'node:crypto';
 import { TextEncoder, TextDecoder } from 'util'
+import { Crypto } from "@peculiar/webcrypto"
+import {cleanup} from '@testing-library/react';
 
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
@@ -10,8 +12,17 @@ import '@testing-library/jest-dom'
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 Element.prototype.scrollIntoView = () => {}
 
-// to make calculating the sha work in jest
-Object.assign(window, {
-    crypto: crypto,
-})
+// // to make calculating the sha work in jest
+Object.defineProperty(globalThis, 'crypto', {
+    value: new Crypto(),
+});
+
 Object.assign(global, { TextDecoder, TextEncoder })
+
+// This is a fix for userEvents and fireEvents not resolving promises and causing issues with apollo mocks.
+// Found this fix in https://github.com/testing-library/react-testing-library/issues/1197#issuecomment-2076120296
+vi.useFakeTimers({
+    shouldAdvanceTime: true
+})
+
+afterEach(cleanup)
