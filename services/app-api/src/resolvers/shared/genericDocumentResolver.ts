f13d8ec4750e723f55295843c2e87ab56fcfedf3
pearl-truss <pearl@truss.works>
2024-06-11 11:32:45 -0400

    wip debug memory leak

--- services/app-api/src/resolvers/shared/genericDocumentResolver.ts
+++ services/app-api/src/resolvers/shared/genericDocumentResolver.ts
@@ -20,7 +20,7 @@ export function genericDocumentResolver(): Resolvers['GenericDocument'] {
                     return 'err'
                 }
                 
-                let s3Client: S3ClientT
+                // let s3Client: S3ClientT
                 const s3LocalURL = 'http://localhost:4569'
                 const s3DocumentsBucket = 'local-uploads'
                 const s3QABucket = 'local-qa'
@@ -32,18 +32,18 @@ export function genericDocumentResolver(): Resolvers['GenericDocument'] {
                 // s3Client = newAmplifyS3Client(S3_BUCKETS_CONFIG)
                 // if (process.env.REACT_APP_AUTH_MODE !== 'LOCAL') {
                 // } else if (s3LocalURL) {
-                    s3Client = newLocalS3Client(s3LocalURL, S3_BUCKETS_CONFIG)
+                    const s3Client = newLocalS3Client(s3LocalURL, S3_BUCKETS_CONFIG)
                 // } else {
                 //     throw new Error(
                 //         'You must set either REACT_APP_S3_REGION or REACT_APP_S3_LOCAL_URL depending on what environment you are in'
                 //     )
                 // }
                 const url = await s3Client.getURL(key, 'HEALTH_PLAN_DOCS')
-
+                
                 return url
             } catch (e) {
                 console.error(e)
-                return 'test'
+                throw e
             }
         },
     }
