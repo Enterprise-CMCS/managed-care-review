3d43cec5607c44eadb6efc9bce566fa42dcb5cce
pearl-truss <pearl@truss.works>
2024-06-11 12:31:02 -0400

    cleanup code

--- services/app-api/src/resolvers/shared/genericDocumentResolver.ts
+++ services/app-api/src/resolvers/shared/genericDocumentResolver.ts
@@ -5,7 +5,7 @@ import {
     parseBucketName,
     parseKey,
 } from '../../s3'
-// import type { S3BucketConfigType } from '../../s3/s3Amplify'
+import type { S3BucketConfigType } from '../../s3/s3Amplify'
 import type { S3ClientT } from '../../s3'
 
 export function genericDocumentResolver(): Resolvers['GenericDocument'] {
@@ -16,34 +16,56 @@ export function genericDocumentResolver(): Resolvers['GenericDocument'] {
                 const key = parseKey(s3URL)
                 const bucket = parseBucketName(s3URL)
                 if (key instanceof Error || bucket instanceof Error) {
-                    // todo throw error
-                    return 'err'
+                    const err = new Error(
+                        'S3 needs to be provided a valid key and bucket'
+                    )
+                    throw err
                 }
-                
-                // let s3Client: S3ClientT
-                const s3LocalURL = 'http://localhost:4569'
-                const s3DocumentsBucket = 'local-uploads'
-                const s3QABucket = 'local-qa'
-                // const S3_BUCKETS_CONFIG: S3BucketConfigType = {
-                //     HEALTH_PLAN_DOCS: s3DocumentsBucket,
-                //     QUESTION_ANSWER_DOCS: s3QABucket,
-                // }
+                // S3 Region and LocalUrl are mutually exclusive.
+                // One is used in AWS and one is used locally.
+                const s3Region = process.env.REACT_APP_S3_REGION
+                const s3LocalURL = process.env.REACT_APP_S3_LOCAL_URL
+                const s3DocumentsBucket =
+                    process.env.REACT_APP_S3_DOCUMENTS_BUCKET
+                const s3QABucket = process.env.REACT_APP_S3_QA_BUCKET
 
-                // s3Client = newAmplifyS3Client(S3_BUCKETS_CONFIG)
-                // if (process.env.REACT_APP_AUTH_MODE !== 'LOCAL') {
-                // } else if (s3LocalURL) {
-                    // const s3Client = newLocalS3Client(s3LocalURL, S3_BUCKETS_CONFIG)
-                // } else {
-                //     throw new Error(
-                //         'You must set either REACT_APP_S3_REGION or REACT_APP_S3_LOCAL_URL depending on what environment you are in'
-                //     )
-                // }
-                // const url = await s3Client.getURL(key, 'HEALTH_PLAN_DOCS')
-                
-                return ''
-            } catch (e) {
-                console.error(e)
-                throw e
+                if (
+                    s3DocumentsBucket === undefined ||
+                    s3QABucket === undefined
+                ) {
+                    throw new Error(
+                        'To configure s3, you  must set REACT_APP_S3_DOCUMENTS_BUCKET and REACT_APP_S3_QA_BUCKET'
+                    )
+                }
+
+                if (s3Region !== undefined && s3LocalURL !== undefined) {
+                    throw new Error(
+                        'You cant set both REACT_APP_S3_REGION and REACT_APP_S3_LOCAL_URL. Pick one depending on what environment you are in'
+                    )
+                }
+
+                let s3Client: S3ClientT
+                const S3_BUCKETS_CONFIG: S3BucketConfigType = {
+                    HEALTH_PLAN_DOCS: s3DocumentsBucket,
+                    QUESTION_ANSWER_DOCS: s3QABucket,
+                }
+
+                if (process.env.REACT_APP_AUTH_MODE !== 'LOCAL') {
+                    s3Client = newAmplifyS3Client(S3_BUCKETS_CONFIG)
+                } else if (s3LocalURL) {
+                    s3Client = newLocalS3Client(s3LocalURL, S3_BUCKETS_CONFIG)
+                } else {
+                    throw new Error(
+                        'You must set either REACT_APP_S3_REGION or REACT_APP_S3_LOCAL_URL depending on what environment you are in'
+                    )
+                }
+
+                const url = await s3Client.getURL(key, 'HEALTH_PLAN_DOCS')
+
+                return url
+            } catch (err) {
+                console.error(err)
+                throw err
             }
         },
     }
