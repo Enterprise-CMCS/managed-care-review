ff873c88b1f7da5d46ceed7e0f278d2f3556599b
Mojo Talantikite <mojo.talantikite@gmail.com>
2024-06-12 10:21:39 -0400

    remove comments

--- services/app-api/scripts/prepare-prisma-layer.sh
+++ services/app-api/scripts/prepare-prisma-layer.sh
@@ -26,9 +26,7 @@ function preparePrismaLayer() {
     rsync -av ../../node_modules/@prisma/client/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/client
     rsync -av ../../node_modules/prisma/ lambda-layers-prisma-client-migration/nodejs/node_modules/prisma
     rsync -av ../../node_modules/.prisma/ lambda-layers-prisma-client-migration/nodejs/node_modules/.prisma
-    #cp ../../node_modules/@prisma/engines/migration-engine-rhel-openssl-1.0.x lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/
     cp ../../node_modules/@prisma/engines/migration-engine-rhel-openssl-3.0.x lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/
-    #cp ../../node_modules/@prisma/engines/libquery_engine-rhel-openssl-1.0.x.so.node lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/
     cp ../../node_modules/@prisma/engines/libquery_engine-rhel-openssl-3.0.x.so.node lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/
     cp ../../node_modules/@prisma/engines/package.json lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/package.json
     rsync -av ../../node_modules/@prisma/engines/dist/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/dist
