4b7f48c6300c7ada890b7a21dcfe000af8e4a110
pearl-truss <pearl@truss.works>
2024-06-13 11:36:41 -0400

    add additional info to log

--- services/app-api/src/s3/helpers.ts
+++ services/app-api/src/s3/helpers.ts
@@ -14,13 +14,15 @@ import Url from 'url-parse'
 
 const parseBucketName = (maybeS3URL: string): string | Error => {
     const url = new Url(maybeS3URL)
-    if (!isValidS3URLFormat(url)) throw new Error('Not valid S3URL')
+    if (!isValidS3URLFormat(url))
+        throw new Error(`Not valid S3URL: ${maybeS3URL}`)
     return url.hostname
 }
 
 const parseKey = (maybeS3URL: string): string | Error => {
     const url = new Url(maybeS3URL)
-    if (!isValidS3URLFormat(url)) return new Error('Not valid S3URL')
+    if (!isValidS3URLFormat(url))
+        return new Error(`Not valid S3URL: ${maybeS3URL}`)
     return url.pathname.split('/')[1]
 }
 
