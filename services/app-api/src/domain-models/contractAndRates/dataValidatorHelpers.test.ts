e717aba73cc297d3416096d4867755bf9902cd1c
Jason Lin <maolin@truss.works>
2024-06-24 14:35:23 -0400

    Add more validation helpers and tests.

--- services/app-api/src/domain-models/contractAndRates/dataValidatorHelpers.test.ts
+++ services/app-api/src/domain-models/contractAndRates/dataValidatorHelpers.test.ts
@@ -1,15 +1,20 @@
-import { validateContractDraftRevisionFormDataInput } from './dataValidatorHelpers'
-import { mockGqlContractDraftRevisionFormDataInput } from '../../testHelpers'
+import { validateContractDraftRevisionInput } from './dataValidatorHelpers'
+import {
+    mockGqlContractDraftRevisionFormDataInput,
+    mockGQLContractDraftRevisionInput,
+} from '../../testHelpers'
+import type { ContractDraftRevisionFormDataInput } from '../../gen/gqlServer'
 
-describe('validateContractDraftRevisionFormDataInput', () => {
+describe('validateContractDraftRevisionInput', () => {
     it('Validates input form data and removes statutoryRegulatoryAttestationDescription', () => {
+        const stateCode = 'FL'
         const formData = {
-            ...mockGqlContractDraftRevisionFormDataInput(),
+            ...mockGqlContractDraftRevisionFormDataInput(stateCode),
             contractDateStart: new Date(2025, 5, 1),
             contractDateEnd: new Date(2026, 4, 30),
             statutoryRegulatoryAttestation: true,
             statutoryRegulatoryAttestationDescription:
-                'Hi, I should be gone after update.',
+                'Hi, I should be gone after validation.',
         }
 
         const expectedResult = {
@@ -21,20 +26,39 @@ describe('validateContractDraftRevisionFormDataInput', () => {
             },
         }
 
-        const validatedFormData = validateContractDraftRevisionFormDataInput(
-            formData,
+        const draftRevisionInput = mockGQLContractDraftRevisionInput(
+            {
+                formData: formData,
+            },
+            stateCode
+        )
+
+        const validatedFormData = validateContractDraftRevisionInput(
+            draftRevisionInput,
+            stateCode,
             { '438-attestation': true }
         )
 
         expect(validatedFormData).toEqual(expectedResult)
     })
     it('Returns error for invalid data', () => {
-        const formData = {
-            ...mockGqlContractDraftRevisionFormDataInput(),
+        const stateCode = 'FL'
+        const formData: ContractDraftRevisionFormDataInput = {
+            ...mockGqlContractDraftRevisionFormDataInput(stateCode),
             contractDateStart: new Date(2025, 5, 1),
             contractDateEnd: new Date(2026, 4, 30),
             statutoryRegulatoryAttestation: false,
             statutoryRegulatoryAttestationDescription: undefined,
+            stateContacts: [
+                {
+                    name: 'Bill',
+                    titleRole: 'A Title',
+                    email: 'abc123@gmail',
+                },
+            ],
+            programIDs: ['imNotAValidProgramID'],
+            submissionType: 'CONTRACT_AND_RATES',
+            populationCovered: 'CHIP',
         }
 
         const expectedResult = {
@@ -42,14 +66,31 @@ describe('validateContractDraftRevisionFormDataInput', () => {
             error: expect.arrayContaining([]),
         }
 
-        const validatedFormData = validateContractDraftRevisionFormDataInput(
-            formData,
+        const draftRevisionInput = mockGQLContractDraftRevisionInput(
+            {
+                formData: formData,
+            },
+            stateCode
+        )
+
+        const validatedFormData = validateContractDraftRevisionInput(
+            draftRevisionInput,
+            stateCode,
             { '438-attestation': true }
         )
 
-        expect(validatedFormData).toEqual(expectedResult)
+        expect(validatedFormData).toEqual(
+            expect.objectContaining(expectedResult)
+        )
         expect(validatedFormData.error?.message).toContain(
             'statutoryRegulatoryAttestationDescription must be defined when statutoryRegulatoryAttestation is false'
+        )
+        expect(validatedFormData.error?.message).toContain('Invalid email')
+        expect(validatedFormData.error?.message).toContain(
+            `programIDs are invalid for the state ${stateCode}`
+        )
+        expect(validatedFormData.error?.message).toContain(
+            `populationCoveredSchema of CHIP cannot be submissionType of CONTRACT_AND_RATES`
         )
     })
 })
--- services/app-api/src/domain-models/contractAndRates/dataValidatorHelpers.ts
+++ services/app-api/src/domain-models/contractAndRates/dataValidatorHelpers.ts
@@ -1,12 +1,17 @@
 import type { FeatureFlagSettings } from 'app-web/src/common-code/featureFlags'
-import type { ContractDraftRevisionFormDataInput } from '../../gen/gqlServer'
+import type {
+    ContractDraftRevisionFormDataInput,
+    ContractDraftRevisionInput,
+} from '../../gen/gqlServer'
 import { contractFormDataSchema } from './formDataTypes'
 import {
     contractTypeSchema,
+    populationCoveredSchema,
     submissionTypeSchema,
 } from '../../../../app-web/src/common-code/proto/healthPlanFormDataProto/zodSchemas'
 import { z } from 'zod'
 import type { SafeParseReturnType } from 'zod'
+import { findStatePrograms } from '../../postgres'
 
 const updateDraftContractFormDataSchema = contractFormDataSchema.extend({
     submissionType: submissionTypeSchema.optional(),
@@ -19,38 +24,88 @@ type UpdateDraftContractFormDataType = z.infer<
     typeof updateDraftContractFormDataSchema
 >
 
-const validateContractDraftRevisionFormDataInput = (
+const validateStatutoryRegulatoryAttestation = (
     formData: ContractDraftRevisionFormDataInput,
+    featureFlags?: FeatureFlagSettings
+) =>
+    z
+        .string()
+        .optional()
+        .transform((val, ctx) => {
+            if (featureFlags?.['438-attestation']) {
+                if (!formData.statutoryRegulatoryAttestation && !val) {
+                    ctx.addIssue({
+                        code: z.ZodIssueCode.custom,
+                        message:
+                            'statutoryRegulatoryAttestationDescription must be defined when statutoryRegulatoryAttestation is false',
+                    })
+                }
+
+                // Clear out existing statutoryRegulatoryAttestationDescription if statutoryRegulatoryAttestation is true
+                if (formData.statutoryRegulatoryAttestation && val) {
+                    return undefined
+                }
+            }
+
+            return val
+        })
+
+const validateProgramIDs = (stateCode: string) => {
+    const allPrograms = findStatePrograms(stateCode)
+
+    return z.array(z.string()).superRefine((programs, ctx) => {
+        if (allPrograms instanceof Error) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: allPrograms.message,
+            })
+        } else {
+            const allProgramIDS = allPrograms.map((p) => p.id)
+
+            if (!programs.every((program) => allProgramIDS.includes(program))) {
+                ctx.addIssue({
+                    code: z.ZodIssueCode.custom,
+                    message: `programIDs are invalid for the state ${stateCode}`,
+                })
+            }
+        }
+    })
+}
+
+const validatePopulationCovered = (
+    formData: ContractDraftRevisionFormDataInput
+) =>
+    populationCoveredSchema.optional().superRefine((coveredType, ctx) => {
+        if (
+            coveredType === 'CHIP' &&
+            formData.submissionType === 'CONTRACT_AND_RATES'
+        ) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message:
+                    'populationCoveredSchema of CHIP cannot be submissionType of CONTRACT_AND_RATES',
+            })
+        }
+    })
+
+const validateContractDraftRevisionInput = (
+    draftRevision: ContractDraftRevisionInput,
+    stateCode: string,
     featureFlags?: FeatureFlagSettings
 ): SafeParseReturnType<
     ContractDraftRevisionFormDataInput,
     UpdateDraftContractFormDataType
 > => {
+    const formData = draftRevision.formData
+
     return updateDraftContractFormDataSchema
         .extend({
-            statutoryRegulatoryAttestationDescription: z
-                .string()
-                .optional()
-                .transform((val, ctx) => {
-                    if (featureFlags?.['438-attestation']) {
-                        if (!formData.statutoryRegulatoryAttestation && !val) {
-                            ctx.addIssue({
-                                code: z.ZodIssueCode.custom,
-                                message:
-                                    'statutoryRegulatoryAttestationDescription must be defined when statutoryRegulatoryAttestation is false',
-                            })
-                        }
-
-                        // Clear out existing statutoryRegulatoryAttestationDescription if statutoryRegulatoryAttestation is true
-                        if (formData.statutoryRegulatoryAttestation && val) {
-                            return undefined
-                        }
-                    }
-
-                    return val
-                }),
+            statutoryRegulatoryAttestationDescription:
+                validateStatutoryRegulatoryAttestation(formData, featureFlags),
+            programIDs: validateProgramIDs(stateCode),
+            populationCovered: validatePopulationCovered(formData),
         })
         .safeParse(formData)
 }
 
-export { validateContractDraftRevisionFormDataInput }
+export { validateContractDraftRevisionInput }
