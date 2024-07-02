7cc2f609a7fbc20dbac5984a76589b0673c7a8fa
pearl-truss <67110378+pearl-truss@users.noreply.github.com>
2024-07-02 15:32:13 -0400

    MCR-4143 add unlock contract (#2533)
    * add new types for unlockContract
    
    * add unlockContract resolver, wip
    
    * create and use emailer funcs for unlockContract
    
    * code clean up, return contract
    
    * update frontend to use new unlockContract mutation
    
    * unlock working without emails
    
    * fix email in unlockContract
    
    * add unit test for unlock contract resolver
    
    * Add test for unlockContractCMSEmail
    
    * add test for unlockContractStateEmail
    
    * code cleanup
    
    * add unlockedContract resolvr

---
+++ services/app-api/src/emailer/emails/unlockContractCMSEmail.ts
@@ -0,0 +1,80 @@
+import { formatCalendarDate } from '../../../../app-web/src/common-code/dateHelpers'
+import type { ContractRevisionWithRatesType } from '../../domain-models'
+import { packageName as generatePackageName } from '../../../../app-web/src/common-code/healthPlanFormDataType'
+
+import {
+    stripHTMLFromTemplate,
+    renderTemplate,
+    findContractPrograms,
+    generateCMSReviewerEmailsForContract,
+} from '../templateHelpers'
+import type { EmailData, EmailConfiguration, StateAnalystsEmails } from '../'
+import type { ProgramType, UpdateInfoType } from '../../domain-models'
+
+export const unlockContractCMSEmail = async (
+    contractRev: ContractRevisionWithRatesType,
+    updateInfo: UpdateInfoType,
+    config: EmailConfiguration,
+    stateAnalystsEmails: StateAnalystsEmails,
+    statePrograms: ProgramType[]
+): Promise<EmailData | Error> => {
+    const isTestEnvironment = config.stage !== 'prod'
+    const reviewerEmails = generateCMSReviewerEmailsForContract(
+        config,
+        contractRev,
+        stateAnalystsEmails
+    )
+
+    if (reviewerEmails instanceof Error) {
+        return reviewerEmails
+    }
+
+    //This checks to make sure all programs contained in submission exists for the state.
+    const packagePrograms = findContractPrograms(contractRev, statePrograms)
+    if (packagePrograms instanceof Error) {
+        return packagePrograms
+    }
+
+    const packageName = generatePackageName(
+        contractRev.contract.stateCode,
+        contractRev.contract.stateNumber,
+        contractRev.formData.programIDs,
+        packagePrograms
+    )
+
+    const isContractAndRates =
+        contractRev.formData.submissionType === 'CONTRACT_AND_RATES'
+
+    const data = {
+        packageName,
+        unlockedBy: updateInfo.updatedBy,
+        unlockedOn: formatCalendarDate(updateInfo.updatedAt),
+        unlockedReason: updateInfo.updatedReason,
+        shouldIncludeRates:
+            contractRev.formData.submissionType === 'CONTRACT_AND_RATES',
+        rateInfos:
+            isContractAndRates &&
+            contractRev.rateRevisions.map((rate) => ({
+                rateName: rate.formData.rateCertificationName,
+            })),
+    }
+
+    const result = await renderTemplate<typeof data>(
+        'unlockPackageCMSEmail',
+        data
+    )
+    if (result instanceof Error) {
+        return result
+    } else {
+        return {
+            toAddresses: reviewerEmails,
+            replyToAddresses: [config.helpDeskEmail],
+            sourceEmail: config.emailSource,
+            subject: `${
+                isTestEnvironment ? `[${config.stage}] ` : ''
+            }${packageName} was unlocked`,
+            bodyText: stripHTMLFromTemplate(result),
+            bodyHTML: result,
+        }
+    }
+}
