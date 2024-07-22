dcf47b6e02525b9d57d85fc5af3304db9f7904a5
Hana Worku <hana@truss.works>
2024-07-22 13:21:06 -0500

    Sketch out withdrawRate postgres function

---
+++ services/app-api/src/postgres/contractAndRates/withdrawRate.ts
@@ -0,0 +1,63 @@
+import { findRateWithHistory } from './findRateWithHistory'
+import type { RateType } from '../../domain-models/contractAndRates'
+import type { PrismaTransactionType } from '../prismaTypes'
+
+type WithdrawDateArgsType = {
+    rateID: string
+    withdrawnByUserID: string
+    withdrawReason: string
+}
+
+async function withdrawRate(
+    tx: PrismaTransactionType,
+    args: WithdrawDateArgsType
+): Promise<RateType | Error> {
+    const { rateID, withdrawReason, withdrawnByUserID } = args
+
+    try {
+        // check rate is not already withdrawn
+        const rate = await tx.rateTable.findUniqueOrThrow({
+            where: {
+                id: rateID,
+            },
+        })
+
+        if (rate.withdrawInfoID !== undefined) {
+            const err = `PRISMA ERROR: Cannot withdraw rate more than once. See rate id: ${rateID}`
+            return new Error(err)
+        }
+
+        // generate withdraw info and update rate
+        const currentDateTime = new Date()
+        const withdrawInfo = await tx.updateInfoTable.create({
+            data: {
+                updatedAt: currentDateTime,
+                updatedByID: withdrawnByUserID,
+                updatedReason: withdrawReason,
+            },
+        })
+
+        const updatedRate = await tx.rateTable.update({
+            where: {
+                id: rateID,
+            },
+            data: {
+                withdrawInfo: {
+                    connect: { id: withdrawInfo.id },
+                },
+            },
+        })
+
+        if (updatedRate instanceof Error) {
+            throw updatedRate
+        }
+
+        return findRateWithHistory(tx, rateID)
+    } catch (err) {
+        console.error('Prisma error finding rate to withdraw', err)
+        return new Error(err)
+    }
+}
+
+export { withdrawRate }
+export type { WithdrawDateArgsType }
