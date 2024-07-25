58892f364d6075fc0811db04e0aa3adbd29e47b9
Mojo Talantikite <mojo.talantikite@gmail.com>
2024-07-25 14:43:35 -0400

    more prisma deps

--- services/app-api/scripts/prepare-prisma-layer.sh
+++ services/app-api/scripts/prepare-prisma-layer.sh
@@ -26,18 +26,22 @@ function preparePrismaLayer() {
     rsync -av node_modules/@prisma/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma
     rsync -av node_modules/prisma/ lambda-layers-prisma-client-migration/nodejs/node_modules/prisma
     rsync -av ./../../node_modules/.pnpm/@prisma+client@5.17.0_prisma@5.17.0/node_modules/.prisma/ lambda-layers-prisma-client-migration/nodejs/node_modules/.prisma 
-    rsync -av ./../../node_modules/.pnpm/node_modules/@prisma/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma
-    #cp ./../../node_modules/.pnpm/node_modules/@prisma/engines/libquery_engine-rhel-openssl-3.0.x.so.node lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/
-    #cp ./../../node_modules/.pnpm/node_modules/@prisma/engines/schema-engine-rhel-openssl-3.0.x lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/
-    #cp ./../../node_modules/.pnpm/node_modules/@prisma/engines/package.json lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/package.json
-    #rsync -av ./../../node_modules/.pnpm/node_modules/@prisma/engines/dist/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/dist
+    cp ./../../node_modules/.pnpm/node_modules/@prisma/engines/libquery_engine-rhel-openssl-3.0.x.so.node lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/
+    cp ./../../node_modules/.pnpm/node_modules/@prisma/engines/schema-engine-rhel-openssl-3.0.x lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/
+    cp ./../../node_modules/.pnpm/node_modules/@prisma/engines/package.json lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/package.json
+    rsync -av ./../../node_modules/.pnpm/node_modules/@prisma/engines/dist/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/dist
+    rsync -av ./../../node_modules/.pnpm/node_modules/@prisma/debug/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/debug
+    rsync -av ./../../node_modules/.pnpm/node_modules/@prisma/fetch-engine/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/fetch-engine
+    rsync -av ./../../node_modules/.pnpm/node_modules/@prisma/get-platform/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/get-platform
 
     echo "Prepare Prisma Client Engine lambda layer"
-    rsync -av ./../../node_modules/.pnpm/node_modules/@prisma/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma
     rsync -av node_modules/@prisma/client/ lambda-layers-prisma-client-engine/nodejs/node_modules/@prisma/client
     rsync -av node_modules/prisma/ lambda-layers-prisma-client-engine/nodejs/node_modules/prisma
     rsync -av ./../../node_modules/.pnpm/@prisma+client@5.17.0_prisma@5.17.0/node_modules/.prisma/ lambda-layers-prisma-client-engine/nodejs/node_modules/.prisma 
-    #rsync -av ./../../node_modules/.pnpm/node_modules/@prisma/engines/dist/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/dist
+    rsync -av ./../../node_modules/.pnpm/node_modules/@prisma/engines/dist/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/engines/dist
+    rsync -av ./../../node_modules/.pnpm/node_modules/@prisma/debug/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/debug
+    rsync -av ./../../node_modules/.pnpm/node_modules/@prisma/fetch-engine/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/fetch-engine
+    rsync -av ./../../node_modules/.pnpm/node_modules/@prisma/get-platform/ lambda-layers-prisma-client-migration/nodejs/node_modules/@prisma/get-platform
 
     echo "Copy schema migration files to layer..."
     rsync -av prisma/ lambda-layers-prisma-client-migration/nodejs/prisma
