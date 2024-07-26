00f1642ace2398fe2078aee6f26a4eb0c5f7f03c
Mojo Talantikite <mojo.talantikite@gmail.com>
2024-07-26 13:53:02 -0400

    Fix PrismaTransactionType

--- services/app-api/src/postgres/prismaTypes.ts
+++ services/app-api/src/postgres/prismaTypes.ts
@@ -3,7 +3,7 @@ import type { PrismaClient } from '@prisma/client'
 // This is the type returned by client.$transaction
 type PrismaTransactionType = Omit<
     PrismaClient,
-    '$connect' | '$disconnect' | '$on' | '$transaction' | '$use'
+    '$connect' | '$disconnect' | '$on' | '$transaction' | '$use' | '$extends'
 >
 
 export type { PrismaTransactionType }
