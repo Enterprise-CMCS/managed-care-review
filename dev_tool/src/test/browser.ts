5c368da492629f2f15b2c168b439c44ffecbe0bb
Jason Lin <maolin@truss.works>
2024-08-01 13:30:16 -0400

    Revert COGNITO env var names to match yaml.

--- dev_tool/src/test/browser.ts
+++ dev_tool/src/test/browser.ts
@@ -82,7 +82,7 @@ export async function runBrowserTestsAgainstAWS(
 
     // Now, we've confirmed we are configured to pull data out of serverless x cloudformation
     console.info('Access confirmed. Fetching config vars')
-    const { region, idPool, userPool, userPoolClient, userPoolDomain } =
+    const { region, idPool, userPool, userPoolClient } =
         getWebAuthVars(stageName)
 
     const apiBase = commandMustSucceedSync(
@@ -96,14 +96,6 @@ export async function runBrowserTestsAgainstAWS(
     const appUrl = commandMustSucceedSync(
         './output.sh',
         ['ui', 'CloudFrontEndpointUrl', stageName],
-        {
-            cwd: './services',
-        }
-    )
-
-    const s3Region = commandMustSucceedSync(
-        './output.sh',
-        ['uploads', 'Region', stageName],
         {
             cwd: './services',
         }
@@ -123,12 +115,10 @@ export async function runBrowserTestsAgainstAWS(
     process.env.PORT = '5432' // run hybrid on a different port
     process.env.VITE_APP_AUTH_MODE = apiAuthMode // override local_login in .env
     process.env.VITE_APP_API_URL = apiBase
-    process.env.VITE_APP_COGNITO_REGION = region
-    process.env.VITE_APP_COGNITO_ID_POOL_ID = idPool
-    process.env.VITE_APP_COGNITO_USER_POOL_ID = userPool
-    process.env.VITE_APP_COGNITO_USER_POOL_CLIENT_ID = userPoolClient
-    process.env.VITE_APP_COGNITO_USER_POOL_CLIENT_DOMAIN = userPoolDomain
-    process.env.VITE_APP_S3_REGION = s3Region
+    process.env.COGNITO_REGION = region
+    process.env.COGNITO_IDENTITY_POOL_ID = idPool
+    process.env.COGNITO_USER_POOL_ID = userPool
+    process.env.COGNITO_USER_POOL_WEB_CLIENT_ID = userPoolClient
 
     //run Cypress with configuring baseUrl to appUrl
     const args = ['cypress'].concat(['open', '--config', `baseUrl=${appUrl}`])
--- services/cypress/cypress.config.ts
+++ services/cypress/cypress.config.ts
@@ -43,12 +43,12 @@ module.exports = defineConfig({
             // Configure env for Amplify authorization
             newConfig.env.API_URL = process.env.VITE_APP_API_URL
             newConfig.env.COGNITO_USER_POOL_ID =
-                process.env.VITE_APP_COGNITO_USER_POOL_ID
-            newConfig.env.COGNITO_REGION = process.env.VITE_APP_COGNITO_REGION
+                process.env.COGNITO_USER_POOL_ID
+            newConfig.env.COGNITO_REGION = process.env.COGNITO_REGION
             newConfig.env.COGNITO_IDENTITY_POOL_ID =
-                process.env.VITE_APP_COGNITO_ID_POOL_ID
+                process.env.COGNITO_IDENTITY_POOL_ID
             newConfig.env.COGNITO_USER_POOL_WEB_CLIENT_ID =
-                process.env.VITE_APP_COGNITO_USER_POOL_CLIENT_ID
+                process.env.COGNITO_USER_POOL_WEB_CLIENT_ID
             on('before:browser:launch', (browser, launchOptions) => {
                 prepareAudit(launchOptions)
             })
