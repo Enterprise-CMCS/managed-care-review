518d9c78d3a4a8711bb24fd26e5046a5492e4bb4
Jason Lin <maolin@truss.works>
2024-08-06 07:47:12 -0400

    Run tests for both CMS users and CMS approver users.

--- services/app-web/src/pages/CMSDashboard/RateReviewsDashboard/RateReviewsDashboard.test.tsx
+++ services/app-web/src/pages/CMSDashboard/RateReviewsDashboard/RateReviewsDashboard.test.tsx
@@ -3,54 +3,63 @@ import { RateReviewsDashboard } from './RateReviewsDashboard'
 import {
     fetchCurrentUserMock,
     indexRatesMockFailure,
-    mockValidCMSUser,
+    iterableCmsUsersMockData,
 } from '../../../testHelpers/apolloMocks'
 import { indexRatesMockSuccess } from '../../../testHelpers/apolloMocks'
 import { screen, waitFor } from '@testing-library/react'
 
 describe('RateReviewsDashboard', () => {
-    it('renders dashboard with rates correctly', async () => {
-        renderWithProviders(<RateReviewsDashboard />, {
-            apolloProvider: {
-                mocks: [
-                    fetchCurrentUserMock({
-                        statusCode: 200,
-                        user: mockValidCMSUser(),
-                    }),
-                    indexRatesMockSuccess(),
-                ],
-            },
-        })
+    describe.each(iterableCmsUsersMockData)(
+        '$userRole RateReviewsDashboard tests',
+        ({ userRole, mockUser }) => {
+            it('renders dashboard with rates correctly', async () => {
+                renderWithProviders(<RateReviewsDashboard />, {
+                    apolloProvider: {
+                        mocks: [
+                            fetchCurrentUserMock({
+                                statusCode: 200,
+                                user: mockUser(),
+                            }),
+                            indexRatesMockSuccess(),
+                        ],
+                    },
+                })
 
-        // Wait for accordion and table components to load on page
-        await waitFor(() => {
-            expect(screen.queryByTestId('accordion')).toBeInTheDocument()
-            expect(screen.queryByTestId('table')).toBeInTheDocument()
-        })
+                // Wait for accordion and table components to load on page
+                await waitFor(() => {
+                    expect(
+                        screen.queryByTestId('accordion')
+                    ).toBeInTheDocument()
+                    expect(screen.queryByTestId('table')).toBeInTheDocument()
+                })
 
-        // Expect 3 rates to be displayed
-        expect(
-            screen.getByText('Displaying 3 of 3 rate reviews')
-        ).toBeInTheDocument()
-    })
+                // Expect 3 rates to be displayed
+                expect(
+                    screen.getByText('Displaying 3 of 3 rate reviews')
+                ).toBeInTheDocument()
+            })
 
-    it('renders error failed request page', async () => {
-        renderWithProviders(<RateReviewsDashboard />, {
-            apolloProvider: {
-                mocks: [
-                    fetchCurrentUserMock({
-                        statusCode: 200,
-                        user: mockValidCMSUser(),
-                    }),
-                    indexRatesMockFailure(),
-                ],
-            },
-        })
+            it('renders error failed request page', async () => {
+                renderWithProviders(<RateReviewsDashboard />, {
+                    apolloProvider: {
+                        mocks: [
+                            fetchCurrentUserMock({
+                                statusCode: 200,
+                                user: mockUser(),
+                            }),
+                            indexRatesMockFailure(),
+                        ],
+                    },
+                })
 
-        await waitFor(() => {
-            expect(
-                screen.queryByText("We're having trouble loading this page.")
-            ).toBeInTheDocument()
-        })
-    })
+                await waitFor(() => {
+                    expect(
+                        screen.queryByText(
+                            "We're having trouble loading this page."
+                        )
+                    ).toBeInTheDocument()
+                })
+            })
+        }
+    )
 })
