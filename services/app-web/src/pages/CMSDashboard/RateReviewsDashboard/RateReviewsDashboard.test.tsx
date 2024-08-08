fb2af385177145a78ebee2b032e1737467ad2bc3
Jason Lin <98117700+JasonLin0991@users.noreply.github.com>
2024-08-08 13:28:38 -0400

    MCR-4150: Do not display withdrawn rates on rates dashboard. (#2637)
    * Do not display withdrawn rates on rates dashboard.
    
    * Import the Rate

--- services/app-web/src/pages/CMSDashboard/RateReviewsDashboard/RateReviewsDashboard.test.tsx
+++ services/app-web/src/pages/CMSDashboard/RateReviewsDashboard/RateReviewsDashboard.test.tsx
@@ -4,9 +4,11 @@ import {
     fetchCurrentUserMock,
     indexRatesMockFailure,
     iterableCmsUsersMockData,
+    rateDataMock,
 } from '../../../testHelpers/apolloMocks'
 import { indexRatesMockSuccess } from '../../../testHelpers/apolloMocks'
 import { screen, waitFor } from '@testing-library/react'
+import { Rate } from '../../../gen/gqlClient'
 
 describe('RateReviewsDashboard', () => {
     describe.each(iterableCmsUsersMockData)(
@@ -36,6 +38,57 @@ describe('RateReviewsDashboard', () => {
                 // Expect 3 rates to be displayed
                 expect(
                     screen.getByText('Displaying 3 of 3 rate reviews')
+                ).toBeInTheDocument()
+            })
+
+            it('renders dashboard without withdrawn rates', async () => {
+                const rates: Rate[] = [
+                    { ...rateDataMock(), id: 'test-id-123', stateNumber: 3 },
+                    {
+                        ...rateDataMock(),
+                        id: 'test-id-124-withdrawn',
+                        stateNumber: 2,
+                        withdrawInfo: {
+                            updatedAt: new Date(),
+                            updatedBy: 'admin@example.com',
+                            updatedReason: 'retreat!',
+                        },
+                    },
+                    {
+                        ...rateDataMock(),
+                        id: 'test-id-125-withdrawn',
+                        stateNumber: 2,
+                        withdrawInfo: {
+                            updatedAt: new Date(),
+                            updatedBy: 'admin@example.com',
+                            updatedReason: 'retreat!',
+                        },
+                    },
+                    { ...rateDataMock(), id: 'test-id-126', stateNumber: 1 },
+                ]
+                renderWithProviders(<RateReviewsDashboard />, {
+                    apolloProvider: {
+                        mocks: [
+                            fetchCurrentUserMock({
+                                statusCode: 200,
+                                user: mockUser(),
+                            }),
+                            indexRatesMockSuccess(rates),
+                        ],
+                    },
+                })
+
+                // Wait for accordion and table components to load on page
+                await waitFor(() => {
+                    expect(
+                        screen.queryByTestId('accordion')
+                    ).toBeInTheDocument()
+                    expect(screen.queryByTestId('table')).toBeInTheDocument()
+                })
+
+                // Expect 2 rates to be displayed
+                expect(
+                    screen.getByText('Displaying 2 of 2 rate reviews')
                 ).toBeInTheDocument()
             })
 
