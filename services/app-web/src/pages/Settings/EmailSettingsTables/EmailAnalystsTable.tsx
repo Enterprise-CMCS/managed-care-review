f16e708a30f16e5921e5199d682cb22356fa75f9
Jason Lin <maolin@truss.works>
2024-08-22 16:47:24 -0400

    Copy change for state analyst setting page.

--- services/app-web/src/pages/Settings/EmailSettingsTables/EmailAnalystsTable.tsx
+++ services/app-web/src/pages/Settings/EmailSettingsTables/EmailAnalystsTable.tsx
@@ -13,7 +13,7 @@ import {
     FilterSelect,
     FilterSelectedOptionsType,
 } from '../../../components/FilterAccordion'
-import { DoubleColumnGrid } from '../../../components'
+import { DoubleColumnGrid, LinkWithLogging } from '../../../components'
 import { formatEmails } from './EmailSettingsTables'
 import { Table } from '@trussworks/react-uswds'
 
@@ -21,6 +21,7 @@ import styles from '../Settings.module.scss'
 import { pluralize } from '../../../common-code/formatters'
 import { useTealium } from '../../../hooks'
 import useDeepCompareEffect from 'use-deep-compare-effect'
+import { useStringConstants } from '../../../hooks/useStringConstants'
 
 type StateAnalystsInDashboardType = {
     emails: string[]
@@ -43,6 +44,8 @@ const EmailAnalystsTable = ({
         filters: columnFilters,
     })
     const { logFilterEvent } = useTealium()
+    const stringConstants = useStringConstants()
+    const MAIL_TO_SUPPORT = stringConstants.MAIL_TO_SUPPORT
 
     const tableColumns = useMemo(
         () => [
@@ -143,8 +146,19 @@ const EmailAnalystsTable = ({
         <>
             <h2>State Analyst emails</h2>
             <p>
-                State analysts email settings. Currently a standalone
-                configuration based on the state programs spreadsheet.
+                Below is a list of the DMCO staff assigned to states. If this
+                list is out of date please contact
+                <span>
+                    <LinkWithLogging
+                        href={`mailto: ${MAIL_TO_SUPPORT}`}
+                        variant="unstyled"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        {' '}
+                        {MAIL_TO_SUPPORT}
+                    </LinkWithLogging>
+                </span>
             </p>
 
             <DoubleColumnGrid>
