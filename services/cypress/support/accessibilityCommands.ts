36ceb712fe827d7466dd78d3b3c440d982855788
Jason Lin <maolin@truss.works>
2024-08-28 22:55:47 -0400

    Add accessibility commands

---
+++ services/cypress/support/accessibilityCommands.ts
@@ -0,0 +1,32 @@
+function terminalLog(violations: Record<string, any>[]) {
+    console.log(violations)
+    cy.task(
+        'log',
+        `${violations.length} accessibility violation${
+            violations.length === 1 ? '' : 's'
+        } ${violations.length === 1 ? 'was' : 'were'} detected`
+    )
+    // pluck specific keys to keep the table readable
+    const violationData: Record<string, any>[] = violations.map(
+        ({ id, impact, description, nodes }) => ({
+            id,
+            impact,
+            description,
+            nodes: nodes.length
+        })
+    )
+
+    cy.task('table', violationData)
+}
+
+Cypress.Commands.add(
+    'checkA11yWithSection508',
+    () => {
+        cy.checkA11y('', {
+            runOnly: {
+                type: 'tag',
+                values: ['section508']
+            },
+        }, terminalLog)
+    }
+)
