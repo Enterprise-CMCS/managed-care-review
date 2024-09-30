cab9f0b10d0f955d743ced0958d0cd25d824b51b
Jason Lin <maolin@truss.works>
2024-09-30 15:00:07 -0400

    Edit state assignments test

--- services/cypress/integration/adminWorkflow/settings.spec.ts
+++ services/cypress/integration/adminWorkflow/settings.spec.ts
@@ -1,5 +1,3 @@
-import {interceptors} from 'axios';
-
 describe('Admin user can view application level settings', () => {
     beforeEach(() => {
         cy.stubFeatureFlags()
@@ -17,11 +15,11 @@ describe('Admin user can view application level settings', () => {
         cy.findByRole('table', {name: 'Division assignments'}).should('exist')
         cy.findByText('Zuko').should('exist')
         cy.findAllByText('Hotman').should('have.length.at.least', 1)
-        cy.assignDivisionToCMSUser({userEmail: 'zuko@example.com', division: 'DMCO'})
+        cy.assignDivisionToCMSUser({cmsUser: 'ZUKO', division: 'DMCO'})
         cy.findByText('Zuko').should('exist').siblings().should('include.text', 'selected.DMCO') // not accesible but dropdowns are not good idea
-        cy.assignDivisionToCMSUser({userEmail: 'zuko@example.com', division: 'DMCP'})
+        cy.assignDivisionToCMSUser({cmsUser: 'ZUKO', division: 'DMCP'})
         cy.findByText('Zuko').should('exist').siblings().should('include.text', 'selected.DMCP') // not accesible but dropdowns are not good idea
-        cy.assignDivisionToCMSUser({userEmail: 'zuko@example.com', division: 'OACT'})
+        cy.assignDivisionToCMSUser({cmsUser: 'ZUKO', division: 'OACT'})
         cy.findByText('Zuko').should('exist').siblings().should('include.text', 'selected.OACT') // not accesible but dropdowns are not good idea
     })
 
@@ -57,7 +55,90 @@ describe('Admin user can view application level settings', () => {
         // Table data is also less rows long
         cy.findAllByRole('row').should('have.length.lessThan', lengthBeforeFilter)
         });
+    })
 
+    it('can update state assignment and see in on the state assignment table', () => {
+        cy.interceptFeatureFlags({'read-write-state-assignments': true})
 
+        // // make sure cms users are in db first
+        cy.logInAsCMSUser({ cmsUser: 'ZUKO', initialURL: '/'})
+        cy.logOut()
+
+        cy.logInAsCMSUser({ cmsUser: 'AZULA', initialURL: '/'})
+        cy.logOut()
+
+        // Assign DMCO division to CMS users.
+        cy.logInAsAdminUser({initialURL: '/mc-review-settings'})
+        cy.findByRole('link', { name: 'Division assignments'}).click()
+        cy.wait('@indexUsersQuery', { timeout: 20_000 })
+        cy.findByRole('table', {name: 'Division assignments'}).should('exist')
+
+        cy.findByText('Zuko').should('exist')
+        cy.findAllByText('Hotman').should('have.length.at.least', 1)
+        cy.assignDivisionToCMSUser({cmsUser: 'ZUKO', division: 'DMCO'})
+        cy.findByText('Zuko').should('exist').siblings().should('include.text', 'selected.DMCO') // not accesible but dropdowns are not good idea
+
+        cy.findByText('Azula').should('exist')
+        cy.findAllByText('Hotman').should('have.length.at.least', 1)
+        cy.assignDivisionToCMSUser({cmsUser: 'AZULA', division: 'DMCO'})
+        cy.findByText('Azula').should('exist').siblings().should('include.text', 'selected.DMCO') // not accesible but dropdowns are not good idea
+        cy.findByRole('link', { name: 'State assignments'}).should('exist').click()
+        cy.findByRole('table', { name: 'State assignments' }).should('exist')
+
+        // Navigate to edit AL state assignments
+        cy.findByTestId('edit-link-AL').should('exist').click()
+        cy.wait('@fetchMcReviewSettingsQuery')
+        cy.findByText('Edit state assignment')
+        cy.findByText('AL')
+
+        //Clear out existing assignments
+        cy.get('[class*="select__clear-indicator"]').should('exist').click()
+
+        // Assign Zuko
+        cy.findByRole('combobox').should('exist').click()
+        cy.findByRole('option', { name: 'Zuko Hotman'}).should('exist').click()
+
+        // Assign Azula
+        cy.findByRole('combobox').should('exist').click()
+        cy.findByRole('option', { name: 'Azula Hotman'}).should('exist').click()
+
+        // Save changes
+        cy.findByRole('button', { name: 'Save changes'}).should('exist').click()
+        cy.wait('@fetchMcReviewSettingsQuery')
+
+        // Check for confirmation banner.
+        cy.findByText(`Alabama's assigned staff has been updated`)
+        cy.findByText(`Zuko Hotman was assigned to this state`)
+        cy.findByText(`Azula Hotman was assigned to this state`)
+
+        // Check to AL assignments have been saved.
+        cy.findAllByRole('row').should('exist').eq(1).within(row => {
+            cy.findByText('AL')
+            cy.findByText('Zuko Hotman, Azula Hotman')
+        })
+
+        // Remove Azula from assignment
+        cy.findByTestId('edit-link-AL').should('exist').click()
+        cy.wait('@fetchMcReviewSettingsQuery')
+        cy.findByText('Edit state assignment')
+        cy.findByText('AL')
+
+        // Remove Azula
+        cy.findByRole('button', { name: 'Remove Azula Hotman'}).should('exist').click()
+
+        // Save changes
+        cy.findByRole('button', { name: 'Save changes'}).should('exist').click()
+        cy.wait('@fetchMcReviewSettingsQuery')
+
+        // Check for confirmation banner.
+        cy.findByText(`Alabama's assigned staff has been updated`)
+        cy.findByText(`Zuko Hotman was assigned to this state`)
+        cy.findByText(`Azula Hotman was removed`)
+
+        // Check to AL assignments have been saved.
+        cy.findAllByRole('row').should('exist').eq(1).within(row => {
+            cy.findByText('AL')
+            cy.findByText('Zuko Hotman')
+        })
     })
 })
