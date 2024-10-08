c22cde6edae46303ef118945fda1b6a744b87ed8
Jason Lin <maolin@truss.works>
2024-10-08 02:24:46 -0400

    Update formatCalendarDate to handle date display in ET.

--- services/app-web/src/common-code/dateHelpers/calendarDate.ts
+++ services/app-web/src/common-code/dateHelpers/calendarDate.ts
@@ -2,11 +2,13 @@ import { dayjs } from './dayjs'
 
 // We store calendar dates (dates with no time info) in UTC for consistency.
 // This formats a date correctly like '12/02/2022' for display.
-function formatCalendarDate(date: Date | undefined): string {
-    if (!date) {
+// There are two timezones
+function formatCalendarDate(date: Date | undefined | string, timeZone?: 'UTC' | 'America/New_York'): string {
+    const tz = timeZone || 'UTC'
+    if (!date || !dayjs(date).isValid()) {
         return ''
     }
-    return dayjs(date).tz('UTC').format('MM/DD/YYYY')
+    return dayjs(date).tz(tz).format('MM/DD/YYYY')
 }
 
 function formatRateNameDate(date: Date | undefined): string {
