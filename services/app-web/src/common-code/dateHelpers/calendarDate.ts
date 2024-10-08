3bdcddd6dc45b5da753ee7b17e1202e0ee67f24e
Jason Lin <maolin@truss.works>
2024-10-08 13:44:16 -0400

    Add format date helper for date with time.

--- services/app-web/src/common-code/dateHelpers/calendarDate.ts
+++ services/app-web/src/common-code/dateHelpers/calendarDate.ts
@@ -1,14 +1,21 @@
 import { dayjs } from './dayjs'
 
-// We store calendar dates (dates with no time info) in UTC for consistency.
-// This formats a date correctly like '12/02/2022' for display.
-// There are two timezones
+// We store calendar dates in UTC for consistency. This formats a date like '12/02/2022'.
+// These formatted dates, should only be used for display, never saved to the database.
 function formatCalendarDate(date: Date | undefined | string, timeZone?: 'UTC' | 'America/New_York'): string {
     const tz = timeZone || 'UTC'
     if (!date || !dayjs(date).isValid()) {
         return ''
     }
     return dayjs(date).tz(tz).format('MM/DD/YYYY')
+}
+
+function formatDateTime(date: Date | undefined | string, timeZone?: 'UTC' | 'America/New_York'): string {
+    const tz = timeZone || 'America/New_York'
+    if (!date || !dayjs(date).isValid()) {
+        return ''
+    }
+    return dayjs(date).tz(tz).format('MM/DD/YYYY h:mma')
 }
 
 function formatRateNameDate(date: Date | undefined): string {
@@ -18,4 +25,4 @@ function formatRateNameDate(date: Date | undefined): string {
     return dayjs(date).tz('UTC').format('YYYYMMDD')
 }
 
-export { formatCalendarDate, formatRateNameDate }
+export { formatCalendarDate, formatRateNameDate, formatDateTime }
