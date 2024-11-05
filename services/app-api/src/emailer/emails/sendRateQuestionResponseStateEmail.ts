a96bc4446743dcd503dcc4421ad57a3fd66a80d0
Jason Lin <maolin@truss.works>
2024-11-05 10:39:23 -0500

    new rate response state email function

---
+++ services/app-api/src/emailer/emails/sendRateQuestionResponseStateEmail.ts
@@ -0,0 +1,85 @@
+import { formatCalendarDate } from '../../../../app-web/src/common-code/dateHelpers'
+import { pruneDuplicateEmails } from '../formatters'
+import type { EmailConfiguration, EmailData } from '..'
+import type { RateQuestionType, RateType} from '../../domain-models'
+import {
+    stripHTMLFromTemplate,
+    renderTemplate,
+    getQuestionRound, getActuaryContactEmails, getRateSubmitterEmails, getRateStateContactEmails,
+} from '../templateHelpers'
+import { rateQuestionResponseURL } from '../generateURLs'
+
+export const sendRateQuestionResponseStateEmail = async (
+    rate: RateType,
+    config: EmailConfiguration,
+    questions: RateQuestionType[],
+    currentQuestion: RateQuestionType
+): Promise<EmailData | Error> => {
+    const rateFormData = rate.packageSubmissions[0].rateRevision.formData
+    const parentContractID = rate.parentContractID
+    const shouldIncludeActuaries =
+        rateFormData.actuaryCommunicationPreference === 'OACT_TO_ACTUARY'
+    const division = currentQuestion.division
+
+    if (!rateFormData.rateCertificationName) {
+        return new Error(
+            'Error getting rate name. Rate certification name was undefined.'
+        )
+    }
+
+    const submitterEmails = getRateSubmitterEmails(rate)
+    const stateContactEmails = getRateStateContactEmails(rate)
+    const actuaryEmails = shouldIncludeActuaries
+        ? getActuaryContactEmails(rate)
+        : []
+
+    const toAddresses = pruneDuplicateEmails([
+        ...submitterEmails,
+        ...stateContactEmails,
+        ...actuaryEmails,
+    ])
+
+    const questionRound = getQuestionRound(
+        questions,
+        currentQuestion
+    )
+
+    if (questionRound instanceof Error) {
+        return questionRound
+    }
+
+    const data = {
+        packageName: rateFormData.rateCertificationName,
+        questionResponseURL: rateQuestionResponseURL(
+            parentContractID,
+            currentQuestion.rateID,
+            config.baseUrl
+        ),
+        cmsRequestorDivision: division,
+        dateAsked: formatCalendarDate(
+            currentQuestion.createdAt,
+            'America/New_York'
+        ),
+        questionRound,
+    }
+
+    const result = await renderTemplate<typeof data>(
+        'sendRateQuestionResponseStateEmail',
+        data
+    )
+
+    if (result instanceof Error) {
+        return result
+    } else {
+        return {
+            toAddresses: toAddresses,
+            replyToAddresses: [],
+            sourceEmail: config.emailSource,
+            subject: `${
+                config.stage !== 'prod' ? `[${config.stage}] ` : ''
+            }Response submitted to CMS for ${rateFormData.rateCertificationName}`,
+            bodyText: stripHTMLFromTemplate(result),
+            bodyHTML: result,
+        }
+    }
+}
