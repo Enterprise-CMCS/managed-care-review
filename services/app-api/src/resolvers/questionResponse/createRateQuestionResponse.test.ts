1f083ab118c54e5dfc82c74cc5b1d4e1021535b1
pearl-truss <pearl@truss.works>
2024-11-06 09:53:47 -0500

    begin adding email test to resolver

--- services/app-api/src/resolvers/questionResponse/createRateQuestionResponse.test.ts
+++ services/app-api/src/resolvers/questionResponse/createRateQuestionResponse.test.ts
@@ -8,7 +8,11 @@ import {
     createTestRateQuestionResponse,
 } from '../../testHelpers/gqlHelpers'
 import { createAndSubmitTestContractWithRate } from '../../testHelpers/gqlContractHelpers'
-import { assertAnError, assertAnErrorCode } from '../../testHelpers'
+import { assertAnError, assertAnErrorCode, must } from '../../testHelpers'
+import { testEmailConfig, testEmailer } from '../../testHelpers/emailerHelpers'
+import { getTestStateAnalystsEmails } from '../../testHelpers/parameterStoreHelpers'
+// import { testLDService } from '../../testHelpers/launchDarklyHelpers'
+// import { sharedTestPrismaClient } from '../../testHelpers/storeHelpers'
 
 describe('createRateQuestionResponse', () => {
     const cmsUser = testCMSUser()
@@ -106,6 +110,60 @@ describe('createRateQuestionResponse', () => {
         expect(assertAnErrorCode(questionResponseResult)).toBe('FORBIDDEN')
         expect(assertAnError(questionResponseResult).message).toBe(
             'user not authorized to create a question response'
+        )
+    })
+
+    it('sends CMS email', async () => {
+        const emailConfig = testEmailConfig()
+        const mockEmailer = testEmailer(emailConfig)
+        const oactCMS = testCMSUser({
+            divisionAssignment: 'OACT' as const,
+        })
+        const stateServer = await constructTestPostgresServer({
+            emailer: mockEmailer,
+        })
+        const cmsServer = await constructTestPostgresServer({
+            context: {
+                user: oactCMS,
+            },
+            emailer: mockEmailer,
+        })
+
+        const submittedContractAndRate =
+            await createAndSubmitTestContractWithRate(stateServer)
+        const rateRevision =
+            submittedContractAndRate.packageSubmissions[0].rateRevisions[0]
+        const rateID = rateRevision.rateID
+
+        must(await createTestRateQuestion(cmsServer, rateID))
+
+        const rateName = rateRevision.formData.rateCertificationName
+        const stateAnalystsEmails = getTestStateAnalystsEmails(
+            submittedContractAndRate.stateCode
+        )
+        const cmsRecipientEmails = [
+            ...stateAnalystsEmails,
+            ...emailConfig.devReviewTeamEmails,
+            ...emailConfig.oactEmails,
+        ]
+
+        expect(mockEmailer.sendEmail).toHaveBeenNthCalledWith(
+            5, // New response CMS email notification is the fifth email
+            expect.objectContaining({
+                subject: expect.stringContaining(
+                    `[LOCAL] New Responses for ${rateName}`
+                ),
+                sourceEmail: emailConfig.emailSource,
+                toAddresses: expect.arrayContaining(
+                    Array.from(cmsRecipientEmails)
+                ),
+                bodyText: expect.stringContaining(
+                    `The state submitted responses to OACT's questions about ${rateName}`
+                ),
+                bodyHTML: expect.stringContaining(
+                    `<a href="http://localhost/submissions/${rateID}/question-and-answers">View submission Q&A</a>`
+                ),
+            })
         )
     })
 })
