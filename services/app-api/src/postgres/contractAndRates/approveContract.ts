e2584f0d9e361ea1783c01c09bf1b3d01f673969
pearl-truss <pearl@truss.works>
2024-11-13 14:41:34 -0500

    create approveContract postgres function

---
+++ services/app-api/src/postgres/contractAndRates/approveContract.ts
@@ -0,0 +1,68 @@
+import { findContractWithHistory } from './findContractWithHistory'
+import { NotFoundError } from '../postgresErrors'
+import type { ContractType } from '../../domain-models/contractAndRates'
+import type { PrismaTransactionType } from '../prismaTypes'
+
+async function approveContractInsideTransaction(
+    tx: PrismaTransactionType,
+    args: ApproveContractArgsType
+): Promise<ContractType | Error> {
+    const { contractID, updatedByID } = args
+    try {
+        // Find the latest contract revision with contractID and with no submitInfo,
+        const contract = await tx.contractTable.findFirst({
+            where: {
+                id: contractID,
+            },
+            include: {
+                reviewStatusActions: true,
+            },
+        })
+
+        if (!contract) {
+            const err = `PRISMA ERROR: Cannot find the current rev to update with contract id: ${contractID}`
+            console.error(err)
+            return new NotFoundError(err)
+        }
+
+        // generate approval notice info and update contract
+        const currentDateTime = new Date()
+        const approvalNotice = await tx.contractActionTable.create({
+            data: {
+                updatedAt: currentDateTime,
+                updatedByID: updatedByID,
+                updatedReason: '',
+                actionType: 'APPROVAL_NOTICE',
+                contractID: contractID,
+            },
+        })
+
+        const updatedContract = await tx.contractTable.update({
+            where: {
+                id: contractID,
+            },
+            data: {
+                reviewStatusActions: {
+                    connect: { id: approvalNotice.id },
+                },
+            },
+        })
+
+        if (updatedContract instanceof Error) {
+            return updatedContract
+        }
+
+        return findContractWithHistory(tx, contractID)
+    } catch (err) {
+        console.error('Prisma error finding contract to approve', err)
+        return new Error(err)
+    }
+}
+
+type ApproveContractArgsType = {
+    contractID: string
+    updatedByID: string
+}
+
+export { approveContractInsideTransaction }
+export type { ApproveContractArgsType }
