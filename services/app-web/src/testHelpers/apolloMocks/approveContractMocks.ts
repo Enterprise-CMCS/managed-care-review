4929bb08622a11fa8264c36ffca15e30d2fdfbfd
Jason Lin <98117700+JasonLin0991@users.noreply.github.com>
2024-11-20 10:37:23 -0500

    MCR-4590: cms and state sees approval banner (#2948)
    * Add new banner component
    
    * Order reviewStatusActions by latest first.
    
    * Add approval test mock
    
    * Show approval banner
    
    * Tests for approval function, approval banner, and refactor iterable tests.
    
    * Add another timeout for vitest to look for loading state
    
    * Add optional note to approval banner.

---
+++ services/app-web/src/testHelpers/apolloMocks/approveContractMocks.ts
@@ -0,0 +1,64 @@
+import {MockedResponse} from '@apollo/client/testing';
+import {ApproveContractDocument, ApproveContractMutation, Contract} from '../../gen/gqlClient';
+import {mockContractPackageSubmittedWithQuestions} from './contractPackageDataMock';
+
+const approveContractMockSuccess = (params: {
+    contractID?: string,
+    contractData?: Partial<Contract>,
+    updatedReason?: string
+} = {}): MockedResponse<ApproveContractMutation> => {
+    const {
+        contractID= 'test-abc-123',
+        contractData,
+        updatedReason = 'Approve contract'
+    } = params
+    const contract =
+        mockContractPackageSubmittedWithQuestions(
+            contractID,
+            {
+                __typename: 'Contract',
+                status: 'RESUBMITTED',
+                reviewStatus: 'APPROVED',
+                reviewStatusActions: [
+                    {
+                        __typename: 'ContractReviewStatusActions',
+                        actionType: 'APPROVAL_NOTICE',
+                        contractID: contractID,
+                        updatedAt: new Date(),
+                        updatedBy: {
+                            __typename: 'UpdatedBy',
+                            email: 'cmsapprover@example.com',
+                            familyName: 'Smith',
+                            givenName: 'John',
+                            role: 'CMS_APPROVER_USER'
+                        },
+                        updatedReason: 'You are approved'
+                    }
+                ],
+                ...contractData
+            }
+        )
+
+    return {
+        request: {
+            query: ApproveContractDocument,
+            variables: {
+                input: {
+                    contractID: 'test-abc-123',
+                    updatedReason: updatedReason
+                }
+            }
+        },
+        result: {
+            data: {
+                approveContract: {
+                    contract
+                }
+            }
+        }
+    }
+}
+
+export {
+    approveContractMockSuccess
+}
