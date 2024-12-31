1c92fc2704142328d5d9bb2fbea77b145cc25570
pearl-truss <67110378+pearl-truss@users.noreply.github.com>
2024-12-31 13:16:45 -0500

    MCR-4790 withdraw api used in UI (#3035)
    * Add WithdrawnRatesJoinTable in schema and add migration.
    
    * Fix dev command
    
    * Add withdrawnRates to postgres queries
    
    * Add withdrawnRates to graphql schema
    
    * Add withdrawnRates to domain type and update parser functions.
    
    * Update withdrawRate resolver, postgres function, and add tests.
    
    * Add check for withdrawn rates on submit resolver.
    
    * Update test data
    
    * Resubmit the rate before adding review action.
    
    * Update test to not care about state assignment order.
    
    * Remove validation of withdrawn rates as withdrawing will automatically remove these from the draft or unlocked rate.
    
    * Fix finding cms user assignment names.
    
    * withdraw API pulled in, banner integrated, working on test
    
    * add forgotten file
    
    * fix web test
    
    * remove withdraw button when parent contract has been approved
    
    * cleanup logic on showing withdraw rate btn
    
    ---------
    
    Co-authored-by: Jason Lin <maolin@truss.works>

--- services/app-web/src/pages/RateSummary/RateSummary.tsx
+++ services/app-web/src/pages/RateSummary/RateSummary.tsx
@@ -21,7 +21,7 @@ import { SingleRateSummarySection } from '../../components/SubmissionSummarySect
 import { useAuth } from '../../contexts/AuthContext'
 import { ErrorForbiddenPage } from '../Errors/ErrorForbiddenPage'
 import { Error404 } from '../Errors/Error404Page'
-import { RateWithdrawnBanner } from '../../components/Banner'
+import { RateReplacedBanner, RateWithdrawBanner } from '../../components/Banner'
 import { hasCMSUserPermissions } from '@mc-review/helpers'
 import { useLDClient } from 'launchdarkly-react-client-sdk'
 import { featureFlags } from '@mc-review/common-code'
@@ -172,7 +172,15 @@ export const RateSummary = (): React.ReactElement => {
     const parentContractSubmissionID = rate.parentContractID
     const isUnlocked = rate.status === 'UNLOCKED'
     const parentContractIsApproved = contract.consolidatedStatus === 'APPROVED'
-
+    const latestRateAction = rate.reviewStatusActions?.[0]
+    const showWithdrawBanner =
+        showWithdrawRate &&
+        latestRateAction &&
+        rate.consolidatedStatus === 'WITHDRAWN'
+    const showWithdrawRateBtn =
+        showWithdrawRate &&
+        rate.consolidatedStatus !== 'WITHDRAWN' &&
+        !parentContractIsApproved
     return (
         <div className={styles.background}>
             <GridContainer
@@ -180,9 +188,16 @@ export const RateSummary = (): React.ReactElement => {
                 className={styles.container}
             >
                 {withdrawInfo && (
-                    <RateWithdrawnBanner
+                    <RateReplacedBanner
                         withdrawInfo={withdrawInfo}
                         className={styles.banner}
+                    />
+                )}
+                {showWithdrawBanner && (
+                    <RateWithdrawBanner
+                        updatedAt={latestRateAction.updatedAt}
+                        updatedBy={latestRateAction.updatedBy}
+                        reasonForWithdraw={latestRateAction.updatedReason}
                     />
                 )}
                 {isStateUser && (
@@ -233,7 +248,7 @@ export const RateSummary = (): React.ReactElement => {
                                     Unlock rate
                                 </UnlockRateButton>
                             )}
-                            {showWithdrawRate && (
+                            {showWithdrawRateBtn && (
                                 <ButtonWithLogging
                                     disabled={isUnlocked}
                                     className="usa-button usa-button--outline"
--- services/app-web/src/pages/RateWithdraw/RateWithdraw.test.tsx
+++ services/app-web/src/pages/RateWithdraw/RateWithdraw.test.tsx
@@ -2,7 +2,12 @@ import { renderWithProviders } from '../../testHelpers'
 import {
     fetchCurrentUserMock,
     mockValidCMSUser,
+    fetchContractMockSuccess,
     fetchRateMockSuccess,
+    withdrawRateMockFailure,
+    mockContractPackageSubmitted,
+    mockRateSubmittedWithQuestions,
+    withdrawRateMockSuccess,
 } from '@mc-review/mocks'
 import { Route, Routes } from 'react-router'
 import { RoutesRecord } from '@mc-review/constants'
@@ -10,8 +15,164 @@ import { RateSummary } from '../SubmissionSummary'
 import { RateWithdraw } from './RateWithdraw'
 import { waitFor, screen } from '@testing-library/react'
 import { RateSummarySideNav } from '../SubmissionSideNav/RateSummarySideNav'
+import { Rate } from '../../gen/gqlClient'
 
 describe('RateWithdraw', () => {
+    it('can withdraw a rate', async () => {
+        const contract = mockContractPackageSubmitted({
+            id: 'test-abc-123',
+        })
+        const rate = mockRateSubmittedWithQuestions({
+            id: 'test-abc-123',
+            parentContractID: 'test-abc-123',
+        })
+        const withdrawnRate: Rate = {
+            ...rate,
+            reviewStatus: 'WITHDRAWN',
+            consolidatedStatus: 'WITHDRAWN',
+            reviewStatusActions: [
+                {
+                    __typename: 'RateReviewStatusActions',
+                    actionType: 'WITHDRAW',
+                    rateID: rate.id,
+                    updatedReason: 'a valid note',
+                    updatedAt: new Date(),
+                    updatedBy: {
+                        __typename: 'UpdatedBy',
+                        email: 'cmsapprover@example.com',
+                        familyName: 'Smith',
+                        givenName: 'John',
+                        role: 'CMS_APPROVER_USER',
+                    },
+                },
+            ],
+        }
+
+        const { user } = renderWithProviders(
+            <Routes>
+                <Route element={<RateSummarySideNav />}>
+                    <Route
+                        path={RoutesRecord.RATES_SUMMARY}
+                        element={<RateSummary />}
+                    />
+                    <Route
+                        path={RoutesRecord.RATE_WITHDRAW}
+                        element={<RateWithdraw />}
+                    />
+                </Route>
+            </Routes>,
+            {
+                apolloProvider: {
+                    mocks: [
+                        fetchCurrentUserMock({
+                            user: mockValidCMSUser(),
+                            statusCode: 200,
+                        }),
+                        fetchContractMockSuccess({
+                            contract,
+                        }),
+                        fetchRateMockSuccess(rate),
+                        withdrawRateMockSuccess({ rateData: rate }),
+                        fetchRateMockSuccess(withdrawnRate),
+                        fetchContractMockSuccess({
+                            contract,
+                        }),
+                        fetchCurrentUserMock({
+                            user: mockValidCMSUser(),
+                            statusCode: 200,
+                        }),
+                    ],
+                },
+                routerProvider: {
+                    route: '/rate-reviews/test-abc-123/withdraw-rate',
+                },
+                featureFlags: {
+                    'withdraw-rate': true,
+                },
+            }
+        )
+
+        await waitFor(() => {
+            expect(screen.getByText('Withdraw a rate')).toBeInTheDocument()
+            expect(
+                screen.getByRole('button', { name: 'Withdraw rate' })
+            ).toBeInTheDocument()
+        })
+
+        const withdrawBtn = screen.getByRole('button', {
+            name: 'Withdraw rate',
+        })
+        const withdrawReasonInput = screen.getByTestId('rateWithdrawReason')
+        await user.type(withdrawReasonInput, 'a valid note')
+
+        await user.click(withdrawBtn)
+
+        await waitFor(() => {
+            expect(screen.getByTestId('rateWithdrawBanner')).toBeInTheDocument()
+        })
+    })
+
+    it('renders generic API error on failed withdraw', async () => {
+        const contract = mockContractPackageSubmitted({ id: 'test-abc-123' })
+        const rate = mockRateSubmittedWithQuestions({
+            id: 'test-abc-123',
+            parentContractID: 'test-abc-123',
+        })
+
+        const { user } = renderWithProviders(
+            <Routes>
+                <Route element={<RateSummarySideNav />}>
+                    <Route
+                        path={RoutesRecord.RATES_SUMMARY}
+                        element={<RateSummary />}
+                    />
+                    <Route
+                        path={RoutesRecord.RATE_WITHDRAW}
+                        element={<RateWithdraw />}
+                    />
+                </Route>
+            </Routes>,
+            {
+                apolloProvider: {
+                    mocks: [
+                        fetchCurrentUserMock({
+                            user: mockValidCMSUser(),
+                            statusCode: 200,
+                        }),
+                        fetchContractMockSuccess({
+                            contract,
+                        }),
+                        fetchRateMockSuccess(rate),
+                        withdrawRateMockFailure(),
+                    ],
+                },
+                routerProvider: {
+                    route: '/rate-reviews/test-abc-123/withdraw-rate',
+                },
+                featureFlags: {
+                    'withdraw-rate': true,
+                },
+            }
+        )
+
+        await waitFor(() => {
+            expect(
+                screen.queryByTestId('rateWithdrawReason')
+            ).toBeInTheDocument()
+        })
+
+        const withdrawBtn = screen.getByRole('button', {
+            name: 'Withdraw rate',
+        })
+        const dateInput = screen.getByTestId('rateWithdrawReason')
+        await user.type(dateInput, 'withdraw rate')
+
+        await user.click(withdrawBtn)
+        await waitFor(() => {
+            expect(screen.getByTestId('error-alert')).toBeInTheDocument()
+        })
+    })
+
     it('renders form validation error when required withdraw reason field is missing', async () => {
         const { user } = renderWithProviders(
             <Routes>
