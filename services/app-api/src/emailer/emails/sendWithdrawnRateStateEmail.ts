78bc01a56a4287c357b690fda7314b142e2cfb17
Jason Lin <maolin@truss.works>
2025-01-08 17:54:11 -0500

    Use common validation function

--- services/app-api/src/emailer/emails/sendWithdrawnRateStateEmail.ts
+++ services/app-api/src/emailer/emails/sendWithdrawnRateStateEmail.ts
@@ -2,14 +2,9 @@ import { formatCalendarDate } from '@mc-review/dates'
 import type { ProgramType, RateType } from '../../domain-models'
 import type { EmailData } from '../emailer'
 import type { EmailConfiguration } from '../emailer'
-import {
-    findContractPrograms,
-    renderTemplate,
-    stripHTMLFromTemplate,
-} from '../templateHelpers'
-import { packageName as generatePackageName } from '@mc-review/hpp'
-import { submissionSummaryURL } from '../generateURLs'
+import { renderTemplate, stripHTMLFromTemplate } from '../templateHelpers'
 import { pruneDuplicateEmails } from '../formatters'
+import { validateAndParseWithdrawnRate } from './sendWithdrawnRateCMSEmail'
 
 type WithdrawnFromContractData = {
     contractName: string
@@ -29,76 +24,23 @@ export const sendWithdrawnRateStateEmail = async (
     rate: RateType,
     statePrograms: ProgramType[]
 ): Promise<EmailData | Error> => {
-    if (rate.consolidatedStatus !== 'WITHDRAWN') {
-        return new Error('Rate consolidated status is not WITHDRAWN')
+    // validate the withdrawn rate
+    const withdrawnRateData = validateAndParseWithdrawnRate(
+        rate,
+        statePrograms,
+        config
+    )
+
+    if (withdrawnRateData instanceof Error) {
+        return withdrawnRateData
     }
 
-    //check to make sure rate has a withdrawn action
-    const latestRateRev = rate.packageSubmissions[0].rateRevision
-    const reviewStatusActions = rate.reviewStatusActions
-
-    if (!reviewStatusActions || reviewStatusActions.length === 0) {
-        return new Error('Rate does not any have review actions')
-    }
-
-    const latestAction = reviewStatusActions.sort(
-        (a, b) =>
-            new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()
-    )[0]
-
-    if (latestAction.actionType !== 'WITHDRAW') {
-        return new Error('Rate latest review action was not a withdraw action')
-    }
-
-    // These contracts will not include linked contracts that are unlocked or in draft
-    const withdrawnFromContracts = rate.withdrawnFromContracts
-
-    if (!withdrawnFromContracts || withdrawnFromContracts.length === 0) {
-        return new Error(
-            'Rate was withdrawn, but was not associated with any contracts'
-        )
-    }
-
-    const stateContactEmails: string[] = []
-    const withdrawnFromContractData = []
-
-    // parse contract data and collect state contact emails
-    for (const contract of withdrawnFromContracts) {
-        // Get latest revision to generate package name. Use draft revision if contract is unlocked.
-        const latestContractRev =
-            contract.consolidatedStatus === 'UNLOCKED'
-                ? contract.draftRevision!
-                : contract.packageSubmissions[0].contractRevision
-
-        const pkgPrograms = findContractPrograms(
-            latestContractRev,
-            statePrograms
-        )
-
-        if (pkgPrograms instanceof Error) {
-            return new Error(
-                `Error parsing withdrawn from contract data for contract with ID: ${contract.id}. ${pkgPrograms.message}`
-            )
-        }
-
-        const packageName = generatePackageName(
-            contract.stateCode,
-            contract.stateNumber,
-            latestContractRev.formData.programIDs,
-            pkgPrograms
-        )
-
-        const submissionURL = submissionSummaryURL(contract.id, config.baseUrl)
-
-        withdrawnFromContractData.push({
-            contractName: packageName,
-            submissionURL,
-        })
-
-        latestContractRev.formData.stateContacts.forEach((contact) => {
-            if (contact.email) stateContactEmails.push(contact.email)
-        })
-    }
+    const {
+        latestRateRev,
+        latestAction,
+        withdrawnFromContractData,
+        stateContactEmails,
+    } = withdrawnRateData
 
     const toAddresses = pruneDuplicateEmails([
         ...stateContactEmails,
