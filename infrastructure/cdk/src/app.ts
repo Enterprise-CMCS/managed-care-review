4b7967f4a10dac3baa8ec1864c73638bb9c2fccb
Mojo Talantikite <mojo.talantikite@gmail.com>
2025-01-16 16:19:46 -0500

    ci-force-run custom synth

--- infrastructure/cdk/src/app.ts
+++ infrastructure/cdk/src/app.ts
@@ -10,13 +10,33 @@ const env = {
     region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
 }
 
+const customSynthesizer = new cdk.DefaultStackSynthesizer({
+    qualifier: 'mcreview',
+    fileAssetsBucketName: `cdk-one-assets-${env.account}-${env.region}`,
+    bucketPrefix: '',
+    cloudFormationExecutionRole: `arn:aws:iam::${env.account}:role/delegatedadmin/developer/cdk-mcreview-cfn-exec-role-${env.account}-${env.region}`,
+    deployRoleArn: `arn:aws:iam::${env.account}:role/delegatedadmin/developer/cdk-mcreview-deploy-role-${env.account}-${env.region}`,
+    fileAssetPublishingRoleArn: `arn:aws:iam::${env.account}:role/delegatedadmin/developer/cdk-mcreview-file-publishing-role-${env.account}-${env.region}`,
+    imageAssetPublishingRoleArn: `arn:aws:iam::${env.account}:role/delegatedadmin/developer/cdk-mcreview-image-publishing-role-${env.account}-${env.region}`,
+    lookupRoleArn: `arn:aws:iam::${env.account}:role/delegatedadmin/developer/cdk-mcreview-lookup-role-${env.account}-${env.region}`,
+})
+
 // Create the UI stack
 new UiStack(app, `${stage}-ui`, {
-    synthesizer: new cdk.DefaultStackSynthesizer({
-        qualifier: 'mcreview',
-    }),
+    synthesizer: customSynthesizer,
     stage,
     env,
+    permissionsBoundary: cdk.PermissionsBoundary.fromArn(
+        cdk.Arn.format({
+            partition: 'aws',
+            service: 'iam',
+            region: '',
+            account: env.account,
+            resource: 'policy',
+            resourceName:
+                'cms-cloud-admin/ct-ado-poweruser-permissions-boundary-policy',
+        })
+    ),
     tags: {
         Environment: stage,
         Service: 'ui',
