51ad8bd4178b7ebefbe5da06f2f82ec9a597d30b
Jason Lin <maolin@truss.works>
2025-01-21 20:39:35 -0500

    Extend prisma to restrict new table methods

--- services/app-api/src/postgres/prismaClient.ts
+++ services/app-api/src/postgres/prismaClient.ts
@@ -1,14 +1,69 @@
 import { PrismaClient } from '@prisma/client'
 
-async function NewPrismaClient(connURL: string): Promise<PrismaClient | Error> {
-    try {
-        const prismaClient = new PrismaClient({
-            datasources: {
-                db: {
-                    url: connURL,
+const errorMessages = {
+    delete: 'Deletion of records is not allowed',
+    create: 'Creation of new records is not allowed',
+}
+
+/**
+ * Creates an extended instance of the PrismaClient with custom behavior for certain operations.
+ *
+ * This function extends the PrismaClient to throw errors when attempting to perform
+ * create or delete operations on the `applicationSettings` and `emailSettings` models.
+ *
+ * @returns {PrismaClient} An extended instance of the PrismaClient with the custom behavior.
+ */
+function extendedPrismaClient() {
+    return new PrismaClient().$extends({
+        query: {
+            applicationSettings: {
+                delete: () => {
+                    throw new Error(
+                        `ApplicationSettings: ${errorMessages.delete}`
+                    )
+                },
+                deleteMany: () => {
+                    throw new Error(
+                        `ApplicationSettings: ${errorMessages.delete}`
+                    )
+                },
+                create: () => {
+                    throw new Error(
+                        `ApplicationSettings: ${errorMessages.create}`
+                    )
+                },
+                createMany: () => {
+                    throw new Error(
+                        `ApplicationSettings: ${errorMessages.create}`
+                    )
                 },
             },
-        })
+            emailSettings: {
+                delete: () => {
+                    throw new Error(`EmailSettings: ${errorMessages.delete}`)
+                },
+                deleteMany: () => {
+                    throw new Error(`EmailSettings: ${errorMessages.delete}`)
+                },
+                create: () => {
+                    throw new Error(`EmailSettings: ${errorMessages.create}`)
+                },
+                createMany: () => {
+                    throw new Error(`EmailSettings: ${errorMessages.create}`)
+                },
+            },
+        },
+    })
+}
+
+type ExtendedPrismaClient = ReturnType<typeof extendedPrismaClient>
+
+async function NewPrismaClient(
+    connURL: string
+): Promise<ExtendedPrismaClient | Error> {
+    try {
+        const prismaClient = extendedPrismaClient()
+
         return prismaClient
     } catch (e: unknown) {
         if (e instanceof Error) {
