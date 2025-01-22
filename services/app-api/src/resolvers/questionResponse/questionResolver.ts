689112c8d3a030b5e85080ce072697c8c750ade6
pearl-truss <pearl@truss.works>
2025-01-22 11:19:03 -0500

    Add resolver test, clean up resolver

--- services/app-api/src/resolvers/questionResponse/questionResolver.ts
+++ services/app-api/src/resolvers/questionResponse/questionResolver.ts
@@ -2,16 +2,21 @@ import type { Resolvers } from '../../gen/gqlServer'
 
 import type { ContractQuestionType } from '../../domain-models'
 import type { Store } from '../../postgres'
-
 import { GraphQLError } from 'graphql'
+
 export function questionResolver(store: Store): Resolvers['QuestionResolver'] {
     return {
         round: async (parent: ContractQuestionType) => {
             const questions = await store.findAllQuestionsByContract(
                 parent.contractID
             )
+            if (!questions) {
+                throw new Error(
+                    `Questions not found for contract: ${parent.contractID}`
+                )
+            }
             if (questions instanceof Error) {
-                const errMessage = `Issue finding contract message: ${questions.message}`
+                const errMessage = `Issue return questions for contract message: ${questions.message}`
 
                 throw new GraphQLError(errMessage, {
                     extensions: {
@@ -22,55 +27,20 @@ export function questionResolver(store: Store): Resolvers['QuestionResolver'] {
             }
 
             const divisionQuestions = questions
-                ?.filter((q) => q.division === parent.division)
+                .filter((q) => q.division === parent.division)
                 .sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime())
 
-            const matchingQuestion = divisionQuestions?.find(
+            const matchingQuestion = divisionQuestions.find(
                 (question) => question.id == parent.id
             )
 
             if (!matchingQuestion) {
                 return 0
             } else {
-                return divisionQuestions?.indexOf(matchingQuestion) !==
-                    undefined
-                    ? divisionQuestions?.indexOf(matchingQuestion) + 1
+                return divisionQuestions.indexOf(matchingQuestion) !== undefined
+                    ? divisionQuestions.indexOf(matchingQuestion) + 1
                     : 0
             }
         },
-        // responses: async (parent: any) => {
-        //     const questions = await store.findAllQuestionsByContract(parent.contractID)
-        //      if (questions instanceof Error) {
-        //          const errMessage = `Issue finding contract message: ${questions.message}`
-
-        //          throw new GraphQLError(errMessage, {
-        //              extensions: {
-        //                  code: 'INTERNAL_SERVER_ERROR',
-        //                  cause: 'DB_ERROR',
-        //              },
-        //          })
-        //     }
-
-        //     let round = 0
-        //     const divisionQuestions = questions?.
-        //         filter((q) => q.division === parent.division).
-        //         sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime())
-
-        //     const matchingQuestion = divisionQuestions?.find(
-        //         (question) => question.id == parent.id)
-
-        //     if (!matchingQuestion) {
-        //         round = 0
-        //     } else {
-        //         divisionQuestions?.indexOf(matchingQuestion) !== undefined
-        //             ? round = divisionQuestions?.indexOf(matchingQuestion) + 1
-        //             : round = 0
-        //     }
-        //     const responses = parent.responses.map((res: any) => {
-        //         res.round = round
-        //         return round
-        //     })
-        //     return responses
-        // }
     }
 }
