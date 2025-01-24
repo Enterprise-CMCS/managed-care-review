ab63bc763277dfa574e0e9838b430b17f81ceaf0
Jason Lin <maolin@truss.works>
2025-01-23 21:44:25 -0500

    Update schema

--- services/app-api/src/domain-models/SettingType.tsx
+++ services/app-api/src/domain-models/SettingType.tsx
@@ -1,15 +1,15 @@
 import { z } from 'zod'
 
 const emailSettingsSchema = z.object({
-    emailSource: z.string(),
-    devReviewTeamEmails: z.array(z.string()),
-    cmsReviewHelpEmailAddress: z.array(z.string()),
-    cmsRateHelpEmailAddress: z.array(z.string()),
-    oactEmails: z.array(z.string()),
-    dmcpReviewEmails: z.array(z.string()),
-    dmcpSubmissionEmails: z.array(z.string()),
-    dmcoEmails: z.array(z.string()),
-    helpDeskEmail: z.array(z.string()),
+    emailSource: z.string().email(),
+    devReviewTeamEmails: z.array(z.string().email()),
+    cmsReviewHelpEmailAddress: z.array(z.string().email()),
+    cmsRateHelpEmailAddress: z.array(z.string().email()),
+    oactEmails: z.array(z.string().email()),
+    dmcpReviewEmails: z.array(z.string().email()),
+    dmcpSubmissionEmails: z.array(z.string().email()),
+    dmcoEmails: z.array(z.string().email()),
+    helpDeskEmail: z.array(z.string().email()),
 })
 
 const applicationSettingsSchema = z.object({
