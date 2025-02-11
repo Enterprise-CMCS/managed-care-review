5ea56278a3caca9d1b1319932aa5aa41b0726aab
Mojo Talantikite <mojo.talantikite@gmail.com>
2025-02-11 16:49:14 -0500

    compile options

--- services/postgres/esbuild.config.js
+++ services/postgres/esbuild.config.js
@@ -1,13 +1,11 @@
-const process = require('process')
+const process = require('process');
 
 module.exports = () => {
     return {
         packager: 'pnpm',
         bundle: true,
-        minify: process.env.NODE_ENV === 'production',
         exclude: ['aws-sdk'],
+        sourcemap: true,
         entryPoints: ['src/rotator.ts', 'scripts/uploadScripts.ts'],
-        format: 'esm',
-        outdir: 'dist',
-    }
-}
+    };
+};
