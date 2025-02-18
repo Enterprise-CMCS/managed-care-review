41371b253e9403ea7b85d54b27d301749a7e4644
Jason Lin <maolin@truss.works>
2025-02-18 15:46:38 -0500

    Add undo withdraw API

--- services/app-api/src/postgres/postgresStore.ts
+++ services/app-api/src/postgres/postgresStore.ts
@@ -1,5 +1,4 @@
 import type {
-    PrismaClient,
     Division,
     RateRevisionTable,
     ContractRevisionTable,
@@ -83,7 +82,11 @@ import type { WithdrawRateArgsType } from './contractAndRates/withdrawRate'
 import { withdrawRate } from './contractAndRates/withdrawRate'
 import { findEmailSettings } from './settings/findEmailSettings'
 import { updateEmailSettings } from './settings/updateEmailSettings'
-import type  { ExtendedPrismaClient } from './prismaClient'
+import type { ExtendedPrismaClient } from './prismaClient'
+import {
+    undoWithdrawRate,
+    type UndoWithdrawRateArgsType,
+} from './contractAndRates/undoWithdrawRate'
 
 type Store = {
     findPrograms: (
@@ -160,6 +163,10 @@ type Store = {
     ) => Promise<ContractType | Error>
 
     withdrawRate: (args: WithdrawRateArgsType) => Promise<RateType | Error>
+
+    undoWithdrawRate: (
+        args: UndoWithdrawRateArgsType
+    ) => Promise<RateType | Error>
 
     findContractWithHistory: (
         contractID: string
@@ -280,6 +287,7 @@ function NewPostgresStore(client: ExtendedPrismaClient): Store {
         insertDraftContract: (args) => insertDraftContract(client, args),
         approveContract: (args) => approveContract(client, args),
         withdrawRate: (args) => withdrawRate(client, args),
+        undoWithdrawRate: (args) => undoWithdrawRate(client, args),
         findContractWithHistory: (args) =>
             findContractWithHistory(client, args),
         findRateWithHistory: (args) => findRateWithHistory(client, args),
