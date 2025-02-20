4d28a2e928abbf16a06db27a68c4df6848d01ddf
Jason Lin <maolin@truss.works>
2025-02-20 18:54:14 -0500

    Add undo rate withdraw mocks

--- packages/mocks/src/apollo/withdrawRateGQLMock.ts
+++ packages/mocks/src/apollo/withdrawRateGQLMock.ts
@@ -1,7 +1,9 @@
 import {   
     Rate,
     WithdrawRateDocument,
+    UndoWithdrawnRateDocument,
     WithdrawRateMutation,
+    UndoWithdrawnRateMutation
 } from '../gen/gqlClient'
 import { MockedResponse } from '@apollo/client/testing'
 import { GraphQLError } from 'graphql/index'
@@ -63,6 +65,101 @@ const withdrawRateMockSuccess = (
     }
 }
 
+const undoWithdrawRateMockSuccess = (
+    params: {
+        rateID?: string
+        rateData?: Partial<Rate>
+        updatedReason?: string
+    } = {}
+): MockedResponse<UndoWithdrawnRateMutation> => {
+    const {
+        rateID = 'test-abc-123',
+        rateData,
+        updatedReason = 'Undo rate withdraw',
+    } = params
+    const rate = mockRateSubmittedWithQuestions({
+        __typename: 'Rate',
+        id: rateData?.id || rateID,
+        parentContractID: rateData?.parentContractID || 'parent-contract-id',
+        reviewStatus: 'UNDER_REVIEW',
+        consolidatedStatus: 'RESUBMITTED',
+        reviewStatusActions: [
+            {
+                __typename: 'RateReviewStatusActions',
+                actionType: 'UNDER_REVIEW',
+                rateID: rateID,
+                updatedReason,
+                updatedAt: new Date('2024-5-2'),
+                updatedBy: {
+                    __typename: 'UpdatedBy',
+                    email: 'cmsapprover@example.com',
+                    familyName: 'Smith',
+                    givenName: 'John',
+                    role: 'CMS_APPROVER_USER',
+                },
+            },
+            {
+                __typename: 'RateReviewStatusActions',
+                actionType: 'WITHDRAW',
+                rateID: rateID,
+                updatedReason: 'Withdrawing invalid rate',
+                updatedAt: new Date('2024-5-1'),
+                updatedBy: {
+                    __typename: 'UpdatedBy',
+                    email: 'cmsapprover@example.com',
+                    familyName: 'Smith',
+                    givenName: 'John',
+                    role: 'CMS_APPROVER_USER',
+                },
+            },
+        ],
+    })
+
+    return {
+        request: {
+            query: UndoWithdrawnRateDocument,
+            variables: {
+                input: {
+                    rateID: 'test-abc-123',
+                    updatedReason,
+                },
+            },
+        },
+        result: {
+            data: {
+                undoWithdrawRate: {
+                    rate,
+                },
+            },
+        },
+    }
+}
+
+const undoWithdrawRateMockFailure = (): MockedResponse<UndoWithdrawnRateMutation> => {
+    const graphQLError = new GraphQLError('Issue undoing a withdrawn rate', {
+        extensions: {
+            code: 'NOT_FOUND',
+            cause: 'DB_ERROR',
+        },
+    })
+
+    return {
+        request: {
+            query: UndoWithdrawnRateDocument,
+            variables: {
+                input: {
+                    rateID: 'test-abc-123',
+                    updatedReason: 'Undo withdraw rate'
+                },
+            },
+        },
+        result: {
+            data: null,
+            errors: [graphQLError],
+        },
+    }
+}
+
 const withdrawRateMockFailure = (): MockedResponse<WithdrawRateMutation> => {
     const graphQLError = new GraphQLError('Issue withdrawing rate', {
         extensions: {
@@ -88,4 +185,9 @@ const withdrawRateMockFailure = (): MockedResponse<WithdrawRateMutation> => {
     }
 }
 
-export { withdrawRateMockFailure, withdrawRateMockSuccess }
+export {
+    withdrawRateMockFailure,
+    withdrawRateMockSuccess,
+    undoWithdrawRateMockSuccess,
+    undoWithdrawRateMockFailure
+}
