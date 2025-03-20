287e8b86938592b8a1bc000fb47fe40890bcc782
ruizajtruss <ruizaj@truss.works>
2025-03-20 00:42:34 +0000

    added form validation

--- services/app-web/src/pages/SubmissionWithdraw/SubmissionWithdraw.tsx
+++ services/app-web/src/pages/SubmissionWithdraw/SubmissionWithdraw.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import styles from './SubmissionWithdraw.module.scss'
-import { ActionButton, Breadcrumbs } from '../../components'
+import { ActionButton, Breadcrumbs, PoliteErrorMessage } from '../../components'
 import { RoutesRecord } from '@mc-review/constants'
 import { useNavigate, useParams } from 'react-router-dom'
 import { useFetchContractQuery } from '../../gen/gqlClient'
-import { Formik } from 'formik'
+import { Formik, FormikErrors } from 'formik'
 import {
     ButtonGroup,
     Form,
@@ -13,19 +13,35 @@ import {
     Textarea,
 } from '@trussworks/react-uswds'
 import { PageActionsContainer } from '../StateSubmission/PageActions'
+import * as Yup from 'yup'
+import { ErrorOrLoadingPage } from '../StateSubmission'
+import { handleAndReturnErrorState } from '../StateSubmission/ErrorOrLoadingPage'
+import { GenericErrorPage } from '../Errors/GenericErrorPage'
 
 type SubmissionWithdrawValues = {
     submissionWithdrawReason: string
 }
 
+type FormError =
+    FormikErrors<SubmissionWithdrawValues>[keyof FormikErrors<SubmissionWithdrawValues>]
+
+const submissionWithdrawSchema = Yup.object().shape({
+    submissionWithdrawReason: Yup.string().required(
+        'You must provide a reason for withdrawing this submission.'
+    ),
+})
+
 export const SubmissionWithdraw = (): React.ReactElement => {
     const { id } = useParams() as { id: string }
     const navigate = useNavigate()
+    const [shouldValidate, setShouldValidate] = React.useState(false)
+    const showFieldErrors = (error?: FormError): boolean | undefined =>
+        shouldValidate && Boolean(error)
     const formInitialValues: SubmissionWithdrawValues = {
         submissionWithdrawReason: '',
     }
 
-    const { data } = useFetchContractQuery({
+    const { data, loading, error } = useFetchContractQuery({
         variables: {
             input: {
                 contractID: id,
@@ -33,6 +49,15 @@ export const SubmissionWithdraw = (): React.ReactElement => {
         },
     })
     const contract = data?.fetchContract.contract
+
+    if (loading) {
+        return <ErrorOrLoadingPage state="LOADING" />
+    } else if (error) {
+        return <ErrorOrLoadingPage state={handleAndReturnErrorState(error)} />
+    } else if (!contract || !contract.packageSubmissions) {
+        return <GenericErrorPage />
+    }
+
     const contractName =
         contract?.packageSubmissions[0].contractRevision.contractName
 
@@ -58,64 +83,90 @@ export const SubmissionWithdraw = (): React.ReactElement => {
             <Formik
                 initialValues={formInitialValues}
                 onSubmit={() => window.alert('Not implemented')}
-                validationSchema={() => window.alert('Not implemented')}
+                validationSchema={submissionWithdrawSchema}
             >
-                <Form
-                    id="SubmissionWithdrawForm"
-                    className={styles.formContainer}
-                    aria-label="Withdraw submission"
-                    aria-describedby="form-guidance"
-                    onSubmit={() => window.alert('Not implemented')}
-                >
-                    <fieldset className="usa-fieldset">
-                        <h2>Withdraw submission</h2>
-                        <FormGroup className="margin-top-0">
-                            <Label
-                                htmlFor="submissionWithdrawReason"
-                                className="margin-bottom-0 text-bold"
+                {({ handleSubmit, handleChange, errors, values }) => (
+                    <Form
+                        id="SubmissionWithdrawForm"
+                        className={styles.formContainer}
+                        aria-label="Withdraw submission"
+                        aria-describedby="form-guidance"
+                        onSubmit={(e) => {
+                            setShouldValidate(true)
+                            return handleSubmit(e)
+                        }}
+                    >
+                        <fieldset className="usa-fieldset">
+                            <h2>Withdraw submission</h2>
+                            <FormGroup
+                                error={showFieldErrors(
+                                    errors.submissionWithdrawReason
+                                )}
+                                className="margin-top-0"
                             >
-                                Reason for withdrawing
-                            </Label>
-                            <p className="margin-bottom-0 margin-top-05 usa-hint">
-                                Required
-                            </p>
-                            <p className="margin-bottom-0 margin-top-05 usa-hint">
-                                Provide a reason for withdrawing the submission.
-                            </p>
-                            <Textarea
-                                name="submissionWithdrawReason"
-                                id="submissionWithdrawReason"
-                                data-testid="submissionWithdrawReason"
-                                aria-labelledby="submissionWithdrawReason"
-                                aria-required
-                            />
-                        </FormGroup>
-                    </fieldset>
-                    <PageActionsContainer>
-                        <ButtonGroup type="default">
-                            <ActionButton
-                                type="button"
-                                variant="outline"
-                                data-testid="page-actions-left-secondary"
-                                parent_component_type="page-body"
-                                link_url={`/submissions/${id}`}
-                                onClick={() => navigate(`/submissions/${id}`)}
-                            >
-                                Cancel
-                            </ActionButton>
-                            <ActionButton
-                                type="submit"
-                                variant="default"
-                                data-testid="page-actions-right-primary"
-                                parent_component_type="page body"
-                                link_url={`/submissions/${id}`}
-                                animationTimeout={1000}
-                            >
-                                Withdraw submission
-                            </ActionButton>
-                        </ButtonGroup>
-                    </PageActionsContainer>
-                </Form>
+                                <Label
+                                    htmlFor="submissionWithdrawReason"
+                                    className="margin-bottom-0 text-bold"
+                                >
+                                    Reason for withdrawing the submission.
+                                </Label>
+                                <p className="margin-bottom-0 margin-top-05 usa-hint">
+                                    Required
+                                </p>
+                                {showFieldErrors(
+                                    errors.submissionWithdrawReason
+                                ) && (
+                                    <PoliteErrorMessage formFieldLabel="">
+                                        {errors.submissionWithdrawReason}
+                                    </PoliteErrorMessage>
+                                )}
+                                <Textarea
+                                    name="submissionWithdrawReason"
+                                    id="submissionWithdrawReason"
+                                    data-testid="submissionWithdrawReason"
+                                    aria-labelledby="submissionWithdrawReason"
+                                    aria-required
+                                    defaultValue={
+                                        values.submissionWithdrawReason
+                                    }
+                                    onChange={handleChange}
+                                    error={showFieldErrors(
+                                        errors.submissionWithdrawReason
+                                    )}
+                                />
+                            </FormGroup>
+                        </fieldset>
+                        <PageActionsContainer>
+                            <ButtonGroup type="default">
+                                <ActionButton
+                                    type="button"
+                                    variant="outline"
+                                    data-testid="page-actions-left-secondary"
+                                    parent_component_type="page-body"
+                                    link_url={`/submissions/${id}`}
+                                    onClick={() =>
+                                        navigate(`/submissions/${id}`)
+                                    }
+                                >
+                                    Cancel
+                                </ActionButton>
+                                <ActionButton
+                                    type="submit"
+                                    variant="default"
+                                    data-testid="page-actions-right-primary"
+                                    parent_component_type="page body"
+                                    link_url={`/submissions/${id}`}
+                                    animationTimeout={1000}
+                                    disabled={showFieldErrors(
+                                        errors.submissionWithdrawReason
+                                    )}
+                                >
+                                    Withdraw submission
+                                </ActionButton>
+                            </ButtonGroup>
+                        </PageActionsContainer>
+                    </Form>
+                )}
             </Formik>
         </div>
     )
