179b52ddd6bf23ed0c6ef322dedc882e680c84d0
ruizajtruss <ruizaj@truss.works>
2025-03-25 22:44:52 +0000

    banner logic

--- services/app-web/src/components/Banner/WithdrawSubmissionBanner/WithdrawSubmissionBanner.tsx
+++ services/app-web/src/components/Banner/WithdrawSubmissionBanner/WithdrawSubmissionBanner.tsx
@@ -1,23 +1,41 @@
 import React from 'react'
 import { Alert } from '@trussworks/react-uswds'
 import styles from '../Banner.module.scss'
+import { RatesToNotBeWithdrawn } from '../../../pages/SubmissionWithdraw/SubmissionWithdraw'
+import { NavLinkWithLogging } from '../../TealiumLogging'
+
+interface WithdrawSubmissionBannerProps
+    extends React.HTMLAttributes<HTMLDivElement> {
+    rates: RatesToNotBeWithdrawn[]
+}
 
 export const WithdrawSubmissionBanner = ({
     className,
-}: React.HTMLAttributes<HTMLDivElement>) => {
+    rates,
+}: WithdrawSubmissionBannerProps) => {
     return (
         <Alert
             role="alert"
             type="warning"
-            heading="Rates on multiple contract actions will not be withdrawn"
+            heading="Rate on multiple contract actions"
             headingLevel="h4"
             data-testid="withdrawSubmissionBanner"
             className={className}
         >
             <div className={styles.bannerBodyText}>
                 <p className="usa-alert__text">
-                    This contract action has rates on multiple contract actions.
+                    Withdrawing this submission will not withdraw the following
+                    rate(s) that are on multiple contract actions:
                 </p>
+                <ul>
+                    {rates.map((rate: RatesToNotBeWithdrawn) => (
+                        <li>
+                            <NavLinkWithLogging to={rate.rateURL}>
+                                {rate.rateName}
+                            </NavLinkWithLogging>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </Alert>
     )
--- services/app-web/src/pages/SubmissionWithdraw/SubmissionWithdraw.tsx
+++ services/app-web/src/pages/SubmissionWithdraw/SubmissionWithdraw.tsx
@@ -3,7 +3,10 @@ import styles from './SubmissionWithdraw.module.scss'
 import { ActionButton, Breadcrumbs, PoliteErrorMessage } from '../../components'
 import { RoutesRecord } from '@mc-review/constants'
 import { useNavigate, useParams } from 'react-router-dom'
-import { useFetchContractQuery } from '../../gen/gqlClient'
+import {
+    useFetchContractQuery,
+    useIndexRatesStrippedWithRelatedContractsQuery,
+} from '../../gen/gqlClient'
 import { Formik, FormikErrors } from 'formik'
 import {
     ButtonGroup,
@@ -17,9 +20,20 @@ import * as Yup from 'yup'
 import { ErrorOrLoadingPage } from '../StateSubmission'
 import { handleAndReturnErrorState } from '../StateSubmission/ErrorOrLoadingPage'
 import { GenericErrorPage } from '../Errors/GenericErrorPage'
+import { WithdrawSubmissionBanner } from '../../components/Banner/WithdrawSubmissionBanner/WithdrawSubmissionBanner'
 
 type SubmissionWithdrawValues = {
     submissionWithdrawReason: string
+}
+
+export type RatesToNotBeWithdrawn = {
+    rateName: string
+    rateURL: string
+}
+
+type RelatedContracts = {
+    consolidatedStatus: string
+    id: string
 }
 
 type FormError =
@@ -41,25 +55,117 @@ export const SubmissionWithdraw = (): React.ReactElement => {
         submissionWithdrawReason: '',
     }
 
-    const { data, loading, error } = useFetchContractQuery({
+    //Fetching contract to be withdrawn
+    const {
+        data: contractData,
+        loading: contractLoading,
+        error: contractError,
+    } = useFetchContractQuery({
         variables: {
             input: {
                 contractID: id,
             },
         },
+        fetchPolicy: 'cache-and-network',
     })
-    const contract = data?.fetchContract.contract
+    const contract = contractData?.fetchContract.contract
+    //Extracting rateIDs to query for parent contract data
+    const rateIDs = contract
+        ? contract.packageSubmissions[0].rateRevisions.map((rr) => rr.rate!.id)
+        : []
 
-    if (loading) {
+    //Fetching rates associated with above contract to determine whether or not they will be withdrawn (banner display)
+    //This query will be skipped if rateIDs comes up empty
+    const {
+        data: ratesData,
+        loading: ratesLoading,
+        error: ratesError,
+    } = useIndexRatesStrippedWithRelatedContractsQuery({
+        variables: {
+            input: {
+                stateCode: undefined,
+                rateIDs: rateIDs,
+            },
+        },
+        skip: rateIDs.length === 0,
+        fetchPolicy: 'cache-and-network',
+    })
+
+    if (contractLoading || ratesLoading) {
         return <ErrorOrLoadingPage state="LOADING" />
-    } else if (error) {
-        return <ErrorOrLoadingPage state={handleAndReturnErrorState(error)} />
+    } else if (contractError || ratesError) {
+        const queryError = contractError ?? ratesError
+        return (
+            <ErrorOrLoadingPage
+                state={handleAndReturnErrorState(queryError!)}
+            />
+        )
     } else if (!contract || !contract.packageSubmissions) {
+        return <GenericErrorPage />
+    } else if (
+        rateIDs.length !== 0 &&
+        (!ratesData || !ratesData.indexRatesStripped.edges)
+    ) {
         return <GenericErrorPage />
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const shouldWithdraw = (rate: {
+        __typename?: 'RateStrippedEdge' | undefined
+        node: any
+    }) => {
+        const parentContractID = rate.node.parentContractID
+        const rateStatus = rate.node.consolidatedStatus
+        const relatedContracts = rate.node.relatedContracts
+        const parentContract = relatedContracts!.find(
+            (contract: RelatedContracts) => contract.id === parentContractID
+        )
+
+        //If not in a submitted status it should not be withdrawn
+        if (!['RESUBMITTED', 'SUBMITTED'].includes(rateStatus)) {
+            return false
+        }
+
+        //If the parent contract is not in a withdrawn state then the LINKED rate is not withdrawn
+        if (
+            parentContractID !== id &&
+            parentContract?.consolidatedStatus !== 'WITHDRAWN'
+        ) {
+            return false
+        }
+
+        //If ANY of the linked submissions are in a submitted or approved state excluding the parent it will not be withdrawn
+        for (const contract of relatedContracts!) {
+            if (
+                contract.id !== id &&
+                ['APPROVED', 'SUBMITTED', 'RESUBMITTED'].includes(
+                    contract.consolidatedStatus
+                )
+            ) {
+                return false
+            }
+        }
+
+        return true
+    }
+
     const contractName =
-        contract?.packageSubmissions[0].contractRevision.contractName
+        contract.packageSubmissions[0].contractRevision.contractName
+
+    const ratesToNotBeWithdrawn: RatesToNotBeWithdrawn[] = []
+    if (ratesData && ratesData.indexRatesStripped.edges.length > 0) {
+        const ratesWithContractData = ratesData.indexRatesStripped.edges
+        ratesWithContractData.forEach((rate) => {
+            if (!shouldWithdraw(rate)) {
+                ratesToNotBeWithdrawn.push({
+                    rateName:
+                        rate.node.latestSubmittedRevision.formData
+                            .rateCertificationName!,
+                    rateURL: `/rates/${rate.node.id}`,
+                })
+            }
+        })
+    }
 
     return (
         <div className={styles.submissionWithdrawContainer}>
@@ -80,6 +186,9 @@ export const SubmissionWithdraw = (): React.ReactElement => {
                     },
                 ]}
             />
+            {ratesToNotBeWithdrawn.length > 0 && (
+                <WithdrawSubmissionBanner rates={ratesToNotBeWithdrawn} />
+            )}
             <Formik
                 initialValues={formInitialValues}
                 onSubmit={() => window.alert('Not implemented')}
