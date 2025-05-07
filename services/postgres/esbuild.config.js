3534f860638d66d1ee903e216336101f0e64cf60
Mojo Talantikite <mojo.talantikite@gmail.com>
2025-05-07 17:37:02 -0400

    fix deploy. add lambda layer

--- services/postgres/esbuild.config.js
+++ services/postgres/esbuild.config.js
@@ -4,7 +4,7 @@ module.exports = () => {
     return {
         packager: 'pnpm',
         bundle: true,
-        exclude: ['aws-sdk'],
+        exclude: ['aws-sdk', 'prisma', '@prisma/client'],
         sourcemap: true,
         entryPoints: ['src/rotator.ts'],
     };
