c3e7265b1dd140abec8bd47de04d40eded73b614
Mojo Talantikite <mojo.talantikite@gmail.com>
2025-05-13 14:19:21 -0400

    fix esbuild to copy files

--- services/postgres/esbuild.config.js
+++ services/postgres/esbuild.config.js
@@ -1,4 +1,4 @@
-const process = require('process');
+const fse = require('fs-extra');
 
 module.exports = () => {
     return {
@@ -7,5 +7,32 @@ module.exports = () => {
         exclude: ['aws-sdk', 'prisma', '@prisma/client'],
         sourcemap: true,
         entryPoints: ['src/rotator.ts'],
+        plugins: [
+            {
+                name: 'copy-dummy-docs',
+                setup(build) {
+                    build.onStart(async () => {
+                        try {
+                            await fse.ensureDir('.esbuild/.build/files/');
+                        } catch (err) {
+                            console.error('Error making directory: ', err);
+                        }
+                    });
+
+                    build.onEnd(async () => {
+                        try {
+                            await fse.copy(
+                                './files/',
+                                '.esbuild/.build/files/',
+                                { overwrite: true }
+                            );
+                            console.log('Dummy files for replacements copied.');
+                        } catch (err) {
+                            console.error('Error copying dummy files:', err);
+                        }
+                    });
+                },
+            },
+        ],
     };
 };
