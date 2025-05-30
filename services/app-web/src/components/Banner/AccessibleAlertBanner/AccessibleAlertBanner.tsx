d0dfc09f026de46cf8d16e4008e5f8895e532e26
Jason Lin <maolin@truss.works>
2025-05-29 22:35:40 -0400

    Match header announcement to alert announcement.

--- services/app-web/src/components/Banner/AccessibleAlertBanner/AccessibleAlertBanner.tsx
+++ services/app-web/src/components/Banner/AccessibleAlertBanner/AccessibleAlertBanner.tsx
@@ -26,19 +26,21 @@ export const AccessibleAlertBanner = ({
             aria-labelledby={heading ? headerID : undefined}
             {...rest}
         >
-            {/*Hidden element for the screen reader when navigating by headers, This will announce what this header is for.*/}
-            <span
-                tabIndex={-1}
-                aria-hidden
-                id="alert-context"
-                className="usa-sr-only"
-            >{`${role}, ${heading}`}</span>
             {heading && (
                 <Heading
                     id={headerID}
                     className="usa-alert__heading"
-                    aria-labelledby="alert-context"
+                    aria-describedby={`${headerID}-alert-context`}
                 >
+                    {/*Hidden element for the screen reader when navigating by headers, This will announce what this header is for.*/}
+                    <span
+                        tabIndex={-1}
+                        aria-hidden
+                        id={`${headerID}-alert-context`}
+                        className="usa-sr-only"
+                    >
+                        {role}
+                    </span>
                     {heading}
                 </Heading>
             )}
