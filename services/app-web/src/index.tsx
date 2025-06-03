9ca3008a3e440b50e587e9f082c83acd3a9d81c2
Jason Lin <maolin@truss.works>
2025-06-03 17:13:10 -0400

    Remove testing code

--- services/app-web/src/index.tsx
+++ services/app-web/src/index.tsx
@@ -78,11 +78,6 @@ const cache = new InMemoryCache({
                 },
             },
         },
-        // GenericDocument: {
-        //     keyFields: (doc, context) => {
-        //         console.log(doc)
-        //     }
-        // }
     },
 })
 const defaultOptions: DefaultOptions = {
