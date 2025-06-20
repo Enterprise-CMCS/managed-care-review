87c9348796940640f52dd7443789e22939204853
Shamoon Siddiqui <shamoon@develop.io>
2025-06-20 10:19:38 -0400

    Add user field to OauthClient GraphQL type and resolver
    - Add user: User\! field to OauthClient type in GraphQL schema
    - Add OauthClient field resolver to handle user field resolution
    - Import and use toDomainUser to convert Prisma User to domain UserType
    - Regenerate GraphQL types to include user field
    - All OAuth resolver tests passing (20/20)

--- services/app-api/src/resolvers/configureResolvers.ts
+++ services/app-api/src/resolvers/configureResolvers.ts
@@ -2,6 +2,7 @@ import { GraphQLDate, GraphQLDateTime } from 'graphql-scalars'
 import type { Emailer } from '../emailer'
 import type { Resolvers } from '../gen/gqlServer'
 import type { Store } from '../postgres'
+import { toDomainUser } from '../domain-models'
 import {
     createHealthPlanPackageResolver,
     fetchHealthPlanPackageResolver,
@@ -197,6 +198,9 @@ export function configureResolvers(
         ContractRevision: contractRevisionResolver(store),
         GenericDocument: genericDocumentResolver(s3Client),
         Document: questionResponseDocumentResolver(s3Client),
+        OauthClient: {
+            user: (parent) => toDomainUser(parent.user),
+        },
     }
 
     return resolvers
