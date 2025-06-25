40f147ec32899b7325d2d0fd78c6442c0038c8e5
Shamoon Siddiqui <shamoons@users.noreply.github.com>
2025-06-25 12:35:16 -0400

    Update services/app-api/src/resolvers/contract/fetchContract.ts
    Co-authored-by: Mao Lin <98117700+JasonLin0991@users.noreply.github.com>

--- services/app-api/src/resolvers/contract/fetchContract.ts
+++ services/app-api/src/resolvers/contract/fetchContract.ts
@@ -75,6 +75,7 @@ export function fetchContractResolver(
             })
         }
 
+        logSuccess('fetchContract')
         setSuccessAttributesOnActiveSpan(span)
         return { contract: contractWithHistory }
     }
