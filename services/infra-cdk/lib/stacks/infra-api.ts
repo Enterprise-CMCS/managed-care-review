29ff1da4d56d679b215fa2e51df7e4e98bd3e427
Mojo Talantikite <mojo.talantikite@gmail.com>
2025-08-12 09:25:27 -0400

    api gateway fix

--- services/infra-cdk/lib/stacks/infra-api.ts
+++ services/infra-cdk/lib/stacks/infra-api.ts
@@ -1,10 +1,6 @@
 import { BaseStack, type BaseStackProps } from '../constructs/base/base-stack'
 import { type Construct } from 'constructs'
-import {
-    RestApi,
-    GatewayResponse,
-    ResponseType,
-} from 'aws-cdk-lib/aws-apigateway'
+import { CfnRestApi, CfnGatewayResponse } from 'aws-cdk-lib/aws-apigateway'
 import { CfnWebACL } from 'aws-cdk-lib/aws-wafv2'
 import { CfnOutput } from 'aws-cdk-lib'
 import { ResourceNames } from '../config'
@@ -14,7 +10,7 @@ import { ResourceNames } from '../config'
  * Matches the infra-api serverless service functionality
  */
 export class InfraApiStack extends BaseStack {
-    public readonly apiGateway: RestApi
+    public readonly apiGateway: CfnRestApi
     public readonly webAcl: CfnWebACL
     public readonly apiGatewayUrl: string
 
@@ -24,34 +20,30 @@ export class InfraApiStack extends BaseStack {
             description: 'Infrastructure API - Core API Gateway and WAF',
         })
 
-        // Create API Gateway (matches serverless infra-api)
-        this.apiGateway = new RestApi(this, 'AppApiGateway', {
-            restApiName: `${ResourceNames.apiName('infra-api', this.stage)}-app-api-gateway`,
+        // Create API Gateway (matches serverless infra-api exactly)
+        this.apiGateway = new CfnRestApi(this, 'AppApiGateway', {
+            name: `${ResourceNames.apiName('infra-api', this.stage)}-app-api-gateway`,
             description: 'Core API Gateway for app-api service',
-            deploy: true,
-            deployOptions: {
-                stageName: this.stage,
-            },
         })
 
-        this.apiGatewayUrl = `https://${this.apiGateway.restApiId}.execute-api.${this.region}.amazonaws.com/${this.stage}`
+        this.apiGatewayUrl = `https://${this.apiGateway.ref}.execute-api.${this.region}.amazonaws.com/${this.stage}`
 
         // Add CORS gateway responses (matches serverless config)
-        new GatewayResponse(this, 'GatewayResponseDefault4XX', {
-            restApi: this.apiGateway,
-            type: ResponseType.DEFAULT_4XX,
-            responseHeaders: {
-                'Access-Control-Allow-Origin': "'*'",
-                'Access-Control-Allow-Headers': "'*'",
+        new CfnGatewayResponse(this, 'GatewayResponseDefault4XX', {
+            restApiId: this.apiGateway.ref,
+            responseType: 'DEFAULT_4XX',
+            responseParameters: {
+                'gatewayresponse.header.Access-Control-Allow-Origin': "'*'",
+                'gatewayresponse.header.Access-Control-Allow-Headers': "'*'",
             },
         })
 
-        new GatewayResponse(this, 'GatewayResponseDefault5XX', {
-            restApi: this.apiGateway,
-            type: ResponseType.DEFAULT_5XX,
-            responseHeaders: {
-                'Access-Control-Allow-Origin': "'*'",
-                'Access-Control-Allow-Headers': "'*'",
+        new CfnGatewayResponse(this, 'GatewayResponseDefault5XX', {
+            restApiId: this.apiGateway.ref,
+            responseType: 'DEFAULT_5XX',
+            responseParameters: {
+                'gatewayresponse.header.Access-Control-Allow-Origin': "'*'",
+                'gatewayresponse.header.Access-Control-Allow-Headers': "'*'",
             },
         })
 
@@ -90,13 +82,16 @@ export class InfraApiStack extends BaseStack {
             },
         })
 
+        // No methods needed - app-api service will add them later
+        // Using CfnRestApi bypasses CDK's validation requirement
+
         this.createOutputs()
     }
 
     private createOutputs(): void {
         // API Gateway outputs (required by app-api and ui-auth)
         new CfnOutput(this, 'ApiGatewayRestApiId', {
-            value: this.apiGateway.restApiId,
+            value: this.apiGateway.ref,
             exportName: this.exportName('ApiGatewayRestApiId'),
             description: 'API Gateway REST API ID',
         })
@@ -108,7 +103,7 @@ export class InfraApiStack extends BaseStack {
         })
 
         new CfnOutput(this, 'AppApiGatewayRootResourceId', {
-            value: this.apiGateway.restApiRootResourceId,
+            value: this.apiGateway.attrRootResourceId,
             exportName: this.exportName('AppApiGatewayRootResourceId'),
             description: 'API Gateway root resource ID',
         })
