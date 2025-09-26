09998846b8b9b289cfc7785cc74d8652f3040770
Mojo Talantikite <mojo.talantikite@gmail.com>
2025-09-26 13:53:24 -0400

    add guard duty to oidc

--- services/infra-cdk/lib/stacks/github-oidc-service-role-stack.ts
+++ services/infra-cdk/lib/stacks/github-oidc-service-role-stack.ts
@@ -70,6 +70,7 @@ export class GitHubOidcServiceRoleStack extends BaseStack {
             'ecr:*',
             'events:*',
             'firehose:*',
+            'guardduty:*',
             'iam:*',
             'kms:*',
             'lambda:*',
