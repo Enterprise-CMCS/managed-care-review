6ad11fe658a4c50eb865a2eba879c08165db262b
Mojo Talantikite <mojo.talantikite@gmail.com>
2025-09-29 16:51:59 -0400

    first phase cleanup

--- services/infra-cdk/lib/stacks/index.ts
+++ services/infra-cdk/lib/stacks/index.ts
@@ -1,26 +1,19 @@
-// Export all stacks
-export * from './foundation-stack'
-export * from './network-stack'
-export * from './data-stack'
-export * from './auth-stack'
-export * from './database-operations-stack'
-export * from './monitoring-stack'
-export * from './guardduty-malware-protection-stack'
-export * from './frontend-stack'
+// Export GitHub OIDC stack (used by bin/oidc.ts)
 export * from './github-oidc-service-role-stack'
 
-// New elegant micro-stacks (replacing ApiComputeStack bloat)
+// Planned micro-stacks (not yet used in CI/CD)
 export * from './graphql-api-stack'
 export * from './public-api-stack'
 export * from './file-ops-stack'
 export * from './scheduled-tasks-stack'
 export * from './auth-extensions-stack'
 
-// Testing review environments (individual service stacks)
+// Currently deployed review environment stacks
 export * from './network'
 export * from './postgres'
 export * from './uploads'
-export * from './frontend'
 export * from './cognito'
-export * from './storybook'
 export * from './app-api'
+export * from './frontend-infra'
+export * from './frontend-app'
+export * from './virus-scanning'
