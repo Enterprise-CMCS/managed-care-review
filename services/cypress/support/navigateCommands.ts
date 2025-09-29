bd7bc23725ffaed292695947da606352a321dc45
Mao Lin <98117700+JasonLin0991@users.noreply.github.com>
2025-09-29 10:10:58 -0400

    MCR-5322 remove HPP api usage from cypress (#3699)
    * Remove HPP commands
    
    * Remove HPP and deprecated code from tests

--- services/cypress/support/navigateCommands.ts
+++ services/cypress/support/navigateCommands.ts
@@ -11,39 +11,6 @@ const buttonsWithLabels: FormButtons = {
     BACK: 'Back',
 }
 
-// can be deleted when Contract API is complete
-Cypress.Commands.add(
-    'deprecatedNavigateV1Form',
-    (buttonKey: FormButtonKey, waitForLoad = true) => {
-        cy.findByRole('button', {
-            name: buttonsWithLabels[buttonKey],
-        }).should('not.have.attr', 'aria-disabled')
-        cy.findByRole('button', {
-            name: buttonsWithLabels[buttonKey],
-        }).safeClick()
-
-        if (buttonKey === 'SAVE_DRAFT') {
-            if(waitForLoad) {
-                cy.wait('@updateHealthPlanFormDataMutation', { timeout: 50_000})
-             }
-             cy.get('[data-testid="saveAsDraftSuccessBanner"]').should('exist')
-        } else if (buttonKey === 'CONTINUE_FROM_START_NEW') {
-            if (waitForLoad) {
-                // cy.wait('@createHealthPlanPackageMutation', { timeout: 50_000 })
-                cy.wait('@fetchContractWithQuestionsQuery')
-            }
-            cy.findByTestId('state-submission-form-page').should('exist')
-        } else if (buttonKey === 'CONTINUE') {
-            if (waitForLoad) {
-                cy.findAllByTestId('errorMessage').should('have.length', 0)
-                cy.wait('@updateHealthPlanFormDataMutation', { timeout: 50_000})
-            }
-            cy.findByTestId('state-submission-form-page').should('exist')
-        } else {
-            cy.findByTestId('state-submission-form-page').should('exist')
-        }
-    }
-)
 // navigate helper for v2 forms
 Cypress.Commands.add(
     'navigateContractRatesForm',
