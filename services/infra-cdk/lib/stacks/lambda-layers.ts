8a0d291a3bfde0d2f8b90f2de90a02de74a29aa0
Mojo Talantikite <mojo.talantikite@gmail.com>
2025-10-22 12:17:45 -0400

    bump the layer to latest

--- services/infra-cdk/lib/stacks/lambda-layers.ts
+++ services/infra-cdk/lib/stacks/lambda-layers.ts
@@ -10,8 +10,9 @@ import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib'
 import { join } from 'path'
 
 // AWS OTEL Lambda Layer ARN - update version here when needed
+// Latest: v1.30.0 with OpenTelemetry JavaScript Core v1.30.0, AWS Lambda Instrumentation v0.50.3, ADOT Collector v0.43.0
 export const AWS_OTEL_LAYER_ARN =
-    'arn:aws:lambda:us-east-1:901920570463:layer:aws-otel-nodejs-amd64-ver-1-18-1:4'
+    'arn:aws:lambda:us-east-1:901920570463:layer:aws-otel-nodejs-amd64-ver-1-30-0:1'
 
 export interface LambdaLayersProps extends BaseStackProps {
     // No additional props needed - layers are built from local directories
