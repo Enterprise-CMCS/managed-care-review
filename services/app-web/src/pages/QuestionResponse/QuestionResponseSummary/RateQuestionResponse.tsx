9635bc6ea41f36aed2987b116bd8ddbf1167904d
ruizajtruss <ruizaj@truss.works>
2025-10-22 17:46:37 +0000

    removing rate name per design's input

--- services/app-web/src/pages/QuestionResponse/QuestionResponseSummary/RateQuestionResponse.tsx
+++ services/app-web/src/pages/QuestionResponse/QuestionResponseSummary/RateQuestionResponse.tsx
@@ -103,7 +103,7 @@ export const RateQuestionResponse = () => {
                 ) : (
                     <StateQuestionResponseTable
                         indexQuestions={rate.questions}
-                        header={`Rate Q&A - Managed Care Review: ${rateCertificationName}`}
+                        header={`Rate Q&A - Managed Care Review`}
                     />
                 )}
             </GridContainer>
