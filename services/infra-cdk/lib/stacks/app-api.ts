30e57c30cadebab7c0d7dfe1570cb9b7ddaabb19
Mojo Talantikite <mojo.talantikite@gmail.com>
2025-12-11 16:35:18 -0500

    least privs

--- services/infra-cdk/lib/stacks/app-api.ts
+++ services/infra-cdk/lib/stacks/app-api.ts
@@ -788,7 +788,7 @@ export class AppApiStack extends BaseStack {
         role.addToPolicy(
             new PolicyStatement({
                 effect: Effect.ALLOW,
-                actions: ['s3:*'],
+                actions: ['s3:GetObject', 's3:PutObject', 's3:DeleteObject'],
                 resources: [
                     `${documentUploadsBucketArn}/allusers/*`,
                     `${qaUploadsBucketArn}/allusers/*`,
@@ -813,7 +813,7 @@ export class AppApiStack extends BaseStack {
         role.addToPolicy(
             new PolicyStatement({
                 effect: Effect.ALLOW,
-                actions: ['s3:*'],
+                actions: ['s3:GetObject', 's3:PutObject', 's3:DeleteObject'],
                 resources: [
                     `${legacyDocumentsBucket}/allusers/*`,
                     `${legacyQABucket}/allusers/*`,
