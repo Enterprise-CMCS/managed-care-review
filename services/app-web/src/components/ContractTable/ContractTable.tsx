e5b2ff6b6e9c8a6e5257558410cc6975264ff251
vyakusha <vyakusha@gmail.com>
2026-01-20 15:22:38 -0500

    use label values for contract submission type filter

--- services/app-web/src/components/ContractTable/ContractTable.tsx
+++ services/app-web/src/components/ContractTable/ContractTable.tsx
@@ -225,10 +225,23 @@ const getSelectedFiltersFromUrl = (
     })
     const filterValues = valuesFromUrl
         .filter((item) => item.id === id)
-        .map((item) => ({
-            value: item.value,
-            label: stateNameToStateCode(item.value),
-        }))
+        .map((item) => {
+            //special treatement for "Health plan" and "EQRO"
+            if (id === 'contractSubmissionType') {
+                const option = contractTypeOptions.find(
+                    (opt) => opt.value === item.value
+                )
+                return {
+                    value: item.value,
+                    label: option?.label || item.value,
+                }
+            }
+            //convert state name to state code
+            return {
+                value: item.value,
+                label: stateNameToStateCode(item.value),
+            }
+        })
     return filterValues as FilterOptionType[]
 }
 
