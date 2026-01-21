c3f87b101aef349d7ceb90afa0ba6fb1b4f36cb2
Mojo Talantikite <mojo.talantikite@gmail.com>
2026-01-21 14:58:28 -0500

    datasource

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,9 @@
-import { env, defineConfig } from 'prisma/config'
+import { defineConfig } from 'prisma/config'
 import type { PrismaConfig } from 'prisma'
 
 export default defineConfig({
     schema: './packages/database/prisma/schema.prisma',
     migrations: {
         path: './packages/database/prisma/migrations',
-    },
-    datasource: {
-        url: env('DATABASE_URL'),
     },
 }) satisfies PrismaConfig
