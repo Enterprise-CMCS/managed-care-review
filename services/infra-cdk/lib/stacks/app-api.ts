7ca8575b20fc75d29eacdc6568d40f402bd68e42
Brady Todhunter <btodhunter@truss.works>
2026-01-28 15:11:01 -0800

    reduce memory & timeout based on migration test. Set the STAGE_NAME env var
    Signed-off-by: Brady Todhunter <btodhunter@truss.works>

--- services/infra-cdk/lib/stacks/app-api.ts
+++ services/infra-cdk/lib/stacks/app-api.ts
@@ -388,11 +388,12 @@ export class AppApiStack extends BaseStack {
             'migrate_protobuf_data',
             'main',
             {
-                timeout: Duration.minutes(15),
-                memorySize: 1024,
+                timeout: Duration.minutes(5),
+                memorySize: 512,
                 environment: {
                     ...environment,
                     CONNECT_TIMEOUT: '60',
+                    STAGE_NAME: this.stage,
                 },
                 role,
                 layers: [this.prismaEngineLayer, this.otelLayer],
@@ -1038,6 +1039,12 @@ export class AppApiStack extends BaseStack {
             value: this.cleanupFunction.functionName,
             exportName: this.exportName('CleanupFunctionName'),
             description: 'Cleanup Lambda function name',
+        })
+
+        new CfnOutput(this, 'ProtobufMigrationFunctionName', {
+            value: this.migratePotobufDataFunction.functionName,
+            exportName: this.exportName('MigrateProtobufDataFunctionName'),
+            description: 'Probuf data migrate Lambda function name',
         })
 
         new CfnOutput(this, 'MigrateFunctionName', {
