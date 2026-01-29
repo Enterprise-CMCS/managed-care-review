95da34048837fdb8db3ba36f88aaf2673fd94865
ruizajtruss <ruizaj@truss.works>
2026-01-29 06:09:09 +0000

    still need this variable for testing

--- services/app-api/setupTests.ts
+++ services/app-api/setupTests.ts
@@ -1,2 +1,3 @@
+process.env.JWT_SECRET = '123abc' // pragma: allowlist secret
 process.env.OAUTH_JWT_SECRET = '123abc' // pragma: allowlist secret
 process.env.stage = 'test' // pragma: allowlist secret
