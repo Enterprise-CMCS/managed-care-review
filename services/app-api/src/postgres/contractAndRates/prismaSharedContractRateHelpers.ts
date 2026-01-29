9705525198cedc95dcfebd622794b14d9dfce93f
Jason Lin <maolin@truss.works>
2026-01-28 20:59:14 -0500

    insert review action for EQRO submission review determination.

--- services/app-api/src/postgres/contractAndRates/prismaSharedContractRateHelpers.ts
+++ services/app-api/src/postgres/contractAndRates/prismaSharedContractRateHelpers.ts
@@ -166,6 +166,10 @@ function getConsolidatedContractStatus(
     // if the reviewStatus hasn't been changed (i.e it's still set as UNDER_REVIEW).
     // However, if reviewStatus has been changed then reviewStatus takes precedence for the consolidatedStatus
     if (reviewStatus !== 'UNDER_REVIEW') {
+        // UNLOCKED takes precedence over NOT_SUBJECT_TO_REVIEW.
+        if (status === 'UNLOCKED' && reviewStatus === 'NOT_SUBJECT_TO_REVIEW') {
+            return status
+        }
         return reviewStatus
     } else {
         return status
