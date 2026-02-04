a23200da6134c1b8978d85ebfa67ba895dc55cdb
Mao Lin <98117700+JasonLin0991@users.noreply.github.com>
2026-02-04 10:48:10 -0500

    Fix EQRO submission summary routing (#4035)
    * Add Submission summary router.
    
    * Use SubmissionSummaryRoutes component
    
    * Update unit test

--- services/app-web/src/pages/SubmissionSummary/index.ts
+++ services/app-web/src/pages/SubmissionSummary/index.ts
@@ -1,3 +1,4 @@
 export { SubmissionSummary } from './SubmissionSummary'
 export { RateSummary } from '../RateSummary'
 export { EQROSubmissionSummary } from './EQROSubmissionSummary/EQROSubmissionSummary'
+export { SubmissionSummaryRoutes } from './SubmissionSummaryRoutes'
