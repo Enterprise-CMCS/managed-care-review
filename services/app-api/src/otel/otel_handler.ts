75ba59e1d5b2268d4a47bce6501bc96e565cef3d
Mojo Talantikite <mojo.talantikite@gmail.com>
2026-02-20 10:15:25 -0500

    Remove unused uploads service (#4095)
    * remove unused uploads service
    
    * regenerate lock

--- services/app-api/src/otel/otel_handler.ts
+++ services/app-api/src/otel/otel_handler.ts
@@ -1,12 +1,55 @@
-import type { Tracer } from '@opentelemetry/api'
+import opentelemetry, { type Tracer, SpanStatusCode } from '@opentelemetry/api'
 import { trace } from '@opentelemetry/api'
 import { Resource } from '@opentelemetry/resources'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
-import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base'
+import {
+    SimpleSpanProcessor,
+    BatchSpanProcessor,
+} from '@opentelemetry/sdk-trace-base'
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
 import { AWSXRayPropagator } from '@opentelemetry/propagator-aws-xray'
 import { AWSXRayIdGenerator } from '@opentelemetry/id-generator-aws-xray'
+
+export function initTracer(serviceName: string, otelCollectorURL: string) {
+    console.info('-----Setting OTEL instrumentation-----')
+
+    const resource = new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+    })
+
+    const exporter = new OTLPTraceExporter({
+        url: otelCollectorURL,
+        headers: {},
+    })
+    const provider = new NodeTracerProvider({
+        idGenerator: new AWSXRayIdGenerator(),
+        resource: resource,
+        spanProcessors: [new BatchSpanProcessor(exporter)],
+    })
+
+    provider.register()
+}
+
+export function recordException(
+    error: string | Error,
+    serviceName: string,
+    spanName: string
+) {
+    const tracer = opentelemetry.trace.getTracer(serviceName)
+    const span = tracer.startSpan(spanName)
+
+    try {
+        span.recordException(error)
+        span.setStatus({
+            code: SpanStatusCode.ERROR,
+            message: typeof error === 'string' ? error : error.message,
+        })
+        console.error(error)
+    } finally {
+        span.end()
+    }
+}
 
 export function createTracer(serviceName: string): Tracer {
     const provider = new NodeTracerProvider({
