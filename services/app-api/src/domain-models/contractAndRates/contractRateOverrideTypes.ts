57f24ebc62a6bb252e7316deacf34ed83fcc7772
Jason Lin <maolin@truss.works>
2026-02-25 17:01:34 -0500

    Abstract out override data types

--- services/app-api/src/domain-models/contractAndRates/contractRateOverrideTypes.ts
+++ services/app-api/src/domain-models/contractAndRates/contractRateOverrideTypes.ts
@@ -1,29 +1,39 @@
 import { z } from 'zod'
 import { baseUserSchema } from '../UserType'
+import { preprocessNulls } from './formDataTypes'
+
+const contractOverrideDataFragmentSchema = z.object({
+    initiallySubmittedAt: preprocessNulls(z.date().optional()),
+})
 
 const contractDataOverrideSchema = z.object({
     id: z.uuid(),
     createdAt: z.date(),
     updatedBy: baseUserSchema.optional(),
     description: z.string(),
-    overrides: z.object({
-        initiallySubmittedAt: z.date().optional(),
-    }),
+    overrides: contractOverrideDataFragmentSchema,
 })
 
 type ContractDataOverrideType = z.infer<typeof contractDataOverrideSchema>
+
+const rateOverrideDataFragmentSchema = z.object({
+    initiallySubmittedAt: preprocessNulls(z.date().optional()),
+})
 
 const rateDataOverrideSchema = z.object({
     id: z.uuid(),
     createdAt: z.date(),
     updatedBy: baseUserSchema.optional(),
     description: z.string(),
-    overrides: z.object({
-        initiallySubmittedAt: z.date().optional(),
-    }),
+    overrides: rateOverrideDataFragmentSchema,
 })
 
 type RateDataOverrideType = z.infer<typeof rateDataOverrideSchema>
 
-export { contractDataOverrideSchema, rateDataOverrideSchema }
+export {
+    contractDataOverrideSchema,
+    rateDataOverrideSchema,
+    contractOverrideDataFragmentSchema,
+    rateOverrideDataFragmentSchema,
+}
 export type { ContractDataOverrideType, RateDataOverrideType }
