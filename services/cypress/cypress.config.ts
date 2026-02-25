d74e8b472980d1f0d40262cbb400cc32c685774d
pearl-truss <pearl@truss.works>
2026-02-25 13:26:43 -0500

    use correct env

--- services/cypress/cypress.config.ts
+++ services/cypress/cypress.config.ts
@@ -51,7 +51,7 @@ module.exports = defineConfig({
             newConfig.env.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID
             newConfig.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY
             newConfig.env.AWS_SESSION_TOKEN = process.env.AWS_SESSION_TOKEN
-            newConfig.env.VITE_APP_S3_QA_BUCKET = process.env.VITE_APP_S3_QA_BUCKET
+            newConfig.env.VITE_APP_S3_DOCUMENTS_BUCKET = process.env.VITE_APP_S3_DOCUMENTS_BUCKET
             // Reads graphql schema and converts it to gql for apollo client.
             on('task', {
                 readGraphQLSchema() {
