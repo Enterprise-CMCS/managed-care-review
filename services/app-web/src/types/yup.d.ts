fe950a57c6c12f4e25a1df42cb85d59c19ab2ded
vyakusha <vyakusha@gmail.com>
2026-02-26 19:15:29 -0500

    interface update

--- services/app-web/src/types/yup.d.ts
+++ services/app-web/src/types/yup.d.ts
@@ -4,8 +4,4 @@ declare module 'yup' {
     interface StringSchema {
         validateDateStringFormat(errorMessage?: string): this
     }
-    
-    interface DateSchema {
-        validateDateFormat(formats: string, parseStrict: boolean): this
-    }
 }
