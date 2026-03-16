82b74b7fe9f12414d7a4c269e4980210ff92d0e2
Jason Lin <maolin@truss.works>
2026-03-16 07:57:50 -0400

    Remove unused local only flag

--- packages/common-code/src/featureFlags/flags.ts
+++ packages/common-code/src/featureFlags/flags.ts
@@ -89,14 +89,6 @@ const featureFlags = {
         flag: 'site-under-maintenance-banner',
         defaultValue: 'OFF',
     },
-    /**
-     * Used in testing to simulate errors in fetching flag value.
-     * This flag does not exist in LaunchDarkly dashboard so fetching this will return the defaultValue.
-     */
-    TEST_ERROR_FETCHING_FLAG: {
-        flag: 'test-error-fetching-flag',
-        defaultValue: undefined,
-    },
 } as const
 
 /**
