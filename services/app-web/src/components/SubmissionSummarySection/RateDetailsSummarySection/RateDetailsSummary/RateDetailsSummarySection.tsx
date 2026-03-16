3d367ed73b796a3d7fd2ce8da69b351e1a32e83b
vyakusha <vyakusha@gmail.com>
2026-03-16 12:20:56 -0400

    fix withdrawn formatting in rate details

--- services/app-web/src/components/SubmissionSummarySection/RateDetailsSummarySection/RateDetailsSummary/RateDetailsSummarySection.tsx
+++ services/app-web/src/components/SubmissionSummarySection/RateDetailsSummarySection/RateDetailsSummary/RateDetailsSummarySection.tsx
@@ -567,17 +567,16 @@ export const RateDetailsSummarySection = ({
                   )}
             {withdrawnRateRevisions.length > 0 &&
                 withdrawnRateRevisions.map((rateRev) => (
-                        <SectionCard
-                            className={styles.summarySection}
-                            id={`withdrawn-rate-${rateRev.id}`}
-                            key={rateRev.id}
-                        >
+                    <SectionCard
+                        className={styles.summarySection}
+                        id={`withdrawn-rate-${rateRev.id}`}
+                        key={rateRev.id}
+                    >
                         <h3
                             aria-label={`Rate ID: ${rateRev.formData.rateCertificationName}`}
                             className={styles.rateNameWithdrawn}
                         >
-                            <InfoTag color="gray">WITHDRAWN</InfoTag>{' '}
-                            <br />
+                            <InfoTag color="gray">WITHDRAWN</InfoTag> <br />
                             {rateCertificationNameHeader(
                                 rateRev.rateID,
                                 rateRev.formData.rateCertificationName
