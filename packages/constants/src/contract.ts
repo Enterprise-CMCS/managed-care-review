54c62ff660fce4f0f6620a79f71deaf535c147df
Jason Lin <maolin@truss.works>
2026-03-30 17:21:31 -0400

    Use constants for repeated text

--- packages/constants/src/contract.ts
+++ packages/constants/src/contract.ts
@@ -1,10 +1,15 @@
 import { ContractReviewStatus } from './gen/gqlClient'
 
+const ReviewDecisionRecord: Record<Extract<ContractReviewStatus, 'NOT_SUBJECT_TO_REVIEW' | 'UNDER_REVIEW'>, string> = {
+    UNDER_REVIEW: 'Subject to review',
+    NOT_SUBJECT_TO_REVIEW: 'Not subject to review',
+}
+
 const SubmissionReviewStatusRecord: Record<ContractReviewStatus, string> = {
     APPROVED: 'Approved',
     UNDER_REVIEW: 'Under review',
     WITHDRAWN: 'Withdrawn',
-    NOT_SUBJECT_TO_REVIEW: 'Not subject to review',
+    NOT_SUBJECT_TO_REVIEW: ReviewDecisionRecord['NOT_SUBJECT_TO_REVIEW'],
 }
 
-export { SubmissionReviewStatusRecord }
+export { SubmissionReviewStatusRecord, ReviewDecisionRecord }
