6fa283e59e624fe3f412ab586ab55e228f2bf80f
ruizajtruss <ruizaj@truss.works>
2026-03-31 22:08:35 +0000

    updated types and associated fixtures. Also had to mark some false positive secrets

--- packages/submissions/src/statePrograms/StateCodeType.ts
+++ packages/submissions/src/statePrograms/StateCodeType.ts
@@ -73,6 +73,8 @@ interface ProgramArgType {
     name: string // short name. This is used most often in the application including in submission name
     fullName: string // full name is used in submission summary page
     isRateProgram: boolean // specifies if program relates to rates rather than contract
+    isDeprecated: boolean // Specifies if a program has been deprecated and replaced by another program
+    deprecatedByProgramId?: string | null // If deprecated this will contain the uuid of the program that replaced it
 }
 
 function isValidStateCode(
