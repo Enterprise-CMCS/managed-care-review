0199d11f872df457e8279748727bdf73f00ca7bb
Jason Lin <maolin@truss.works>
2026-04-15 18:55:00 -0400

    Widen fetch wrappers for Apollo v4 HttpLink signature

--- services/app-web/src/api/localGQLFetch.ts
+++ services/app-web/src/api/localGQLFetch.ts
@@ -5,16 +5,17 @@ import { fakeAmplifyFetch } from './fakeAmplifyFetch'
 // localGQLfetch calls Amplify fetch but puts the user in the cognito header for
 // the local Express server to map into the Lambda event
 export async function localGQLFetch(
-    uri: string,
-    options: RequestInit
+    uri: URL | RequestInfo,
+    options?: RequestInit
 ): Promise<Response> {
     const currentUser = await getLoggedInUser()
+    const opts: RequestInit = options ?? {}
 
-    options.headers = Object.assign({}, options.headers, {
+    opts.headers = Object.assign({}, opts.headers, {
         'cognito-authentication-provider': currentUser
             ? JSON.stringify(currentUser)
             : 'NO_USER',
     })
 
-    return fakeAmplifyFetch(uri, options)
+    return fakeAmplifyFetch(uri, opts)
 }
