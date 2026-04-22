e19fea04b2aafa4a2602a050c1d30b20a85eafed
Jason Lin <maolin@truss.works>
2026-04-22 01:58:27 -0400

    Configure vite for uswds v3

--- services/app-web/lint-staged.config.js
+++ services/app-web/lint-staged.config.js
@@ -1,5 +1,8 @@
 const config = {
-    '**/*.{js,jsx,ts,tsx}': ['eslint --max-warnings=0', 'prettier --write'],
+    '**/*.{js,jsx,ts,tsx}': [
+        'eslint --max-warnings=0 --no-warn-ignored',
+        'prettier --write',
+    ],
     '**/*.{ts,tsx}': () => 'tsc --noEmit',
     '**/*.scss': ["npx stylelint '**/*.scss'", 'prettier --write'],
 };
