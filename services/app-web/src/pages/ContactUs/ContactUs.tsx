1393bbbe893b325d55b160aa537da17d6368e56d
vyakusha <vyakusha@gmail.com>
2026-04-27 22:39:37 -0400

    fix margins

--- services/app-web/src/pages/ContactUs/ContactUs.tsx
+++ services/app-web/src/pages/ContactUs/ContactUs.tsx
@@ -6,9 +6,9 @@ import styles from './ContactUs.module.scss'
 export const ContactUs = (): React.ReactElement => {
     return (
         <GridContainer className={styles.pageContainer}>
-            <h1 className={styles.heading}>Contact us</h1>
+            <h1>Contact us</h1>
 
-            <p className={styles.intro}>
+            <p className={styles.contactIntro}>
                 Have a question? Visit the{' '}
                 <a
                     href="https://www.medicaid.gov/resources-for-states/managed-care-review-mc-review/managed-care-review-faqs"
@@ -20,26 +20,28 @@ export const ContactUs = (): React.ReactElement => {
                 for answers to common questions about MC-Review.
             </p>
 
-            <p className={styles.intro}>If you don&apos;t find an answer:</p>
+            <p className={styles.contactIntro}>
+                If you don&apos;t find an answer:
+            </p>
 
             <ul className={styles.contactList}>
-                <li className={styles.contactItem}>
-                    <p className={styles.contactHeading}>
+                <li>
+                    <p className={styles.contactListItem}>
                         For program, contract, and operational questions
                     </p>
-                    <p className={styles.contactCopy}>
+                    <p>
                         Email your primary CMS contact or{' '}
                         <a href="mailto:MCGDMCOactions@cms.hhs.gov">
                             MCGDMCOactions@cms.hhs.gov
                         </a>
                     </p>
                 </li>
-                <li className={styles.contactItem}>
-                    <p className={styles.contactHeading}>
+                <li>
+                    <p className={styles.contactListItem}>
                         For technical assistance with MC-Review, including
                         logging in
                     </p>
-                    <p className={styles.contactCopy}>
+                    <p>
                         Email{' '}
                         <a href="mailto:MC_Review_HelpDesk@cms.hhs.gov">
                             MC_Review_HelpDesk@cms.hhs.gov
