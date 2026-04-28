8f2380bb381bf65846e84ae752600ad761cafafe
vyakusha <vyakusha@gmail.com>
2026-04-28 14:52:53 -0400

    fix spacing and resizing

--- services/app-web/src/pages/ContactUs/ContactUs.tsx
+++ services/app-web/src/pages/ContactUs/ContactUs.tsx
@@ -5,50 +5,52 @@ import styles from './ContactUs.module.scss'
 
 export const ContactUs = (): React.ReactElement => {
     return (
-        <GridContainer className={styles.pageContainer}>
-            <h1>Contact us</h1>
+        <div className={styles.pageWrapper}>
+            <GridContainer className={styles.pageContainer}>
+                <h1>Contact us</h1>
 
-            <p className={styles.contactIntro}>
-                Have a question? Visit the{' '}
-                <a
-                    href="https://www.medicaid.gov/resources-for-states/managed-care-review-mc-review/managed-care-review-faqs"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    FAQ page
-                </a>{' '}
-                for answers to common questions about MC-Review.
-            </p>
+                <p className={styles.contactIntro}>
+                    Have a question? Visit the{' '}
+                    <a
+                        href="https://www.medicaid.gov/resources-for-states/managed-care-review-mc-review/managed-care-review-faqs"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        FAQ page
+                    </a>{' '}
+                    for answers to common questions about MC-Review.
+                </p>
 
-            <p className={styles.contactIntro}>
-                If you don&apos;t find an answer:
-            </p>
+                <p className={styles.contactLead}>
+                    If you don&apos;t find an answer:
+                </p>
 
-            <ul className={styles.contactList}>
-                <li>
-                    <p className={styles.contactListItem}>
-                        For program, contract, and operational questions
-                    </p>
-                    <p>
-                        Email your primary CMS contact or{' '}
-                        <a href="mailto:MCGDMCOactions@cms.hhs.gov">
-                            MCGDMCOactions@cms.hhs.gov
-                        </a>
-                    </p>
-                </li>
-                <li>
-                    <p className={styles.contactListItem}>
-                        For technical assistance with MC-Review, including
-                        logging in
-                    </p>
-                    <p>
-                        Email{' '}
-                        <a href="mailto:MC_Review_HelpDesk@cms.hhs.gov">
-                            MC_Review_HelpDesk@cms.hhs.gov
-                        </a>
-                    </p>
-                </li>
-            </ul>
-        </GridContainer>
+                <ul className={styles.contactList}>
+                    <li>
+                        <p className={styles.contactListItem}>
+                            For program, contract, and operational questions
+                        </p>
+                        <p className={styles.contactListItemLast}>
+                            Email your primary CMS contact or{' '}
+                            <a href="mailto:MCGDMCOactions@cms.hhs.gov">
+                                MCGDMCOactions@cms.hhs.gov
+                            </a>
+                        </p>
+                    </li>
+                    <li>
+                        <p className={styles.contactListItem}>
+                            For technical assistance with MC-Review, including
+                            logging in
+                        </p>
+                        <p className={styles.contactListItemLast}>
+                            Email{' '}
+                            <a href="mailto:MC_Review_HelpDesk@cms.hhs.gov">
+                                MC_Review_HelpDesk@cms.hhs.gov
+                            </a>
+                        </p>
+                    </li>
+                </ul>
+            </GridContainer>
+        </div>
     )
 }
