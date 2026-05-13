9bbc485e1fa45eae9460563a48883dadb7c48400
vyakusha <vyakusha@gmail.com>
2026-05-12 22:59:57 -0400

    fix rendering in mobile view

--- services/app-web/src/components/Footer/Footer.tsx
+++ services/app-web/src/components/Footer/Footer.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import { useLDClient } from 'launchdarkly-react-client-sdk'
 import { featureFlags } from '@mc-review/common-code'
-import { RoutesRecord } from '@mc-review/constants'
+import { RouteT } from '@mc-review/constants'
 
 import medicaidLogo from '../../assets/images/medicaidgovlogo.png'
 import { ReactComponent as HHSIcon } from '../../assets/icons/depthealthhumanservices_usa.svg'
 import { ReactComponent as HHSIcon2026 } from '../../assets/icons/depthealthhumanservices_usa_2026.svg'
 import styles from './Footer.module.scss'
+import { FooterNavLinks } from './FooterNavLinks'
 import { Logo } from '../Logo'
 import { GridContainer, Grid } from '@trussworks/react-uswds'
 import { useStringConstants } from '../../hooks/useStringConstants'
 import { useCurrentRoute } from '../../hooks/useCurrentRoute'
 import { ContactSupportLink } from '../ErrorAlert/ContactSupportLink'
-import { ReactRouterLinkWithLogging } from '../TealiumLogging'
 
-type FooterContentProps = {
+type LegacyFooterContentProps = {
     mailToSupport: string
 }
 
 const LegacyFooterContent = ({
     mailToSupport,
-}: FooterContentProps): React.ReactElement => (
+}: LegacyFooterContentProps): React.ReactElement => (
     <>
         <div className={styles.logosRow}>
             <GridContainer>
@@ -60,9 +60,13 @@ const LegacyFooterContent = ({
     </>
 )
 
+type ResourcesNavFooterContentProps = {
+    route: RouteT | 'UNKNOWN_ROUTE'
+}
+
 const ResourcesNavFooterContent = ({
-    mailToSupport: _mailToSupport,
-}: FooterContentProps): React.ReactElement => {
+    route,
+}: ResourcesNavFooterContentProps): React.ReactElement => {
     const medicaidLogo2026 = new URL(
         '../../assets/images/medicaidgovlogo_2026.svg',
         import.meta.url
@@ -71,15 +75,18 @@ const ResourcesNavFooterContent = ({
     return (
         <>
             <div className={styles.logosRow}>
-                <GridContainer>
-                    <Grid row className="flex-justify flex-align-center">
-                        <div className={styles.cmsLogos}>
+                <GridContainer className={styles.resourcesNavContainer}>
+                    <Grid row className={styles.resourcesNavTopRow}>
+                        <div
+                            className={`${styles.cmsLogos} ${styles.resourcesNavCmsLogos}`}
+                        >
                             <Logo
                                 src={medicaidLogo2026}
                                 alt="Medicaid.gov-Keeping America Healthy"
+                                className={styles.resourcesNavLogo}
                             />
                         </div>
-                        <span className={styles.resourcesNavFederalLogos}>
+                        <div className={styles.resourcesNavFederalLogos}>
                             <HHSIcon2026
                                 role="img"
                                 aria-label="Department of Health & Human Services logo"
@@ -90,33 +97,16 @@ const ResourcesNavFooterContent = ({
                                 Services. 7500 Security Boulevard, Baltimore, MD
                                 21244
                             </p>
-                        </span>
+                        </div>
                     </Grid>
                 </GridContainer>
             </div>
             <div
                 className={`${styles.contactRow} ${styles.resourcesNavContactRow}`}
             >
-                <GridContainer>
+                <GridContainer className={styles.resourcesNavContainer}>
                     <Grid row className={styles.resourcesNavLinks}>
-                        <ReactRouterLinkWithLogging
-                            to={RoutesRecord.HELP}
-                            className={styles.footerNavLink}
-                        >
-                            Submission form guidance
-                        </ReactRouterLinkWithLogging>
-                        <ReactRouterLinkWithLogging
-                            to={RoutesRecord.RESOURCES_TRAINING}
-                            className={styles.footerNavLink}
-                        >
-                            Resources and Training
-                        </ReactRouterLinkWithLogging>
-                        <ReactRouterLinkWithLogging
-                            to={RoutesRecord.CONTACT_US}
-                            className={styles.footerNavLink}
-                        >
-                            Contact us
-                        </ReactRouterLinkWithLogging>
+                        <FooterNavLinks route={route} />
                     </Grid>
                 </GridContainer>
             </div>
@@ -139,9 +129,7 @@ export const Footer = (): React.ReactElement => {
     return route !== 'GRAPHQL_EXPLORER' ? (
         <footer>
             {showResourcesNavFooter ? (
-                <ResourcesNavFooterContent
-                    mailToSupport={stringConstants.MAIL_TO_SUPPORT}
-                />
+                <ResourcesNavFooterContent route={route} />
             ) : (
                 <LegacyFooterContent
                     mailToSupport={stringConstants.MAIL_TO_SUPPORT}
