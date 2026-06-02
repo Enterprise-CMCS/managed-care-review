dad90924b354a89c941d859bfa57eceeaa472345
Jason Lin <maolin@truss.works>
2026-06-02 11:06:51 -0400

    Add find override document store functions

--- services/app-api/src/postgres/documents/index.ts
+++ services/app-api/src/postgres/documents/index.ts
@@ -1,3 +1,7 @@
 export { findAllDocuments } from './findAllDocuments'
 export { findDocumentById } from './findDocumentById'
+export {
+    findAllOverrideAddedDocuments,
+    findOverrideAddedDocumentById,
+} from './findOverrideAddedDocuments'
 export * from './zip'
