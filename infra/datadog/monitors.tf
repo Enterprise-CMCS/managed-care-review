d304ef5f4b09066a0255ba923090592fc91c17d5
Mojo Talantikite <mojo.talantikite@gmail.com>
2026-06-02 12:55:29 -0400

    copilot comments

--- infra/datadog/monitors.tf
+++ infra/datadog/monitors.tf
@@ -33,17 +33,17 @@ resource "datadog_monitor" "graphql_errors" {
 }
 
 resource "datadog_monitor" "js_exceptions" {
-  name = "[${var.environment}] MCR - Web Application JS Exceptions"
+  name = "[${var.environment}] MCR - Web Application Trace Errors"
   type = "trace-analytics alert"
 
   query = "traces(\"service:app-web-${var.environment} @error:true\").rollup(\"count\").last(\"5m\") > ${var.js_error_threshold}"
 
   message = <<-EOT
-    JavaScript exceptions detected in the MCR web app in **${var.environment}**.
+    Trace errors detected in the MCR web app in **${var.environment}**.
 
-    Triggered when >${var.js_error_threshold} exceptions are recorded in 5 minutes.
+    Triggered when >${var.js_error_threshold} errors are recorded in OTEL spans from `app-web-${var.environment}` in 5 minutes.
 
-    - Check browser console errors in Datadog APM under service `app-web-${var.environment}`
+    - Review error details in Datadog APM under service `app-web-${var.environment}`
     - Review recent frontend deployments
 
     ${var.notify_slack}
