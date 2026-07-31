ec6eb5049c4ed5974e08432a53b82a640d39eedc
Jason Lin <maolin@truss.works>
2026-07-31 16:46:25 -0400

    Separate actuary contact name inputs behind feature flag.

--- services/app-web/src/pages/StateSubmission/HealthPlanSubmission/RateDetails/rateDetailsHelpers.ts
+++ services/app-web/src/pages/StateSubmission/HealthPlanSubmission/RateDetails/rateDetailsHelpers.ts
@@ -66,8 +66,26 @@ const convertRateFormToGQLRateFormData = (
             ? rateForm.rateMedicaidPopulations
             : [],
         deprecatedRateProgramIDs: [],
-        certifyingActuaryContacts: rateForm.actuaryContacts,
-        addtlActuaryContacts: rateForm.addtlActuaryContacts,
+        certifyingActuaryContacts: rateForm.actuaryContacts.map((contact) => ({
+            ...contact,
+            name: contact.name?.trim(),
+            givenName: contact.givenName?.trim(),
+            familyName: contact.familyName?.trim(),
+            suffix: contact.suffix?.trim(),
+            titleRole: contact.titleRole?.trim(),
+            email: contact.email?.trim(),
+            actuarialFirmOther: contact.actuarialFirmOther?.trim(),
+        })),
+        addtlActuaryContacts: rateForm.addtlActuaryContacts.map((contact) => ({
+            ...contact,
+            name: contact.name?.trim(),
+            givenName: contact.givenName?.trim(),
+            familyName: contact.familyName?.trim(),
+            suffix: contact.suffix?.trim(),
+            titleRole: contact.titleRole?.trim(),
+            email: contact.email?.trim(),
+            actuarialFirmOther: contact.actuarialFirmOther?.trim(),
+        })),
         actuaryCommunicationPreference:
             rateForm.actuaryCommunicationPreference ?? undefined,
     }
