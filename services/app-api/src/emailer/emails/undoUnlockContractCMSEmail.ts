a092dc05137cbd1f5929819464fce41851d55d3b
Ronak Patel <84742904+ronaktruss@users.noreply.github.com>
2026-08-12 17:11:59 -0400

    Actually send email

---
+++ services/app-api/src/emailer/emails/undoUnlockContractCMSEmail.ts
@@ -0,0 +1,96 @@
+import { packageName as generatePackageName } from '@mc-review/submissions'
+import type {
+    ContractType,
+    ProgramType,
+    UpdateInfoType,
+} from '../../domain-models'
+import type {
+    EmailConfiguration,
+    EmailData,
+    StateAnalystsEmails,
+} from '../emailer'
+import {
+    findContractPrograms,
+    renderTemplate,
+    stripHTMLFromTemplate,
+} from '../templateHelpers'
+import { formatCalendarDate } from 'Users/truss-ronak/Projects/forked/managed-care-review/packages/dates/build'
+import { pruneDuplicateEmails } from '../formatters'
+import { submissionSummaryURL } from '../generateURLs'
+
+export const undoUnlockContractCMSEmail = async (
+    contract: ContractType,
+    updateInfo: UpdateInfoType,
+    stateAnalystsEmails: StateAnalystsEmails,
+    statePrograms: ProgramType[],
+    config: EmailConfiguration
+): Promise<EmailData | Error> => {
+    const toAddresses = pruneDuplicateEmails([
+        ...stateAnalystsEmails,
+        ...config.dmcoEmails,
+        ...config.devReviewTeamEmails,
+    ])
+
+    const contractRev = contract.packageSubmissions[0].contractRevision
+    const formData = contractRev.formData
+
+    //This checks to make sure all programs contained in submission exists for the state.
+    const packagePrograms = findContractPrograms(contractRev, statePrograms)
+    if (packagePrograms instanceof Error) {
+        return packagePrograms
+    }
+
+    const packageName = generatePackageName(
+        contract.stateCode,
+        contract.stateNumber,
+        formData.programIDs,
+        packagePrograms
+    )
+
+    const isContractAndRates =
+        formData.submissionType === 'CONTRACT_AND_RATES' &&
+        Boolean(contract.packageSubmissions[0].rateRevisions.length)
+
+    const submissionURL = submissionSummaryURL(
+        contract.id,
+        contract.contractSubmissionType,
+        config.baseUrl
+    )
+
+    const etaData = {
+        packageName,
+        updatedAt: formatCalendarDate(
+            updateInfo.updatedAt,
+            'America/Los_Angeles'
+        ),
+        updatedBy: updateInfo.updatedBy.email,
+        reason: updateInfo.updatedReason,
+        // Do we want any overrides on this status?
+        status: contract.status,
+        shouldIncludeRates: isContractAndRates,
+        rateInfos: contract.packageSubmissions[0].rateRevisions.map((rate) => ({
+            rateName: rate.formData.rateCertificationName,
+        })),
+        submissionURL,
+    }
+
+    const template = await renderTemplate<typeof etaData>(
+        'undoUnlockContractCMSEmail',
+        etaData
+    )
+
+    if (template instanceof Error) {
+        return template
+    } else {
+        return {
+            toAddresses,
+            replyToAddresses: [],
+            sourceEmail: config.emailSource,
+            subject: `${
+                config.stage !== 'prod' ? `[${config.stage}] ` : ''
+            }${packageName} unlock was undone by CMS`,
+            bodyText: stripHTMLFromTemplate(template),
+            bodyHTML: template,
+        }
+    }
+}
