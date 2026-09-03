550074aa1f70a08b980c5c58a1a854bb9fb739a2
ruizajtruss <ruizaj@truss.works>
2026-09-03 21:48:43 +0000

    (fix): updated copy per design

--- services/app-api/src/emailer/emails/resubmitRevisionChanges.test.ts
+++ services/app-api/src/emailer/emails/resubmitRevisionChanges.test.ts
@@ -935,7 +935,7 @@ describe('buildResubmitRevisionChanges', () => {
                             title: 'Added MCR-FL-NEMT-20260501-20260531-CERTIFICATION-20260507',
                             rows: [
                                 {
-                                    label: 'Rate included with another submission',
+                                    label: 'Rate created on another submission',
                                     newValue: 'Yes',
                                 },
                             ],
@@ -986,10 +986,10 @@ describe('buildResubmitRevisionChanges', () => {
                             contactsLabel: 'New and modified actuaries:',
                             contacts: [
                                 {
-                                    value: 'Jalen Brunson, Head of Risk Development, Olivier Wyman, jalen.brunson@ow.com',
+                                    value: 'Jalen Brunson, jalen.brunson@ow.com, Head of Risk Development, Olivier Wyman',
                                 },
                                 {
-                                    value: 'Bill Yard, Executive assistant, Mercer, byard@mercer.com',
+                                    value: 'Bill Yard, byard@mercer.com, Executive assistant, Mercer',
                                 },
                             ],
                         },
